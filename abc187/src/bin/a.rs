9bd48664c72a97b5729b246e02065136537d348b
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-11-29 23:17:26 +0900

    add abc187_a_b

--- abc187/src/bin/a.rs
+++ abc187/src/bin/a.rs
@@ -1,9 +1,15 @@
+use std::cmp::max;
+
 use proconio::input;
+
+fn s(i: i32) -> i32 {
+    let ten = (i / 10) % 10;
+    return i / 100 + ten + i % 10;
+}
 
 fn main() {
     input! {
-       a:i32,
+       a:i32,b:i32,
     }
-    println!("{}", a);
-    todo!();
+    println!("{}", max(s(a), s(b)));
 }
--- abc187/src/bin/b.rs
+++ abc187/src/bin/b.rs
@@ -2,8 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize, xy:[(f64,f64);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut count = 0;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            let slope = (xy[i].1 - xy[j].1) / (xy[i].0 - xy[j].0);
+            if -1.0 <= slope && slope <= 1.0 {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
