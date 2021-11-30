d530895add1af7cc68721e596b6c505d6e925fbf
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-01 08:27:51 +0900

    add abc185_a_b

--- abc185/src/bin/a.rs
+++ abc185/src/bin/a.rs
@@ -1,9 +1,14 @@
+use std::cmp::min;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       a:[i32;4],
     }
-    println!("{}", a);
-    todo!();
+    let mut m = 101;
+    for e in a {
+        m = min(m, e);
+    }
+    println!("{}", m);
 }
--- abc185/src/bin/b.rs
+++ abc185/src/bin/b.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        n:i32,m:i32,t:i32,
+        ab:[(i32,i32);m]
     }
-    println!("{}", a);
-    todo!();
+    let mut bat = n;
+    let mut time = 0;
+    for &(a, b) in &ab {
+        let bef = bat - (a - time);
+        if bef <= 0 {
+            println!("No");
+            return;
+        }
+        bat = (bef + (b - a)).min(n);
+        time = b;
+    }
+    println!("{}", if bat <= (t - time) { "No" } else { "Yes" });
 }
