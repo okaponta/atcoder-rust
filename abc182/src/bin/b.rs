4fc867b7697f0986da1a2ef748a1e36abbceb632
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-04 16:07:50 +0900

    add 182_b

--- abc182/src/bin/b.rs
+++ abc182/src/bin/b.rs
@@ -2,8 +2,35 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32, a:[i32;n]
     }
-    println!("{}", a);
-    todo!();
+    let mut no_prime = vec![];
+    let mut prime = vec![];
+    for i in 2..1000 {
+        if no_prime.contains(&i) {
+            continue;
+        } else {
+            prime.push(i);
+            let mut num = i;
+            while num < 1000 {
+                num += i;
+                no_prime.push(num);
+            }
+        }
+    }
+    let mut max_count = 0;
+    let mut ans = 0;
+    for e in prime {
+        let mut count = 0;
+        for ai in &a {
+            if ai % e == 0 {
+                count += 1;
+            }
+        }
+        if max_count < count {
+            max_count = count;
+            ans = e;
+        }
+    }
+    println!("{}", ans);
 }
