03519231a3c58491c948d2424ba4e5e6f9404172
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-05 12:58:43 +0900

    add 181_c

--- abc181/src/bin/c.rs
+++ abc181/src/bin/c.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       xy: [(i32,i32);n]
     }
-    println!("{}", a);
-    todo!();
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                let x1 = xy[j].0 - xy[i].0;
+                let x2 = xy[k].0 - xy[i].0;
+                let y1 = xy[j].1 - xy[i].1;
+                let y2 = xy[k].1 - xy[i].1;
+                if x1 * y2 == x2 * y1 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+    println!("No");
 }
