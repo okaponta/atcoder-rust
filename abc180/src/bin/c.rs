ce1a7f7996ec7949d8f5f406414143d53297bc47
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-06 14:39:29 +0900

    add 180_c

--- abc180/src/bin/c.rs
+++ abc180/src/bin/c.rs
@@ -1,9 +1,19 @@
+use itertools::Itertools;
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = vec![];
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            ans.push(i);
+            ans.push(n / i);
+        }
+    }
+    ans.sort();
+    ans.dedup();
+    println!("{}", ans.iter().join("\n"));
 }
