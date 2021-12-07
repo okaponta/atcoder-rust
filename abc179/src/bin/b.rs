c6514415dfcaebdabac4d89690809b2fd4616e21
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-07 14:04:47 +0900

    add 179_b

--- abc179/src/bin/b.rs
+++ abc179/src/bin/b.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       d:[(i32,i32);n]
     }
-    println!("{}", a);
-    todo!();
+    let mut count = 0;
+    for i in 0..n {
+        if d[i].0 == d[i].1 {
+            count += 1;
+            if count == 3 {
+                println!("Yes");
+                return;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    println!("No");
 }
