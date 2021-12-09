3bc99fcc9d20c3120ae9774987be9832ca09ac2f
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-09 13:44:09 +0900

    add 177_b

--- abc177/src/bin/b.rs
+++ abc177/src/bin/b.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       s:String,t:String,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 10000;
+    for i in 0..=s.len() - t.len() {
+        let mut count = 0;
+        for j in 0..t.len() {
+            if s.chars().nth(i + j).unwrap() == t.chars().nth(j).unwrap() {
+                count += 1;
+            }
+        }
+        ans = ans.min(t.len() - count);
+    }
+    println!("{}", ans);
 }
