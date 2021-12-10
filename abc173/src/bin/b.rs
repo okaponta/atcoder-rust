3970aa1e7327927a6fb8733b7d8bc32c3d259eab
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-10 22:15:51 +0900

    add abc173_b

--- abc173/src/bin/b.rs
+++ abc173/src/bin/b.rs
@@ -2,8 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       s:[String;n]
     }
-    println!("{}", a);
-    todo!();
+    let ac = s.iter().filter(|&s| s == "AC").count();
+    let wa = s.iter().filter(|&s| s == "WA").count();
+    let tle = s.iter().filter(|&s| s == "TLE").count();
+    let re = s.iter().filter(|&s| s == "RE").count();
+    println!("AC x {}", ac);
+    println!("WA x {}", wa);
+    println!("TLE x {}", tle);
+    println!("RE x {}", re);
 }
