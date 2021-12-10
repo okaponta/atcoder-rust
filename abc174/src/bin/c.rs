d70a1ab23f31c39c45610c8e5af2b5ef1cb00760
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-10 13:27:58 +0900

    add abc174_c

--- abc174/src/bin/c.rs
+++ abc174/src/bin/c.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       k:i32,
     }
-    println!("{}", a);
-    todo!();
+    if k == 1 || k == 7 {
+        println!("{}", 1);
+        return;
+    }
+    let mut st = 7;
+    let mut rem = 7;
+    for i in 2..=k {
+        st = st * 10 % k;
+        rem = (rem + st) % k;
+        if rem == 0 {
+            println!("{}", i);
+            return;
+        }
+    }
+    println!("{}", -1);
 }
