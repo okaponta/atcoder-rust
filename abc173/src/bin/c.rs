2a7ad59cadcb942f19d68bfbdca194e2d362fb88
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-11 10:52:50 +0900

    add abc173_c

--- abc173/src/bin/c.rs
+++ abc173/src/bin/c.rs
@@ -1,9 +1,21 @@
-use proconio::input;
+use itertools::iproduct;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       h:usize,w:usize,k:usize,
+       c:[Chars;h]
     }
-    println!("{}", a);
-    todo!();
+    let count = iproduct!(0..1 << h, 0..1 << w)
+        .filter(|&(row, col)| {
+            iproduct!(
+                (0..h).filter(|&i| (row >> i) % 2 == 1),
+                (0..w).filter(|&j| (col >> j) % 2 == 1)
+            )
+            .filter(|&(i, j)| c[i][j] == '#')
+            .count()
+                == k
+        })
+        .count();
+    println!("{}", count);
 }
