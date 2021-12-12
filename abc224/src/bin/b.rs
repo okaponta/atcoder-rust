3b08a6b2194ecb8bf138f2b5ac56b394b09d5e9c
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-12 10:24:12 +0900

    add abc224_b

--- abc224/src/bin/b.rs
+++ abc224/src/bin/b.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       h:usize,w:usize,
+       a:[[i32;w];h]
     }
-    println!("{}", a);
-    todo!();
+    for i1 in 0..h - 1 {
+        for i2 in i1 + 1..h {
+            for j1 in 0..w - 1 {
+                for j2 in j1 + 1..w {
+                    if a[i1][j1] + a[i2][j2] > a[i2][j1] + a[i1][j2] {
+                        println!("No");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    println!("Yes");
 }
