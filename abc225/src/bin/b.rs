77f9c4427b663d3f3d1e7ed601bbb2a746b1ff00
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-13 13:24:22 +0900

    add abc225_b

--- abc225/src/bin/b.rs
+++ abc225/src/bin/b.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       ab:[usize;2*n-2]
     }
-    println!("{}", a);
-    todo!();
+    let mut count = vec![0; n];
+    ab.iter().for_each(|&x| count[x - 1] += 1);
+    println!(
+        "{}",
+        if count.iter().any(|&e| e == n - 1) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
