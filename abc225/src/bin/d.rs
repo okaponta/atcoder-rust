6a4dd5b1d350af2ec78c3c501f1e6a95391edacb
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-13 22:38:44 +0900

    add abc225_d

--- abc225/src/bin/d.rs
+++ abc225/src/bin/d.rs
@@ -2,8 +2,48 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,q:i32,
     }
-    println!("{}", a);
-    todo!();
+    let mut train = (0..=n).into_iter().map(|_e| (0, 0)).collect::<Vec<_>>();
+    for _i in 0..q {
+        input! {
+           qq:i32,
+        }
+        if qq == 1 {
+            input! { x:usize,y:usize, }
+            let x_af = (train[x].0, y as i32);
+            let y_af = (x as i32, train[y].1);
+            train[x] = x_af;
+            train[y] = y_af;
+        }
+        if qq == 2 {
+            input! { x:usize,y:usize, }
+            let x_af = (train[x].0, 0);
+            let y_af = (0, train[y].1);
+            train[x] = x_af;
+            train[y] = y_af;
+        }
+        if qq == 3 {
+            input! { x:usize,}
+            let mut first = x as i32;
+            let mut tmp = train[x];
+            while tmp.0 != 0 {
+                first = tmp.0;
+                tmp = train[tmp.0 as usize];
+            }
+            let mut out = vec![first];
+            while tmp.1 != 0 {
+                out.push(tmp.1);
+                tmp = train[tmp.1 as usize];
+            }
+            println!(
+                "{} {}",
+                out.len(),
+                out.iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            );
+        }
+    }
 }
