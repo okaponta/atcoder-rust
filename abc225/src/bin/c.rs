92208f30840cde66331656c7147e85f46cbeeb02
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-13 21:00:03 +0900

    add abc225_c

--- abc225/src/bin/c.rs
+++ abc225/src/bin/c.rs
@@ -2,8 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       b:[[i32;m];n],
     }
-    println!("{}", a);
-    todo!();
+    for i in 0..n {
+        for j in 0..m {
+            if i == 0 {
+                if j != 0 {
+                    if b[i][j] != b[i][j - 1] + 1 {
+                        println!("No");
+                        return;
+                    }
+                    if b[i][j] % 7 == 1 {
+                        println!("No");
+                        return;
+                    }
+                }
+            } else {
+                if b[i][j] != b[i - 1][j] + 7 {
+                    println!("No");
+                    return;
+                }
+            }
+        }
+    }
+    println!("Yes");
 }
