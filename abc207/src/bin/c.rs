33ed0fa936c68fa41a144055a860717a2bb55587
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-14 10:03:09 +0900

    add abc207_c

--- abc207/src/bin/c.rs
+++ abc207/src/bin/c.rs
@@ -1,8 +1,21 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       mut tlr:[(i32,i32,i32);n]
     }
-    println!("{}", a);
+    let ans = tlr
+        .iter()
+        .map(|&(t, l, r)| match t {
+            2 => (l * 2, r * 2 - 1),
+            3 => (l * 2 + 1, r * 2),
+            4 => (l * 2 + 1, r * 2 - 1),
+            _ => (l * 2, r * 2),
+        })
+        .tuple_combinations()
+        .filter(|&(a, b)| a.0.max(b.0) <= a.1.min(b.1))
+        .count();
+    println!("{}", ans);
 }
