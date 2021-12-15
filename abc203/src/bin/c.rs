a9f0448bad7fceea3f1d69eb8d9d293198c84f72
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-15 10:24:20 +0900

    add abc203_c

--- abc203/src/bin/c.rs
+++ abc203/src/bin/c.rs
@@ -2,7 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       mut k:i64,
+       mut ab:[(i64,i64);n]
     }
-    println!("{}", a);
+    ab.sort_by(|a, b| a.0.cmp(&(b.0)));
+    let mut ans: i64 = 0;
+    for i in 0..n {
+        let after = k - ab[i].0 + ans;
+        if after < 0 {
+            println!("{}", ans + k);
+            return;
+        } else {
+            ans = ab[i].0;
+            k = after + ab[i].1;
+        }
+    }
+    println!("{}", ans + k);
 }
