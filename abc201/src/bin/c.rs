12eac431a054310724a7f4b07c923edec231b294
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-16 04:33:18 +0900

    add abc201_c

--- abc201/src/bin/c.rs
+++ abc201/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       s:Chars,
     }
-    println!("{}", a);
+    let o = s.iter().filter(|&c| *c == 'o').count();
+    let q = s.iter().filter(|&c| *c == '?').count();
+    let ans = match o {
+        0 => q * q * q * q,
+        1 => (q + 1) * (q + 1) * (q + 1) * (q + 1) - q * q * q * q,
+        2 => 2 * 2 * 2 * 2 - 2 + 4 * (2 * 2 * 2 - 2) * q + 6 * 2 * q * q,
+        3 => 3 * (3 * 3 * 3 - 2 * 2 * 2 * 2 + 1) + 4 * 6 * q,
+        4 => 4 * 3 * 2,
+        _ => 0,
+    };
+    println!("{}", ans);
 }
