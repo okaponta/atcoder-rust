b376a929b5a02f0a8e20b5e428f1029ea7286b9c
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-17 20:24:43 +0900

    add abc209_c

--- abc209/src/bin/c.rs
+++ abc209/src/bin/c.rs
@@ -1,9 +1,15 @@
 use proconio::input;
 
+const MOD: i64 = 1000000007;
+
 fn main() {
     input! {
-       a:i32,
+       n:usize, mut c:[i64;n],
     }
-    println!("{}", a);
-    todo!();
+    c.sort();
+    let mut ans: i64 = 1;
+    for i in 0..n {
+        ans = (ans * (c[i] - i as i64)) % MOD;
+    }
+    println!("{}", ans);
 }
