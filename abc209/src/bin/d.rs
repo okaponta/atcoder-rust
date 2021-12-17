ee05f5a515c0ec6281a4194e85336567fe50fc96
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-17 23:19:16 +0900

    add abc209_d

--- abc209/src/bin/d.rs
+++ abc209/src/bin/d.rs
@@ -1,9 +1,40 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,q:usize,
+       ab: [(Usize1,Usize1);n-1],
+       cd: [(Usize1,Usize1);q]
     }
-    println!("{}", a);
-    todo!();
+    let mut road = vec![vec![]; n];
+    for (a, b) in ab {
+        road[a].push(b);
+        road[b].push(a);
+    }
+
+    let mut depth = vec![-1; n];
+    let mut s = vec![];
+    depth[0] = 0;
+    s.push(0);
+
+    while let Some(x) = s.pop() {
+        for &y in &road[x] {
+            if depth[y] != -1 {
+                continue;
+            }
+            depth[y] = depth[x] + 1;
+            s.push(y);
+        }
+    }
+
+    for (c, d) in cd {
+        println!(
+            "{}",
+            if (depth[c] - depth[d]) % 2 == 0 {
+                "Town"
+            } else {
+                "Road"
+            }
+        );
+    }
 }
