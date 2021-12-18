389e31a174279741a312c5ebf23e6a194ea76734
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-18 19:43:01 +0900

    add abc229_e

--- abc229/src/bin/e.rs
+++ abc229/src/bin/e.rs
@@ -1,9 +1,27 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       ab:[(Usize1,Usize1);m]
     }
-    println!("{}", a);
-    todo!();
+    let mut road = vec![vec![]; n];
+    for (a, b) in ab {
+        road[a].push(b);
+    }
+
+    let mut uf = UnionFind::new(n);
+    let mut ans = vec![0; n];
+    for i in (1..n).rev() {
+        ans[i - 1] = ans[i] + 1;
+        for &v in &road[i] {
+            if uf.union(i, v) {
+                ans[i - 1] -= 1;
+            }
+        }
+    }
+    for e in ans {
+        println!("{}", e);
+    }
 }
