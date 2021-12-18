edbd6a8b48dbcc6bb36e75b52a42fefad2666251
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-18 10:43:11 +0900

    add abc229_b

--- abc229/src/bin/b.rs
+++ abc229/src/bin/b.rs
@@ -1,9 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       a:Chars,b:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let a_rev = a.iter().rev().collect::<Vec<&char>>();
+    let b_rev = b.iter().rev().collect::<Vec<&char>>();
+    for i in 0..a.len().min(b.len()) {
+        if a_rev[i].to_digit(10).unwrap() + b_rev[i].to_digit(10).unwrap() > 9 {
+            println!("Hard");
+            return;
+        }
+    }
+    println!("Easy");
 }
