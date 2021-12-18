8814ab2a664ccc547ad1fcf3159ba9e2e7663461
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-18 12:01:40 +0900

    add abc229_d

--- abc229/src/bin/d.rs
+++ abc229/src/bin/d.rs
@@ -1,9 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       s:Chars,k:usize
     }
-    println!("{}", a);
-    todo!();
+    let mut cum = s
+        .iter()
+        .scan(0, |state, x| {
+            if *x == '.' {
+                *state += 1;
+            }
+            Some(*state)
+        })
+        .collect::<Vec<_>>();
+    cum.insert(0, 0);
+    let mut r = 0;
+    let mut ans = 0;
+    for l in 0..s.len() {
+        while r < s.len() && cum[r + 1] - cum[l] <= k {
+            r += 1;
+        }
+        ans = ans.max(r - l);
+    }
+    println!("{}", ans);
 }
