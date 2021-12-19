6820237023f3f4da7fd15260403da50c9e42aa2b
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-19 11:24:19 +0900

    add abc228_d

--- abc228/src/bin/d.rs
+++ abc228/src/bin/d.rs
@@ -1,9 +1,26 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
+
+const MOD: i64 = 1048576;
 
 fn main() {
     input! {
-       a:i32,
+       q:usize, tx:[(i64,i64);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut a: Vec<i64> = vec![-1; MOD as usize];
+    let mut set = (0..=MOD).collect::<BTreeSet<_>>();
+    for (t, x) in tx {
+        if t == 1 {
+            let mut h = x % MOD;
+            h = *set.range(h..).next().unwrap();
+            if h == MOD {
+                h = *set.range(0..).next().unwrap();
+            }
+            set.remove(&h);
+            a[h as usize] = x;
+        } else {
+            println!("{}", a[(x % MOD) as usize]);
+        }
+    }
 }
