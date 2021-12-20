cbc852e8c7bc728d8b7cbb1b1e00298899c0fb42
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-20 20:50:59 +0900

    add abc227_b

--- abc227/src/bin/b.rs
+++ abc227/src/bin/b.rs
@@ -1,9 +1,21 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       s:[i32;n]
     }
-    println!("{}", a);
-    todo!();
+    let mut cand: HashSet<i32> = HashSet::new();
+    for a in 1..200 {
+        for b in 1..200 {
+            let area = 4 * a * b + 3 * a + 3 * b;
+            if area > 1000 {
+                break;
+            }
+            cand.insert(area);
+        }
+    }
+    let ans = s.iter().filter(|&x| !cand.contains(x)).count();
+    println!("{}", ans);
 }
