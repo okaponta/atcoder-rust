1608d893c1d5e48cd21d9819392df344f780c0ad
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-20 21:06:43 +0900

    add abc227_c

--- abc227/src/bin/c.rs
+++ abc227/src/bin/c.rs
@@ -1,9 +1,20 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:u64,
     }
-    println!("{}", a);
-    todo!();
+    let mut count = 0;
+    let mut amax: u64 = 1;
+    while amax * amax * amax <= n {
+        amax += 1;
+    }
+    for a in 1..=amax {
+        let bmax = (n / a).sqrt();
+        for b in a..=bmax {
+            count += (n / (a * b)) - b + 1;
+        }
+    }
+    println!("{}", count);
 }
