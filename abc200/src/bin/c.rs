6751989869f96d9420740f8b9c34982e2d43a0a7
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-21 09:24:56 +0900

    add abc200_c

--- abc200/src/bin/c.rs
+++ abc200/src/bin/c.rs
@@ -2,7 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       a:[i64;n],
     }
-    println!("{}", a);
+    let mut rem: Vec<i64> = vec![0; 200];
+    a.iter().map(|e| e % 200).for_each(|e| rem[e as usize] += 1);
+    let ans = rem.iter().fold(0, |acc, x| acc + (x * (x - 1) / 2));
+    println!("{}", ans);
 }
