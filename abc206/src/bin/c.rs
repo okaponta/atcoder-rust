fd46deaa59bf2d757b7eb12c07313a7e24a1568c
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-21 10:02:21 +0900

    add abc206_c

--- abc206/src/bin/c.rs
+++ abc206/src/bin/c.rs
@@ -1,8 +1,23 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,a:[i64;n],
     }
-    println!("{}", a);
+    let mut map: HashMap<i64, i64> = HashMap::new();
+    for ai in a {
+        if map.contains_key(&ai) {
+            let before = *map.get(&ai).unwrap();
+            map.insert(ai, before + 1);
+        } else {
+            map.insert(ai, 1);
+        }
+    }
+    let ans = map
+        .values()
+        .into_iter()
+        .map(|a| (a * (a - 1)) / 2)
+        .fold((n * (n - 1)) / 2, |acc, x| acc - x);
+    println!("{}", ans);
 }
