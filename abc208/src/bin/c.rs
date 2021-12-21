3dbf1215cb5c074bf09527109e5d7a0d8099e23d
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-22 08:46:20 +0900

    add abc208_c

--- abc208/src/bin/c.rs
+++ abc208/src/bin/c.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,k:i64,
+       a:[i64;n],
     }
-    println!("{}", a);
+    let base = k / n;
+    let remain = k % n;
+    let mut ac = a.clone();
+    ac.sort();
+    let a_small = ac.iter().nth(remain as usize).unwrap();
+    for a in a {
+        if &a < a_small {
+            println!("{}", base + 1);
+        } else {
+            println!("{}", base);
+        }
+    }
 }
