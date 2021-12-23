2ecb8d9c0c6843b46e608bd4ee6a3aa378e565c0
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-23 13:09:04 +0900

    add abc205_c

--- abc205/src/bin/c.rs
+++ abc205/src/bin/c.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       mut a:i32,mut b:i32,c:i32,
     }
-    println!("{}", a);
+    if c % 2 == 0 {
+        a = a.abs();
+        b = b.abs();
+    }
+    println!(
+        "{}",
+        if a < b {
+            "<"
+        } else if a == b {
+            "="
+        } else {
+            ">"
+        }
+    )
 }
