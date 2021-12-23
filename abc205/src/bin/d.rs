6033c2a74e9a9ecdc92064353812b0073f3de10d
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-23 13:33:10 +0900

    add abc205_d

--- abc205/src/bin/d.rs
+++ abc205/src/bin/d.rs
@@ -2,7 +2,25 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,q:i64,
+       a:[i64;n],
+       k:[i64;q],
     }
-    println!("{}", a);
+    for ki in k {
+        if ki < a[0] {
+            println!("{}", ki);
+            continue;
+        }
+        let mut l = 0;
+        let mut r = n;
+        while r - l > 1 {
+            let m = (l + r) / 2;
+            if a[m as usize] < ki + m + 1 {
+                l = m
+            } else {
+                r = m
+            }
+        }
+        println!("{}", ki + l + 1);
+    }
 }
