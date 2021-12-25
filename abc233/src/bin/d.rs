29a43e1c0d2092ef5f4cca99f143c9c7d98dd3a5
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-25 23:12:09 +0900

    add abc233_d

--- abc233/src/bin/d.rs
+++ abc233/src/bin/d.rs
@@ -3,40 +3,18 @@ use std::collections::HashMap;
 use proconio::input;
 
 fn main() {
-    // TLE
     input! {
-       n:i64,k:i64,
+       n:usize,k:i64,
        a:[i64;n]
     }
-    let mut s = a
-        .iter()
-        .scan(0, |state, &x| {
-            *state = *state + x;
-            Some(*state)
-        })
-        .collect::<Vec<_>>();
-    s.insert(0, 0);
-    let mut counts: HashMap<i64, Vec<usize>> = HashMap::new();
-    for i in 0..s.len() {
-        if counts.contains_key(&s[i]) {
-            let mut after = counts.get(&s[i]).unwrap().clone();
-            after.push(i);
-            counts.insert(s[i], after);
-        } else {
-            counts.insert(s[i], vec![i]);
-        }
-    }
-    let mut ans = 0;
-    for i in 0..s.len() {
-        let target = s[i] + k;
-        if counts.contains_key(&target) {
-            let cand = counts.get(&target).unwrap();
-            for c in cand {
-                if c > &i {
-                    ans += 1;
-                }
-            }
-        }
+    let mut ans: i64 = 0;
+    let mut map = HashMap::new();
+    let mut sum: i64 = 0;
+    map.insert(0, 1);
+    for ai in a {
+        sum += ai;
+        ans += map.get(&(sum - k)).map_or(0, |p| *p);
+        *map.entry(sum).or_insert(0) += 1;
     }
     println!("{}", ans);
 }
