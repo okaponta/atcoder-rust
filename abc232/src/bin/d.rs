48b0b1b1995fe9ffd4122d84ebd87d6164f5a829
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-26 23:10:24 +0900

    add abc232_d

--- abc232/src/bin/d.rs
+++ abc232/src/bin/d.rs
@@ -1,9 +1,46 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       h:usize,w:usize,
+       c:[Chars;h],
     }
-    println!("{}", a);
-    todo!();
+    let mut grid = vec![vec![0; w]; h];
+    grid[0][0] = 1;
+    for j in 1..w {
+        if c[0][j] == '#' {
+            continue;
+        }
+        if grid[0][j - 1] != 0 {
+            grid[0][j] = grid[0][j - 1] + 1;
+        }
+    }
+    for i in 1..h {
+        for j in 0..w {
+            if c[i][j] == '#' {
+                continue;
+            }
+            if j == 0 {
+                if grid[i - 1][j] != 0 {
+                    grid[i][0] = grid[i - 1][0] + 1;
+                }
+                continue;
+            }
+            let mut cand1 = 0;
+            let mut cand2 = 0;
+            if grid[i - 1][j] != 0 {
+                cand1 = grid[i - 1][j] + 1;
+            }
+            if grid[i][j - 1] != 0 {
+                cand2 = grid[i][j - 1] + 1;
+            }
+            grid[i][j] = cand1.max(cand2);
+        }
+    }
+    let ans = grid
+        .iter()
+        .map(|e| e.iter().max().unwrap_or(&0))
+        .max()
+        .unwrap();
+    println!("{}", ans);
 }
