ac85ab2ab12477039d30ebcf44c9fcade70118f8
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-26 11:56:30 +0900

    add abc232_c

--- abc232/src/bin/c.rs
+++ abc232/src/bin/c.rs
@@ -1,9 +1,29 @@
-use proconio::input;
+use itertools::{iproduct, Itertools};
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       ab:[(Usize1,Usize1);m],
+       cd:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut x = vec![vec![false; n]; n];
+    let mut y = vec![vec![false; n]; n];
+    for (a, b) in ab {
+        x[a][b] = true;
+        x[b][a] = true;
+    }
+    for (c, d) in cd {
+        y[c][d] = true;
+        y[d][c] = true;
+    }
+    let same = (0..n).permutations(n).any(|p| {
+        for (i, j) in iproduct!(0..n, 0..n) {
+            if x[i][j] != y[p[i]][p[j]] {
+                return false;
+            }
+        }
+        true
+    });
+    println!("{}", if same { "Yes" } else { "No" });
 }
