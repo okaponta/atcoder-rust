734ab743dcad5e0e6b9513f04ef0bc20599b3a75
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-27 13:13:05 +0900

    add abc231_c

--- abc231/src/bin/c.rs
+++ abc231/src/bin/c.rs
@@ -2,8 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,q:usize,
+       mut a:[i32;n],
+       x:[i32;q],
     }
-    println!("{}", a);
-    todo!();
+    a.sort();
+
+    for xi in x {
+        if xi <= a[0] {
+            println!("{}", n);
+            continue;
+        }
+        let mut l = 0;
+        let mut r = n;
+        while r - l > 1 {
+            let m = (l + r) / 2;
+            if a[m as usize] < xi {
+                l = m
+            } else {
+                r = m
+            }
+        }
+        println!("{}", n - r);
+    }
 }
