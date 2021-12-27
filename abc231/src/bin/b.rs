f6ec763d38249edd9796d98f470d030f285e4d8d
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-27 12:44:09 +0900

    add abc231_b

--- abc231/src/bin/b.rs
+++ abc231/src/bin/b.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       s:[String;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut map = HashMap::new();
+    for si in s {
+        *map.entry(si).or_insert(0) += 1;
+    }
+    println!("{}", map.iter().max_by(|x, y| x.1.cmp(y.1)).unwrap().0);
 }
