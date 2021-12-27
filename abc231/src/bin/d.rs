c1c9dca3c762a5dedd9f6edaf3021819f1a5813f
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-27 13:35:54 +0900

    add abc231_d

--- abc231/src/bin/d.rs
+++ abc231/src/bin/d.rs
@@ -1,9 +1,30 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       ab:[(Usize1,Usize1);m]
     }
-    println!("{}", a);
-    todo!();
+    if n <= m {
+        println!("No");
+        return;
+    }
+    let mut neighbor = vec![vec![]; n];
+    let mut uf = UnionFind::new(n);
+    for (a, b) in ab {
+        neighbor[a].push(b);
+        neighbor[b].push(a);
+        if !uf.union(a, b) {
+            println!("No");
+            return;
+        }
+    }
+    for i in neighbor {
+        if i.len() > 2 {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
