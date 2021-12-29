788b823384716abda0e0daa52f01f2493cb80b02
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-29 23:41:57 +0900

    add abc196_c

--- abc196/src/bin/c.rs
+++ abc196/src/bin/c.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,
     }
-    println!("{}", a);
+    for i in 0..1000001 {
+        let num: i64 = format!("{}{}", i, i).parse().unwrap();
+        if num > n {
+            println!("{}", i - 1);
+            return;
+        }
+    }
 }
