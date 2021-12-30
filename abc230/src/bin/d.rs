585b08adec17aefcaba36304f87e2bdfb023e251
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-30 12:44:18 +0900

    add abc230_d

--- abc230/src/bin/d.rs
+++ abc230/src/bin/d.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,d:i32,
+       mut lr:[(i32,i32);n],
     }
-    println!("{}", a);
-    todo!();
+    lr.sort_by_key(|&(_l, r)| r);
+    let mut ans = 1;
+    let mut punch = lr[0].1;
+    for wall in lr {
+        if wall.0 < punch + d {
+            continue;
+        } else {
+            ans += 1;
+            punch = wall.1;
+        }
+    }
+    println!("{}", ans);
 }
