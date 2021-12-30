55dc7c5ccebb5289329ef9c68da1c15297af4270
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-30 12:14:35 +0900

    add abc230_c

--- abc230/src/bin/c.rs
+++ abc230/src/bin/c.rs
@@ -1,9 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       _n:usize,a:Usize1,b:Usize1,
+       p:Usize1,q:Usize1,r:Usize1,s:Usize1,
     }
-    println!("{}", a);
-    todo!();
+    let mut first = b as i64 - a as i64 + p as i64;
+    let mut second = (b + a) as i64 - p as i64;
+    for _i in p..=q {
+        let mut ans = vec!['.'; s - r + 1];
+        if r as i64 <= first && first <= s as i64 {
+            ans[first as usize - r] = '#';
+        }
+        if r as i64 <= second && second <= s as i64 {
+            ans[second as usize - r] = '#';
+        }
+        println!("{}", ans.iter().collect::<String>());
+        first += 1;
+        second -= 1;
+    }
 }
