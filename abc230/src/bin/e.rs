085a9d53fecc388ef35f266bd9f059e1a9fcb4a9
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-30 13:27:03 +0900

    add abc230_e

--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans: i64 = 0;
+    let mut i: i64 = 0;
+    while i * i < n {
+        i += 1;
+        ans += n / i;
+    }
+    let mut tmp = n / i;
+    ans -= tmp;
+    while tmp > 0 {
+        let max = n / tmp;
+        ans += (max - i + 1) * tmp;
+        tmp -= 1;
+        i = max + 1;
+    }
+    println!("{}", ans);
 }
