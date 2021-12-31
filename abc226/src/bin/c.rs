7f425ce81e3574edee0e398c4734ae5d4575f901
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-31 16:12:45 +0900

    add abc226_c

--- abc226/src/bin/c.rs
+++ abc226/src/bin/c.rs
@@ -1,9 +1,42 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,
     }
-    println!("{}", a);
-    todo!();
+    let mut tka = vec![vec![]; n as usize];
+    for i in 0..n {
+        input! {
+            t:i64,
+            k:i64,
+            a:[i64;k],
+        }
+        let mut tkai = a;
+        tkai.insert(0, t);
+        tkai.insert(1, k);
+
+        tka[i as usize] = tkai;
+    }
+    tka.insert(0, vec![0]);
+
+    let mut need_waza = vec![n];
+    let mut mastered_waza = HashSet::new();
+    let mut ans = 0;
+    while need_waza.len() > 0 {
+        let target = need_waza.pop().unwrap();
+        if mastered_waza.contains(&target) {
+            continue;
+        }
+        ans += tka[target as usize][0];
+        mastered_waza.insert(target);
+        let k = tka[target as usize][1];
+        if k > 0 {
+            for i in 2..=k + 1 {
+                need_waza.push(tka[target as usize][i as usize]);
+            }
+        }
+    }
+    println!("{}", ans);
 }
