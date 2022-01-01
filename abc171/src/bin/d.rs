86bf0cad759fb2f849a0e1980becbb6ca3096a65
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-01 13:01:57 +0900

    add abc171_d

--- abc171/src/bin/d.rs
+++ abc171/src/bin/d.rs
@@ -1,9 +1,22 @@
+use im_rc::HashMap;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       a:[i64;n],
+       q:i32,
+       bc:[(i64,i64);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = a.iter().sum::<i64>();
+    let mut map: HashMap<i64, i64> = HashMap::new();
+    for ai in a {
+        *map.entry(ai).or_insert(0) += 1;
+    }
+    for bci in bc {
+        let num = map.remove(&bci.0).unwrap_or(0);
+        ans += (bci.1 - bci.0) * num;
+        println!("{}", ans);
+        *map.entry(bci.1).or_insert(0) += num;
+    }
 }
