a19ef2fcb3008a9db252fd68d843db9b5006b65e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-01 12:35:40 +0900

    add abc171_c

--- abc171/src/bin/c.rs
+++ abc171/src/bin/c.rs
@@ -2,8 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       mut n:i64,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = vec![];
+    while n > 0 {
+        n -= 1;
+        ans.push(b'a' + (n % 26) as u8);
+        n /= 26;
+    }
+    println!(
+        "{}",
+        ans.iter().rev().map(|&s| s as char).collect::<String>()
+    );
 }
