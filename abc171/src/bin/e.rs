782478c719e0d2d64690739597b32ebfa35a84c2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-01 19:43:22 +0900

    add abc171_e

--- abc171/src/bin/e.rs
+++ abc171/src/bin/e.rs
@@ -2,8 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
+       a:[i32;n]
     }
-    println!("{}", a);
-    todo!();
+    let xor = a.iter().fold(0, |acc, x| acc ^ x);
+    println!(
+        "{}",
+        a.iter()
+            .map(|e| format!("{}", xor ^ e))
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
