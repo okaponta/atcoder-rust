680b34529355d3a9dd5c62b95e0044848dcf108d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-02 09:51:03 +0900

    add abc170_c

--- abc170/src/bin/c.rs
+++ abc170/src/bin/c.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       x:i32,n:i32,
+       p:[i32;n],
     }
-    println!("{}", a);
-    todo!();
+    for i in 0..101 {
+        if !p.contains(&(x - i)) {
+            println!("{}", x - i);
+            return;
+        }
+        if !p.contains(&(x + i)) {
+            println!("{}", x + i);
+            return;
+        }
+    }
 }
