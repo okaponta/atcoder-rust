1604f67ebb25e46d42a5ed5113cfac5517b3509c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-02 10:33:51 +0900

    add abc170_d

--- abc170/src/bin/d.rs
+++ abc170/src/bin/d.rs
@@ -1,9 +1,30 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,a:[i32;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut ex = vec![true; 1000001];
+    let mut set = HashSet::new();
+    for ai in a.clone() {
+        if set.contains(&ai) {
+            ex[ai as usize] = false;
+            continue;
+        }
+        set.insert(ai);
+        let mut mul = ai * 2;
+        while mul <= 1000000 {
+            ex[mul as usize] = false;
+            mul += ai;
+        }
+    }
+    let mut count = 0;
+    for ai in a {
+        if ex[ai as usize] {
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
