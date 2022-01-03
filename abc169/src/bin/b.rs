1c9a07f1d796f75f0b2d7fa951512b7b954ea817
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-03 17:20:49 +0900

    add abc169_b

--- abc169/src/bin/b.rs
+++ abc169/src/bin/b.rs
@@ -1,9 +1,23 @@
 use proconio::input;
 
+const MAX: i64 = 1_000_000_000_000_000_000;
+
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       mut a:[i64;n],
     }
-    println!("{}", a);
-    todo!();
+    if a.iter().any(|e| e == &0) {
+        println!("{}", 0);
+        return;
+    }
+    let mut ans = 1;
+    for ai in a {
+        if ans > MAX / ai {
+            println!("{}", -1);
+            return;
+        }
+        ans *= ai;
+    }
+    println!("{}", ans);
 }
