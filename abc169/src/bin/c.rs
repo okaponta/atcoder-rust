b1fcf33065907618ce987c65841971af3eb51fd7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-03 17:29:31 +0900

    add abc169_c

--- abc169/src/bin/c.rs
+++ abc169/src/bin/c.rs
@@ -1,9 +1,12 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       a:i64,b:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mul1 = a * (b[0].to_digit(10).unwrap() as i64);
+    let mul2 = a * (b[2].to_digit(10).unwrap() as i64);
+    let mul3 = a * (b[3].to_digit(10).unwrap() as i64);
+    let ans = (mul3 / 10 + mul2) / 10 + mul1;
+    println!("{}", ans);
 }
