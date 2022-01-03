838a8ed9aa197356e5953b22fdc5c1d9d8f5a7b8
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-03 22:22:56 +0900

    add abc169_d

--- abc169/src/bin/d.rs
+++ abc169/src/bin/d.rs
@@ -1,9 +1,38 @@
+use num_integer::Roots;
 use proconio::input;
+
+fn factorize(mut n: i64) -> Vec<(i64, i32)> {
+    let mut res = vec![];
+    for a in 2..=n.sqrt() {
+        if n % a != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % a == 0 {
+            ex += 1;
+            n /= a;
+        }
+        res.push((a, ex));
+    }
+    if n != 1 {
+        res.push((n, 1));
+    }
+    res
+}
 
 fn main() {
     input! {
-       a:i32,
+       n:i64,
     }
-    println!("{}", a);
-    todo!();
+    let factor = factorize(n);
+    let mut ans = 0;
+    for mut e in factor {
+        let mut count = 0;
+        while e.1 > count {
+            ans += 1;
+            count += 1;
+            e.1 -= count;
+        }
+    }
+    println!("{}", ans);
 }
