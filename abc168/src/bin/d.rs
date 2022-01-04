6801f7651c22b93ae6f64bc222ea8b6e8b759a45
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-04 12:05:05 +0900

    add abc168_d

--- abc168/src/bin/d.rs
+++ abc168/src/bin/d.rs
@@ -1,9 +1,33 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       ab:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut road = vec![vec![]; n];
+    for (a, b) in ab {
+        road[a].push(b);
+        road[b].push(a);
+    }
+
+    let mut ans = vec![1_000_000; n];
+    let mut q = VecDeque::new();
+    q.push_back(0);
+
+    while let Some(prev) = q.pop_front() {
+        for &next in road[prev].iter() {
+            if ans[next] == 1_000_000 {
+                ans[next] = prev;
+                q.push_back(next);
+            }
+        }
+    }
+
+    println!("Yes");
+    for i in 1..n {
+        println!("{}", ans[i] + 1);
+    }
 }
