08161aca26c554704743984951ca163a2becce3d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-04 11:26:11 +0900

    add abc168_c

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -1,9 +1,14 @@
+use std::f64::consts::PI;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       a:f64,b:f64,h:f64,m:f64,
     }
-    println!("{}", a);
-    todo!();
+    let degree = (((h * 60.0 + m) / 720.0) - (m / 60.0)) * 2.0 * PI;
+    let x = a * degree.cos();
+    let y = a * degree.sin();
+    let ans = ((b - x) * (b - x) + y * y).sqrt();
+    println!("{}", ans);
 }
