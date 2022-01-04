9948d5ffe89b94903658235d8184520b4d12d2ec
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-04 11:00:21 +0900

    add abc168_a

--- abc168/src/bin/a.rs
+++ abc168/src/bin/a.rs
@@ -2,8 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,
     }
-    println!("{}", a);
-    todo!();
+    let ans = match n % 10 {
+        2 | 4 | 5 | 7 | 9 => "hon",
+        0 | 1 | 6 | 8 => "pon",
+        _ => "bon",
+    };
+    println!("{}", ans);
 }
