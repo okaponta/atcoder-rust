538ed61efa89056b120e0dc177d7e197670b3639
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-05 14:52:55 +0900

    add abc226_d

--- abc226/src/bin/d.rs
+++ abc226/src/bin/d.rs
@@ -1,9 +1,20 @@
+use itertools::Itertools;
+use num_integer::gcd;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       xy:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut v = vec![];
+    for p in xy.iter().permutations(2) {
+        let x = p[0].0 - p[1].0;
+        let y = p[0].1 - p[1].1;
+        let gcd = gcd(x, y);
+        v.push((x / gcd, y / gcd));
+    }
+    v.sort();
+    v.dedup();
+    println!("{}", v.len());
 }
