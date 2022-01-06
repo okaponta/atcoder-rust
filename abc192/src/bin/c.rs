78619f301944a0256285b63c95b13a3659384543
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-07 00:30:24 +0900

    add abc192_c

--- abc192/src/bin/c.rs
+++ abc192/src/bin/c.rs
@@ -1,8 +1,31 @@
+use itertools::Itertools;
 use proconio::input;
+
+fn next(prev: i32) -> i32 {
+    let big = prev
+        .to_string()
+        .chars()
+        .sorted_by(|a, b| Ord::cmp(&b, &a))
+        .collect::<String>()
+        .parse::<i32>()
+        .unwrap();
+    let small = prev
+        .to_string()
+        .chars()
+        .sorted_by(|a, b| Ord::cmp(&a, &b))
+        .collect::<String>()
+        .parse::<i32>()
+        .unwrap();
+    big - small
+}
 
 fn main() {
     input! {
-       a:i32,
+       n:i32,k:i32,
     }
-    println!("{}", a);
+    let mut ans = n;
+    for _i in 0..k {
+        ans = next(ans);
+    }
+    println!("{}", ans);
 }
