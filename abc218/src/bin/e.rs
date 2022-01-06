5204bc9796ef5fd764b09f70156241027382256d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-06 14:36:38 +0900

    add abc218_e

--- abc218/src/bin/e.rs
+++ abc218/src/bin/e.rs
@@ -1,9 +1,18 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       mut abc:[(Usize1,Usize1,i32);m]
     }
-    println!("{}", a);
-    todo!();
+    abc.sort_by_key(|abc| abc.2);
+    let mut uf = UnionFind::new(n);
+    let mut ans = 0;
+    for (a, b, c) in abc {
+        if !uf.union(a, b) && c > 0 {
+            ans += c as i64;
+        }
+    }
+    println!("{}", ans);
 }
