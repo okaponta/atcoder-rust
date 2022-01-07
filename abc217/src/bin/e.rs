c2627a18c6f314ef631c7bd48e20358f296e292b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-07 23:36:52 +0900

    add abc217_e

--- abc217/src/bin/e.rs
+++ abc217/src/bin/e.rs
@@ -1,9 +1,34 @@
+use std::collections::{BTreeSet, VecDeque};
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       qn:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut s = BTreeSet::new();
+    let mut q = VecDeque::new();
+    for i in 0..qn {
+        input! {query:i32}
+        match query {
+            1 => {
+                input! {x: i32}
+                q.push_back((x, i));
+            }
+            2 => {
+                let ans = if let Some(&x) = s.iter().next() {
+                    s.remove(&x);
+                    x
+                } else {
+                    q.pop_front().unwrap()
+                };
+                println!("{}", ans.0);
+            }
+            _ => {
+                while !q.is_empty() {
+                    s.insert(q.pop_front().unwrap());
+                }
+            }
+        }
+    }
 }
