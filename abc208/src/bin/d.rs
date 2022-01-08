b5fd80b641403323fb54c86f6db47d9cf159203e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-08 12:50:07 +0900

    add abc208_d

--- abc208/src/bin/d.rs
+++ abc208/src/bin/d.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use itertools::iproduct;
+use proconio::{input, marker::Usize1};
+
+const INF: usize = 1 << 60;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       abc:[(Usize1,Usize1,usize);m]
     }
-    println!("{}", a);
+    let mut dp = vec![vec![INF; n]; n];
+    for i in 0..n {
+        dp[i][i] = 0;
+    }
+    for (a, b, c) in abc {
+        dp[a][b] = c;
+    }
+    let mut ans = 0;
+    for k in 0..n {
+        let mut e = vec![vec![0; n]; n];
+        for (s, t) in iproduct!(0..n, 0..n) {
+            e[s][t] = dp[s][t].min(dp[s][k] + dp[k][t]);
+            if e[s][t] < INF {
+                ans += e[s][t];
+            }
+        }
+        dp = e;
+    }
+    println!("{}", ans);
 }
