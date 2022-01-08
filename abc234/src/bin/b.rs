d73afa0d34671db1e91acbc87b0a1f60ff655a65
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-08 22:43:54 +0900

    add abc234_b

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -1,9 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       xy:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    let square = xy
+        .iter()
+        .tuple_combinations()
+        .map(|(a, b)| ((a.0 - b.0) * (a.0 - b.0) + (a.1 - b.1) * (a.1 - b.1)))
+        .max()
+        .unwrap() as f64;
+    println!("{}", square.sqrt());
 }
