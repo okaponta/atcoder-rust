48eaa8ca73649a90667c3a588f239693dd88292c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-08 22:44:00 +0900

    add abc234_d

--- abc234/src/bin/d.rs
+++ abc234/src/bin/d.rs
@@ -1,9 +1,20 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,k:usize,
+       p:[i32;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut heap = BinaryHeap::new();
+    for i in 0..k {
+        heap.push(-p[i]);
+    }
+    println!("{}", -heap.peek().unwrap());
+    for i in k..n {
+        heap.push(-p[i]);
+        heap.pop();
+        println!("{}", -heap.peek().unwrap());
+    }
 }
