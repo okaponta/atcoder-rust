91fa03b43e9cf55a64410362ebeda0ad6980eca8
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-09 00:27:30 +0900

    add abc234_e

--- abc234/src/bin/e.rs
+++ abc234/src/bin/e.rs
@@ -1,9 +1,31 @@
+use std::collections::HashSet;
+
 use proconio::input;
+use superslice::Ext;
+
+fn gen_tosasu() -> Vec<i64> {
+    let mut set = HashSet::new();
+    for first in 1..10 {
+        for d in -9..9 {
+            let mut s = String::new();
+            let mut digit = first;
+            for _ in 0..18 {
+                s.push(std::char::from_digit(digit as u32, 10).unwrap());
+                set.insert(s.parse::<i64>().unwrap());
+                digit += d;
+                if !(0 <= digit && digit <= 9) {
+                    break;
+                }
+            }
+        }
+    }
+    let mut res = set.into_iter().collect::<Vec<_>>();
+    res.sort();
+    res
+}
 
 fn main() {
-    input! {
-       a:i32,
-    }
-    println!("{}", a);
-    todo!();
+    input! {x:i64 }
+    let tosasu = gen_tosasu();
+    println!("{}", tosasu[tosasu.lower_bound(&x)]);
 }
