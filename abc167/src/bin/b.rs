c44904e369521b817ba3547777488da81f98c4a9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-09 11:23:32 +0900

    add abc167_b

--- abc167/src/bin/b.rs
+++ abc167/src/bin/b.rs
@@ -2,8 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       a:i64,b:i64,_c:i64,k:i64,
     }
-    println!("{}", a);
-    todo!();
+    println!(
+        "{}",
+        if k < a {
+            k
+        } else if k < a + b {
+            a
+        } else {
+            2 * a + b - k
+        }
+    );
 }
