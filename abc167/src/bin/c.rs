2f65dac0f3211967ccdfe4a7e6059a518d066e34
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-09 11:57:01 +0900

    add abc167_c

--- abc167/src/bin/c.rs
+++ abc167/src/bin/c.rs
@@ -2,8 +2,30 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,x:i32,
+       ca:[(i32,[i32;m]);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 1 << 30;
+    for i in 0..1 << n {
+        let mut books = vec![false; n];
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                books[j] = true;
+            }
+        }
+        let mut cost = 0;
+        let mut skill = vec![0; m];
+        for j in 0..n {
+            if books[j] {
+                cost += ca[j].0;
+                for k in 0..m {
+                    skill[k] += ca[j].1[k];
+                }
+            }
+        }
+        if skill.iter().filter(|e| e < &&x).count() == 0 {
+            ans = ans.min(cost);
+        }
+    }
+    println!("{}", if ans == 1 << 30 { -1 } else { ans });
 }
