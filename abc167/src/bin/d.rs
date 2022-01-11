3bc1aaca09f57f4f095513d85e35c1639e3d9a26
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-11 15:15:14 +0900

    add abc167_d

--- abc167/src/bin/d.rs
+++ abc167/src/bin/d.rs
@@ -1,9 +1,31 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,k:usize,
+       a:[Usize1;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut path = vec![0];
+    let mut uf = UnionFind::new(n);
+    let mut prev = 0;
+    let mut next = a[0];
+    while uf.union(prev, next) {
+        path.push(next);
+        prev = next;
+        next = a[prev];
+    }
+    let mut begin = 0;
+    for i in 0..path.len() {
+        if path[i] == next {
+            begin = i;
+        }
+    }
+    let loop_size = path.len() - begin;
+    let index = if k < begin {
+        k
+    } else {
+        ((k - begin) % loop_size) + begin
+    };
+    println!("{}", path[index] + 1);
 }
