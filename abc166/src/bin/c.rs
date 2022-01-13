2b023622c850765bea7458689c4f25f9a9793a88
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-13 23:00:46 +0900

    add abc166_c

--- abc166/src/bin/c.rs
+++ abc166/src/bin/c.rs
@@ -1,9 +1,23 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       h:[i32;n],
+       ab:[(Usize1,Usize1);m]
     }
-    println!("{}", a);
-    todo!();
+    let mut bad = HashSet::new();
+    for (a, b) in ab {
+        if h[a] > h[b] {
+            bad.insert(b);
+        } else if h[a] == h[b] {
+            bad.insert(a);
+            bad.insert(b);
+        } else {
+            bad.insert(a);
+        }
+    }
+    println!("{}", n - bad.len());
 }
