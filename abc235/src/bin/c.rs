85256a25cc69de1a027ff49ffccda244dd99cf0a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-15 22:45:30 +0900

    add abc235_c

--- abc235/src/bin/c.rs
+++ abc235/src/bin/c.rs
@@ -1,9 +1,28 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,q:usize,
+       a:[usize;n],
+       xk:[(usize,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut map: HashMap<usize, Vec<usize>> = HashMap::new();
+    for i in 0..n {
+        let count = map.entry(a[i]).or_insert(vec![]);
+        count.push(i + 1);
+    }
+    for (x, k) in xk {
+        if !map.contains_key(&x) {
+            println!("{}", -1);
+            continue;
+        }
+        let target = map.get(&x).unwrap();
+        if target.len() < k {
+            println!("{}", -1);
+            continue;
+        }
+        println!("{}", target[k - 1]);
+    }
 }
