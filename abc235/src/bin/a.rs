0152cd02cbc76f7f4cce74abba2af18de38ef0ac
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-15 22:45:24 +0900

    add abc235_a

--- abc235/src/bin/a.rs
+++ abc235/src/bin/a.rs
@@ -5,7 +5,7 @@ fn main() {
        s:Chars,
     }
     let a = s[0].to_digit(10).unwrap() as i32;
-    let b = s[0].to_digit(10).unwrap() as i32;
-    let c = s[0].to_digit(10).unwrap() as i32;
+    let b = s[1].to_digit(10).unwrap() as i32;
+    let c = s[2].to_digit(10).unwrap() as i32;
     println!("{}", (a + b + c) * 111);
 }
