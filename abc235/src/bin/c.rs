62d1857334524c7c3648c4f87d1c6901a2ab0a59
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-15 22:59:46 +0900

    add abc235_c

--- abc235/src/bin/c.rs
+++ abc235/src/bin/c.rs
@@ -10,19 +10,15 @@ fn main() {
     }
     let mut map: HashMap<usize, Vec<usize>> = HashMap::new();
     for i in 0..n {
-        let count = map.entry(a[i]).or_insert(vec![]);
-        count.push(i + 1);
+        map.entry(a[i]).or_insert(vec![]).push(i + 1);
     }
     for (x, k) in xk {
-        if !map.contains_key(&x) {
-            println!("{}", -1);
-            continue;
+        if let Some(val) = map.get(&x) {
+            if val.len() >= k {
+                println!("{}", val[k - 1]);
+                continue;
+            }
         }
-        let target = map.get(&x).unwrap();
-        if target.len() < k {
-            println!("{}", -1);
-            continue;
-        }
-        println!("{}", target[k - 1]);
+        println!("{}", -1);
     }
 }
