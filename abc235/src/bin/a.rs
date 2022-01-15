17c47c7e8c081694f2986c67acf3fc18a2a33c31
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-15 21:03:20 +0900

    join abc235

---
+++ abc235/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+       s:Chars,
+    }
+    let a = s[0].to_digit(10).unwrap() as i32;
+    let b = s[0].to_digit(10).unwrap() as i32;
+    let c = s[0].to_digit(10).unwrap() as i32;
+    println!("{}", (a + b + c) * 111);
+}
