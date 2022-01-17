e50ab0d807fe5dfc2656577d96d0e8cebbd17afa
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-17 13:55:27 +0900

    add abc216_e

--- abc216/src/bin/e.rs
+++ abc216/src/bin/e.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,mut k:i64,
+       mut a:[i64;n]
     }
-    println!("{}", a);
-    todo!();
+    a.push(0);
+    a.sort_by(|a, b| b.cmp(a));
+    let mut ans = 0;
+    let mut count = 1;
+    for i in 1..n + 1 {
+        let until_next = a[i - 1] - a[i];
+        if k <= until_next * count {
+            let quo = k / count;
+            let rem = k % count;
+            let all = a[i - 1] - quo + 1;
+            ans += (all + a[i - 1]) * quo * count / 2;
+            ans += rem * (all - 1);
+            break;
+        }
+        k -= until_next * count;
+        ans += (a[i - 1] + a[i] + 1) * until_next * count / 2;
+        count += 1;
+    }
+    println!("{}", ans);
 }
