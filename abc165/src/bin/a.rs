ff6f86c226e04219f16ef6210180631e47139446
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-18 21:31:35 +0900

    add abc165_a

--- abc165/src/bin/a.rs
+++ abc165/src/bin/a.rs
@@ -2,8 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       k:i32,a:i32,b:i32,
     }
-    println!("{}", a);
-    todo!();
+    for i in a..=b {
+        if i % k == 0 {
+            println!("OK");
+            return;
+        }
+    }
+    println!("NG");
 }
