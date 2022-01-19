3e4548cfa0129a10bd5536203aaf1196a42b211a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-20 02:31:50 +0900

    add abc202_c

--- abc202/src/bin/c.rs
+++ abc202/src/bin/c.rs
@@ -1,8 +1,29 @@
+use std::vec;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       a:[usize;n],
+       b:[usize;n],
+       c:[usize;n],
     }
-    println!("{}", a);
+    let mut a_count = vec![0i64; 100001];
+    for a in a {
+        a_count[a] += 1;
+    }
+    let mut c_count = vec![0; 100001];
+    for c in c {
+        c_count[c] += 1;
+    }
+    let mut b_count = vec![0; 100001];
+    for i in 0..n {
+        b_count[b[i]] += c_count[i + 1];
+    }
+    let mut ans = 0;
+    for i in 0..100001 {
+        ans += a_count[i] * b_count[i];
+    }
+    println!("{}", ans);
 }
