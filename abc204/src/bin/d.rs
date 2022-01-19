70c30bcddc4b94b8da6b212fba99a1bc038dcf6b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-19 23:06:43 +0900

    add abc204_d

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -1,8 +1,28 @@
 use proconio::input;
 
+const MAX: usize = 100001;
+
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       t:[usize;n]
     }
-    println!("{}", a);
+
+    let mut dp = vec![false; MAX];
+    dp[0] = true;
+    // dp[i] = iが組み合わせとして存在するかどうか
+    let sum = t.clone().iter().sum::<usize>();
+    for time in t {
+        for i in (0..MAX - 1).rev() {
+            if dp[i] {
+                dp[i + time] = true;
+            }
+        }
+    }
+    for i in (sum + 1) / 2..MAX - 1 {
+        if dp[i] {
+            println!("{}", i);
+            return;
+        }
+    }
 }
