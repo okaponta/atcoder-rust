d9a480624a10078eacb4ad4352558f39a7f37f7c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-19 13:47:59 +0900

    add abc204_c

--- abc204/src/bin/c.rs
+++ abc204/src/bin/c.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use im_rc::HashSet;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       ab:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
+    let mut path = vec![vec![]; n];
+    for (a, b) in ab {
+        path[a].push(b);
+    }
+    let mut ans: usize = 0;
+    let mut q = VecDeque::new();
+    for i in 0..n {
+        let mut set = HashSet::new();
+        q.push_back(i);
+        while let Some(next) = q.pop_front() {
+            if set.contains(&next) {
+                continue;
+            }
+            ans += 1;
+            set.insert(next);
+            for &j in &path[next] {
+                if !set.contains(&j) {
+                    q.push_back(j);
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
