72cd8348eca6c48ede16c7c51eca5712c509f495
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-20 23:24:29 +0900

    add abc198_e

--- abc198/src/bin/e.rs
+++ abc198/src/bin/e.rs
@@ -1,8 +1,43 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       c:[usize;n],
+       ab:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
+    let mut edge = vec![vec![]; n];
+    for (a, b) in ab {
+        edge[a].push(b);
+        edge[b].push(a);
+    }
+    let mut color = vec![0; 100001];
+    let mut ans = vec![false; n];
+    dfs(&c, &edge, 0, 0, &mut color, &mut ans);
+    for i in 0..n {
+        if ans[i] {
+            println!("{}", i + 1);
+        }
+    }
+}
+
+fn dfs(
+    c: &Vec<usize>,
+    edge: &Vec<Vec<usize>>,
+    cur: usize,
+    prev: usize,
+    count: &mut Vec<usize>,
+    ans: &mut Vec<bool>,
+) {
+    count[c[cur]] += 1;
+    if count[c[cur]] == 1 {
+        ans[cur] = true;
+    }
+    for &v in &edge[cur] {
+        if v == prev {
+            continue;
+        }
+        dfs(c, edge, v, cur, count, ans);
+    }
+    count[c[cur]] -= 1;
 }
