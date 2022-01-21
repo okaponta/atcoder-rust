325fbba4c495707928fac1ca62f16ade00fd53e7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-21 13:43:09 +0900

    add abc197_c

--- abc197/src/bin/c.rs
+++ abc197/src/bin/c.rs
@@ -2,7 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       a:[usize;n],
     }
-    println!("{}", a);
+    let mut ans = 1 << 30;
+    // 2^nのbit全探索
+    for i in 0..1 << n {
+        let mut xor = 0;
+        let mut or = 0;
+        for j in 0..n {
+            or = or | a[j];
+            if i & (1 << j) > 0 {
+                // フラグが立ってたらXOR
+                xor = xor ^ or;
+                or = 0;
+            }
+        }
+        xor = xor ^ or;
+        ans = ans.min(xor);
+    }
+    println!("{}", ans);
 }
