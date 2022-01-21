b5ac82541d89e2163b07fbc6b9ad58d707e508ac
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-21 22:44:46 +0900

    add abc197_d

--- abc197/src/bin/d.rs
+++ abc197/src/bin/d.rs
@@ -1,8 +1,18 @@
 use proconio::input;
+use std::f64::consts::PI;
 
 fn main() {
     input! {
-       a:i32,
+       n:f64,
+       p:(f64,f64),
+       pp:(f64,f64),
     }
-    println!("{}", a);
+    let mid = ((p.0 + pp.0) / 2.0, (p.1 + pp.1) / 2.0);
+    let v = (p.0 - mid.0, p.1 - mid.1);
+    let degree = 2.0 * PI / n;
+    let rot = (
+        degree.cos() * v.0 - degree.sin() * v.1,
+        degree.sin() * v.0 + degree.cos() * v.1,
+    );
+    println!("{} {}", mid.0 + rot.0, mid.1 + rot.1);
 }
