9d35771345f78f4f7d37c3ac73484c8c8020c545
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-23 03:13:35 +0900

    add abc194_e

--- abc194/src/bin/e.rs
+++ abc194/src/bin/e.rs
@@ -1,8 +1,25 @@
+use std::vec;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       a:[usize;n],
     }
-    println!("{}", a);
+    let mut map = vec![vec![-1]; n];
+    for i in 0..n {
+        map[a[i]].push(i as i32);
+    }
+    for i in 0..n {
+        let target = &mut map[i];
+        target.push(n as i32);
+        for j in 0..target.len() - 1 {
+            if target[j + 1] - target[j] > m as i32 {
+                println!("{}", i);
+                return;
+            }
+        }
+    }
+    println!("{}", n);
 }
