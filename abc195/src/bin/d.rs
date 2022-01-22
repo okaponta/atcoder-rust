20797da850ac165b7d7b73d8bacfdbcb84606fe8
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-22 22:14:16 +0900

    add abc195_d

--- abc195/src/bin/d.rs
+++ abc195/src/bin/d.rs
@@ -1,8 +1,35 @@
-use proconio::input;
+use std::cmp::Reverse;
+
+use proconio::{input, marker::Usize1};
+use superslice::Ext;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,q:usize,
+       mut wv:[(usize,usize);n],
+       x:[usize;m],
+       lr:[(Usize1,Usize1);q],
     }
-    println!("{}", a);
+    wv.sort_by_key(|k| Reverse(k.1));
+    for (l, r) in lr {
+        let mut target = vec![];
+        for i in 0..m {
+            if l <= i && i <= r {
+                continue;
+            }
+            target.push(x[i]);
+        }
+        target.sort();
+        let mut ans = 0;
+        for (w, v) in wv.clone() {
+            let i = target.lower_bound(&w);
+            if i == target.len() {
+                // 対象外
+                continue;
+            }
+            ans += v;
+            target.remove(i);
+        }
+        println!("{}", ans);
+    }
 }
