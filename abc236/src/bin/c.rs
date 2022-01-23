9c1da051451b5e97ba239607bae06abb577bffe4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-23 23:12:55 +0900

    add abc236_c

--- abc236/src/bin/c.rs
+++ abc236/src/bin/c.rs
@@ -1,9 +1,22 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       s:[String;n],
+       t:[String;m],
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+    for ti in t {
+        set.insert(ti);
+    }
+    for si in s {
+        if set.contains(&si) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
