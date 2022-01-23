915a1d15fd8e12c9198fea6b3d4862776c2d0d92
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-23 11:59:40 +0900

    add abc194_c

--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -1,8 +1,23 @@
+use im_rc::HashMap;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,
+        a:[i64;n],
     }
-    println!("{}", a);
+    let mut map = HashMap::new();
+    for ai in a {
+        *map.entry(ai).or_insert(0) += 1;
+    }
+    let mut ans = 0;
+    for ai in &map {
+        for aj in &map {
+            if ai.0 <= aj.0 {
+                continue;
+            }
+            ans += (ai.0 - aj.0) * (ai.0 - aj.0) * ai.1 * aj.1;
+        }
+    }
+    println!("{}", ans);
 }
