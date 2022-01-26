e44d7bd3f6a2ce1dc924794673491e55b02d580f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-26 14:11:04 +0900

    add abc191_c

--- abc191/src/bin/c.rs
+++ abc191/src/bin/c.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+        h:usize, w:usize,
+        s:[Chars;h],
     }
-    println!("{}", a);
+    let mut ans = 0;
+    for i in 1..h {
+        for j in 1..w {
+            let mut count = 0;
+            if s[i][j] == '#' {
+                count += 1;
+            }
+            if s[i - 1][j] == '#' {
+                count += 1;
+            }
+            if s[i][j - 1] == '#' {
+                count += 1;
+            }
+            if s[i - 1][j - 1] == '#' {
+                count += 1;
+            }
+            if count == 1 || count == 3 {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
