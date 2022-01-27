d1a4b5891815e08680166107ec1fb5704c9f7192
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-28 01:01:33 +0900

    add abc190_d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -1,3 +1,31 @@
+use num_integer::Roots;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        target:i64,
+    }
+    println!(
+        "{}",
+        divisor(2 * target)
+            .iter()
+            .filter(|&ni| ((2 * target / ni) + 1 - ni) % 2 == 0)
+            .count()
+    );
+}
+
+fn divisor(n: i64) -> Vec<i64> {
+    let mut res = vec![];
+    let mut upper = vec![];
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            res.push(i);
+            if i != n / i {
+                upper.push(n / i);
+            }
+        }
+    }
+    upper.reverse();
+    res.append(&mut upper);
+    res
 }
