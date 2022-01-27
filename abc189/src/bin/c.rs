e73529bbbe4a837b50fc520b6e5ad9998c563493
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-28 02:19:35 +0900

    add abc189_c

--- abc189/src/bin/c.rs
+++ abc189/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n:usize,
+        a:[usize;n],
+    }
+    let mut ans = 0;
+    for l in 0..n {
+        let mut x = a[l];
+        for r in l..n {
+            x = x.min(a[r]);
+            ans = ans.max(x * (r - l + 1));
+        }
+    }
+    println!("{}", ans);
 }
