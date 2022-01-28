6299d6c05cfb34c3149697bc3e311e9517e812a7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-28 21:53:39 +0900

    add abc189_d

--- abc189/src/bin/d.rs
+++ abc189/src/bin/d.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n:usize,
+        s:[String;n],
+    }
+    let mut ans = vec![1i64; 2];
+    for si in s {
+        if si == "AND" {
+            ans[1] += ans[0] + ans[1];
+        } else {
+            ans[0] += ans[0] + ans[1];
+        }
+    }
+    println!("{}", ans[0]);
 }
