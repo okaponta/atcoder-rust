777fac217f1e2d409d64864915c9576d32df3e1a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-29 14:14:14 +0900

    add abc188_c

--- abc188/src/bin/c.rs
+++ abc188/src/bin/c.rs
@@ -1,9 +1,32 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       mut n:usize,
+       a:[i32;1<<n],
     }
-    println!("{}", a);
-    todo!();
+    let mut winner = (0..1 << n).collect_vec();
+    while n != 1 {
+        n -= 1;
+        let mut next = vec![0; 1 << n];
+        for i in 0..1 << n {
+            let pl1 = winner[i * 2];
+            let pl2 = winner[i * 2 + 1];
+            if a[pl1] > a[pl2] {
+                next[i] = pl1;
+            } else {
+                next[i] = pl2;
+            }
+        }
+        winner = next;
+    }
+    println!(
+        "{}",
+        if a[winner[0]] > a[winner[1]] {
+            winner[1] + 1
+        } else {
+            winner[0] + 1
+        }
+    );
 }
