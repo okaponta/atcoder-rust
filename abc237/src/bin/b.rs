f263947610bece5ed628363f67455e29d72650d2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-30 23:20:22 +0900

    add abc237_b

--- abc237/src/bin/b.rs
+++ abc237/src/bin/b.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        h:usize,w:usize,
+        a:[[usize;w];h],
     }
-    println!("{}", a);
-    todo!();
+    let mut b = vec![vec![0; h]; w];
+    for i in 0..h {
+        for j in 0..w {
+            b[j][i] = a[i][j];
+        }
+    }
+    for i in 0..w {
+        for j in 0..h {
+            print!("{} ", b[i][j]);
+        }
+        println!();
+    }
 }
