2e560fcf88d1930748f64a40c0758962f8bff231
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-30 23:43:13 +0900

    add abc237_f

--- abc237/src/bin/f.rs
+++ abc237/src/bin/f.rs
@@ -7,18 +7,18 @@ fn main() {
     input! {
         n:usize,m:usize,
     }
-    let mut dp = vec![vec![vec![0; m + 2]; m + 2]; m + 2];
-    dp[m + 1][m + 1][m + 1] = 1;
+    let mut dp = vec![vec![vec![0; m + 1]; m + 1]; m + 1];
+    dp[0][0][0] = 1;
     for _ in 0..n {
-        let mut next = vec![vec![vec![0; m + 2]; m + 2]; m + 2];
-        for (a, b, c) in iproduct!(1..=m + 1, 1..=m + 1, 1..=m + 1) {
+        let mut next = vec![vec![vec![0; m + 1]; m + 1]; m + 1];
+        for (a, b, c) in iproduct!(0..=m, 0..=m, 0..=m) {
             let d = dp[a][b][c];
             for x in 1..=m {
-                if x <= a {
+                if x <= a || a == 0 {
                     next[x][b][c] = (next[x][b][c] + d) % MOD;
-                } else if x <= b {
+                } else if x <= b || b == 0 {
                     next[a][x][c] = (next[a][x][c] + d) % MOD;
-                } else if x <= c {
+                } else if x <= c || c == 0 {
                     next[a][b][x] = (next[a][b][x] + d) % MOD;
                 }
             }
