9e45e2253cc880444f70711e970180cad7fe610e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-30 23:20:25 +0900

    add abc237_d

--- abc237/src/bin/d.rs
+++ abc237/src/bin/d.rs
@@ -1,9 +1,21 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut q = VecDeque::new();
+    q.push_back(n);
+    for i in (0..n).rev() {
+        if s[i] == 'L' {
+            q.push_back(i);
+        } else {
+            q.push_front(i);
+        }
+    }
+    println!("{}", q.iter().join(" "));
 }
