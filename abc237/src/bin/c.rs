6dbb9e25b15c8fbaa9c7876abc6bed8df717e120
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-30 23:20:23 +0900

    add abc237_c

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -1,9 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        mut s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut start = 0;
+    let mut end = s.len();
+    while s[start] == 'a' {
+        start += 1;
+        if start == s.len() {
+            println!("Yes");
+            return;
+        }
+    }
+    while s[end - 1] == 'a' {
+        end -= 1;
+    }
+    let target = &s[start..end];
+    for i in 0..target.len() {
+        if target[i] != target[target.len() - 1 - i] {
+            println!("No");
+            return;
+        }
+    }
+    if start > s.len() - end {
+        println!("No");
+        return;
+    }
+    println!("Yes");
 }
