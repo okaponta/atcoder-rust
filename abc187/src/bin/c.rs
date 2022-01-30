7a5e144d6373f97ea2ee02ca1e408a063af04ae0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-30 16:13:37 +0900

    add abc187_c

--- abc187/src/bin/c.rs
+++ abc187/src/bin/c.rs
@@ -1,9 +1,26 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       s:[String;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+    let mut ex_set = HashSet::new();
+    for si in s {
+        if si.starts_with("!") {
+            ex_set.insert(si[1..].to_string());
+        } else {
+            set.insert(si);
+        }
+    }
+    for str in set {
+        if ex_set.contains(&str) {
+            println!("{}", str);
+            return;
+        }
+    }
+    println!("satisfiable");
 }
