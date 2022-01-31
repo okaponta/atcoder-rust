32d0dd6c5fb63f53163507272e91e694d6a76965
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-31 13:55:24 +0900

    add abc187_d

--- abc187/src/bin/d.rs
+++ abc187/src/bin/d.rs
@@ -1,9 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       mut ab:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut ao = 0;
+    for (a, _) in &ab {
+        ao += a;
+    }
+    let mut sabun = ab.iter().map(|(a, b)| 2 * a + b).collect_vec();
+    sabun.sort();
+    sabun.reverse();
+    let mut tk = 0;
+    let mut count = 0;
+    for e in sabun {
+        count += 1;
+        tk += e;
+        if tk > ao {
+            println!("{}", count);
+            return;
+        }
+    }
 }
