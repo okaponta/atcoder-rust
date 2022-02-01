d8ffdedd2fce22ad06a3ee07162ccf24e5017997
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-01 12:55:39 +0900

    add abc186_c

--- abc186/src/bin/c.rs
+++ abc186/src/bin/c.rs
@@ -1,9 +1,21 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+    for i in 1..=n {
+        if i.to_string().contains('7') {
+            set.insert(i);
+            continue;
+        }
+        let a = format!("{:o}", i);
+        if a.contains('7') {
+            set.insert(i);
+        }
+    }
+    println!("{}", n - set.len());
 }
