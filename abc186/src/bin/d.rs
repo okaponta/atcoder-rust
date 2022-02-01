7f5f5179d8fb076bfebdd4ae38c823a25ea66faa
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-01 21:41:55 +0900

    add abc186_d

--- abc186/src/bin/d.rs
+++ abc186/src/bin/d.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,
+        mut a:[i64;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut rem = a.iter().sum::<i64>();
+    let mut rem_num = n as i64;
+    a.sort();
+    let mut ans = 0;
+    for ai in a {
+        rem_num -= 1;
+        rem -= ai;
+        ans += rem - ai * rem_num;
+    }
+    println!("{}", ans);
 }
