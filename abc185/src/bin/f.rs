b4996d8f291e160dfba57bbc74518e4e276a4919
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-03 01:30:20 +0900

    add abc185_f

--- abc185/src/bin/f.rs
+++ abc185/src/bin/f.rs
@@ -1,9 +1,80 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,q:usize,
+        a:[usize;n],
+        txy:[(usize,Usize1,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut seg = SegmentTree::new(n, 0, |a, b| a ^ b);
+    for i in 0..n {
+        seg.update(i, a[i]);
+    }
+    for (t, x, y) in txy {
+        if t == 1 {
+            let cur = seg.query(x..x + 1);
+            seg.update(x, cur ^ y);
+        } else {
+            let ans = seg.query(x..y);
+            println!("{}", ans);
+        }
+    }
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let mut m = 1;
+        while m <= size {
+            m <<= 1;
+        }
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, k: usize, value: T) {
+        let mut k = k;
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    /// Get the minimum value in the array in the range
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
 }
