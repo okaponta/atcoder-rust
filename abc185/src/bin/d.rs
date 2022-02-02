3a9750eaadb6c2a7cba59dbd48dc903227515f4d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-03 00:04:24 +0900

    add abc185_d

--- abc185/src/bin/d.rs
+++ abc185/src/bin/d.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,m:usize,
+        mut a:[usize;m],
     }
-    println!("{}", a);
-    todo!();
+    a.push(0);
+    a.push(n + 1);
+    a.sort();
+    a.dedup();
+    let white = a
+        .windows(2)
+        .map(|e| e[1] - e[0] - 1)
+        .filter(|e| e > &0)
+        .collect::<Vec<_>>();
+    if white.len() == 0 {
+        println!("{}", 0);
+        return;
+    }
+    let k = *white.iter().min().unwrap();
+    let mut ans = 0;
+    for wi in white {
+        ans += (wi + k - 1) / k;
+    }
+    println!("{}", ans);
 }
