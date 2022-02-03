dbf5143d9fab6379b566c8eb379a814ea3775ec7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-04 02:20:33 +0900

    add abc188_e

--- abc188/src/bin/e.rs
+++ abc188/src/bin/e.rs
@@ -1,9 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
+
+const INF: i64 = 1 << 60;
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,m:usize,
+        a:[i64;n],
+        xy:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut dp = vec![INF; n];
+    let mut path = vec![vec![]; n];
+    for (x, y) in xy {
+        path[x].push(y);
+    }
+    for i in 0..n {
+        for &j in &path[i] {
+            dp[j] = dp[j].min(dp[i]).min(a[i]);
+        }
+    }
+    println!(
+        "{}",
+        (0..n).into_iter().map(|e| a[e] - dp[e]).max().unwrap()
+    );
 }
