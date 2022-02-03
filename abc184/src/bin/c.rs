1bada12b723ea2cc35b3ff99eb41d78a4cd0d9c4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-03 13:49:18 +0900

    add abc184_c

--- abc184/src/bin/c.rs
+++ abc184/src/bin/c.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        rc:[(i64,i64);2],
     }
-    println!("{}", a);
-    todo!();
+    let x = (rc[0].0 - rc[1].0).abs();
+    let y = (rc[0].1 - rc[1].1).abs();
+    println!(
+        "{}",
+        if x == 0 && y == 0 {
+            0
+        } else if x == y || x + y <= 3 {
+            1
+        } else if (x + y) % 2 == 0 || (x - y).abs() <= 3 || x + y <= 6 {
+            2
+        } else {
+            3
+        }
+    );
 }
