805f16f7215ec30a6eb89e4eca9f4bb2619b4128
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-04 22:16:49 +0900

    add abc185_e

--- abc185/src/bin/e.rs
+++ abc185/src/bin/e.rs
@@ -1,9 +1,26 @@
+use itertools::iproduct;
 use proconio::input;
+
+const INF: usize = 1 >> 60;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,
+       a:[usize;n],
+       b:[usize;m],
     }
-    println!("{}", a);
-    todo!();
+    let mut dp = vec![vec![INF; m + 1]; n + 1];
+    for i in 0..=n {
+        dp[i][0] = i
+    }
+    for j in 0..=m {
+        dp[0][j] = j
+    }
+    for (i, j) in iproduct!(1..=n, 1..=m) {
+        let x = dp[i - 1][j] + 1;
+        let y = dp[i][j - 1] + 1;
+        let z = dp[i - 1][j - 1] + if a[i - 1] == b[j - 1] { 0 } else { 1 };
+        dp[i][j] = x.min(y).min(z);
+    }
+    println!("{}", dp[n][m]);
 }
