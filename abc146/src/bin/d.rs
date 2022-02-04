93aed5107dd57897bba240e57739c0fa74145c4f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-04 23:27:23 +0900

    add abc146_d

--- abc146/src/bin/d.rs
+++ abc146/src/bin/d.rs
@@ -1,9 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        ab:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut edge = vec![vec![]; n];
+    for i in 0..n - 1 {
+        edge[ab[i].0].push((ab[i].1, i));
+    }
+    let mut ans = vec![0; n - 1];
+    dfs(0, 0, &edge, &mut ans);
+    println!("{}", ans.iter().max().unwrap());
+    ans.iter().for_each(|x| println!("{}", x));
+}
+
+fn dfs(cur: usize, used: usize, edge: &Vec<Vec<(usize, usize)>>, ans: &mut Vec<usize>) {
+    let mut c = 1;
+    for &(next, i) in &edge[cur] {
+        if c == used {
+            c += 1;
+        }
+        ans[i] = c;
+        dfs(next, c, edge, ans);
+        c += 1;
+    }
 }
