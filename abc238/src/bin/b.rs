1e8ddd92d96b4261235cae7d47db7f1319d12126
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-05 22:54:29 +0900

    add abc238_b

--- abc238/src/bin/b.rs
+++ abc238/src/bin/b.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        a:[i32;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut cut = vec![false; 360];
+    cut[0] = true;
+    let mut deg = 0;
+    for i in 0..n {
+        deg = (deg + a[i]) % 360;
+        cut[deg as usize] = true;
+    }
+    let mut ans = 0;
+    let mut tmp = 0;
+    for c in cut {
+        tmp += 1;
+        if c {
+            ans = ans.max(tmp);
+            tmp = 0;
+        }
+    }
+    tmp += 1;
+    ans = ans.max(tmp);
+    println!("{}", ans);
 }
