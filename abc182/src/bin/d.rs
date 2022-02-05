32b573050cb4825a47f5cd70317cfdc006f21135
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-05 13:21:13 +0900

    add abc182_d

--- abc182/src/bin/d.rs
+++ abc182/src/bin/d.rs
@@ -1,9 +1,24 @@
+use std::vec;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       a:[i64;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut sum = vec![0; n];
+    let mut max = vec![0; n];
+    let mut ini = vec![0; n];
+    let mut ans = vec![0; n];
+    sum[0] = a[0];
+    max[0] = a[0];
+    ans[0] = a[0];
+    for i in 1..n {
+        ini[i] = ini[i - 1] + sum[i - 1];
+        sum[i] = sum[i - 1] + a[i];
+        max[i] = max[i - 1].max(sum[i]);
+        ans[i] = ini[i] + max[i];
+    }
+    println!("{}", ans.iter().max().unwrap().max(&0));
 }
