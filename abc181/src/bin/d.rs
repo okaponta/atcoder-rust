385ce7ad77679e07df74eb1e067a4934d7360a44
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-07 19:41:23 +0900

    add abc181_d

--- abc181/src/bin/d.rs
+++ abc181/src/bin/d.rs
@@ -1,9 +1,57 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut count = vec![0; 10];
+    for c in &s {
+        count[c.to_digit(10).unwrap() as usize] += 1;
+    }
+    let mut two = vec![];
+    let mut tmp = 0;
+    while tmp < 100 {
+        tmp += 8;
+        let mut c = vec![0; 10];
+        for d in tmp.to_string().chars() {
+            c[d.to_digit(10).unwrap() as usize] += 1;
+        }
+        if c[0] > 0 {
+            continue;
+        }
+        two.push(c);
+    }
+    if s.len() < 3 {
+        if two.contains(&count) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+        return;
+    }
+    let mut three = vec![];
+    while tmp < 1000 {
+        tmp += 8;
+        let mut c = vec![0; 10];
+        for d in tmp.to_string().chars() {
+            c[d.to_digit(10).unwrap() as usize] += 1;
+        }
+        if c[0] > 0 {
+            continue;
+        }
+        three.push(c);
+    }
+    for e in three {
+        let mut flag = true;
+        for i in 1..10 {
+            if e[i] > count[i] {
+                flag = false;
+            }
+        }
+        if flag {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
