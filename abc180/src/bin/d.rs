77a35a970313d00de81fdefca2fb3bebeefbe095
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-08 23:26:22 +0900

    add abc180_d

--- abc180/src/bin/d.rs
+++ abc180/src/bin/d.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       mut x:u128,y:u128,a:u128,b:u128,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 0;
+    while a * x < b + x {
+        x *= a;
+        ans += 1;
+        if y <= x {
+            println!("{}", ans - 1);
+            return;
+        }
+    }
+    let p = (y - x + b - 1) / b;
+    println!("{}", ans + p - 1);
 }
