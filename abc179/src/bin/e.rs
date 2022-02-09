2e6628fb70a26e2ae4f246c63767c388205a37d5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-09 21:36:37 +0900

    add abc179_e

--- abc179/src/bin/e.rs
+++ abc179/src/bin/e.rs
@@ -1,9 +1,42 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,x:usize,m:usize
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+
+    let mut path = vec![x];
+    let mut sum = vec![x];
+
+    let mut prev = x;
+    set.insert(prev);
+    let mut next = x * x % m;
+
+    while !set.contains(&next) {
+        set.insert(next);
+        path.push(next);
+        sum.push(sum.last().unwrap() + next);
+        prev = next;
+        next = (prev * prev) % m;
+    }
+
+    let mut begin = 0;
+    for i in 0..path.len() {
+        if path[i] == next {
+            begin = i;
+        }
+    }
+
+    let target = n - 1;
+    let loop_size = path.len() - begin;
+    let mut loop_sum = *sum.last().unwrap();
+    if begin > 0 {
+        loop_sum -= sum[begin - 1];
+    }
+
+    let ans = ((target - begin) / loop_size) * loop_sum + sum[begin + (target - begin) % loop_size];
+    println!("{}", ans);
 }
