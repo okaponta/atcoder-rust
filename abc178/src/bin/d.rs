194b70d2b3ee15d00e1a944b32c43b1529f29f64
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-10 20:06:17 +0900

    add abc178_d

--- abc178/src/bin/d.rs
+++ abc178/src/bin/d.rs
@@ -1,9 +1,54 @@
 use proconio::input;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
-       a:i32,
+       mut s:usize,
     }
-    println!("{}", a);
-    todo!();
+    let comb = Binom::init(s, MOD);
+    let mut ans = 0;
+    let mut count = 0;
+    while s > 2 {
+        s -= 3;
+        ans += comb.get(s + count, count);
+        ans %= MOD;
+        count += 1;
+    }
+    println!("{}", ans);
+}
+
+struct Binom {
+    fact: Vec<usize>,
+    fact_inv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Binom {
+    pub fn init(n: usize, modulo: usize) -> Self {
+        let mut fact = vec![0; n + 1];
+        let mut inv = vec![0; n + 1];
+        let mut fact_inv = vec![0; n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        inv[1] = 1;
+        fact_inv[0] = 1;
+        fact_inv[1] = 1;
+        for i in 2..(n + 1) {
+            fact[i] = fact[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            fact_inv[i] = fact_inv[i - 1] * inv[i] % modulo;
+        }
+        Self {
+            fact,
+            fact_inv,
+            modulo,
+        }
+    }
+
+    // nCk
+    pub fn get(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fact[n] * self.fact_inv[k] % self.modulo * self.fact_inv[n - k] % self.modulo
+    }
 }
