fa8a12b5dd72c3082eadcd65d0fc87cf2623c624
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-11 09:52:31 +0900

    add abc178_e

--- abc178/src/bin/e.rs
+++ abc178/src/bin/e.rs
@@ -1,9 +1,21 @@
 use proconio::input;
 
+const MAX: usize = 1_000_000_001;
+
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       xy:[(usize,usize);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut min = MAX;
+    let mut max = 0;
+    let mut rev_min = MAX * 2;
+    let mut rev_max = 0;
+    for (x, y) in xy {
+        min = min.min(x + y);
+        max = max.max(x + y);
+        rev_min = rev_min.min(x + MAX - y);
+        rev_max = rev_max.max(x + MAX - y);
+    }
+    println!("{}", (max - min).max(rev_max - rev_min));
 }
