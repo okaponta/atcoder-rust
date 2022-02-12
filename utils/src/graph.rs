9487250c87026fcee1cebc21dc1cec85432c003b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-12 22:40:59 +0900

    add bellman-ford

--- utils/src/graph.rs
+++ utils/src/graph.rs
@@ -2,6 +2,7 @@ use std::{cmp::Reverse, collections::BinaryHeap};
 
 const INF: usize = 1 << 60;
 
+// 計算量は(E+V)logV
 struct Dijkstra {
     distance: Vec<usize>,
     parent: Vec<usize>,
@@ -54,6 +55,46 @@ impl Dijkstra {
     }
 }
 
+// 計算量はE×V
+struct BellmanFord {
+    distance: Vec<i64>,
+    has_neg_loop: bool,
+}
+
+impl BellmanFord {
+    // n:usize 頂点の数
+    // edges: Vec<(usize,usize,i64)> edges[i] = [(0,2,3), (1,3,-1), (From,To,重み)]
+    // init:usize どの頂点を起点に考えるか
+    pub fn new(n: usize, edges: Vec<(usize, usize, i64)>, init: usize) -> Self {
+        let mut distance = vec![1 << 60; n];
+        distance[init] = 0;
+        let mut has_neg_loop = false;
+
+        for i in 0..n {
+            for edge in &edges {
+                let from = edge.0;
+                let to = edge.1;
+                let cost = edge.2;
+                if distance[to] > distance[from] + cost {
+                    distance[to] = distance[from] + cost;
+                    if i == n - 1 {
+                        has_neg_loop = true;
+                        break;
+                    }
+                }
+            }
+        }
+        Self {
+            distance,
+            has_neg_loop,
+        }
+    }
+
+    pub fn distance(&self, target: usize) -> i64 {
+        self.distance[target]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -85,5 +126,28 @@ mod tests {
         assert_eq!(d.get_path(3), vec![0, 4, 5, 3]);
         assert_eq!(d.get_path(4), vec![0, 4]);
         assert_eq!(d.get_path(5), vec![0, 4, 5]);
+    }
+
+    #[test]
+    fn test_bellmanford() {
+        let n = 6;
+        let abc = vec![
+            (0, 1, 5),
+            (0, 3, 9),
+            (0, 4, 2),
+            (1, 2, 2),
+            (2, 3, 3),
+            (3, 5, 2),
+            (4, 5, 3),
+        ];
+        let mut edges = vec![];
+        for (a, b, c) in abc {
+            edges.push((a, b, c));
+            edges.push((b, a, c));
+        }
+        let b = BellmanFord::new(n, edges, 0);
+
+        assert_eq!(b.distance, vec![0, 5, 7, 7, 2, 5]);
+        assert_eq!(b.has_neg_loop, false);
     }
 }
