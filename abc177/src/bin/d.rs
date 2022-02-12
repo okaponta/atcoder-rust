6a119490df0c29bd3a9317910808e3c4b2e3b126
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-12 23:02:53 +0900

    add abc177_d

--- abc177/src/bin/d.rs
+++ abc177/src/bin/d.rs
@@ -1,9 +1,18 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+        n:usize,m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut uf = UnionFind::new(n);
+    for (a, b) in ab {
+        uf.union(a, b);
+    }
+    let mut count = vec![0; n];
+    for i in 0..n {
+        count[uf.find(i)] += 1;
+    }
+    println!("{}", count.iter().max().unwrap());
 }
