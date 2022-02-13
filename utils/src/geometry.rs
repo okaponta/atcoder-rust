3d69a9eef2aa1780936741e03e4fe5442e688003
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-13 14:21:00 +0900

    update tips

--- utils/src/geometry.rs
+++ utils/src/geometry.rs
@@ -42,3 +42,66 @@ fn is_inside(x0: f64, y0: f64, r: f64, xy: &Vec<(i32, i32)>) -> bool {
     !xy.iter()
         .any(|&(x, y)| (x as f64 - x0).hypot(y as f64 - y0) > r)
 }
+
+// 最近点対問題(最も近い2点を分割統治法で求める)
+fn closest(mut points: Vec<(i64, i64)>) -> f64 {
+    points.sort_by_key(|p| (p.0));
+    let square = closest_pair(&points, 0, points.len()).0;
+    (square as f64).sqrt()
+}
+
+// 最近点対問題の補助関数。再起的に呼び出す。
+// 誤差の考慮をなくすために戻り値は距離の二乗
+fn closest_pair(points: &Vec<(i64, i64)>, i: usize, n: usize) -> (i64, Vec<(i64, i64)>) {
+    if n <= 1 {
+        return (1 << 60, vec![points[i]]);
+    }
+    let m = n / 2;
+    let (d1, qs1) = closest_pair(points, i, m);
+    let (d2, qs2) = closest_pair(points, i + m, n - m);
+    let mut d = d1.min(d2);
+
+    let x = points[i + m].0;
+    let qs = merge(qs1, qs2);
+    let mut b: Vec<(i64, i64)> = vec![];
+    for i in 0..n {
+        if (qs[i].0 - x).abs() * (qs[i].0 - x).abs() >= d {
+            continue;
+        }
+        for j in (0..b.len()).rev() {
+            let dx = qs[i].0 - b[j].0;
+            let dy = qs[i].1 - b[j].1;
+            if dy * dy >= d {
+                break;
+            }
+            d = d.min(dx * dx + dy * dy);
+        }
+        b.push(qs[i]);
+    }
+    (d, qs)
+}
+
+// 自前でマージソート実装
+fn merge(left: Vec<(i64, i64)>, right: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    let mut qs = vec![];
+    let mut s = 0;
+    let mut t = 0;
+    while s < left.len() && t < right.len() {
+        if left[s].1 < right[t].1 {
+            qs.push(left[s]);
+            s += 1;
+        } else {
+            qs.push(right[t]);
+            t += 1;
+        }
+    }
+    while s < left.len() {
+        qs.push(left[s]);
+        s += 1;
+    }
+    while t < right.len() {
+        qs.push(right[t]);
+        t += 1;
+    }
+    qs
+}
