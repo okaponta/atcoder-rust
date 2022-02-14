3b9c7539b8b68eaa4598e5f1e38b34fac0802b2c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-15 02:29:47 +0900

    add abc179_d

--- abc179/src/bin/d.rs
+++ abc179/src/bin/d.rs
@@ -1,9 +1,24 @@
 use proconio::input;
 
+const MOD: i64 = 998244353;
+
 fn main() {
     input! {
-       a:i32,
+       n:usize,k:usize,
+       lr:[(usize,usize);k],
     }
-    println!("{}", a);
-    todo!();
+    // 境界値考慮が面倒なので2倍の長さを確保
+    let mut dp = vec![0; 2 * n + 1];
+    dp[0] = 1;
+    dp[1] = -1;
+    for i in 0..n {
+        for (l, r) in &lr {
+            dp[i + l] += dp[i];
+            dp[i + l] %= MOD;
+            dp[i + r + 1] = (dp[i + r + 1] + MOD - dp[i]) % MOD;
+        }
+        dp[i + 1] += dp[i];
+        dp[i + 1] %= MOD;
+    }
+    println!("{}", dp[n - 1]);
 }
