d053d6b7e26023c106d386918e11c68366776c39
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-14 13:44:37 +0900

    add abc174_e

--- abc174/src/bin/e.rs
+++ abc174/src/bin/e.rs
@@ -2,8 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,k:usize,
+       a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_med_over(&a, med, k) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn is_med_over(a: &[usize], med: usize, k: usize) -> bool {
+    // a/kを切り上げ
+    let times = a.iter().map(|&a| ((a + med - 1) / med) - 1).sum::<usize>();
+    times > k
 }
