b401b6180678f1636a5f3a8bc28d79f589685047
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-15 19:52:26 +0900

    add typical90_002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -2,8 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+       n:usize,
     }
-    println!("{}", a);
-    todo!();
+    if n % 2 != 0 {
+        println!();
+        return;
+    }
+    dfs(vec!['('; n], 0, 0, n / 2);
+}
+
+fn dfs(cur: Vec<char>, left: usize, right: usize, max: usize) {
+    if left > max || right > max || left < right {
+        return;
+    }
+    if left == max && right == max {
+        println!("{}", cur.iter().collect::<String>());
+        return;
+    }
+    let target = left + right;
+    let mut for_left = cur.clone();
+    for_left[target] = '(';
+    dfs(for_left, left + 1, right, max);
+    let mut for_right = cur.clone();
+    for_right[target] = ')';
+    dfs(for_right, left, right + 1, max);
 }
