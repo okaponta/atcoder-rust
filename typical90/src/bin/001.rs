6d4579d3aca584885dd72f1a17edd6d2d376de38
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-15 20:34:25 +0900

    add typical90_001

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -2,8 +2,42 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,l:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut len = vec![0; n + 1];
+    for i in 0..n {
+        if i == 0 {
+            len[0] = a[i];
+        } else {
+            len[i] = a[i] - a[i - 1];
+        }
+    }
+    len[n] = l - a[n - 1];
+
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(&len, med, k) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", lower);
+}
+
+fn is_ok(len: &[usize], med: usize, k: usize) -> bool {
+    let mut tmp = 0;
+    let mut count = 0;
+    for i in len {
+        tmp += i;
+        if tmp >= med {
+            tmp = 0;
+            count += 1;
+        }
+    }
+    count > k
 }
