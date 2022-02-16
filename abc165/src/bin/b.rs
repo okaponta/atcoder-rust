4f8e836f4b47814c48a8cc3a6fd074f01c1384c7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-17 02:49:49 +0900

    add abc165_b

--- abc165/src/bin/b.rs
+++ abc165/src/bin/b.rs
@@ -2,8 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       x:i128,
     }
-    println!("{}", a);
-    todo!();
+    let mut m = 100;
+    let mut count = 0;
+    while m < x {
+        m = m * 101 / 100;
+        count += 1;
+    }
+    println!("{}", count);
 }
