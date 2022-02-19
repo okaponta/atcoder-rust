3bdbb21979375dc280f0ce8424d205e694ebffd7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-19 19:24:29 +0900

    add abc228_e

--- abc228/src/bin/e.rs
+++ abc228/src/bin/e.rs
@@ -1,9 +1,24 @@
 use proconio::input;
 
+const MOD: i64 = 998244353;
+
 fn main() {
     input! {
-       a:i32,
+       n:i64, k:i64, m:i64,
     }
-    println!("{}", a);
-    todo!();
+    let r = pow(k, n, MOD - 1);
+    println!("{}", pow(m, r, MOD));
+}
+
+fn pow(mut x: i64, mut n: i64, modulo: i64) -> i64 {
+    x %= modulo;
+    let mut ret = if x == 0 { 0 } else { 1 };
+    while n > 0 {
+        if n & 1 == 1 {
+            ret = ret * x % modulo;
+        }
+        x = x * x % modulo;
+        n >>= 1;
+    }
+    ret
 }
