6a4c1764f969016142c09b877cee9b245e8725f9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-19 22:48:44 +0900

    add abc239_c

--- abc239/src/bin/c.rs
+++ abc239/src/bin/c.rs
@@ -1,9 +1,35 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        xy1:(i64,i64),
+        xy2:(i64,i64),
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+    set.insert(xy1);
+    let mut next = HashSet::new();
+    let mut ans = HashSet::new();
+    for xy in set {
+        next.insert((xy.0 + 1, xy.1 + 2));
+        next.insert((xy.0 + 2, xy.1 + 1));
+        next.insert((xy.0 - 1, xy.1 + 2));
+        next.insert((xy.0 - 2, xy.1 + 1));
+        next.insert((xy.0 + 1, xy.1 - 2));
+        next.insert((xy.0 + 2, xy.1 - 1));
+        next.insert((xy.0 - 1, xy.1 - 2));
+        next.insert((xy.0 - 2, xy.1 - 1));
+    }
+    for xy in next {
+        ans.insert((xy.0 + 1, xy.1 + 2));
+        ans.insert((xy.0 + 2, xy.1 + 1));
+        ans.insert((xy.0 - 1, xy.1 + 2));
+        ans.insert((xy.0 - 2, xy.1 + 1));
+        ans.insert((xy.0 + 1, xy.1 - 2));
+        ans.insert((xy.0 + 2, xy.1 - 1));
+        ans.insert((xy.0 - 1, xy.1 - 2));
+        ans.insert((xy.0 - 2, xy.1 - 1));
+    }
+    println!("{}", if ans.contains(&xy2) { "Yes" } else { "No" });
 }
