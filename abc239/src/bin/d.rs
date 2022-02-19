a236c97ac51c0b454efbc5c39a66eae27ff3e8d1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-19 22:48:45 +0900

    add abc239_d

--- abc239/src/bin/d.rs
+++ abc239/src/bin/d.rs
@@ -1,9 +1,52 @@
+use std::collections::HashSet;
+
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        a:usize,b:usize,c:usize,d:usize,
     }
-    println!("{}", a);
-    todo!();
+    let primes = prime_lists(200);
+    let mut set = HashSet::new();
+    for prime in primes {
+        set.insert(prime);
+    }
+    for i in a..=b {
+        let mut aoki_can_win = false;
+        for j in c..=d {
+            if set.contains(&(i + j)) {
+                aoki_can_win = true;
+            }
+        }
+        if !aoki_can_win {
+            println!("Takahashi");
+            return;
+        }
+    }
+    println!("Aoki");
+}
+
+fn prime_lists(n: usize) -> Vec<usize> {
+    judge_primes(n)
+        .iter()
+        .enumerate()
+        .filter(|(_, &prime)| prime)
+        .map(|(i, _)| i)
+        .collect()
+}
+
+fn judge_primes(n: usize) -> Vec<bool> {
+    let mut res = vec![true; n + 1];
+    res[0] = false;
+    res[1] = false;
+    for i in 2..=n.sqrt() {
+        if !res[i] {
+            continue;
+        }
+        for j in (i * i..=n).step_by(i) {
+            res[j] = false;
+        }
+    }
+    res
 }
