1e91a692af79addc6ccb1a232a778802dd1683db
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-20 23:12:15 +0900

    add abc240_e

--- abc240/src/bin/e.rs
+++ abc240/src/bin/e.rs
@@ -1,9 +1,44 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        uv:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut edge = vec![vec![]; n];
+    for (u, v) in uv {
+        edge[u].push(v);
+        edge[v].push(u);
+    }
+    let mut ans = vec![(0, 0); n];
+    dfs(&mut ans, 0, 0, &mut 1, &edge);
+    for (l, r) in ans {
+        println!("{} {}", l, r);
+    }
+}
+
+fn dfs(
+    ans: &mut Vec<(usize, usize)>,
+    cur: usize,
+    prev: usize,
+    child: &mut usize,
+    edges: &Vec<Vec<usize>>,
+) {
+    if cur != 0 && edges[cur].len() == 1 {
+        // 終点
+        ans[cur] = (*child, *child);
+        *child += 1;
+        return;
+    }
+    let mut left = 2 << 30;
+    let mut right = 0;
+    for &next in edges[cur].iter() {
+        if next == prev {
+            continue;
+        }
+        dfs(ans, next, cur, child, edges);
+        left = left.min(ans[next].0);
+        right = right.max(ans[next].1);
+    }
+    ans[cur] = (left, right);
 }
