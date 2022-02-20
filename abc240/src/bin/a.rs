2736e36ae0d299ef2c7079c735414989640a1d5b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-20 23:12:09 +0900

    add abc240_a

--- abc240/src/bin/a.rs
+++ abc240/src/bin/a.rs
@@ -2,8 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        a:i32,b:i32,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = false;
+    if (a - b).abs() == 1 {
+        ans = true;
+    }
+    if a == 10 && b == 1 || a == 1 && b == 10 {
+        ans = true;
+    }
+    println!("{}", if ans { "Yes" } else { "No" });
 }
