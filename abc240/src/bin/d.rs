33fd720fdbf5ac8d67e4b881411f06da1802fd5b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-20 23:12:13 +0900

    add abc240_d

--- abc240/src/bin/d.rs
+++ abc240/src/bin/d.rs
@@ -1,9 +1,30 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut count = 0;
+    for ai in a {
+        count += 1;
+        if !q.is_empty() && q.back().unwrap().0 == ai {
+            // 同じ時
+            let back = q.pop_back().unwrap();
+            if back.1 + 1 == ai {
+                //消えた時
+                count -= ai;
+            } else {
+                //消えなかった時
+                q.push_back((ai, back.1 + 1));
+            }
+        } else {
+            // 違う時
+            q.push_back((ai, 1));
+        }
+        println!("{}", count);
+    }
 }
