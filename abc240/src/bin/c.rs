c9c4934582f6fdb6c04e490e9ff624f34867db57
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-20 23:12:12 +0900

    add abc240_c

--- abc240/src/bin/c.rs
+++ abc240/src/bin/c.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,x:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut able = vec![false; x + 200];
+    able[0] = true;
+    for (a, b) in ab {
+        let mut next = vec![false; x + 200];
+        for i in 0..x + 1 {
+            if able[i] {
+                next[i + a] = true;
+                next[i + b] = true;
+            }
+        }
+        able = next;
+    }
+    println!("{}", if able[x] { "Yes" } else { "No" });
 }
