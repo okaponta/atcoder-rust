ce6bc538507745acf8f9ea917ae324b5aed34c7c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-22 23:42:18 +0900

    add abc155_a

--- abc155/src/bin/a.rs
+++ abc155/src/bin/a.rs
@@ -2,8 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        a:i32,b:i32,c:i32,
     }
-    println!("{}", a);
-    todo!();
+    if a == b && b == c {
+        println!("No");
+        return;
+    }
+    if a == b || b == c || c == a {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
