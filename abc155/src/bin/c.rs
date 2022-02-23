f2103eafee5953abd2de0fc948a41f46b94571fd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-23 18:43:04 +0900

    add abc155_c

--- abc155/src/bin/c.rs
+++ abc155/src/bin/c.rs
@@ -1,9 +1,24 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        s:[String;n]
     }
-    println!("{}", a);
-    todo!();
+    let mut map = HashMap::new();
+    for si in s {
+        *map.entry(si).or_insert(0) += 1;
+    }
+    let count = map.values().max().unwrap();
+    let mut ans = map
+        .iter()
+        .filter(|(_, v)| v == &count)
+        .map(|(k, _)| k)
+        .collect::<Vec<_>>();
+    ans.sort();
+    for s in ans {
+        println!("{}", s);
+    }
 }
