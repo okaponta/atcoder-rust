4efbcc5441bc9eb84035ad85be9635366a1adb94
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-26 22:47:53 +0900

    add abc241_c

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -1,9 +1,77 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        s:[Chars;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut grid = vec![vec![false; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == '#' {
+                grid[i][j] = true;
+            }
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..n - 5 {
+            let mut count = 0;
+            for k in 0..6 {
+                if grid[i][j + k] {
+                    count += 1;
+                }
+            }
+            if count > 3 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    for i in 0..n - 5 {
+        for j in 0..n {
+            let mut count = 0;
+            for k in 0..6 {
+                if grid[i + k][j] {
+                    count += 1;
+                }
+            }
+            if count > 3 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    for i in 0..n - 5 {
+        for j in 0..n - 5 {
+            let mut count = 0;
+            for k in 0..6 {
+                if grid[i + k][j + k] {
+                    count += 1;
+                }
+            }
+            if count > 3 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    for i in 0..n - 5 {
+        for j in 5..n {
+            let mut count = 0;
+            for k in 0..6 {
+                if grid[i + k][j - k] {
+                    count += 1;
+                }
+            }
+            if count > 3 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
