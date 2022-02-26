3b2f8be20262c93bcc1ff4b106615e5e1f434fac
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-27 00:31:55 +0900

    add abc241_d

--- abc241/src/bin/d.rs
+++ abc241/src/bin/d.rs
@@ -1,9 +1,29 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        qn:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut s = BTreeSet::new();
+    for i in 0..qn {
+        input! {query:i32}
+        match query {
+            1 => {
+                input! {x: i64}
+                s.insert((x, i));
+            }
+            2 => {
+                input! {x: i64 , k:usize}
+                let ans = s.range(..(x, qn)).rev().nth(k - 1).map_or(-1, |p| p.0);
+                println!("{}", ans);
+            }
+            _ => {
+                input! {x: i64 , k:usize}
+                let ans = s.range((x, 0)..).nth(k - 1).map_or(-1, |p| p.0);
+                println!("{}", ans);
+            }
+        }
+    }
 }
