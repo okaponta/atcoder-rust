420a45f3709c8d0d645df3dd5f14ae0a426d8c7d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-02-28 13:50:37 +0900

    add abc172_d

--- abc172/src/bin/d.rs
+++ abc172/src/bin/d.rs
@@ -2,8 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
     }
-    println!("{}", a);
-    todo!();
+    let half = n / 2;
+    let mut ans = 0;
+    for i in 1..=half {
+        let mx = n - n % i;
+        ans += (i + mx) * (mx / i) / 2;
+    }
+    ans += (half + 1 + n) * (n - half) / 2;
+    println!("{}", ans);
 }
