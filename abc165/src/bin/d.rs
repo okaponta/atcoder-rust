57de5177f3048f45e836fb7780bd9d59a6ac321c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-03 13:39:39 +0900

    add abc165_d

--- abc165/src/bin/d.rs
+++ abc165/src/bin/d.rs
@@ -2,8 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       a:u64,b:u64,n:u64,
     }
-    println!("{}", a);
-    todo!();
+    let x = std::cmp::min(b - 1, n);
+    println!("{}", a * x / b);
 }
