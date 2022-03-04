6a6190533dcd14755fa63881bc832607d0e18234
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-04 22:55:24 +0900

    add abc165_c

--- abc165/src/bin/c.rs
+++ abc165/src/bin/c.rs
@@ -1,9 +1,42 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,m:usize,q:usize,
+       abcd:[(Usize1,Usize1,usize,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut a = vec![1; n];
+    let mut ans = 0;
+    while a[0] != m {
+        let mut point = 0;
+        for (ai, bi, ci, di) in &abcd {
+            if a[*bi] - a[*ai] == *ci {
+                point += di;
+            }
+        }
+        ans = ans.max(point);
+        next(&mut a, n, m);
+    }
+    let mut point = 0;
+    for (ai, bi, ci, di) in &abcd {
+        if a[*bi] - a[*ai] == *ci {
+            point += di;
+        }
+    }
+    ans = ans.max(point);
+    println!("{}", ans);
+}
+
+fn next(prev: &mut Vec<usize>, n: usize, m: usize) {
+    let mut index = 100;
+    for i in (0..n).rev() {
+        if prev[i] != m {
+            index = i;
+            break;
+        }
+    }
+    prev[index] += 1;
+    for i in index..n {
+        prev[i] = prev[index];
+    }
 }
