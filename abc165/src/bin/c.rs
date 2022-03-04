8e2f6b2d6d3ce62d5b74f655f21d052ddce52317
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-04 23:01:46 +0900

    add abc165_c

--- abc165/src/bin/c.rs
+++ abc165/src/bin/c.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use proconio::{input, marker::Usize1};
 
 fn main() {
@@ -5,38 +6,15 @@ fn main() {
        n:usize,m:usize,q:usize,
        abcd:[(Usize1,Usize1,usize,usize);q],
     }
-    let mut a = vec![1; n];
     let mut ans = 0;
-    while a[0] != m {
+    for a in (0..m).combinations_with_replacement(n) {
         let mut point = 0;
-        for (ai, bi, ci, di) in &abcd {
-            if a[*bi] - a[*ai] == *ci {
+        for &(ai, bi, ci, di) in &abcd {
+            if a[bi] - a[ai] == ci {
                 point += di;
             }
         }
         ans = ans.max(point);
-        next(&mut a, n, m);
     }
-    let mut point = 0;
-    for (ai, bi, ci, di) in &abcd {
-        if a[*bi] - a[*ai] == *ci {
-            point += di;
-        }
-    }
-    ans = ans.max(point);
-    println!("{}", ans);
-}
-
-fn next(prev: &mut Vec<usize>, n: usize, m: usize) {
-    let mut index = 100;
-    for i in (0..n).rev() {
-        if prev[i] != m {
-            index = i;
-            break;
-        }
-    }
-    prev[index] += 1;
-    for i in index..n {
-        prev[i] = prev[index];
-    }
+    print!("{}", ans);
 }
