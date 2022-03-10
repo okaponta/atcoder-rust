83c37e1d4b0efb81c789285ac219349718987867
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-10 21:59:58 +0900

    add abc162_c

--- abc162/src/bin/c.rs
+++ abc162/src/bin/c.rs
@@ -1,9 +1,24 @@
+use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        k:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut g = vec![vec![0; k + 1]; k + 1];
+    for (a, b) in iproduct!(1..=k, 1..=k) {
+        g[a][b] = gcd(a, b);
+    }
+    let mut ans = 0;
+    for (a, b, c) in iproduct!(1..=k, 1..=k, 1..=k) {
+        ans += g[g[a][b]][c];
+    }
+    println!("{}", ans);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
