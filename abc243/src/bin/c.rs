5e532c3fc12f6b09f179af24b0b6e714b7cea8a5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-12 22:57:36 +0900

    add abc243_c

--- abc243/src/bin/c.rs
+++ abc243/src/bin/c.rs
@@ -1,9 +1,38 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut xy:[(usize,usize);n],
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut xys = vec![];
+    for i in 0..n {
+        xys.push((xy[i].0, xy[i].1, s[i]));
+    }
+    xys.sort_by_key(|x| x.1);
+    let mut prev = 2 << 60;
+    let mut right = 2 << 60;
+    let mut left = 0;
+    for (x, y, s) in xys {
+        if prev != y {
+            if right < left {
+                println!("Yes");
+                return;
+            }
+            right = 2 << 60;
+            left = 0;
+        }
+        if s == 'R' {
+            right = right.min(x);
+        } else {
+            left = left.max(x);
+        }
+        prev = y;
+    }
+    if right < left {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
