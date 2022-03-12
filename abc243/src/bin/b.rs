340a1f3d0f40d31554cf0b04a546d51faae2df24
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-12 22:57:35 +0900

    add abc243_b

--- abc243/src/bin/b.rs
+++ abc243/src/bin/b.rs
@@ -1,9 +1,30 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        a:[usize;n],
+        b:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut hit = 0;
+    for i in 0..n {
+        if a[i] == b[i] {
+            hit += 1;
+        }
+    }
+    let mut blow = 0;
+    let mut set = HashSet::new();
+    for i in 0..n {
+        set.insert(a[i]);
+    }
+    for bi in b {
+        if set.contains(&bi) {
+            blow += 1;
+        }
+    }
+    blow -= hit;
+    println!("{}", hit);
+    println!("{}", blow);
 }
