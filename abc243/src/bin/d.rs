c9ef1f49ef8f73ed95465c10f866da53260fd874
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-12 22:57:38 +0900

    add abc243_d

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -1,9 +1,32 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize, mut x:u128,
+        s:Chars
     }
-    println!("{}", a);
-    todo!();
+    let mut s_cmp = vec![];
+    let mut count = 0;
+    for i in (0..n).rev() {
+        if s[i] == 'U' {
+            count += 1;
+        } else {
+            if count > 0 {
+                count -= 1;
+            } else {
+                s_cmp.push(s[i]);
+            }
+        }
+    }
+    for _ in 0..count {
+        s_cmp.push('U');
+    }
+    for c in s_cmp.iter().rev() {
+        match c {
+            'U' => x /= 2,
+            'L' => x = 2 * x,
+            _ => x = 2 * x + 1,
+        }
+    }
+    println!("{}", x);
 }
