ac505d9e94a16d24171fc6f7260c7f9d3cdf8485
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-16 22:02:05 +0900

    add abc214_d

--- abc214/src/bin/d.rs
+++ abc214/src/bin/d.rs
@@ -1,9 +1,22 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut uvw:[(Usize1,Usize1,usize);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut uf = UnionFind::new(n);
+
+    uvw.sort_by_key(|e| e.2);
+    let mut count = vec![1; n];
+    let mut ans = 0;
+    for (u, v, w) in uvw {
+        let uc = count[uf.find(u)];
+        let vc = count[uf.find(v)];
+        ans += w * uc * vc;
+        uf.union(u, v);
+        count[uf.find(v)] = uc + vc;
+    }
+    println!("{}", ans);
 }
