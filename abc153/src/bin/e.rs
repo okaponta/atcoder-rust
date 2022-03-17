b6ddbd87ec5f72cb98ca734d764051d9a082b0ec
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-17 22:18:57 +0900

    add abc153_e

--- abc153/src/bin/e.rs
+++ abc153/src/bin/e.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        h:usize, n:usize,
+        ab:[(usize,usize);n],
+    }
+    let mut ans = vec![1 << 60; h + 10002];
+    ans[0] = 0;
+    for i in 0..h {
+        for &(a, b) in &ab {
+            ans[i + a] = ans[i + a].min(ans[i] + b);
+        }
+    }
+    let mut a = 1 << 60;
+    for i in h..h + 10001 {
+        a = a.min(ans[i]);
     }
     println!("{}", a);
-    todo!();
 }
