c060c02cc47f1fca2f8a2f72fe502db8627741c0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-18 23:12:02 +0900

    add abc227_d

--- abc227/src/bin/d.rs
+++ abc227/src/bin/d.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,k:u128,
+       a:[u128;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(&a, med, k) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", lower);
+}
+
+fn is_ok(a: &[u128], med: u128, k: u128) -> bool {
+    a.iter().map(|i| i.min(&med)).sum::<u128>() >= med * k
 }
