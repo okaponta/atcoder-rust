104a1831815b4bd8f3d288bf5de8fe5b3a195d38
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-20 00:24:40 +0900

    add arc137_d

--- arc137/src/bin/d.rs
+++ arc137/src/bin/d.rs
@@ -4,24 +4,24 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,m:usize,
-        a:[usize;n],
+        mut a:[usize;n],
     }
-    let mut ans = vec![0; m];
-    for i in 0..m {
-        let mut ansi = 0;
-        for j in 0..n {
-            if is_nck_odd(i + n - j - 1, n - j - 1) {
-                ansi = ansi ^ a[j];
+    let cap = n.max(m).next_power_of_two();
+    a.reverse();
+    a.resize(cap, 0);
+    let mut bit = 0;
+    while (1 << bit) < cap {
+        for i in 0..cap {
+            if (i & 1 << bit) == 0 {
+                a[i | 1 << bit] ^= a[i];
             }
         }
-        ans[i] = ansi;
+        bit += 1;
     }
+    a.reverse();
+    a.truncate(m);
     println!(
         "{}",
-        ans.iter().map(|i| i.to_string()).collect_vec().join(" ")
+        a.iter().map(|i| i.to_string()).collect_vec().join(" ")
     );
-}
-
-fn is_nck_odd(n: usize, k: usize) -> bool {
-    n & k == k
 }
