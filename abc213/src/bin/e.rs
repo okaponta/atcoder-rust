348e7a7504a3ab87b2f80b1ab83ff5cc4fc5a6f4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-19 17:07:09 +0900

    add abc213_e

--- abc213/src/bin/e.rs
+++ abc213/src/bin/e.rs
@@ -1,6 +1,7 @@
 use std::collections::VecDeque;
 
-use itertools::iproduct;
+use itertools::{iproduct, Itertools};
+use num::Signed;
 use proconio::{input, marker::Chars};
 
 fn main() {
@@ -8,8 +9,6 @@ fn main() {
         h:usize,w:usize,
         s:[Chars;h],
     }
-    let hh = h as i32;
-    let ww = w as i32;
     let mut is_road = vec![vec![false; w]; h];
     let mut ans = vec![vec![1 << 30; w]; h];
     for (i, j) in iproduct!(0..h, 0..w) {
@@ -19,52 +18,38 @@ fn main() {
     }
 
     let walk = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
-    let punch = vec![
-        (-2, -1),
-        (-2, 0),
-        (-2, 1),
-        (-1, -2),
-        (-1, -1),
-        (-1, 1),
-        (-1, 2),
-        (0, -2),
-        (0, 2),
-        (1, -2),
-        (1, -1),
-        (1, 1),
-        (1, 2),
-        (2, -1),
-        (2, 0),
-        (2, 1),
-    ];
+    let punch = (-2..3)
+        .cartesian_product(-2..3)
+        .filter(|(i, j)| i.abs() + j.abs() != 4)
+        .collect_vec();
 
     let mut q = VecDeque::new();
     q.push_back((0, 0, 0));
     while let Some((i, j, c)) = q.pop_front() {
-        let ii = i as usize;
-        let jj = j as usize;
-        if ans[ii][jj] <= c {
+        if ans[i][j] <= c {
             continue;
         }
-        ans[ii][jj] = c;
+        ans[i][j] = c;
         // となり
         for (x, y) in &walk {
-            if is_available(i + x, j + y, hh, ww) {
-                if is_road[(i + x) as usize][(j + y) as usize] {
-                    q.push_front((i + x, j + y, c));
+            let next = ((i as i32 + x), (j as i32 + y));
+            if is_available(next.0, next.1, h, w) {
+                if is_road[next.0 as usize][next.1 as usize] {
+                    q.push_front((next.0 as usize, next.1 as usize, c));
                 }
             }
         }
         // パンチ
         for (x, y) in &punch {
-            if is_available(i + x, j + y, hh, ww) {
-                q.push_back((i + x, j + y, c + 1));
+            let next = ((i as i32 + x), (j as i32 + y));
+            if is_available(next.0, next.1, h, w) {
+                q.push_back((next.0 as usize, next.1 as usize, c + 1));
             }
         }
     }
     println!("{}", ans[h - 1][w - 1]);
 }
 
-fn is_available(i: i32, j: i32, h: i32, w: i32) -> bool {
-    0 <= i && i < h && 0 <= j && j < w
+fn is_available(i: i32, j: i32, h: usize, w: usize) -> bool {
+    0 <= i && i < h as i32 && 0 <= j && j < w as i32
 }
