ef0d9ca5f5837d4397c6f148306ad070a3c4215a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-20 23:13:36 +0900

    add abc244_b

--- abc244/src/bin/b.rs
+++ abc244/src/bin/b.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        _:usize,
+        t:Chars
     }
-    println!("{}", a);
-    todo!();
+    let mut dir = 0;
+    let mut x = 0;
+    let mut y = 0;
+    for c in t {
+        if c == 'S' {
+            match dir % 4 {
+                0 => x += 1,
+                1 => y -= 1,
+                2 => x -= 1,
+                _ => y += 1,
+            }
+        } else {
+            dir += 1;
+        }
+    }
+    println!("{} {}", x, y);
 }
