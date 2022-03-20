394966fb58f2150a78ee6800d43d69225177baf3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-20 23:13:38 +0900

    add abc244_d

--- abc244/src/bin/d.rs
+++ abc244/src/bin/d.rs
@@ -2,8 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        s:[String;3],
+        t:[String;3],
     }
-    println!("{}", a);
-    todo!();
+    if (s[0] == t[0] && s[1] == t[1] && s[2] == t[2])
+        || (s[0] == t[2] && s[1] == t[0] && s[2] == t[1])
+        || (s[0] == t[1] && s[1] == t[2] && s[2] == t[0])
+    {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
