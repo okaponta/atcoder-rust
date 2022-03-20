88c3055941754a58ac7c1c9f42617104c9f5befe
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-20 23:13:37 +0900

    add abc244_c

--- abc244/src/bin/c.rs
+++ abc244/src/bin/c.rs
@@ -1,9 +1,27 @@
-use proconio::input;
+use std::{collections::HashSet, io::stdin};
 
 fn main() {
-    input! {
-        a:i32,
+    let n: usize = read().parse().unwrap();
+    let mut set = HashSet::new();
+    loop {
+        for i in 1..=2 * n + 1 {
+            if set.contains(&i) {
+                continue;
+            }
+            set.insert(i);
+            println!("{}", i);
+            break;
+        }
+        let a: usize = read().parse().unwrap();
+        if a == 0 {
+            return;
+        }
+        set.insert(a);
     }
-    println!("{}", a);
-    todo!();
+}
+
+fn read() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).ok();
+    buf.split_whitespace().next().unwrap().to_string()
 }
