06b6bfffead2d1eb489df2acb80f9efe5a4919fd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-20 23:13:41 +0900

    add abc244_f

--- abc244/src/bin/f.rs
+++ abc244/src/bin/f.rs
@@ -1,9 +1,44 @@
-use proconio::input;
+use std::collections::{HashSet, VecDeque};
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut edge = vec![vec![]; n];
+    for (u, v) in uv {
+        edge[u].push(v);
+        edge[v].push(u);
+    }
+    let mut ans = 0;
+    let mut ans_set = HashSet::new();
+    let mut set = HashSet::new();
+    ans_set.insert(0);
+    let mut q = VecDeque::new();
+    for i in 0..n {
+        q.push_back((1 << i, i, 1));
+        set.insert((1 << i, i));
+        ans_set.insert(1 << i);
+        ans += 1;
+    }
+    while ans_set.len() != 1 << n {
+        let target = q.pop_front().unwrap();
+        for &next_one in &edge[target.1] {
+            let next_zero = target.0 ^ (1 << next_one);
+            if set.contains(&(next_zero, next_one)) {
+                continue;
+            }
+            let next_two = target.2 + 1;
+            q.push_back((next_zero, next_one, next_two));
+            set.insert((next_zero, next_one));
+            if ans_set.contains(&next_zero) {
+                continue;
+            }
+            ans_set.insert(next_zero);
+            ans += next_two;
+        }
+    }
+    println!("{}", ans);
 }
