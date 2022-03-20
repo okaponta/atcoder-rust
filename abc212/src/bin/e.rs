da8848d0c278eeeeac255ea87bf969cfd5903647
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-21 00:54:38 +0900

    add abc212_e

--- abc212/src/bin/e.rs
+++ abc212/src/bin/e.rs
@@ -1,9 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
+
+const MOD: i64 = 998244353;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,m:usize,k:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut edge = vec![vec![]; n];
+    for (u, v) in uv {
+        edge[u].push(v);
+        edge[v].push(u);
+    }
+
+    let mut dp = vec![0; n];
+    dp[0] = 1;
+    for _ in 0..k {
+        let mut next = vec![0; n];
+        let sum = dp.iter().sum::<i64>();
+        for i in 0..n {
+            let mut sub = dp[i];
+            for &prev in &edge[i] {
+                sub += dp[prev];
+            }
+            next[i] = (sum - sub) % MOD;
+        }
+        dp = next;
+    }
+    println!("{}", dp[0]);
 }
