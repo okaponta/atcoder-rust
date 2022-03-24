9cc1030a9ffac8fe70cf9423e1d741bd0c968667
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-24 20:18:37 +0900

    add abc161_b

--- abc161/src/bin/b.rs
+++ abc161/src/bin/b.rs
@@ -2,7 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,m:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    a.reverse();
+    let all = a.iter().sum::<usize>();
+    let is_ok = a[m - 1] * 4 * m >= all;
+    println!("{}", if is_ok { "Yes" } else { "No" });
 }
