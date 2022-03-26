31f63fa124687bb0d0b21ffac507e737e533ea58
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 23:03:48 +0900

    add abc245_c

--- abc245/src/bin/c.rs
+++ abc245/src/bin/c.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,k:i64,
+        a:[i64;n],
+        b:[i64;n],
     }
-    println!("{}", n);
+    let mut prev1 = a[0];
+    let mut prev2 = b[0];
+    for i in 1..n {
+        let mut next1 = -1;
+        let mut next2 = -1;
+        if ((prev1 - a[i]).abs()).min((prev2 - a[i]).abs()) <= k {
+            next1 = a[i];
+        }
+        if ((prev1 - b[i]).abs()).min((prev2 - b[i]).abs()) <= k {
+            next2 = b[i];
+        }
+        if next1 == -1 && next2 == -1 {
+            println!("No");
+            return;
+        }
+        prev1 = next1;
+        prev2 = next2;
+    }
+    println!("Yes");
 }
