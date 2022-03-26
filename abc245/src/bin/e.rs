ba831b6079c0a576ff3002406e3eb9f97b5698a2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 23:03:51 +0900

    add abc245_e

--- abc245/src/bin/e.rs
+++ abc245/src/bin/e.rs
@@ -1,8 +1,42 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,m:usize,
+        a:[usize;n],
+        b:[usize;n],
+        c:[usize;m],
+        d:[usize;m],
     }
-    println!("{}", n);
+    let mut abcd = vec![];
+    for i in 0..n {
+        abcd.push((a[i], b[i], 0));
+    }
+    for i in 0..m {
+        abcd.push((c[i], d[i], 1));
+    }
+    abcd.sort_by(|a, b| match b.0.cmp(&a.0) {
+        std::cmp::Ordering::Equal => match b.1.cmp(&a.1) {
+            std::cmp::Ordering::Equal => b.2.cmp(&a.2),
+            other => other,
+        },
+        other => other,
+    });
+    let mut set = BTreeSet::new();
+    for (i, (_, y, is_choco)) in abcd.iter().enumerate() {
+        if *is_choco != 0 {
+            set.insert((*y, i));
+        } else {
+            let bo = set.range((*y, 0)..).next().map_or((0, 0), |i| *i);
+            if bo.0 == 0 {
+                println!("No");
+                return;
+            } else {
+                set.remove(&bo);
+            }
+        }
+    }
+    println!("Yes");
 }
