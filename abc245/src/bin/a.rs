c336142c72af8b8047b72a6348b4dbb6689aad1c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 23:03:45 +0900

    add abc245_a

--- abc245/src/bin/a.rs
+++ abc245/src/bin/a.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        b:usize,
+        c:usize,
+        d:usize,
     }
-    println!("{}", n);
+    let mut is_t = false;
+    if a < c {
+        is_t = true;
+    } else if a == c {
+        if b <= d {
+            is_t = true;
+        }
+    }
+    println!("{}", if is_t { "Takahashi" } else { "Aoki" });
 }
