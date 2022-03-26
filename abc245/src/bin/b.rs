39e914856c31df361c42beb9ac300ec8b59d36b4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 23:03:46 +0900

    add abc245_b

--- abc245/src/bin/b.rs
+++ abc245/src/bin/b.rs
@@ -1,8 +1,20 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for ai in a {
+        set.insert(ai);
+    }
+    for i in 0..2005 {
+        if !set.contains(&i) {
+            println!("{}", i);
+            return;
+        }
+    }
 }
