b9ff3d50b80588a9f96fbe6fccee95cdad5ddb68
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 23:03:49 +0900

    add abc245_d

--- abc245/src/bin/d.rs
+++ abc245/src/bin/d.rs
@@ -1,8 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,m:usize,
+        mut a:[i64;n+1],
+        mut c:[i64;n+m+1],
     }
-    println!("{}", n);
+    a.reverse();
+    c.reverse();
+    let mut b = vec![0; m + 1];
+    for i in 0..=m {
+        b[i] = c[i] / a[0];
+        for j in 0..=n {
+            c[i + j] -= b[i] * a[j];
+        }
+    }
+    println!("{}", b.iter().rev().map(|i| i.to_string()).join(" "));
 }
