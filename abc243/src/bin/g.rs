c60522fef21f05566884c69caeb6c9ba26b1612f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-26 19:47:20 +0900

    add abc243_g

--- abc243/src/bin/g.rs
+++ abc243/src/bin/g.rs
@@ -1,9 +1,30 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        t:usize,
+        x:[usize;t],
     }
-    println!("{}", a);
-    todo!();
+    x.iter().for_each(|xi| testcase(*xi));
+}
+
+fn testcase(x: usize) {
+    let max = x.sqrt().sqrt();
+    let mut dp = vec![0; max + 1];
+    dp[1] = 1;
+    for i in 1..=max {
+        let mut count = 0;
+        for j in 1..=i.sqrt() {
+            count += dp[j];
+        }
+        dp[i] = count;
+    }
+
+    let sq = x.sqrt();
+    let mut ans = 0;
+    for i in 1..=max {
+        ans += (sq - i * i + 1) * dp[i];
+    }
+    println!("{}", ans);
 }
