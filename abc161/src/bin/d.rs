6e116141cd3d342a1f66c41cfb1868e041ac220b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-28 21:26:52 +0900

    add abc161_d

--- abc161/src/bin/d.rs
+++ abc161/src/bin/d.rs
@@ -2,7 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut ans = 1;
+    for _ in 1..k {
+        ans = lunlun(ans);
+    }
+    println!("{}", ans);
+}
+
+fn lunlun(v: usize) -> usize {
+    if v < 10 {
+        return v + 1;
+    }
+    let first = v % 10;
+    let second = (v / 10) % 10;
+    if first == 9 || first == second + 1 {
+        // くりあがり
+        let next_over_sec = lunlun(v / 10);
+        let next_first = (next_over_sec % 10).max(1) - 1;
+        return next_over_sec * 10 + next_first;
+    }
+    v + 1
 }
