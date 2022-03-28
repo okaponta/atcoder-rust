23f4508eb82b0973bcea42e9a3eebeb60e435211
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-28 21:34:53 +0900

    add abc245_f

--- abc245/src/bin/f.rs
+++ abc245/src/bin/f.rs
@@ -1,8 +1,35 @@
-use proconio::input;
+use std::collections::{HashSet, VecDeque};
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n:usize,
+        n: usize, m: usize,
+        uv: [(Usize1, Usize1); m],
     }
-    println!("{}", n);
+    let mut edge = vec![HashSet::new(); n];
+    let mut edge_rev = vec![HashSet::new(); n];
+    for &(u, v) in uv.iter() {
+        edge[u].insert(v);
+        edge_rev[v].insert(u);
+    }
+    let mut queue = VecDeque::new();
+    for i in 0..n {
+        if edge[i].is_empty() {
+            queue.push_back(i);
+        }
+    }
+
+    let mut ans = n;
+    while let Some(q) = queue.pop_front() {
+        ans -= 1;
+        for &p in &edge_rev[q] {
+            edge[p].remove(&q);
+            if edge[p].is_empty() {
+                queue.push_back(p);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
