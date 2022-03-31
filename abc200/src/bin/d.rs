a4f8550240a92ac69ebd7489971bc5d90e4f766a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-03-31 22:31:47 +0900

    add abc200_d

--- abc200/src/bin/d.rs
+++ abc200/src/bin/d.rs
@@ -1,8 +1,30 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+        mut n: usize,
+        a: [usize; n],
     }
-    println!("{}", a);
+    let mut map = HashMap::new();
+    n = n.min(8);
+    for i in 1..(1 << n) {
+        let mut sum = 0;
+        let mut b = vec![];
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                sum += a[j];
+                b.push(j + 1);
+            }
+        }
+        if let Some(c) = map.insert(sum % 200, b.clone()) {
+            println!("Yes");
+            println!("{} {}", b.len(), b.iter().join(" "));
+            println!("{} {}", c.len(), c.iter().join(" "));
+            return;
+        }
+    }
+    println!("No");
 }
