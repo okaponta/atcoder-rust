4c8ac237e794ba42142f0f887704b71592c3d913
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-02 23:04:14 +0900

    add abc246_a

--- abc246/src/bin/a.rs
+++ abc246/src/bin/a.rs
@@ -1,8 +1,26 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        xy:[(i32,i32);3],
     }
-    println!("{}", n);
+    let mut xset = HashSet::new();
+    let mut yset = HashSet::new();
+    for (x, y) in xy {
+        if xset.contains(&x) {
+            xset.remove(&x);
+        } else {
+            xset.insert(x);
+        }
+        if yset.contains(&y) {
+            yset.remove(&y);
+        } else {
+            yset.insert(y);
+        }
+    }
+    let x = xset.iter().next().unwrap();
+    let y = yset.iter().next().unwrap();
+    println!("{} {}", x, y);
 }
