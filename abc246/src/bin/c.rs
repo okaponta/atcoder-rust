6f5f6917091ed5006d4539b66070f8f2402f38a4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-02 23:04:39 +0900

    add abc246_c

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -2,7 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,mut k:usize,x:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    for i in 0..n {
+        if k > 0 && a[i] >= x {
+            let max = a[i] / x;
+            let sub = max.min(k);
+            a[i] -= sub * x;
+            k -= sub;
+        }
+    }
+    a.sort();
+    let mut index = n;
+    while k > 0 && index > 0 {
+        index -= 1;
+        a[index] = 0;
+        k -= 1;
+    }
+    let ans = a.iter().sum::<usize>();
+    println!("{}", ans);
 }
