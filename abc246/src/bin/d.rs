e56f48bdcf2ab12d13f230da15e15588bc2bb5e0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-02 23:16:07 +0900

    add abc246_d

--- abc246/src/bin/d.rs
+++ abc246/src/bin/d.rs
@@ -1,8 +1,22 @@
 use proconio::input;
 
+const MAX: i64 = 1_000_000;
+
 fn main() {
     input! {
-        n:usize,
+        n:i64,
     }
-    println!("{}", n);
+    let mut ans = 1 << 60;
+    let mut b = MAX;
+    for a in 0..MAX {
+        while f(a, b) >= n && b >= 0 {
+            ans = ans.min(f(a, b));
+            b -= 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn f(a: i64, b: i64) -> i64 {
+    (a + b) * (a * a + b * b)
 }
