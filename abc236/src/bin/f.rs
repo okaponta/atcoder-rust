7ef82a7d642b0fd0e79efbf995b9ba583a651b94
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-07 02:43:09 +0900

    add abc236_f

--- abc236/src/bin/f.rs
+++ abc236/src/bin/f.rs
@@ -1,9 +1,27 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       c:[usize;(1<<n)-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut c = c.iter().enumerate().map(|e| (e.0 + 1, e.1)).collect_vec();
+    c.sort_by_key(|e| e.1);
+    let mut chosen = vec![];
+    let mut ans = vec![];
+    for (mut i, &c) in c {
+        if chosen.len() == n {
+            break;
+        }
+        for e in &chosen {
+            i = i.min(i ^ e);
+        }
+        if i == 0 {
+            continue;
+        }
+        chosen.push(i);
+        ans.push(c);
+    }
+    println!("{:?}", ans.iter().sum::<usize>());
 }
