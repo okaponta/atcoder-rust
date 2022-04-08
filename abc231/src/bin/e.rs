5d489c2c8ecce4f0dd7b74c15814692e687fd08e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-09 02:40:00 +0900

    add abc231_e

--- abc231/src/bin/e.rs
+++ abc231/src/bin/e.rs
@@ -1,9 +1,28 @@
+use std::collections::HashMap;
+
+use num::Integer;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       x:usize,
+       a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    println!("{}", dfs(x, &a, 0, &mut HashMap::new()));
+}
+
+fn dfs(x: usize, a: &Vec<usize>, i: usize, map: &mut HashMap<usize, usize>) -> usize {
+    if map.contains_key(&x) {
+        return map[&x];
+    }
+    if i == a.len() - 1 {
+        return x / a[i];
+    }
+    let ls = (x / a[i + 1]) * a[i + 1];
+    let ov = x.div_ceil(&a[i + 1]) * a[i + 1];
+    let less = (x - ls) / a[i] + dfs(ls, a, i + 1, map);
+    let over = (ov - x) / a[i] + dfs(ov, a, i + 1, map);
+    map.insert(x, over.min(less));
+    over.min(less)
 }
