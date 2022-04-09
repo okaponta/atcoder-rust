a6734f2dc25bfb40e93f47c1ab4314823ac30514
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 00:45:58 +0900

    add arc138_b

--- arc138/src/bin/b.rs
+++ arc138/src/bin/b.rs
@@ -1,8 +1,31 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[u8;n],
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    for ai in a {
+        q.push_back(ai);
+    }
+    let mut flipped = false;
+    while !q.is_empty() {
+        let f = q.front().unwrap();
+        let b = q.back().unwrap();
+        if (!flipped && b == &0) || (flipped && b == &1) {
+            q.pop_back();
+        } else {
+            if (!flipped && f == &0) || (flipped && f == &1) {
+                q.pop_front();
+                flipped = !flipped;
+            } else {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
