9ec568e201d039007f7c658649d4f623c38ba5a6
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 23:02:14 +0900

    add abc247_a

--- abc247/src/bin/a.rs
+++ abc247/src/bin/a.rs
@@ -1,8 +1,12 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ans = vec!['0'; 4];
+    for i in 0..3 {
+        ans[i + 1] = s[i];
+    }
+    println!("{}", ans.iter().collect::<String>());
 }
