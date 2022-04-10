056b70b4409eca9622f607e3b8cf9a837e6de967
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 23:02:16 +0900

    add abc247_b

--- abc247/src/bin/b.rs
+++ abc247/src/bin/b.rs
@@ -1,8 +1,45 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        st:[(String,String);n],
     }
-    println!("{}", n);
+    let mut once = HashSet::new();
+    let mut twice = HashSet::new();
+    for (s, t) in st.clone() {
+        if s == t {
+            if once.contains(&s) {
+                twice.insert(s);
+            } else {
+                once.insert(s);
+            }
+            continue;
+        }
+        if once.contains(&s) {
+            twice.insert(s);
+        } else {
+            once.insert(s);
+        }
+        if once.contains(&t) {
+            twice.insert(t);
+        } else {
+            once.insert(t);
+        }
+    }
+    for (s, t) in st {
+        if s == t {
+            if twice.contains(&s) {
+                println!("No");
+                return;
+            }
+        }
+        if twice.contains(&s) && twice.contains(&t) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
