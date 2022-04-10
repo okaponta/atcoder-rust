e19fe4435a8b4b3eb6878a7213ef86a65e4a1bf2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 23:02:17 +0900

    add abc247_c

--- abc247/src/bin/c.rs
+++ abc247/src/bin/c.rs
@@ -1,8 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut prev = vec![];
+    for i in 0..n {
+        let mut next = prev.clone();
+        next.push(i + 1);
+        for e in prev {
+            next.push(e);
+        }
+        prev = next;
+    }
+    println!("{}", prev.iter().join(" "));
 }
