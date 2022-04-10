a54a36fe2c48d203c37ab9f81d9d145067d64fc9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 23:02:19 +0900

    add abc247_d

--- abc247/src/bin/d.rs
+++ abc247/src/bin/d.rs
@@ -1,8 +1,33 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        query:usize,
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    for _ in 0..query {
+        input! {i: usize}
+        if i == 1 {
+            input! {x: i64, c:i64}
+            q.push_back((x, c));
+        } else {
+            input! {mut c: i64}
+            let mut ans = 0;
+            while c > 0 {
+                let (xx, mut cc) = q.pop_front().unwrap();
+                if c > cc {
+                    ans += xx * cc;
+                    c -= cc;
+                    continue;
+                }
+                ans += xx * c;
+                cc -= c;
+                c -= c;
+                q.push_front((xx, cc));
+            }
+            println!("{}", ans);
+        }
+    }
 }
