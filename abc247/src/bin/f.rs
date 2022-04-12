ca927a2de755dd8360097d1945c56f71f49b9d63
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-12 12:27:14 +0900

    add abc247_f

--- abc247/src/bin/f.rs
+++ abc247/src/bin/f.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        p:[Usize1;n],
+        q:[Usize1;n],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for i in 0..n {
+        uf.union(p[i], q[i]);
+    }
+    let mut count = vec![0; n];
+    for i in 0..n {
+        count[uf.find(i)] += 1;
+    }
+    if n == 1 {
+        println!("1");
+        return;
+    }
+    let mut v = vec![0i64; n + 1];
+    v[1] = 1;
+    v[2] = 3;
+    for i in 3..=n {
+        v[i] = (v[i - 1] + v[i - 2]) % 998244353;
+    }
+    let mut ans: i64 = 1;
+    for i in 0..n {
+        if count[i] == 0 {
+            continue;
+        }
+        ans = (ans * v[count[i]]) % 998244353;
+    }
+    println!("{}", ans);
 }
