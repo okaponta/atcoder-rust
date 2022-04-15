6e541ba309911bb0c8509e47fa10c0fdde02de7b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-15 19:48:57 +0900

    add abc176_e

--- abc176/src/bin/e.rs
+++ abc176/src/bin/e.rs
@@ -1,9 +1,39 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        _:usize,_:usize,m:usize,
+        hw:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut hmap = HashMap::new();
+    let mut wmap = HashMap::new();
+    for (h, w) in hw.clone() {
+        *hmap.entry(h).or_insert(0i64) += 1;
+        *wmap.entry(w).or_insert(0i64) += 1;
+    }
+    let hmax = *hmap.values().max().unwrap();
+    let wmax = *wmap.values().max().unwrap();
+    let hkey = hmap
+        .iter()
+        .filter(|&e| e.1 == &hmax)
+        .map(|e| *e.0)
+        .collect::<HashSet<_>>();
+    let wkey = wmap
+        .iter()
+        .filter(|&e| e.1 == &wmax)
+        .map(|e| *e.0)
+        .collect::<HashSet<_>>();
+    let mut ans = hmax + wmax;
+    let mut count = hkey.len() * wkey.len();
+    for (h, w) in hw {
+        if hkey.contains(&h) && wkey.contains(&w) {
+            count -= 1;
+        }
+    }
+    if count == 0 {
+        ans -= 1;
+    }
+    println!("{}", ans);
 }
