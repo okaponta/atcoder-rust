b492a51bcf0f5fd4f49282bded5d6ddf6a8dbd7e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-16 22:49:09 +0900

    add abc248_b

--- abc248/src/bin/b.rs
+++ abc248/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut a:usize,
+        b:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    while a < b {
+        a *= k;
+        ans += 1;
+    }
+    println!("{}", ans);
 }
