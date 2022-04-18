e655261a7dec73e686c3bf254abee0ef8c4f3b17
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-18 22:18:13 +0900

    add abc232_f

--- abc232/src/bin/f.rs
+++ abc232/src/bin/f.rs
@@ -1,9 +1,31 @@
 use proconio::input;
 
+const INF: i64 = 2_000_000_000_000_000_000;
+
 fn main() {
     input! {
-       a:i32,
+        n:usize,
+        x: i64,
+        y: i64,
+        a: [i64; n],
+        b: [i64; n],
     }
-    println!("{}", a);
-    todo!();
+    // dp[S] bitが立っているやつは揃っているやつ
+    let mut dp = vec![INF; 1 << n];
+    dp[0] = 0;
+    for bit in 0usize..(1 << n) {
+        let cnt = bit.count_ones() as usize;
+        for p in 0..n {
+            if (bit >> p) & 1 == 1 {
+                // 既にそろっているので操作不要
+                continue;
+            }
+            let next = dp[bit]
+                + (a[p] - b[cnt]).abs() * x
+                + (bit & ((1_usize << n) - (1_usize << p))).count_ones() as i64 * y;
+            dp[bit | 1 << p] = dp[bit | 1 << p].min(next);
+        }
+    }
+
+    println!("{}", dp[(1 << n) - 1]);
 }
