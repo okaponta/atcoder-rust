b91035aa61a224a38d60db6eb24635d4988b3de2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-18 23:10:01 +0900

    add abc232_f

--- abc232/src/bin/f.rs
+++ abc232/src/bin/f.rs
@@ -10,14 +10,15 @@ fn main() {
         a: [i64; n],
         b: [i64; n],
     }
-    // dp[S] bitが立っているやつは揃っているやつ
+    // dp[S] bitが立っているやつは使ったやつ(頭から決めていく)
     let mut dp = vec![INF; 1 << n];
     dp[0] = 0;
     for bit in 0usize..(1 << n) {
+        // 既に決めた数字たち
         let cnt = bit.count_ones() as usize;
         for p in 0..n {
             if (bit >> p) & 1 == 1 {
-                // 既にそろっているので操作不要
+                // 既に使っているので操作不要
                 continue;
             }
             let next = dp[bit]
