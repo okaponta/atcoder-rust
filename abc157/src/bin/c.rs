907a1e57a3db5a4dab48dd47b6a598995b2e366a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-21 23:09:14 +0900

    add abc157_c

--- abc157/src/bin/c.rs
+++ abc157/src/bin/c.rs
@@ -1,8 +1,39 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,m:usize,
+        sc:[(Usize1,i32);m],
     }
-    println!("{}", n);
+    let mut ans = vec![-1; n];
+    for (s, c) in sc {
+        if ans[s] == -1 || ans[s] == c {
+            ans[s] = c;
+        } else {
+            println!("-1");
+            return;
+        }
+    }
+    if ans[0] == 0 {
+        if n == 1 {
+            println!("0");
+            return;
+        }
+        println!("-1");
+        return;
+    }
+    if ans[0] == -1 {
+        if n == 1 {
+            println!("0");
+            return;
+        }
+        ans[0] = 1;
+    }
+    for i in 1..n {
+        if ans[i] == -1 {
+            ans[i] = 0;
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
