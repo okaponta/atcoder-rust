ee08351049c47bca83965f96326fc0ff6be8d0c3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-22 21:03:27 +0900

    add abc225_e

--- abc225/src/bin/e.rs
+++ abc225/src/bin/e.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       mut xy:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    xy.sort_by(|a, b| ((a.0 * (b.1 - 1)).cmp(&((a.1 - 1) * b.0))));
+    let mut cur = (0, 1);
+    let mut count = 0;
+    for (x, y) in xy {
+        if cur.0 * y <= cur.1 * (x - 1) {
+            count += 1;
+            cur = (x, y - 1);
+        }
+    }
+    println!("{}", count);
 }
