2c2e414a09defd16b177acf58d0876a7a1c153d9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-23 23:17:11 +0900

    add abc249_a

--- abc249/src/bin/a.rs
+++ abc249/src/bin/a.rs
@@ -2,7 +2,30 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i32,b:i32,c:i32,
+        d:i32,e:i32,f:i32,
+        x:i32
     }
-    println!("{}", n);
+    let mut t = (x / (a + c)) * a * b;
+    let mut ao = (x / (d + f)) * d * e;
+    let xt = x % (a + c);
+    let xa = x % (d + f);
+    if xt > a {
+        t += a * b;
+    } else {
+        t += xt * b;
+    }
+    if xa > d {
+        ao += d * e;
+    } else {
+        ao += xa * e;
+    }
+    let ans = if t > ao {
+        "Takahashi"
+    } else if t == ao {
+        "Draw"
+    } else {
+        "Aoki"
+    };
+    println!("{}", ans);
 }
