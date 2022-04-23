1bd4a9bcac3f19713355c15fd566675a810d78b3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-23 23:17:13 +0900

    add abc249_b

--- abc249/src/bin/b.rs
+++ abc249/src/bin/b.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    let a = s.iter().any(|c| c.is_uppercase());
+    let b = s.iter().any(|c| c.is_lowercase());
+    let len = s.len();
+    s.sort();
+    s.dedup();
+    let c = len == s.len();
+    println!("{}", if a && b && c { "Yes" } else { "No" });
 }
