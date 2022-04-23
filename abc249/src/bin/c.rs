ff0df7f410eb02c4a526f4aed038f182e88472bf
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-23 23:17:14 +0900

    add abc249_c

--- abc249/src/bin/c.rs
+++ abc249/src/bin/c.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut sets = vec![];
+    for ch in s {
+        let mut set = HashSet::new();
+        for c in ch {
+            set.insert(c);
+        }
+        sets.push(set);
+    }
+    let mut ans = 0;
+    for i in 0..1 << n {
+        let mut count = vec![0; 26];
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                for k in 0..26 {
+                    let ch = (b'a' + k as u8) as char;
+                    if sets[j].contains(&ch) {
+                        count[k] += 1;
+                    }
+                }
+            }
+        }
+        let tmp = count.iter().filter(|i| i == &&k).count();
+        ans = ans.max(tmp);
+    }
+    println!("{}", ans);
 }
