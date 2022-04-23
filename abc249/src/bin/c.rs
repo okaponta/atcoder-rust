9ef80aaac9d5a141a5b6cad42c4e6302650b558b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-24 01:18:21 +0900

    add abc249_c

--- abc249/src/bin/c.rs
+++ abc249/src/bin/c.rs
@@ -10,14 +10,19 @@ fn main() {
     }
     let mut sets = vec![];
     for ch in s {
+        // 計算量落とすためにsetで管理した
         let set = ch.into_iter().collect::<HashSet<_>>();
         sets.push(set);
     }
     let mut ans = 0;
+    // bit全探索
     for i in 0..1 << n {
+        // A-Zまでの登場回数を記録
         let mut count = vec![0; 26];
         for j in 0..n {
+            // bitが立っていれば処理する
             if i >> j & 1 == 1 {
+                // A-Zそれぞれについて含まれるか計算
                 for k in 0..26 {
                     let ch = (b'a' + k as u8) as char;
                     if sets[j].contains(&ch) {
