5224d7f37e562471b690d787e925fadb208e2cf7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-24 23:42:14 +0900

    add arc139_a

--- arc139/src/bin/a.rs
+++ arc139/src/bin/a.rs
@@ -3,6 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:[i64;n],
     }
-    println!("{}", n);
+    let mut ans: i64 = 0;
+    let mut prevt = -1;
+    for ti in t {
+        if ti > prevt {
+            ans = ans >> ti;
+            ans = ans << ti;
+            ans += 1 << ti;
+        } else if ti < prevt {
+            ans += 1 << ti;
+        } else {
+            ans += 1 << (ti + 1);
+        }
+        if ans >> ti & 1 == 0 {
+            ans += 1 << ti;
+        }
+        prevt = ti;
+    }
+    println!("{}", ans);
 }
