f9db3a9e0e10b12e316d2f8eaf4f087b33e209c5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-29 17:18:46 +0900

    add abc148_b

--- abc148/src/bin/b.rs
+++ abc148/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 0..n {
+        ans.push(s[i]);
+        ans.push(t[i]);
+    }
+    println!("{}", ans.iter().collect::<String>());
 }
