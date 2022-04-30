632cba5781443eb984746e81a4dca24ef6abdac5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-30 17:56:25 +0900

    add abc148_c

--- abc148/src/bin/c.rs
+++ abc148/src/bin/c.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i64,b:i64
     }
-    println!("{}", n);
+    println!("{}", lcm(a, b));
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a * b / gcd(a, b)
 }
