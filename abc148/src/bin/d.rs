d49e5b96e9e87db7ecd0ebf807c15efeb5ce9cc1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-01 19:29:21 +0900

    add abc148_d

--- abc148/src/bin/d.rs
+++ abc148/src/bin/d.rs
@@ -3,6 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut count = 1;
+    for ai in a {
+        if ai == count {
+            count += 1;
+        } else {
+            ans += 1;
+        }
+    }
+    println!("{}", if ans == n { -1 } else { ans as i32 });
 }
