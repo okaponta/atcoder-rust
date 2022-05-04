fad459ee7297b51bd657a9bc6aad860306d5b073
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-04 23:34:29 +0900

    add abc144_a

--- abc144/src/bin/a.rs
+++ abc144/src/bin/a.rs
@@ -2,7 +2,7 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i32,b:i32,
     }
-    println!("{}", n);
+    println!("{}", if a > 9 || b > 9 { -1 } else { a * b });
 }
