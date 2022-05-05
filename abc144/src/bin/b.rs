2d590c50c5f58b53dde7f85e1307c8f57c14a59f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-05 23:18:30 +0900

    add abc144_b

--- abc144/src/bin/b.rs
+++ abc144/src/bin/b.rs
@@ -1,8 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let ans = (0..10)
+        .cartesian_product(0..10)
+        .into_iter()
+        .map(|(a, b)| a * b)
+        .any(|i| i == n);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
