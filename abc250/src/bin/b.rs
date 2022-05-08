35e95171b4f7d2d8cbb3a1476827ac464388656d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-08 22:45:03 +0900

    add abc250_b

--- abc250/src/bin/b.rs
+++ abc250/src/bin/b.rs
@@ -3,6 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize,
     }
-    println!("{}", n);
+    for i in 0..n {
+        for _ in 0..a {
+            let mut row = vec![];
+            for j in 0..n {
+                for _ in 0..b {
+                    if (i + j) % 2 == 0 {
+                        row.push('.');
+                    } else {
+                        row.push('#');
+                    }
+                }
+            }
+            println!("{}", row.iter().collect::<String>());
+        }
+    }
 }
