e4dcf1657917142be54127ec73dc66068371cac5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-10 02:13:49 +0900

    add abc144_c

--- abc144/src/bin/c.rs
+++ abc144/src/bin/c.rs
@@ -1,8 +1,15 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut ans = 1 << 60;
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            ans = ans.min(i + n / i - 2);
+        }
+    }
+    println!("{}", ans);
 }
