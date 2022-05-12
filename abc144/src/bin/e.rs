2198c21b3d2cdd24cbfd8a966b1119d1a70960f1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-12 21:24:14 +0900

    add abc144_e

--- abc144/src/bin/e.rs
+++ abc144/src/bin/e.rs
@@ -3,6 +3,30 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:i64,
+        mut a:[i64;n],
+        mut f:[i64;n],
     }
-    println!("{}", n);
+    a.sort();
+    f.sort();
+    f.reverse();
+    let mut lower = -1;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if !is_ok(med, k, n, &a, &f) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn is_ok(med: i64, k: i64, n: usize, a: &Vec<i64>, f: &Vec<i64>) -> bool {
+    let mut count = 0;
+    for i in 0..n {
+        count += (a[i] - med / f[i]).max(0);
+    }
+    count <= k
 }
