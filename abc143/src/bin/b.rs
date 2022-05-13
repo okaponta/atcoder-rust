837e0f829afba357681b13a9243ebcefbc9baa5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-13 22:05:31 +0900

    add abc143_b

--- abc143/src/bin/b.rs
+++ abc143/src/bin/b.rs
@@ -1,8 +1,13 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        d:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        d.iter().tuple_combinations().fold(0, |s, (x, y)| s + x * y)
+    );
 }
