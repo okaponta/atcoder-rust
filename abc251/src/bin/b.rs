f0dc05e9659896ec16e3ed8d07964d153750c69c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-14 22:48:27 +0900

    add abc251_b

--- abc251/src/bin/b.rs
+++ abc251/src/bin/b.rs
@@ -1,8 +1,32 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,w:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = vec![false; w + 1];
+    // 1つ
+    for ai in &a {
+        if ai <= &w {
+            ans[*ai] = true;
+        }
+    }
+    // 2つ
+    for c in a.iter().combinations(2) {
+        let s = c[0] + c[1];
+        if s <= w {
+            ans[s] = true;
+        }
+    }
+    // 3つ
+    for c in a.iter().combinations(3) {
+        let s = c[0] + c[1] + c[2];
+        if s <= w {
+            ans[s] = true;
+        }
+    }
+
+    println!("{}", ans.iter().filter(|b| **b).count());
 }
