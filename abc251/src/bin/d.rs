f4729b080d817a4243728e6d1cae0d9d7538b3f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-14 22:48:30 +0900

    add abc251_d

--- abc251/src/bin/d.rs
+++ abc251/src/bin/d.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        _w:usize,
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 1..100 {
+        ans.push(i);
+    }
+    for i in 1..100 {
+        ans.push(i * 100);
+    }
+    for i in 1..=100 {
+        ans.push(i * 10000);
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
