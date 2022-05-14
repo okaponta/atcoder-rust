84dd398833d818581fa36f9495c81738af7a7d01
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-14 22:48:29 +0900

    add abc251_c

--- abc251/src/bin/c.rs
+++ abc251/src/bin/c.rs
@@ -1,8 +1,26 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        st:[(String,i64);n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    let mut point = -1;
+    let mut ans = 0;
+    for i in 0..n {
+        let s = &st[i].0;
+        let t = st[i].1;
+        if set.contains(&s) {
+            continue;
+        }
+        set.insert(s);
+        if point < t {
+            point = t;
+            ans = i + 1;
+        }
+    }
+    println!("{}", ans);
 }
