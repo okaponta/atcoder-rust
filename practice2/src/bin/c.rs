2787738d69461816275d9f53c2679c79723157cd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-16 00:03:35 +0900

    add practice2_c

--- practice2/src/bin/c.rs
+++ practice2/src/bin/c.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        t:usize,
+        nmab:[(usize,usize,usize,usize);t],
     }
-    println!("{}", n);
+    for (n, m, a, b) in nmab {
+        println!("{}", floor_sum(n, m, a, b));
+    }
+}
+
+fn floor_sum(n: usize, m: usize, mut a: usize, mut b: usize) -> usize {
+    let mut res = 0;
+    if a >= m {
+        res += n * (n - 1) * (a / m) / 2;
+        a %= m;
+    }
+    if b >= m {
+        res += n * (b / m);
+        b %= m;
+    }
+    let max = a * n + b;
+    if max >= m {
+        res += floor_sum(max / m, a, m, max % m);
+    }
+    res
 }
