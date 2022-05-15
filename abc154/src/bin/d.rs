0527f74a0b06052fbf13db1b0364ef5fa352836e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-16 00:20:08 +0900

    add abc154_d

--- abc154/src/bin/d.rs
+++ abc154/src/bin/d.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        p:[usize;n],
     }
-    println!("{}", n);
+    let mut sum = 0;
+    for i in 0..k {
+        sum += p[i];
+    }
+    let mut max = sum;
+    for i in k..n {
+        sum = sum + p[i] - p[i - k];
+        max = max.max(sum);
+    }
+    println!("{}", (max + k) as f64 / 2.0);
 }
