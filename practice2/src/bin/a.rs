af4897a3ffeca5ec7009dfed9ece2ad67d420852
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-15 21:43:55 +0900

    add practice2_a

--- practice2/src/bin/a.rs
+++ practice2/src/bin/a.rs
@@ -1,8 +1,18 @@
+use petgraph::unionfind::UnionFind;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        tuv:[(u8,usize,usize);q],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for (t, u, v) in tuv {
+        if t == 0 {
+            uf.union(u, v);
+        } else {
+            println!("{}", if uf.equiv(u, v) { "1" } else { "0" });
+        }
+    }
 }
