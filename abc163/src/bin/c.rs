2ac5c96ba3d1653f0a139762298cf11b163a58a6
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-19 08:44:10 +0900

    add abc163_c

--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use std::collections::HashMap;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[usize;n-1],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for ai in a {
+        *map.entry(ai).or_insert(0) += 1;
+    }
+    for i in 1..=n {
+        println!("{}", map.get(&i).unwrap_or(&0));
+    }
 }
