f490cf3cf445ade25147b40d0daab6a7f1cd930e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-20 00:23:44 +0900

    add abc163_e

--- abc163/src/bin/e.rs
+++ abc163/src/bin/e.rs
@@ -3,6 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut ord = vec![];
+    for i in 0..n {
+        ord.push((a[i], i as i64));
+    }
+    ord.sort();
+    let mut dp = vec![0; n + 1];
+    for i in 0..n {
+        let mut next = vec![0; n + 1];
+        let (a, idx) = ord[n - i - 1];
+        for j in 0..=i {
+            next[j] = next[j].max(dp[j] + a * (idx - (n + j - i - 1) as i64).abs());
+            next[j + 1] = dp[j] + a * (idx - j as i64).abs();
+        }
+        dp = next;
+    }
+    println!("{}", dp.iter().max().unwrap());
 }
