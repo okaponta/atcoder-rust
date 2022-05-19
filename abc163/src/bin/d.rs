b2c0de5bf0f2ff9e5d29b4c24ae5e0f0daed4675
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-19 09:06:39 +0900

    add abc163_d

--- abc163/src/bin/d.rs
+++ abc163/src/bin/d.rs
@@ -2,7 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,k:usize,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        (k..=(n + 1))
+            .into_iter()
+            .fold(0, |s, i| (s + i * (n + 1 - i) + 1) % 1_000_000_007)
+    );
 }
