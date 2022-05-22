e36a447eb34d332de804e4aafe1f9f6e16d38ea3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-23 01:00:49 +0900

    add abc224_f

--- abc224/src/bin/f.rs
+++ abc224/src/bin/f.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+
+const MOD: i64 = 998244353;
 
 fn main() {
     input! {
-       a:i32,
+       s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let n = s.len();
+    let mut pow2 = vec![1];
+    for i in 0..n {
+        pow2.push(pow2[i] * 2 % MOD);
+    }
+    let mut unfix = 0;
+    let mut temp = 0;
+    let mut sum = 0;
+    for idx in 0..n {
+        let i = s[idx].to_digit(10).unwrap() as i64;
+        unfix = (unfix * 10 + i * pow2[idx]) % MOD;
+        temp = (sum + unfix) % MOD;
+        sum = (sum + temp) % MOD;
+    }
+    println!("{}", temp);
 }
