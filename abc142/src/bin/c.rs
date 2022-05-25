40373c4e209b03ef92e6d935f62dbde3ced95133
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-25 09:33:13 +0900

    add abc142_c

--- abc142/src/bin/c.rs
+++ abc142/src/bin/c.rs
@@ -1,8 +1,17 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        (0..n)
+            .sorted_by_key(|&i| a[i])
+            .into_iter()
+            .map(|i| i + 1)
+            .join(" ")
+    );
 }
