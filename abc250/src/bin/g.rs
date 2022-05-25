2d172b5a07d33dc5bea35d7ca27d1010c0f570ce
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-26 00:13:45 +0900

    add abc250_g

--- abc250/src/bin/g.rs
+++ abc250/src/bin/g.rs
@@ -1,8 +1,27 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        p:[i64;n],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for pi in p.iter() {
+        if let Some(Reverse(min)) = heap.peek() {
+            if &pi < min {
+                heap.push(Reverse(pi));
+            } else {
+                heap.pop();
+                heap.push(Reverse(pi));
+                heap.push(Reverse(pi));
+            }
+        } else {
+            heap.push(Reverse(pi));
+        }
+    }
+    let sum: i64 = p.iter().sum();
+    let ans = heap.iter().fold(-sum, |s: i64, Reverse(i)| s + *i);
+    println!("{}", ans);
 }
