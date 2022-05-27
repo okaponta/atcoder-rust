4c4694a7302b9608b023141b0c4b2aafcca4eef3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-28 03:05:46 +0900

    add abc143_d

--- abc143/src/bin/d.rs
+++ abc143/src/bin/d.rs
@@ -1,8 +1,19 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        mut l:[i64;n],
     }
-    println!("{}", n);
+    l.sort();
+    let mut ans = 0;
+    for i in 0..n - 2 {
+        for j in i + 1..n - 1 {
+            let min = (l.upper_bound(&(l[j] - l[i])) + 1).max(j + 1);
+            let max = l.lower_bound(&(l[i] + l[j]));
+            ans += max.saturating_sub(min);
+        }
+    }
+    println!("{}", ans);
 }
