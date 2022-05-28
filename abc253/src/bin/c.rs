a20f65ebc8c70128fe9480e99414efc089f264e1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-28 22:56:04 +0900

    add abc253_c

--- abc253/src/bin/c.rs
+++ abc253/src/bin/c.rs
@@ -1,8 +1,31 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        q:usize,
     }
-    println!("{}", n);
+    let mut map = BTreeMap::new();
+    for _ in 0..q {
+        input! {query:u8}
+        if query == 1 {
+            input! {x:usize}
+            *map.entry(x).or_insert(0usize) += 1;
+        } else if query == 2 {
+            input! {x:usize,c:usize}
+            if let Some(before) = map.get(&x) {
+                let after = before.saturating_sub(c);
+                if after == 0 {
+                    map.remove(&x);
+                } else {
+                    map.insert(x, after);
+                }
+            }
+        } else {
+            let min = *map.iter().next().unwrap().0;
+            let max = *map.iter().last().unwrap().0;
+            println!("{}", max - min);
+        }
+    }
 }
