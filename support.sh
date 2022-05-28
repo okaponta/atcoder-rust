7ebd84e07e18be41ea31f65e648e8d02befa19f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-28 19:21:43 +0900

    add command for commentary page for qiita

--- support.sh
+++ support.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage at ./abcXXX, execute 'source ../support.sh'
+# Usage: at ./abcXXX, execute 'source ../support.sh'
 export CONTEST=`basename $PWD`
 
 alias join="git add .&&git commit -m 'join $CONTEST'"
@@ -29,4 +29,27 @@ function problem(){
   command open https://atcoder.jp/contests/$1/tasks/$1_d
   command open https://atcoder.jp/contests/$1/tasks/$1_e
   command open https://atcoder.jp/contests/$1/tasks/$1_f
+}
+
+function qiita(){
+  command touch commentary.md
+  command echo "[${CONTEST}](https://atcoder.jp/contests/${CONTEST})お疲れ様でした！" >> ./commentary.md
+  command echo "早速解説です！！(A-Dまで)" >> ./commentary.md
+  command echo "" >> ./commentary.md
+  command echo "質問、感想はこの記事のコメントでも、[twitter](https://twitter.com/okaponta_)でも、大歓迎です！:muscle:" >> ./commentary.md
+  command echo "" >> ./commentary.md
+  p_lower=(a b c d e f)
+  p_upper=(A B C D E F)
+  for i in $(seq 1 6)
+  do
+    command echo "## [問題${p_upper[i]}](https://atcoder.jp/contests/${CONTEST}/tasks/${CONTEST}_${p_lower[i]})" >> ./commentary.md
+    command echo "" >> ./commentary.md
+    command echo "### 考察" >> ./commentary.md
+    command echo "" >> ./commentary.md
+    command echo "### 解答" >> ./commentary.md
+    command echo "" >> ./commentary.md
+    command echo '```Rust' >> ./commentary.md
+    command echo '```' >> ./commentary.md
+    command echo "" >> ./commentary.md
+  done
 }
