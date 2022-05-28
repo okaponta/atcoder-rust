c794e8ddd47fc727341e20e37b663ca2a4af3659
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-28 22:56:02 +0900

    add abc253_b

--- abc253/src/bin/b.rs
+++ abc253/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    let mut koma = vec![];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == 'o' {
+                koma.push((i as i32, j as i32));
+            }
+        }
+    }
+    let ans = (koma[0].0 - koma[1].0).abs() + (koma[0].1 - koma[1].1).abs();
+    println!("{}", ans);
 }
