651278ca2b6901f2d8733ae7e26d8bf1b353c72d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-28 22:56:05 +0900

    add abc253_d

--- abc253/src/bin/d.rs
+++ abc253/src/bin/d.rs
@@ -2,7 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:i64,
+        a:i64,
+        b:i64,
     }
-    println!("{}", n);
+    let all = tousa_sum(n, 1);
+    let asum = tousa_sum(n, a);
+    let bsum = tousa_sum(n, b);
+    let absum = tousa_sum(n, lcm(a, b));
+    println!("{}", all - asum - bsum + absum);
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
+}
+
+fn tousa_sum(max: i64, c: i64) -> i64 {
+    let n = max / c;
+    (c + n * c) * n / 2
 }
