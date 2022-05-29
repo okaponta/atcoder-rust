f9510a85718f4b17eb9955d2eef9c932a892cac9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-05-29 09:41:07 +0900

    add abc253_g

--- abc253/src/bin/g.rs
+++ abc253/src/bin/g.rs
@@ -1,8 +1,41 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        l:Usize1,
+        r:Usize1,
     }
-    println!("{}", n);
+    let mut dict = vec![0; n];
+    for i in 1..n {
+        dict[i] = dict[i - 1] + n - i;
+    }
+    let from = dict.upper_bound(&l) - 1;
+    let to = dict.upper_bound(&r) - 1;
+    let mut ans = (1..=n).collect_vec();
+
+    let start = l + from + 1 - dict[from];
+    let end = r + to + 1 - dict[to];
+
+    if from == to {
+        loopswap(&mut ans, from, start, end);
+        println!("{}", ans.iter().join(" "));
+        return;
+    }
+
+    loopswap(&mut ans, from, start, n - 1);
+
+    ans[from + 1..].rotate_right(to - from - 1);
+    ans[from + 1..to].reverse();
+
+    loopswap(&mut ans, to, to + 1, end);
+    println!("{}", ans.iter().join(" "));
+}
+
+fn loopswap(ans: &mut Vec<usize>, first: usize, start: usize, end: usize) {
+    for i in start..=end {
+        ans.swap(first, i);
+    }
 }
