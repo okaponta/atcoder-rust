0d50e65761ef36d12c0984d5cb31eaebc98a563c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-02 22:53:10 +0900

    add abc252_c

--- abc252/src/bin/c.rs
+++ abc252/src/bin/c.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use std::vec;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut indexes = vec![vec![0; n]; 10];
+    for i in 0..n {
+        for j in 0..10 {
+            let num = s[i][j].to_digit(10).unwrap() as usize;
+            indexes[num][i] = j;
+        }
+    }
+
+    let mut min = 1 << 60;
+    for i in 0..10 {
+        let mut ans = vec![0; 10];
+        for j in 0..n {
+            ans[indexes[i][j]] += 1;
+        }
+        let mut temp = 0;
+        for j in 0..10 {
+            temp = temp.max(ans[j] * 10 + j);
+        }
+        min = min.min(temp - 10);
+    }
+    println!("{}", min);
 }
