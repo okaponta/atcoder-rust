07e5fe14f0405029ec99258728080451a04cf965
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-02 23:28:08 +0900

    add abc252_d

--- abc252/src/bin/d.rs
+++ abc252/src/bin/d.rs
@@ -1,8 +1,23 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for ai in a {
+        *map.entry(ai).or_insert(0) += 1;
+    }
+    let mut dp = vec![0i64; 3];
+    for val in map.values() {
+        let mut next = dp.clone();
+        next[0] += val;
+        next[1] += dp[0] * val;
+        next[2] += dp[1] * val;
+        dp = next;
+    }
+    println!("{}", dp[2]);
 }
