fb25b9d66e891238cdd63c45656c12655fec58f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-02 22:32:16 +0900

    add abc252_b

--- abc252/src/bin/b.rs
+++ abc252/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
+        b:[Usize1;k],
     }
-    println!("{}", n);
+    let max = *a.iter().max().unwrap();
+    let mut set = HashSet::new();
+    for i in 0..n {
+        if a[i] == max {
+            set.insert(i);
+        }
+    }
+    let is_ok = b.iter().filter(|&i| set.contains(i)).count() > 0;
+    println!("{}", if is_ok { "Yes" } else { "No" });
 }
