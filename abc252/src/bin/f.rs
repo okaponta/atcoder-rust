5fe00175659ce96242544a20b4e6ac51539e1438
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-04 01:11:45 +0900

    add abc252_f

--- abc252/src/bin/f.rs
+++ abc252/src/bin/f.rs
@@ -1,8 +1,26 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        l:i64,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    let sum = a.iter().sum::<i64>();
+    for ai in a {
+        heap.push(-ai);
+    }
+    if sum != l {
+        heap.push(sum - l);
+    }
+    let mut ans = 0;
+    while heap.len() > 1 {
+        let join = heap.pop().unwrap() + heap.pop().unwrap();
+        ans -= join;
+        heap.push(join);
+    }
+    println!("{}", ans);
 }
