1125c5d29678308d9080e82059505f43abb10dba
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-04 11:56:10 +0900

    update support.sh

--- support.sh
+++ support.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Usage: at ./abcXXX, execute 'source ../support.sh'
-export CONTEST=`basename $PWD`
+export CONTEST=$(basename $PWD)
 
 alias join="git add .&&git commit -m 'join $CONTEST'"
+
+source ~/repos/rust-kyopro-type-converter/convert.sh
 
 function test() {
   command cargo compete test $1
@@ -22,7 +24,7 @@ function commit() {
   command git commit -m "add ${CONTEST}_$1"
 }
 
-function problem(){
+function problem() {
   command open https://atcoder.jp/contests/$1/tasks/$1_a
   command open https://atcoder.jp/contests/$1/tasks/$1_b
   command open https://atcoder.jp/contests/$1/tasks/$1_c
@@ -31,25 +33,24 @@ function problem(){
   command open https://atcoder.jp/contests/$1/tasks/$1_f
 }
 
-function qiita(){
+function qiita() {
   command touch commentary.md
-  command echo "[${CONTEST}](https://atcoder.jp/contests/${CONTEST})お疲れ様でした！" >> ./commentary.md
-  command echo "早速解説です！！(A-Fまで)" >> ./commentary.md
-  command echo "" >> ./commentary.md
-  command echo "質問、感想はこの記事のコメントでも、[twitter](https://twitter.com/okaponta_)でも、大歓迎です！:muscle:" >> ./commentary.md
-  command echo "" >> ./commentary.md
+  command echo "[${CONTEST}](https://atcoder.jp/contests/${CONTEST})お疲れ様でした！" >>./commentary.md
+  command echo "早速解説です！！(A-Fまで)" >>./commentary.md
+  command echo "" >>./commentary.md
+  command echo "質問、感想はこの記事のコメントでも、[twitter](https://twitter.com/okaponta_)でも、大歓迎です！:muscle:" >>./commentary.md
+  command echo "" >>./commentary.md
   p_lower=(a b c d e f)
   p_upper=(A B C D E F)
-  for i in $(seq 1 6)
-  do
-    command echo "## [問題${p_upper[i]}](https://atcoder.jp/contests/${CONTEST}/tasks/${CONTEST}_${p_lower[i]})" >> ./commentary.md
-    command echo "" >> ./commentary.md
-    command echo "### 考察" >> ./commentary.md
-    command echo "" >> ./commentary.md
-    command echo "### 解答" >> ./commentary.md
-    command echo "" >> ./commentary.md
-    command echo '```Rust' >> ./commentary.md
-    command echo '```' >> ./commentary.md
-    command echo "" >> ./commentary.md
+  for i in $(seq 1 6); do
+    command echo "## [問題${p_upper[i]}](https://atcoder.jp/contests/${CONTEST}/tasks/${CONTEST}_${p_lower[i]})" >>./commentary.md
+    command echo "" >>./commentary.md
+    command echo "### 考察" >>./commentary.md
+    command echo "" >>./commentary.md
+    command echo "### 解答" >>./commentary.md
+    command echo "" >>./commentary.md
+    command echo '```Rust' >>./commentary.md
+    command echo '```' >>./commentary.md
+    command echo "" >>./commentary.md
   done
 }
