82b2fc6b9d222f9b6d460eb2c9d3239a2a16b404
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-06 00:38:36 +0900

    add abc141_b

--- abc141/src/bin/b.rs
+++ abc141/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    for i in 0..s.len() {
+        if i % 2 == 0 && s[i] == 'L' {
+            println!("No");
+            return;
+        }
+        if i % 2 == 1 && s[i] == 'R' {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
