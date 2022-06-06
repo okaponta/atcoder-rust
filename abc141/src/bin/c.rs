28568e34e8feea1cb6de37bc7f74df13d03d9063
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-07 00:34:32 +0900

    add abc141_c

--- abc141/src/bin/c.rs
+++ abc141/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:i64,
+        q:usize,
+        a:[Usize1;q],
     }
-    println!("{}", n);
+    let mut points = vec![k - q as i64; n];
+    for ai in a {
+        points[ai] += 1;
+    }
+    for p in points {
+        println!("{}", if 0 < p { "Yes" } else { "No" });
+    }
 }
