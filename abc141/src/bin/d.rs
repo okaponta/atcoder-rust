66cdcd9ee4b8685c317df2b6b16f99f0633f7f91
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-08 23:18:34 +0900

    add abc141_d

--- abc141/src/bin/d.rs
+++ abc141/src/bin/d.rs
@@ -3,6 +3,46 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:i64,
+        mut a:[i64;n],
     }
-    println!("{}", n);
+    let mut lower = -1;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if !is_ok(med, m, n, &a) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    let mut count = 0;
+    for i in 0..n {
+        while upper < a[i] {
+            a[i] >>= 1;
+            count += 1;
+        }
+    }
+    a.sort();
+    a.reverse();
+    let mut sum = 0;
+    for i in 0..n {
+        if i < (m - count) as usize {
+            a[i] >>= 1;
+        }
+        sum += a[i];
+    }
+    println!("{}", sum);
+}
+
+fn is_ok(med: i64, m: i64, n: usize, a: &Vec<i64>) -> bool {
+    let mut count = 0;
+    for i in 0..n {
+        let mut j = 0;
+        while med < a[i] >> j {
+            j += 1;
+        }
+        count += j;
+    }
+    count <= m
 }
