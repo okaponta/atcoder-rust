89297cc3464c3b3ca745adee817d045c3ef91638
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-09 22:21:05 +0900

    add abc142_d

--- abc142/src/bin/d.rs
+++ abc142/src/bin/d.rs
@@ -1,8 +1,35 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i64,b:i64,
     }
-    println!("{}", n);
+    println!("{}", factorize(gcd(a, b)).len() + 1);
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
+}
+
+fn factorize(mut n: i64) -> Vec<(i64, i64)> {
+    let mut res = vec![];
+    for i in 2..=n.sqrt() {
+        if n % i != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % i == 0 {
+            ex += 1;
+            n /= i;
+        }
+        res.push((i, ex));
+    }
+    if n != 1 {
+        res.push((n, 1));
+    }
+    res
 }
