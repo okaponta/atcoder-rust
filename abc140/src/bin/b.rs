4a2088728d78d8fce12c8a1c1759fd81401ac606
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-11 00:36:28 +0900

    add abc140_b

--- abc140/src/bin/b.rs
+++ abc140/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n],
+        b:[usize;n],
+        c:[usize;n-1],
     }
-    println!("{}", n);
+    let mut ans = b[a[0]];
+    for i in 1..n {
+        ans += b[a[i]];
+        if a[i - 1] + 1 == a[i] {
+            ans += c[a[i - 1]];
+        }
+    }
+    println!("{}", ans);
 }
