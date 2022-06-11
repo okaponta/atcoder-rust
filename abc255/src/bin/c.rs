cfde095e92cf7135043bf0603ad16311aa2755dd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-11 22:52:26 +0900

    add abc255_c

--- abc255/src/bin/c.rs
+++ abc255/src/bin/c.rs
@@ -2,7 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        x:i64,
+        a:i64,
+        d:i64,
+        n:i64,
     }
-    println!("{}", n);
+    if d == 0 {
+        println!("{}", (x - a).abs());
+        return;
+    }
+    let end = a + (n - 1) * d;
+    let min = a.min(end);
+    let max = a.max(end);
+    let c = d.abs();
+    if x < min {
+        println!("{}", min - x);
+        return;
+    }
+    if max < x {
+        println!("{}", x - max);
+        return;
+    }
+    let t = x - min;
+    let a1 = t % c;
+    let a2 = c - a1;
+    println!("{}", a1.min(a2));
 }
