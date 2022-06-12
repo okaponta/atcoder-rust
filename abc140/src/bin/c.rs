a3de6b42cbd3837e1ed0fe2a2bb47539799d7e35
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-12 19:14:49 +0900

    add abc140_c

--- abc140/src/bin/c.rs
+++ abc140/src/bin/c.rs
@@ -3,6 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        b:[usize;n-1],
     }
-    println!("{}", n);
+    let mut ans = b[0] + b[n - 2];
+    for i in 1..n - 1 {
+        ans += b[i - 1].min(b[i]);
+    }
+    println!("{}", ans);
 }
