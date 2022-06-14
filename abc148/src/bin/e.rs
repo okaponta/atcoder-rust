1f172bdd550518c70d43bc4351d480b440a5b741
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-14 19:35:06 +0900

    add abc148_e

--- abc148/src/bin/e.rs
+++ abc148/src/bin/e.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:usize,
     }
-    println!("{}", n);
+    if n % 2 == 1 {
+        println!("0");
+        return;
+    }
+    let mut ans = 0;
+    n /= 2;
+    while 0 < n {
+        n /= 5;
+        ans += n;
+    }
+    println!("{}", ans);
 }
