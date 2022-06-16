48e3b4298ec13c2c04dfadf19fc91d95736e40e1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-16 19:35:52 +0900

    add abc254_e

--- abc254/src/bin/e.rs
+++ abc254/src/bin/e.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        n:usize,m:usize,
+        ab:[(usize,usize);m],
+        q:usize,
+        xk:[(usize,usize);q],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; n + 1];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+    for (x, k) in xk {
+        let mut vis = HashSet::new();
+        vis.insert(x);
+        dfs(&mut vis, x, k, &edges);
+        println!("{}", vis.iter().sum::<usize>());
+    }
+}
+
+fn dfs(vis: &mut HashSet<usize>, cur: usize, depth: usize, edges: &Vec<Vec<usize>>) {
+    if depth == 0 {
+        return;
+    }
+    for &next in &edges[cur] {
+        vis.insert(next);
+        dfs(vis, next, depth - 1, edges);
+    }
 }
