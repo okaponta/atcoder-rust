625bcb56128577090518c9f4d5e792be8fa7a159
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-16 19:12:36 +0900

    add abc254_c

--- abc254/src/bin/c.rs
+++ abc254/src/bin/c.rs
@@ -1,8 +1,27 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut sorted = a.clone();
+    sorted.sort();
+    for i in 0..k {
+        let mut map = HashMap::new();
+        let mut index = i;
+        while index < n {
+            *map.entry(a[index]).or_insert(0) += 1;
+            *map.entry(sorted[index]).or_insert(0) -= 1;
+            index += k;
+        }
+        if map.values().any(|v| v != &0) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
