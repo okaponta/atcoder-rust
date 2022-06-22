f0fbc11cc053162aa44013e59039141519d7186a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-22 23:37:11 +0900

    add abc140_d

--- abc140/src/bin/d.rs
+++ abc140/src/bin/d.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,k:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut prev = 'A';
+    let mut count = 0;
+    for c in s {
+        if c == prev {
+            count += 1;
+        }
+        prev = c;
+    }
+    let ans = (count + k * 2).min(n - 1);
+    println!("{}", ans);
 }
