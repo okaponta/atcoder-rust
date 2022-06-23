66748193785fe27682579d4d7f242fe8599a8248
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-24 00:40:07 +0900

    add abc256_e

--- abc256/src/bin/e.rs
+++ abc256/src/bin/e.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        x:[Usize1;n],
+        c:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut uf = UnionFind::new(n);
+    for i in 0..n {
+        if !uf.union(i, x[i]) {
+            let mut cost = c[i];
+            let mut tmp = i;
+            loop {
+                tmp = x[tmp];
+                cost = cost.min(c[tmp]);
+                if tmp == i {
+                    break;
+                }
+            }
+            ans += cost;
+        }
+    }
+    println!("{}", ans);
 }
