82f766a615cc3c78d86ef31b8033e7c916d88da7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-23 23:10:58 +0900

    add abc256_b

--- abc256/src/bin/b.rs
+++ abc256/src/bin/b.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut grid = vec![0; 4];
+    let mut idx = 0;
+    let mut ans = n.saturating_sub(4);
+    for i in n.saturating_sub(4)..n {
+        grid[idx] = a[i];
+        idx += 1;
+    }
+    let mut s = 0;
+    for i in (0..4).rev() {
+        s += grid[i];
+        if 3 < s {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
