95136b4a8b208e7d7d346b68052f8c6a048fd92e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-23 23:38:41 +0900

    add abc256_d

--- abc256/src/bin/d.rs
+++ abc256/src/bin/d.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        lr:[(usize,usize);n],
     }
-    println!("{}", n);
+    let max = 200_010;
+    let mut kukan = vec![0; max];
+    for (l, r) in lr {
+        kukan[l] += 1;
+        kukan[r] -= 1;
+    }
+    let mut s = 0;
+    let mut is_ans = false;
+    let mut l = 0;
+    for i in 0..max {
+        s += kukan[i];
+        if 0 < s && !is_ans {
+            l = i;
+            is_ans = !is_ans;
+        }
+        if 0 == s && is_ans {
+            println!("{} {}", l, i);
+            is_ans = !is_ans;
+        }
+    }
 }
