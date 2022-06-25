db72a267d801b5f4df8f3f446e234e51e7a5b0be
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-25 22:42:38 +0900

    add abc257_a

--- abc257/src/bin/a.rs
+++ abc257/src/bin/a.rs
@@ -3,6 +3,8 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
     }
-    println!("{}", n);
+    let idx = (x - 1) / n;
+    println!("{}", (b'A' + idx as u8) as char);
 }
