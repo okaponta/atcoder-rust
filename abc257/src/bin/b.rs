df83c9cb2fdcff9b907cca9c12fd1a9a22f55eb1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-25 22:42:39 +0900

    add abc257_b

--- abc257/src/bin/b.rs
+++ abc257/src/bin/b.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        q:usize,
+        mut a:[usize;k],
+        l:[Usize1;q],
     }
-    println!("{}", n);
+    for li in l {
+        let cur = a[li];
+        if li == k - 1 {
+            if cur == n {
+                continue;
+            }
+            a[li] += 1;
+        } else {
+            let next = a[li + 1];
+            if cur + 1 == next {
+                continue;
+            }
+            a[li] += 1;
+        }
+    }
+    println!("{}", a.iter().join(" "));
 }
