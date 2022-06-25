78624b229b2d00304e61ab6194281bcbf57d8dcd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-25 23:02:52 +0900

    add abc257_e

--- abc257/src/bin/e.rs
+++ abc257/src/bin/e.rs
@@ -2,7 +2,25 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:usize,
+        mut c:[usize;9],
     }
-    println!("{}", n);
+    let mn = *c.iter().min().unwrap();
+    c.insert(0, mn);
+    let len = n / mn;
+    n -= mn * len;
+    for i in 0..10 {
+        c[i] -= mn;
+    }
+    let mut ans = String::new();
+    for _ in 0..len {
+        for j in (1..10).rev() {
+            if c[j] <= n {
+                n -= c[j];
+                ans.push((b'0' + j as u8) as char);
+                break;
+            }
+        }
+    }
+    println!("{}", ans);
 }
