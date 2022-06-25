dcabb4ceba2e3a78a34d8f1a98ba9e5623810c93
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-25 23:45:02 +0900

    add abc257_d

--- abc257/src/bin/d.rs
+++ abc257/src/bin/d.rs
@@ -3,6 +3,46 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xyp:[(i64,i64,i64);n],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; n];
+    for i in 0..n {
+        for j in 0..n {
+            let dist = (xyp[i].0 - xyp[j].0).abs() + (xyp[i].1 - xyp[j].1).abs();
+            edges[i].push((j, (dist + xyp[i].2 - 1) / xyp[i].2));
+        }
+    }
+    let wf = WarshallFloyd::new(n, &edges);
+    let mut ans = 1 << 60;
+    for i in 0..n {
+        let mut temp = 0;
+        for j in 0..n {
+            temp = temp.max(wf.distance[i][j]);
+        }
+        ans = ans.min(temp);
+    }
+    println!("{}", ans);
+}
+
+pub struct WarshallFloyd {
+    distance: Vec<Vec<i64>>,
+}
+
+impl WarshallFloyd {
+    pub fn new(n: usize, edges: &Vec<Vec<(usize, i64)>>) -> Self {
+        let mut distance = vec![vec![1 << 60; n]; n];
+        for i in 0..n {
+            for &(j, c) in &edges[i] {
+                distance[i][j] = c;
+            }
+        }
+        for k in 0..n {
+            for i in 0..n {
+                for j in 0..n {
+                    distance[i][j] = distance[i][j].min(distance[i][k].max(distance[k][j]));
+                }
+            }
+        }
+        Self { distance }
+    }
 }
