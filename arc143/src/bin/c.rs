16ea843f2efce7976a9fa9d60677abd1d3c0b8d6
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-26 23:01:08 +0900

    add arc143_c

--- arc143/src/bin/c.rs
+++ arc143/src/bin/c.rs
@@ -2,7 +2,34 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,x:usize,y:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut flag = false;
+    let mut t_count = 0;
+    let mut a_count = 0;
+    for ai in a {
+        if x <= ai % (x + y) {
+            t_count += 1;
+            flag = true;
+        } else {
+            t_count -= 1;
+        }
+        if y <= ai % (x + y) {
+            a_count += 1;
+        } else {
+            a_count -= 1;
+        }
+    }
+    if a_count < t_count {
+        println!("First");
+        return;
+    }
+    if a_count == t_count {
+        if flag {
+            println!("First");
+            return;
+        }
+    }
+    println!("Second");
 }
