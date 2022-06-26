9ee01c09b96cb055ad9ade4859a04049c129eed2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-26 23:01:05 +0900

    add arc143_a

--- arc143/src/bin/a.rs
+++ arc143/src/bin/a.rs
@@ -2,7 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:[i64;3]
     }
-    println!("{}", n);
+    let mut count = 0;
+    n.sort();
+    loop {
+        let a = n[0];
+        let b = n[1];
+        let c = n[2];
+        let big = c - a;
+        let small = b - a;
+        let rem = big - small;
+        let subb = rem / 2;
+        let suba = rem - subb;
+        n = vec![a - suba, a - subb, a];
+        count += big;
+        if n[0] == n[1] && n[0] == n[2] {
+            break;
+        }
+    }
+    if n[0] < 0 {
+        println!("-1");
+    } else {
+        println!("{}", count + n[0]);
+    }
 }
