7c032d309d7e11956fdbb68149659ed5b049acda
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-27 00:41:52 +0900

    add abc184_d

--- abc184/src/bin/d.rs
+++ abc184/src/bin/d.rs
@@ -1,9 +1,35 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       p:[usize;3]
     }
-    println!("{}", a);
-    todo!();
+    let mut map = HashMap::new();
+    map.insert(p, 1.0);
+    let mut ans = 0.0;
+    let mut count = 0;
+    while !map.is_empty() {
+        let mut next = HashMap::new();
+        for (p, v) in map {
+            if p.iter().any(|e| e == &100) {
+                ans += count as f64 * v;
+                continue;
+            }
+            let sum = p[0] + p[1] + p[2];
+            let prob = p.iter().map(|i| *i as f64 / sum as f64).collect_vec();
+            for i in 0..3 {
+                if p[i] != 0 {
+                    let mut k = p.clone();
+                    k[i] += 1;
+                    *next.entry(k).or_insert(0.0) += v * prob[i];
+                }
+            }
+        }
+        map = next;
+        count += 1;
+    }
+    println!("{}", ans);
 }
