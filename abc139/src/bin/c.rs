90aadc5b76c4bd9a2109ef6b5d259ba8c1975210
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-01 22:24:46 +0900

    add abc139_c

--- abc139/src/bin/c.rs
+++ abc139/src/bin/c.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut h:[usize;n]
     }
-    println!("{}", n);
+    h.push(1 << 60);
+    let mut ans = 0;
+    let mut temp = 0;
+    for i in 1..=n {
+        if h[i - 1] >= h[i] {
+            temp += 1;
+        } else {
+            ans = ans.max(temp);
+            temp = 0;
+        }
+    }
+    println!("{}", ans);
 }
