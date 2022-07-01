82ef96e0699c0e981e53f66a289c676e8071871c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-02 08:37:37 +0900

    add abc157_d

--- abc157/src/bin/d.rs
+++ abc157/src/bin/d.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use itertools::Itertools;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        k:usize,
+        ab:[(Usize1,Usize1);m],
+        cd:[(Usize1,Usize1);k],
     }
-    println!("{}", n);
+    let mut ans = vec![0; n];
+    let mut uf = UnionFind::new(n);
+    for &(a, b) in &ab {
+        uf.union(a, b);
+    }
+    let mut count = vec![0; n];
+    for i in 0..n {
+        count[uf.find(i)] += 1;
+    }
+    for i in 0..n {
+        ans[i] = count[uf.find(i)] - 1;
+    }
+    for (a, b) in ab {
+        ans[a] -= 1;
+        ans[b] -= 1;
+    }
+    for (c, d) in cd {
+        if uf.find(c) == uf.find(d) {
+            ans[c] -= 1;
+            ans[d] -= 1;
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
