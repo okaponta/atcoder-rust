f8edf2625694657c78a06949990f4bba5216a4f9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-03 19:45:25 +0900

    add abc258_d

--- abc258/src/bin/d.rs
+++ abc258/src/bin/d.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut ans = 1 << 60;
+    let mut base = 0;
+    for i in 0..n {
+        let tmp = base + ab[i].0 + (ab[i].1) * (x - i);
+        ans = ans.min(tmp);
+        base += ab[i].0 + ab[i].1;
+    }
+    println!("{}", ans);
 }
