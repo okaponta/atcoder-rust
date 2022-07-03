0de3af8085bf1a11f8af4a811473c17d822ade9e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-03 19:19:00 +0900

    add abc258_b

--- abc258/src/bin/b.rs
+++ abc258/src/bin/b.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        a:[Chars;n],
     }
-    println!("{}", n);
+    let mut grid = vec![vec![0; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            grid[i][j] = a[i][j].to_digit(10).unwrap();
+        }
+    }
+    let dx = vec![0, 1, 1, 1, 0, -1, -1, -1];
+    let dy = vec![1, 1, 0, -1, -1, -1, 0, 1];
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..8 {
+                let mut cand = String::new();
+                cand.push(a[i][j]);
+                let mut x = i;
+                let mut y = j;
+                for _ in 1..n {
+                    let nextx = x as i32 + dx[k];
+                    let nexty = y as i32 + dy[k];
+                    x = nextx.rem_euclid(n as i32) as usize;
+                    y = nexty.rem_euclid(n as i32) as usize;
+                    cand.push(a[x][y]);
+                }
+                ans = ans.max(cand.parse::<i64>().unwrap());
+            }
+        }
+    }
+    println!("{}", ans);
 }
