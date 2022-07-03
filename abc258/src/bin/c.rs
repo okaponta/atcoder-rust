0691a0bc333a6f26f48c9590d971d446e75b5aa0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-03 19:39:59 +0900

    add abc258_c

--- abc258/src/bin/c.rs
+++ abc258/src/bin/c.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        s:Chars,
+        query:[(usize,usize);q],
     }
-    println!("{}", n);
+    let mut rot = 0;
+    for (t, x) in query {
+        if t == 1 {
+            rot += n - x;
+            rot %= n;
+        } else {
+            println!("{}", s[(rot + x - 1) % n]);
+        }
+    }
 }
