f1f87106e1d3a715afe757a7faaefefa3ac44110
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-07 21:21:21 +0900

    add abc138_b

--- abc138/src/bin/b.rs
+++ abc138/src/bin/b.rs
@@ -3,6 +3,8 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[f64;n],
     }
-    println!("{}", n);
+    let ans = 1.0 / a.iter().fold(0.0, |acc, x| acc + 1.0 / *x);
+    println!("{}", ans);
 }
