a5fd1d4e82f54a076ff121dc55e5c25bad309130
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-08 22:44:08 +0900

    add abc138_c

--- abc138/src/bin/c.rs
+++ abc138/src/bin/c.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut v:[usize;n],
     }
-    println!("{}", n);
+    v.sort();
+    let mut ans = v[0] as f64;
+    for i in 1..n {
+        ans = (ans + v[i] as f64) / 2.0;
+    }
+    println!("{}", ans);
 }
