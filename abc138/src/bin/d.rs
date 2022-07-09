d460cf0d444274ad8a2836f2b766b9174733bb3f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-09 12:28:20 +0900

    add abc138_d

--- abc138/src/bin/d.rs
+++ abc138/src/bin/d.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        ab:[(Usize1,Usize1);n-1],
+        px:[(Usize1,usize);q],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+    let mut count = vec![0; n];
+    let mut ans = vec![0; n];
+    for (p, x) in px {
+        count[p] += x;
+    }
+    dfs(0, 0, &count, &edges, &mut ans);
+    println!("{}", ans.iter().join(" "));
+}
+
+fn dfs(cur: usize, prev: usize, count: &Vec<usize>, edges: &Vec<Vec<usize>>, ans: &mut Vec<usize>) {
+    ans[cur] = ans[prev] + count[cur];
+    for &next in &edges[cur] {
+        if next == prev {
+            continue;
+        }
+        dfs(next, cur, count, edges, ans);
+    }
 }
