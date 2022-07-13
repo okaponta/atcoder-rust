b816c9921cd441261b5500b32db1344fea28472e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-13 16:59:06 +0900

    add abc259_b

--- abc259/src/bin/b.rs
+++ abc259/src/bin/b.rs
@@ -1,8 +1,12 @@
+use nalgebra::{Rotation2, Vector2};
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        (a,b,d):(f64,f64,f64)
     }
-    println!("{}", n);
+    let p = Vector2::new(a, b);
+    let rot = Rotation2::new(d * 2.0 * std::f64::consts::PI / 360.0);
+    let res = rot * p;
+    println!("{} {}", res[0], res[1]);
 }
