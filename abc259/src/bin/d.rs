cef9a9bdd200625bdcdc8c600abcd0b3d3a942a3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-13 19:01:29 +0900

    add abc259_d

--- abc259/src/bin/d.rs
+++ abc259/src/bin/d.rs
@@ -11,46 +11,37 @@ fn main() {
     let mut uf = UnionFind::new(n);
     for i in 0..n {
         for j in i..n {
-            let d = dist(xyr[i].0, xyr[i].1, xyr[j].0, xyr[j].1);
-            if is_inside(xyr[i].0, xyr[i].1, xyr[i].2, xyr[j].0, xyr[j].1) {
-                if pow2(xyr[i].2 - xyr[j].2) <= d {
-                    uf.union(i, j);
-                }
-            } else {
-                if d <= pow2(xyr[i].2 + xyr[j].2) {
-                    uf.union(i, j);
-                }
+            if is_cross_circumference(xyr[i].0, xyr[i].1, xyr[i].2, xyr[j].0, xyr[j].1, xyr[j].2) {
+                uf.union(i, j);
             }
         }
     }
     let mut start = 0;
-    for i in 0..n {
-        let d = dist(xyr[i].0, xyr[i].1, s.0, s.1);
-        if d == pow2(xyr[i].2) {
-            start = i;
-            break;
-        }
-    }
     let mut end = 0;
     for i in 0..n {
-        let d = dist(xyr[i].0, xyr[i].1, t.0, t.1);
-        if d == pow2(xyr[i].2) {
+        let sd = dist(xyr[i].0, xyr[i].1, s.0, s.1);
+        if sd == (xyr[i].2).pow(2) {
+            start = i;
+        }
+        let td = dist(xyr[i].0, xyr[i].1, t.0, t.1);
+        if td == (xyr[i].2).pow(2) {
             end = i;
-            break;
         }
     }
     println!("{}", if uf.equiv(start, end) { "Yes" } else { "No" });
 }
 
-fn dist(x1: i64, y1: i64, x2: i64, y2: i64) -> i64 {
-    pow2(x1 - x2) + pow2(y1 - y2)
-}
-
-fn pow2(i: i64) -> i64 {
-    i * i
-}
-
-fn is_inside(x1: i64, y1: i64, r: i64, x2: i64, y2: i64) -> bool {
+fn is_cross_circumference(x1: i64, y1: i64, r1: i64, x2: i64, y2: i64, r2: i64) -> bool {
     let d = dist(x1, y1, x2, y2);
-    d < pow2(r)
+    if (r1 + r2).pow(2) < d {
+        return false;
+    }
+    if d < (r1 - r2).pow(2) {
+        return false;
+    }
+    true
+}
+
+fn dist(x1: i64, y1: i64, x2: i64, y2: i64) -> i64 {
+    (x1 - x2).pow(2) + (y1 - y2).pow(2)
 }
