56991771bcff0c2f25303257e2827765adc4fa98
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-14 21:14:36 +0900

    add abc136_b

--- abc136/src/bin/b.rs
+++ abc136/src/bin/b.rs
@@ -4,5 +4,16 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for mut i in 1..=n {
+        let mut count = 1;
+        while i / 10 != 0 {
+            i /= 10;
+            count += 1;
+        }
+        if count % 2 == 1 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
