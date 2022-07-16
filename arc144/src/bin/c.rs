d6483a500492e1b20a75222851586735db9279fd
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-16 23:42:01 +0900

    add arc144_c

--- arc144/src/bin/c.rs
+++ arc144/src/bin/c.rs
@@ -1,8 +1,35 @@
+use std::collections::BTreeSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:i64,
+        k:i64,
     }
-    println!("{}", n);
+    if k > n / 2 {
+        println!("-1");
+        return;
+    }
+    let mut ans = vec![];
+    let mut remain: BTreeSet<i64> = (1..=n).into_iter().collect();
+    for i in 1..=n {
+        let small = i - k;
+        let big = i + k;
+        if remain.contains(&big) && n - k < big {
+            ans.push(big);
+            remain.remove(&big);
+        } else {
+            if let Some(&x) = remain.range(..=small).next() {
+                ans.push(x);
+                remain.remove(&x);
+            } else {
+                let &x = remain.range((i + k)..).next().unwrap();
+                ans.push(x);
+                remain.remove(&x);
+            }
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
