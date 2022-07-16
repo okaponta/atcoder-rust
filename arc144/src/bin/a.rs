983fc1536a9a52d77e27b481a860acd5d8d00dd9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-16 23:22:51 +0900

    add arc144_a

--- arc144/src/bin/a.rs
+++ arc144/src/bin/a.rs
@@ -4,5 +4,13 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    println!("{}", 2 * n);
+    let mut ans = (n % 4).to_string();
+    if n % 4 == 0 {
+        ans = "".to_string();
+    }
+    for _ in 0..n / 4 {
+        ans = ans + "4";
+    }
+    println!("{}", ans);
 }
