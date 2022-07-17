c26672345b246c47eb58b80d89155cb1a938c60c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-17 22:54:56 +0900

    add abc260_d

--- abc260/src/bin/d.rs
+++ abc260/src/bin/d.rs
@@ -1,8 +1,50 @@
-use proconio::input;
+use std::collections::{BTreeSet, HashMap};
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        n:usize,k:usize,
+        p:[i64;n],
     }
-    println!("{}", n);
+    if k == 1 {
+        let mut ans = vec![0; n];
+        for i in 0..n {
+            ans[p[i] as usize - 1] = i + 1;
+        }
+        for pi in ans {
+            println!("{}", pi);
+        }
+        return;
+    }
+    let mut ans = vec![-1; n];
+    let mut set = BTreeSet::new();
+    let mut map: HashMap<i64, Vec<i64>> = HashMap::new();
+    for i in 0..n {
+        if let Some(&(x, num)) = set.range((p[i], 0)..).next() {
+            if num == k - 1 {
+                // 食べる
+                set.remove(&(x, num));
+                ans[p[i] as usize - 1] = i as i64 + 1;
+                let v = map.remove(&x).unwrap_or(Vec::new());
+                for vi in v {
+                    ans[vi as usize - 1] = i as i64 + 1;
+                }
+            } else {
+                //重ねる
+                set.remove(&(x, num));
+                set.insert((p[i], num + 1));
+                let mut val = map.remove(&x).unwrap_or(Vec::new());
+                val.push(p[i]);
+                map.insert(p[i], val);
+            }
+        } else {
+            set.insert((p[i], 1));
+            map.insert(p[i], vec![p[i]]);
+        }
+    }
+    for pi in ans {
+        println!("{}", pi);
+    }
 }
