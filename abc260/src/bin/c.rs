29e61f9d105adab8e8bf77420e84e2853266c4df
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-17 22:54:55 +0900

    add abc260_c

--- abc260/src/bin/c.rs
+++ abc260/src/bin/c.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,x:usize,y:usize,
     }
-    println!("{}", n);
+    let mut red = vec![0; 12];
+    let mut blue = vec![0; 12];
+    blue[0] = 1;
+    for i in 0..11 {
+        let b = red[i] + blue[i] * y;
+        let r = b * x + red[i];
+        red[i + 1] = r;
+        blue[i + 1] = b;
+    }
+    println!("{}", red[n - 1]);
 }
