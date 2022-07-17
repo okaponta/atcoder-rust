ed80933660819f298744de28928a28baf026290a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-17 22:54:52 +0900

    add abc260_a

--- abc260/src/bin/a.rs
+++ abc260/src/bin/a.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    if s[0] != s[1] && s[0] != s[2] {
+        println!("{}", s[0]);
+        return;
+    }
+    if s[1] != s[0] && s[1] != s[2] {
+        println!("{}", s[1]);
+        return;
+    }
+    if s[2] != s[0] && s[2] != s[1] {
+        println!("{}", s[2]);
+        return;
+    }
+    println!("-1");
 }
