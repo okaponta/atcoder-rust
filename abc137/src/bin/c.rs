a6590333a23ca2401e16d86a84409f662c31d318
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-22 22:30:51 +0900

    add abc137_c

--- abc137/src/bin/c.rs
+++ abc137/src/bin/c.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for mut si in s {
+        si.sort();
+        *map.entry(si.iter().collect::<String>()).or_insert(0i64) += 1;
+    }
+    let ans = map.values().fold(0, |s, v| s + v * (v - 1) / 2);
+    println!("{}", ans);
 }
