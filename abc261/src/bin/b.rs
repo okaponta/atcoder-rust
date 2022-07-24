4c80812419b80c8d9fec380ac68c73b309b5f938
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-24 12:15:09 +0900

    add abc261_b

--- abc261/src/bin/b.rs
+++ abc261/src/bin/b.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        a:[Chars;n],
     }
-    println!("{}", n);
+    for i in 0..n {
+        for j in i + 1..n {
+            if a[i][j] == 'W' && a[j][i] != 'L' {
+                println!("incorrect");
+                return;
+            }
+            if a[i][j] == 'D' && a[j][i] != 'D' {
+                println!("incorrect");
+                return;
+            }
+            if a[i][j] == 'L' && a[j][i] != 'W' {
+                println!("incorrect");
+                return;
+            }
+        }
+    }
+    println!("correct");
 }
