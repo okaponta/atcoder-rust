afb2550d0e9175a8796109e67baa71ca373069ca
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-24 12:32:35 +0900

    add abc261_c

--- abc261/src/bin/c.rs
+++ abc261/src/bin/c.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        s:[String;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for si in s {
+        if let Some(num) = map.get(&si) {
+            println!("{}({})", si, num);
+        } else {
+            println!("{}", si);
+        }
+        *map.entry(si).or_insert(0) += 1;
+    }
 }
