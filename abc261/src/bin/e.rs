0b37cdf8fb3abb89d278c2d7f2b91c539f624360
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-24 13:15:27 +0900

    add abc261_e

--- abc261/src/bin/e.rs
+++ abc261/src/bin/e.rs
@@ -1,8 +1,38 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        c:usize,
+        ta:[(usize,usize);n],
     }
-    println!("{}", n);
+    let n = 30;
+    let mut ans = c;
+    let mut zero = 0;
+    let mut one = (1 << 30) - 1;
+    for (t, a) in ta {
+        if t == 1 {
+            zero &= a;
+            one &= a;
+        }
+        if t == 2 {
+            zero |= a;
+            one |= a;
+        }
+        if t == 3 {
+            zero ^= a;
+            one ^= a;
+        }
+        let mut next = 0;
+        for i in 0..n {
+            if ans >> i & 1 == 0 {
+                next += (zero >> i & 1) * (1 << i);
+            } else {
+                next += (one >> i & 1) * (1 << i);
+            }
+        }
+        println!("{}", next);
+        ans = next;
+    }
 }
