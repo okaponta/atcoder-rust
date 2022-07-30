3dfb5b7daeaecc93a5bcb7daed43d9a643964e42
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-30 21:51:54 +0900

    add abc135_b

--- abc135/src/bin/b.rs
+++ abc135/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        p:[Usize1;n],
     }
-    println!("{}", n);
+    let mut count = 0;
+    for i in 0..n {
+        if i != p[i] {
+            count += 1;
+        }
+    }
+    println!("{}", if count <= 2 { "YES" } else { "NO" });
 }
