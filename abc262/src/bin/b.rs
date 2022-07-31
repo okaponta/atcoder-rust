c07da31f6596136dd72228f5dc6038ae417ae93f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-31 23:28:01 +0900

    add abc262_b

--- abc262/src/bin/b.rs
+++ abc262/src/bin/b.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 0..n {
+        map.insert(i, HashSet::new());
+    }
+    for (u, v) in uv {
+        (*map.entry(u).or_insert(HashSet::new())).insert(v);
+        (*map.entry(v).or_insert(HashSet::new())).insert(u);
+    }
+    let mut count = 0;
+    for a in 0..n {
+        for b in a..n {
+            for c in b..n {
+                if map[&a].contains(&b) && map[&b].contains(&c) && map[&c].contains(&a) {
+                    count += 1;
+                }
+            }
+        }
+    }
+    println!("{}", count);
 }
