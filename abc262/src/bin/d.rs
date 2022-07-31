6c8d279e0621487f90cb657bbfc36e12f9e82848
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-31 23:28:05 +0900

    add abc262_d

--- abc262/src/bin/d.rs
+++ abc262/src/bin/d.rs
@@ -1,8 +1,31 @@
 use proconio::input;
 
+const MOD: usize = 998244353;
+
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = n;
+    for ni in 2..=n {
+        // dp[i][j] = 選んだのがiこであまりがj
+        let mut dp = vec![vec![0; ni]; ni];
+        dp[0][0] = 1;
+        for k in 0..n {
+            let num = a[k] % ni;
+            for i in (0..ni).rev() {
+                if i == ni - 1 {
+                    ans += dp[i][(ni - num) % ni];
+                    ans %= MOD;
+                } else {
+                    for j in 0..ni {
+                        dp[i + 1][(j + num) % ni] += dp[i][j];
+                        dp[i + 1][(j + num) % ni] %= MOD;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
