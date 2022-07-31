ff70b248d3191c0e793965550da4bba892769d9f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-31 23:28:03 +0900

    add abc262_c

--- abc262/src/bin/c.rs
+++ abc262/src/bin/c.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut ans = 0i64;
+    let mut count = 0;
+    for i in 0..n {
+        if i == a[i] {
+            count += 1;
+        }
+        if i < a[i] {
+            if a[a[i]] == i {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans + count * (count - 1) / 2);
 }
