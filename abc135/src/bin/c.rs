46fd4ac6b891fdc6c04ddce24a0f41bf90bb0aca
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-03 21:32:40 +0900

    add abc135_c

--- abc135/src/bin/c.rs
+++ abc135/src/bin/c.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n+1],
+        mut b:[usize;n],
     }
-    println!("{}", n);
+    let bef = a.iter().sum::<usize>();
+    for i in 0..n {
+        let ai_bef = a[i];
+        a[i] = a[i].saturating_sub(b[i]);
+        b[i] -= ai_bef - a[i];
+        a[i + 1] = a[i + 1].saturating_sub(b[i]);
+    }
+    println!("{}", bef - a.iter().sum::<usize>());
 }
