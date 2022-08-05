620aa60593c56510922e1180ff16f5500fda3299
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-05 22:51:13 +0900

    add abc134_b

--- abc134/src/bin/b.rs
+++ abc134/src/bin/b.rs
@@ -3,6 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        d:usize,
     }
-    println!("{}", n);
+    println!("{}", div_ceil(n, 2 * d + 1));
+}
+
+fn div_ceil(a: usize, b: usize) -> usize {
+    (a + b - 1) / b
 }
