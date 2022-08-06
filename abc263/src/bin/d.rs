a01a1ef043e914b6050b7f74194de8a9961f2bce
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-07 00:08:30 +0900

    add abc263_d

--- abc263/src/bin/d.rs
+++ abc263/src/bin/d.rs
@@ -7,87 +7,16 @@ fn main() {
         r:i64,
         a:[i64;n],
     }
-    let mut s = vec![a[0]];
-    for i in 1..n {
-        s.push(s[i - 1] + a[i]);
-    }
-    let mut diff = vec![0; n];
-    let mut seg = SegmentTree::new(n, 0, |a, b| a.max(b));
+    let mut sl = vec![0; n + 1];
     for i in 0..n {
-        diff[i] = s[i] - l * (i + 1) as i64;
-        seg.update(i, diff[i]);
+        sl[i + 1] = (sl[i] + a[i]).min((i + 1) as i64 * l);
     }
-    let mut ans = r * n as i64;
-    for i in 1..=n {
-        // i以降をrにする
-        let mut temp = (n - i) as i64 * r;
-        temp += s[i - 1];
-        temp -= seg.query(0..i).max(0);
-        ans = ans.min(temp);
+    let mut sr = vec![0; n + 1];
+    for i in (0..n).rev() {
+        sr[i] = (sr[i + 1] + a[i]).min((n - i) as i64 * r);
     }
-    println!("{}", ans);
-}
-
-pub struct SegmentTree<T, F> {
-    seg: Vec<T>,
-    n: usize,
-    f: F,
-    initial_value: T,
-}
-
-impl<T: Copy, F> SegmentTree<T, F>
-where
-    F: Fn(T, T) -> T,
-{
-    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
-        let m = size.next_power_of_two();
-        SegmentTree {
-            seg: vec![initial_value; m * 2],
-            n: m,
-            f,
-            initial_value,
-        }
-    }
-
-    pub fn update(&mut self, mut k: usize, value: T) {
-        k += self.n - 1;
-        self.seg[k] = value;
-        while k > 0 {
-            k = (k - 1) >> 1;
-            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
-        }
-    }
-
-    pub fn update_tmp(&mut self, k: usize, value: T) {
-        self.seg[k + self.n - 1] = value;
-    }
-
-    pub fn update_all(&mut self) {
-        for i in (0..self.n - 1).rev() {
-            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
-        }
-    }
-
-    // 半開区間なので注意
-    pub fn query(&self, range: std::ops::Range<usize>) -> T {
-        self.query_range(range, 0, 0..self.n)
-    }
-
-    fn query_range(
-        &self,
-        range: std::ops::Range<usize>,
-        k: usize,
-        seg_range: std::ops::Range<usize>,
-    ) -> T {
-        if seg_range.end <= range.start || range.end <= seg_range.start {
-            self.initial_value
-        } else if range.start <= seg_range.start && seg_range.end <= range.end {
-            self.seg[k]
-        } else {
-            let mid = (seg_range.start + seg_range.end) >> 1;
-            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
-            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
-            (self.f)(x, y)
-        }
-    }
+    println!(
+        "{}",
+        (0..=n).into_iter().map(|i| sl[i] + sr[i]).min().unwrap()
+    );
 }
