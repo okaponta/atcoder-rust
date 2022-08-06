0d4092c55a743a0b9c81ff56550fbf85e9a44822
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-06 23:00:51 +0900

    add abc263_b

--- abc263/src/bin/b.rs
+++ abc263/src/bin/b.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        mut p:[Usize1;n-1],
     }
-    println!("{}", n);
+    p.insert(0, 0);
+    let mut temp = n - 1;
+    let mut count = 0;
+    while temp != 0 {
+        temp = p[temp];
+        count += 1;
+    }
+    println!("{}", count);
 }
