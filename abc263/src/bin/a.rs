75bd95396f18302437fb1ca5864e5dcf824fd753
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-06 23:00:49 +0900

    add abc263_a

--- abc263/src/bin/a.rs
+++ abc263/src/bin/a.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:[usize;5],
     }
-    println!("{}", n);
+    let mut num = vec![0; 14];
+    for ai in a {
+        num[ai] += 1;
+    }
+    num.sort();
+    num.dedup();
+    if num.len() == 3 && num[1] == 2 && num[2] == 3 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
