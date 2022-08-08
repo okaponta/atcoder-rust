43f68106371a8c1f4bc54f8471e7b6f201fd8695
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-08 13:15:58 +0900

    add abc134_c

--- abc134/src/bin/c.rs
+++ abc134/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n]
     }
-    println!("{}", n);
+    let mut max = 0;
+    let mut index = 0;
+    for i in 0..n {
+        if max < a[i] {
+            index = i;
+            max = a[i];
+        }
+    }
+    a.sort();
+    for i in 0..n {
+        if i == index {
+            println!("{}", a[n - 2]);
+        } else {
+            println!("{}", a[n - 1]);
+        }
+    }
 }
