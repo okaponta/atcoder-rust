7ba6c8ce816a97026d02f8016aaedd460d023c91
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-11 18:11:31 +0900

    add abc133_b

--- abc133/src/bin/b.rs
+++ abc133/src/bin/b.rs
@@ -1,8 +1,30 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        d:usize,
+        x:[[i64;d];n]
     }
-    println!("{}", n);
+    let mut count = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            if is_sq(dist(&x[i], &x[j])) {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
+}
+
+fn dist(a: &Vec<i64>, b: &Vec<i64>) -> i64 {
+    a.iter()
+        .zip(b.iter())
+        .map(|(a, b)| (a - b) * (a - b))
+        .sum::<i64>()
+}
+
+fn is_sq(i: i64) -> bool {
+    i.sqrt() * i.sqrt() == i
 }
