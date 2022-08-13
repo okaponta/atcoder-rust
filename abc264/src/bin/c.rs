bb6dd9504ca14be11eda8f0df14ec31f8c5d6c15
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-13 22:48:15 +0900

    add abc264_c

--- abc264/src/bin/c.rs
+++ abc264/src/bin/c.rs
@@ -1,8 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        h:usize, w:usize,
+        a:[[usize;w];h],
+        h2:usize, w2:usize,
+        b:[[usize;w2];h2],
     }
-    println!("{}", n);
+    for hv in (0..h).combinations(h2) {
+        for wv in (0..w).combinations(w2) {
+            let mut new = vec![vec![0; w2]; h2];
+            for i in 0..h2 {
+                for j in 0..w2 {
+                    new[i][j] = a[hv[i]][wv[j]];
+                }
+            }
+            if new == b {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
