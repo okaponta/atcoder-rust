234f6defc51efafecf1305769a9808627b40b3c1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-13 22:48:14 +0900

    add abc264_b

--- abc264/src/bin/b.rs
+++ abc264/src/bin/b.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut r:usize,
+        mut c:usize,
     }
-    println!("{}", n);
+    r = r.min(16 - r);
+    c = c.min(16 - c);
+    let mut is_black = false;
+    if r == 1 || c == 1 {
+        is_black = true;
+    }
+    if (r == 3 && c != 2) || (c == 3 && r != 2) {
+        is_black = true;
+    }
+    if (r == 5 && c != 2 && c != 4) || (c == 5 && r != 2 && r != 4) {
+        is_black = true;
+    }
+    if (r == 7 && c != 2 && c != 4 && c != 6) || (c == 7 && r != 2 && r != 4 && r != 6) {
+        is_black = true;
+    }
+    println!("{}", if is_black { "black" } else { "white" });
 }
