29d1cff7b8e9e4f2636e008b1d8ccb1409846b9e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-15 01:33:52 +0900

    add abc132_a

--- abc132/src/bin/a.rs
+++ abc132/src/bin/a.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for c in s {
+        *map.entry(c).or_insert(0) += 1;
+    }
+    if *map.values().min().unwrap() == 2 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
