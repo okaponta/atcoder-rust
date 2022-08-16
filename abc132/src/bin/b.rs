588ffbd5a6b37fa871f248e163303747cdacaa78
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-16 23:02:45 +0900

    add abc132_b

--- abc132/src/bin/b.rs
+++ abc132/src/bin/b.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        p:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        p.windows(3)
+            .filter(|v| (v[0] < v[1] && v[1] < v[2]) || (v[2] < v[1] && v[1] < v[0]))
+            .count()
+    );
 }
