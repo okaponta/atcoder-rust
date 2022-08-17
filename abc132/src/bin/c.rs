6370444706a8528a511dde059b153cf6b0b68677
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-17 21:20:33 +0900

    add abc132_c

--- abc132/src/bin/c.rs
+++ abc132/src/bin/c.rs
@@ -3,6 +3,8 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut d:[usize;n],
     }
-    println!("{}", n);
+    d.sort();
+    println!("{}", d[n / 2] - d[n / 2 - 1]);
 }
