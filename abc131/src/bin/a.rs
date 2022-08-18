1e15bdf5a554abb9bf8494b5a508dc15949d1b79
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-18 21:14:16 +0900

    add abc131_a

--- abc131/src/bin/a.rs
+++ abc131/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if s.windows(2).filter(|v| v[0] == v[1]).count() > 0 {
+            "Bad"
+        } else {
+            "Good"
+        }
+    );
 }
