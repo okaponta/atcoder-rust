6487649ce51e8b690310f632da30be5c53842601
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-19 23:28:58 +0900

    add abc131_b

--- abc131/src/bin/b.rs
+++ abc131/src/bin/b.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:i64,
+        l:i64
     }
-    println!("{}", n);
+    let mut a = vec![];
+    let mut ans = 0;
+    for i in 0..n {
+        ans += l + i;
+        a.push(l + i);
+    }
+    a.sort_by(|a, b| a.abs().cmp(&b.abs()));
+    println!("{}", ans - a[0]);
 }
