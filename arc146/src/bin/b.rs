ca07c2fd3eda5a9f4abbe1631d2f4bf89afcc3eb
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-20 23:10:16 +0900

    add arc146_b

--- arc146/src/bin/b.rs
+++ arc146/src/bin/b.rs
@@ -3,6 +3,56 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut m:usize,
+        k:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort_by(|a, b| b.cmp(a));
+    let mut ans = 0;
+    let mut flag = false;
+    let mut target = 1 << 32;
+
+    while 0 < target {
+        let (ok, sub) = sub(&a, k, target, m);
+        if ok {
+            m -= sub;
+            ans += target;
+            if a[k - 1] < target {
+                // 順序決定
+                flag = true;
+            }
+            for i in 0..n {
+                if target <= a[i] {
+                    a[i] -= target;
+                } else {
+                    a[i] = 0;
+                }
+            }
+        } else {
+            for i in 0..n {
+                if target <= a[i] {
+                    a[i] -= target;
+                }
+            }
+        }
+        target >>= 1;
+        if !flag {
+            a.sort_by(|a, b| b.cmp(a));
+        }
+    }
+    println!("{}", ans);
+}
+
+fn sub(a: &Vec<usize>, k: usize, n: usize, m: usize) -> (bool, usize) {
+    let mut res = 0;
+    for i in 0..k {
+        if n <= a[i] {
+            continue;
+        }
+        res += n - a[i];
+        if res > m {
+            return (false, 0);
+        }
+    }
+    return (true, res);
 }
