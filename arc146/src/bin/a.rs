c519e9918d9f482a6b3e4b8448d809c04ccf8ec0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-20 23:10:14 +0900

    add arc146_a

--- arc146/src/bin/a.rs
+++ arc146/src/bin/a.rs
@@ -3,6 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    a.reverse();
+    let mut cand = vec![];
+    cand.push(a[0].to_string() + &a[1].to_string() + &a[2].to_string());
+    cand.push(a[0].to_string() + &a[2].to_string() + &a[1].to_string());
+    cand.push(a[1].to_string() + &a[0].to_string() + &a[2].to_string());
+    cand.push(a[1].to_string() + &a[2].to_string() + &a[0].to_string());
+    cand.push(a[2].to_string() + &a[0].to_string() + &a[1].to_string());
+    cand.push(a[2].to_string() + &a[1].to_string() + &a[0].to_string());
+    let mut ans = vec![];
+    for s in cand {
+        ans.push(s.parse::<usize>().unwrap());
+    }
+    ans.sort();
+    ans.reverse();
+    println!("{}", ans[0]);
 }
