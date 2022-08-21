c4bd7574409d0669e32e74d4abd300341950cce3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-21 22:50:14 +0900

    add abc265_d

--- abc265/src/bin/d.rs
+++ abc265/src/bin/d.rs
@@ -3,6 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        p:usize,
+        q:usize,
+        r:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut s = vec![0; n + 1];
+    for i in 0..n {
+        s[i + 1] = s[i] + a[i];
+    }
+    for i in 0..n {
+        let base = s[i];
+        if let Ok(_) = s.binary_search(&(base + p)) {
+            if let Ok(_) = s.binary_search(&(base + p + q)) {
+                if let Ok(_) = s.binary_search(&(base + p + q + r)) {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+    println!("No");
 }
