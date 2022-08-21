3948a6fad3a499ab7edda4f0acd303418308ba94
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-21 22:50:10 +0900

    add abc265_a

--- abc265/src/bin/a.rs
+++ abc265/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
+        x:usize,
+        y:usize,
         n:usize,
     }
-    println!("{}", n);
+    if x * 3 > y {
+        println!("{}", (n / 3) * y + (n % 3) * x);
+    } else {
+        println!("{}", x * n);
+    }
 }
