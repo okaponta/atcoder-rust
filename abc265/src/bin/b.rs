59a9cf68971dfdf3597e4fa159cb5f2fecafe6c5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-21 22:50:12 +0900

    add abc265_b

--- abc265/src/bin/b.rs
+++ abc265/src/bin/b.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut t:i64,
+        a:[i64;n-1],
+        xy:[(Usize1,i64);m],
     }
-    println!("{}", n);
+    let mut bonus = vec![0; n];
+    for (x, y) in xy {
+        bonus[x] += y;
+    }
+    t += bonus[0];
+    for i in 0..n - 1 {
+        if t - a[i] <= 0 {
+            println!("No");
+            return;
+        }
+        t -= a[i];
+        t += bonus[i + 1];
+    }
+    println!("Yes");
 }
