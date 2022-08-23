af0747045401e585f3895bd204d66d57cb8e904c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-24 00:07:29 +0900

    add abc131_d

--- abc131/src/bin/d.rs
+++ abc131/src/bin/d.rs
@@ -3,6 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    ab.sort_by(|a, b| a.1.cmp(&b.1));
+    let mut t = 0;
+    for (a, b) in ab {
+        t += a;
+        if b < t {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
