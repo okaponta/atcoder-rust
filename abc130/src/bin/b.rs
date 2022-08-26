8fb94d795c14ad4ed869dc25cfcbec170ce3900b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-26 10:24:28 +0900

    add abc130_b

--- abc130/src/bin/b.rs
+++ abc130/src/bin/b.rs
@@ -1,8 +1,14 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
-        n:usize,
+        n:usize,x:usize,
+        l:[usize;n],
     }
-    println!("{}", n);
+    let mut s = vec![0; n + 1];
+    for i in 0..n {
+        s[i + 1] = s[i] + l[i];
+    }
+    println!("{}", s.upper_bound(&x));
 }
