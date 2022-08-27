02911ba5db1ee2065f7b4babf63de6e2a6cc1ace
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-27 21:12:05 +0900

    add abc129_b

--- abc129/src/bin/b.rs
+++ abc129/src/bin/b.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        w:[i64;n],
     }
-    println!("{}", n);
+    let mut s = vec![0; n + 1];
+    for i in 0..n {
+        s[i + 1] = s[i] + w[i];
+    }
+    let sum = s[n];
+    println!("{}", s.iter().map(|i| (sum - 2 * i).abs()).min().unwrap());
 }
