d38cd8ee10ea1e6b9e56db1e7c22130ae5da4021
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-28 23:33:41 +0900

    add abc133_c

--- abc133/src/bin/c.rs
+++ abc133/src/bin/c.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut l:usize,
+        mut r:usize,
     }
-    println!("{}", n);
+    r = r.min(l + 2019);
+    let mut ans = 2019 * 2019;
+    for i in l..=r {
+        for j in i + 1..=r {
+            ans = ans.min(((i % 2019) * (j % 2019)) % 2019);
+        }
+    }
+    println!("{}", ans);
 }
