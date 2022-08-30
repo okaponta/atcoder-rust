d8fd457568b0ea2a7ca86de010de5e7df29af3ee
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-30 21:07:55 +0900

    add abc169_e

--- abc169/src/bin/e.rs
+++ abc169/src/bin/e.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       ab:[(usize,usize);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut a = vec![];
+    let mut b = vec![];
+    for (ai, bi) in ab {
+        a.push(ai);
+        b.push(bi);
+    }
+    a.sort();
+    b.sort();
+    if n % 2 == 0 {
+        println!("{}", 1 + b[n / 2] + b[n / 2 - 1] - a[n / 2] - a[n / 2 - 1]);
+    } else {
+        println!("{}", 1 + b[n / 2] - a[n / 2]);
+    }
 }
