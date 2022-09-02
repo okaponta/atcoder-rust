ca99f86b0125a9abab17ec605d87d556815da589
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-02 23:00:05 +0900

    add abc266_c

--- abc266/src/bin/c.rs
+++ abc266/src/bin/c.rs
@@ -11,10 +11,10 @@ fn main() {
     let bc = (c.0 - b.0, c.1 - b.1);
     let cd = (d.0 - c.0, d.1 - c.1);
     let da = (a.0 - d.0, a.1 - d.1);
-    let ipa = inner_product(ab.0, bc.0, ab.1, bc.1);
-    let ipb = inner_product(bc.0, cd.0, bc.1, cd.1);
-    let ipc = inner_product(cd.0, da.0, cd.1, da.1);
-    let ipd = inner_product(da.0, ab.0, da.1, ab.1);
+    let ipa = outer_product(ab.0, bc.0, ab.1, bc.1);
+    let ipb = outer_product(bc.0, cd.0, bc.1, cd.1);
+    let ipc = outer_product(cd.0, da.0, cd.1, da.1);
+    let ipd = outer_product(da.0, ab.0, da.1, ab.1);
     println!(
         "{}",
         if 0 <= ipa && 0 <= ipb && 0 <= ipc && 0 <= ipd {
@@ -25,8 +25,10 @@ fn main() {
     );
 }
 
-// 2つのベクトルの内積を返却
-// これが負なら、ベクトルのなす角は90度以上
-fn inner_product(x0: i32, y0: i32, x1: i32, y1: i32) -> i32 {
-    x0 * x1 + y0 * y1
+// 2つのベクトルの外積を返却
+// これの絶対値がAB/ACのつくる平行四辺形の面積
+// 正なら時計周り、負なら半時計周り、0なら一直線上
+// 外積の絶対値をABの長さで割れば点と線の距離が求まる。
+fn outer_product(x0: i32, y0: i32, x1: i32, y1: i32) -> i32 {
+    x0 * y1 - y0 * x1
 }
