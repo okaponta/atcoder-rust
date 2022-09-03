b4fdfade2f0fb64048466379c159dd14222bb75f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-03 23:07:03 +0900

    add abc267_e

--- abc267/src/bin/e.rs
+++ abc267/src/bin/e.rs
@@ -1,8 +1,103 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; n];
+    for (u, v) in uv {
+        edges[u].push(v);
+        edges[v].push(u);
+    }
+    let mut seg = SegmentTree::new(n, (0, 1 << 60), |a, b| if a.1 < b.1 { a } else { b });
+    for i in 0..n {
+        let mut val = 0;
+        for &j in &edges[i] {
+            val += a[j];
+        }
+        seg.update_tmp(i, (i, val));
+    }
+    seg.update_all();
+    let mut used = vec![false; n];
+    let mut ans = 0;
+    for _ in 0..n {
+        let (k, v) = seg.query(0..n);
+        used[k] = true;
+        ans = ans.max(v);
+        for &i in &edges[k] {
+            if !used[i] {
+                let bef = seg.query(i..i + 1);
+                seg.update(i, (i, bef.1 - a[k]));
+            }
+        }
+        seg.update(k, (k, 1 << 60));
+    }
+    println!("{}", ans);
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
 }
