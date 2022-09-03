026652fa6d7ba6e18d6a5b28fd25fabee7794c8c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-03 23:07:02 +0900

    add abc267_d

--- abc267/src/bin/d.rs
+++ abc267/src/bin/d.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[i64;n]
     }
-    println!("{}", n);
+    let mut dp = vec![-1 << 60; m + 1];
+    dp[0] = 0;
+    for i in 0..n {
+        let mut next = dp.clone();
+        for j in 0..m {
+            next[j + 1] = next[j + 1].max(dp[j] + a[i] * (j as i64 + 1));
+        }
+        dp = next;
+    }
+    println!("{}", dp[m]);
 }
