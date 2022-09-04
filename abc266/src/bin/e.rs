c3434212022ee692b023e16d629de923195bc225
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-04 22:54:23 +0900

    add abc266_e

--- abc266/src/bin/e.rs
+++ abc266/src/bin/e.rs
@@ -4,5 +4,17 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut ans = 0.0;
+    for _ in 0..n {
+        let mut next = 0.0;
+        for i in 1..7 {
+            if ans < i as f64 {
+                next += i as f64;
+            } else {
+                next += ans;
+            }
+        }
+        ans = next / 6.0;
+    }
+    println!("{}", ans);
 }
