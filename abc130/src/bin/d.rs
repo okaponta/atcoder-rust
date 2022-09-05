4e78aa984b3cddbe4bdd89ef0a6724baf09777e4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-06 00:23:52 +0900

    add abc130_d

--- abc130/src/bin/d.rs
+++ abc130/src/bin/d.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    // 半開区間
+    let mut left = 0;
+    let mut right = 0;
+    let mut sum = 0;
+    let mut count = 0;
+    loop {
+        while sum < k && right < n {
+            sum += a[right];
+            right += 1;
+        }
+        if sum < k {
+            // しゃくとり終了
+            break;
+        }
+        count += n + 1 - right;
+        sum -= a[left];
+        left += 1;
+    }
+    println!("{}", count);
 }
