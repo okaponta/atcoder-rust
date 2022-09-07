a274a1aff1da1ca305f1cde0214030213d9b4118
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-08 00:08:27 +0900

    add abc137_d

--- abc137/src/bin/d.rs
+++ abc137/src/bin/d.rs
@@ -1,8 +1,28 @@
+use std::collections::{BinaryHeap, HashMap};
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for (a, b) in ab {
+        (*map.entry(a).or_insert(vec![])).push(b);
+    }
+    let mut heap = BinaryHeap::new();
+    let mut ans = 0;
+    for i in 1..=m {
+        if let Some(v) = map.get(&i) {
+            for b in v {
+                heap.push(b);
+            }
+        }
+        if let Some(b) = heap.pop() {
+            ans += b;
+        }
+    }
+    println!("{}", ans);
 }
