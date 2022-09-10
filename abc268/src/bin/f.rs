6fd954cabb0ecf029b32f3c4c97776e2af2d6acf
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-11 00:51:50 +0900

    add abc268_f

--- abc268/src/bin/f.rs
+++ abc268/src/bin/f.rs
@@ -1,8 +1,24 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut s:[String;n],
     }
-    println!("{}", n);
+    s.sort_by(|a, b| score(b.to_string() + a).cmp(&score(a.to_string() + b)));
+    println!("{}", score(s.iter().join("")));
+}
+
+fn score(s: String) -> usize {
+    let mut x = 0;
+    let mut score = 0;
+    for c in s.chars() {
+        if c == 'X' {
+            x += 1;
+        } else {
+            score += c.to_digit(10).unwrap() as usize * x;
+        }
+    }
+    score
 }
