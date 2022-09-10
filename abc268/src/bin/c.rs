6fa26cfc1f0817cb6b0ad5b37124bbd3066b8cd5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-10 22:55:52 +0900

    add abc268_c

--- abc268/src/bin/c.rs
+++ abc268/src/bin/c.rs
@@ -3,6 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        p:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        let base = (n + p[i] - i) % n;
+        let bef = (n + base - 1) % n;
+        let after = (base + 1) % n;
+        ans[base] += 1;
+        ans[bef] += 1;
+        ans[after] += 1;
+    }
+    println!("{}", ans.iter().max().unwrap_or(&0));
 }
