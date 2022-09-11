c4333ff94e92ac018d9b5b79a12628bfa38acea5
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-11 23:10:11 +0900

    add arc148_a

--- arc148/src/bin/a.rs
+++ arc148/src/bin/a.rs
@@ -3,6 +3,37 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[i64;n],
     }
-    println!("{}", n);
+    a.sort();
+    a.dedup();
+    let n = a.len();
+    if n == 1 {
+        println!("1");
+        return;
+    }
+    if n == 2 {
+        if a[1] - a[0] == 1 {
+            println!("2");
+        } else {
+            println!("1");
+        }
+        return;
+    }
+    let mut sub = vec![];
+    for i in 0..n - 1 {
+        sub.push(a[i + 1] - a[i]);
+    }
+    let mut g = gcd(sub[0], sub[1]);
+    for i in sub {
+        g = gcd(g, i);
+    }
+    println!("{}", if 1 < g { 1 } else { 2 });
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
