e9323e333e2cf925433d953b333d0bb8c29c40a7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-12 00:27:11 +0900

    add arc148_c

--- arc148/src/bin/c.rs
+++ arc148/src/bin/c.rs
@@ -1,8 +1,32 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        mut p:[Usize1;n-1],
     }
-    println!("{}", n);
+    p.insert(0, n + 1);
+    let mut childs = vec![0; n];
+    for i in 1..n {
+        childs[p[i]] += 1;
+    }
+    for _ in 0..q {
+        input! {
+            m:usize,
+            v:[Usize1;m],
+        }
+        let set = v.iter().collect::<HashSet<_>>();
+        let mut ans = 0;
+        for i in 0..m {
+            ans += childs[v[i]] + 1;
+            if set.contains(&p[v[i]]) {
+                ans -= 2;
+            }
+        }
+        println!("{}", ans);
+    }
 }
