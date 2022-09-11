a0dcdf03ba874e824287bc458d997c936b9f08c9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-11 23:10:13 +0900

    add arc148_b

--- arc148/src/bin/b.rs
+++ arc148/src/bin/b.rs
@@ -3,6 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        s:String,
     }
-    println!("{}", n);
+    let rev = s
+        .chars()
+        .rev()
+        .map(|c| if c == 'd' { 'p' } else { 'd' })
+        .collect::<String>();
+    let mut ans = s.clone();
+    for i in 0..n {
+        for j in i + 1..=n {
+            let new = s[0..i].to_string() + &rev[(n - j)..(n - i)] + &s[j..n];
+            ans = ans.min(new);
+        }
+    }
+    println!("{}", ans);
 }
