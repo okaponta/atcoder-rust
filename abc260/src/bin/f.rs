ba45457180f4ba3fd4b612f22e749e202859e440
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-15 02:55:02 +0900

    add abc260_f

--- abc260/src/bin/f.rs
+++ abc260/src/bin/f.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n:usize,
+        s:usize,
+        t:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; s];
+    for (u, v) in uv {
+        edges[u].push(v);
+    }
+    let mut map = vec![vec![!0; t]; t];
+    for i in 0..s {
+        let n = edges[i].len();
+        for j in 0..n {
+            for k in j + 1..n {
+                let (j, k) = (edges[i][j].min(edges[i][k]), edges[i][j].max(edges[i][k]));
+                if map[j - s][k - s] != !0 {
+                    println!("{} {} {} {}", i + 1, j + 1, k + 1, map[j - s][k - s] + 1);
+                    return;
+                } else {
+                    map[j - s][k - s] = i;
+                }
+            }
+        }
+    }
+    println!("-1");
 }
