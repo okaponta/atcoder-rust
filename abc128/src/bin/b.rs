2ac53dedfb56eeec08040f5bd626b8b0a3a5bdeb
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-14 22:52:50 +0900

    add abc128_b

--- abc128/src/bin/b.rs
+++ abc128/src/bin/b.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        sp: [(String,usize);n],
     }
-    println!("{}", n);
+    let mut spi = vec![];
+    for (i, (s, p)) in sp.iter().enumerate() {
+        spi.push((s, p, i + 1));
+    }
+    spi.sort_by(|a, b| a.0.cmp(&b.0).then(b.1.cmp(&a.1)));
+    for (_, _, i) in spi {
+        println!("{}", i);
+    }
 }
