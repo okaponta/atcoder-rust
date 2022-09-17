3da1bdce678244ccb060a634c4ee9f00e89e8a67
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-17 22:58:51 +0900

    add abc269_b

--- abc269/src/bin/b.rs
+++ abc269/src/bin/b.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:[Chars;10],
     }
-    println!("{}", n);
+    let mut ans = vec![0; 4];
+    for i in 0..10 {
+        for j in 0..10 {
+            if s[i][j] == '#' {
+                ans[1] = i + 1;
+                ans[2] = j + 1;
+                break;
+            }
+        }
+    }
+    for i in (0..10).rev() {
+        for j in (0..10).rev() {
+            if s[i][j] == '#' {
+                ans[0] = i + 1;
+                ans[3] = j + 1;
+                break;
+            }
+        }
+    }
+    println!("{} {}", ans[0], ans[1]);
+    println!("{} {}", ans[2], ans[3]);
 }
