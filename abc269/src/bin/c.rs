87853fa083ab257e55f6da7e2adcbed65c6e5ce2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-17 22:58:52 +0900

    add abc269_c

--- abc269/src/bin/c.rs
+++ abc269/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let digits = n.count_ones() as usize;
+    let mut nums = vec![];
+    let mut i = 0usize;
+    while nums.len() < digits {
+        if n >> i & 1 == 1 {
+            nums.push(1 << i);
+        }
+        i += 1;
+    }
+    for i in 0usize..1 << digits {
+        let mut ans = 0usize;
+        for j in 0..digits {
+            if i >> j & 1 == 1 {
+                ans += nums[j];
+            }
+        }
+        println!("{}", ans);
+    }
 }
