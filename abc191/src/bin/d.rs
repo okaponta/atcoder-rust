8f5a0c10bd06ac318407796576996135bc71f10c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-23 01:02:26 +0900

    add abc191_d

--- abc191/src/bin/d.rs
+++ abc191/src/bin/d.rs
@@ -1,8 +1,24 @@
+use num::integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       x:f64,
+       y:f64,
+       r:f64,
     }
-    println!("{}", a);
+    let (x, y, r) = (t(x), t(y), t(r));
+    println!(
+        "{}",
+        ((x - r + 9999).div_euclid(10000)..=(x + r).div_euclid(10000))
+            .into_iter()
+            .fold(0, |ans, x2| {
+                let y2 = (r * r - (x - x2 * 10000).pow(2)).sqrt();
+                return ans + 1 + (y + y2).div_euclid(10000) - (y - y2 + 9999).div_euclid(10000);
+            })
+    );
+}
+
+fn t(a: f64) -> i64 {
+    (a * 10000.0).round() as i64
 }
