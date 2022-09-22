5f303b90310b5ed2132a6fe046b542dcc3e14f32
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-22 23:07:54 +0900

    add abc159_e

--- abc159/src/bin/e.rs
+++ abc159/src/bin/e.rs
@@ -1,8 +1,51 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        k:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    let mut ans = 1010;
+    // 縦横逆にする
+    let mut choco = vec![vec![false; h]; w];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '1' {
+                choco[j][i] = true;
+            }
+        }
+    }
+    'bit: for i in 0usize..1 << (h - 1) {
+        let mut div_cnt = 0;
+        // ホワイトチョコの数
+        let n = i.count_ones() as usize + 1;
+        let mut count = vec![0; n];
+        for j in 0..w {
+            let mut cur_count = vec![0; n];
+            let mut l = 0;
+            for k in 0..h {
+                if choco[j][k] {
+                    cur_count[l] += 1;
+                }
+                if i >> k & 1 == 1 {
+                    l += 1;
+                }
+            }
+            if (0..n).into_iter().any(|i| k < cur_count[i]) {
+                continue 'bit;
+            }
+            if (0..n).into_iter().all(|i| count[i] + cur_count[i] <= k) {
+                for k in 0..n {
+                    count[k] += cur_count[k];
+                }
+            } else {
+                count = cur_count;
+                div_cnt += 1;
+            }
+        }
+        ans = ans.min(div_cnt + n - 1);
+    }
+    println!("{}", ans);
 }
