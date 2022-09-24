5ba97f3a8e87e33bb84870ae641f97a912f9e090
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-24 23:57:18 +0900

    add abc270_b

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -2,7 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut x:i64,
+        mut y:i64,
+        mut z:i64,
     }
-    println!("{}", n);
+    if x < 0 {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+    let ans;
+    if 0 < y && y < x {
+        // ハンマーが必要
+        if 0 < z && z < y {
+            ans = x.abs();
+        } else if z < 0 {
+            ans = 2 * z.abs() + x.abs();
+        } else {
+            println!("-1");
+            return;
+        }
+    } else {
+        ans = x.abs();
+    }
+    println!("{}", ans);
 }
