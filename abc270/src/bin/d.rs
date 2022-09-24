b3fe244a789afb6b61898139be4c2295914f6c9d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-24 23:57:20 +0900

    add abc270_d

--- abc270/src/bin/d.rs
+++ abc270/src/bin/d.rs
@@ -3,6 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;k],
     }
-    println!("{}", n);
+    let mut dp = vec![vec![-1; 2]; 2 * n];
+    dp[0][0] = 0;
+    dp[0][1] = 0;
+    for i in 0..n {
+        for j in 0..k {
+            if dp[i][0] != -1 {
+                dp[i + a[j]][1] = dp[i + a[j]][1].max(dp[i][0] + a[j] as i64);
+            }
+            if dp[i][1] != -1 {
+                if dp[i + a[j]][0] != -1 {
+                    dp[i + a[j]][0] = dp[i + a[j]][0].min(dp[i][1]);
+                } else {
+                    dp[i + a[j]][0] = dp[i][1];
+                }
+            }
+        }
+    }
+    println!("{}", dp[n][0].max(dp[n][1]));
 }
