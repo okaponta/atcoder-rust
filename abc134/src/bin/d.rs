8666bdfa1a0bb9b70a6e687c9dc0285a7680051f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-26 00:10:38 +0900

    add abc134_d

--- abc134/src/bin/d.rs
+++ abc134/src/bin/d.rs
@@ -1,8 +1,30 @@
+use itertools::{self, Itertools};
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[u8;n],
     }
-    println!("{}", n);
+    a.insert(0, 0);
+    let mut balls = vec![false; n + 1];
+    let mut ans = vec![];
+    for i in (1..=n).rev() {
+        let mut tmp = i;
+        let mut put = a[i] == 1;
+        while tmp <= n {
+            if balls[tmp] {
+                put = !put;
+            }
+            tmp += i;
+        }
+        if put {
+            ans.push(i);
+            balls[i] = true;
+        }
+    }
+    println!("{}", ans.len());
+    if 0 < ans.len() {
+        println!("{}", ans.iter().join(" "));
+    }
 }
