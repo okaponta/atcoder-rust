edcff64c60567efe49ac002fbdec9a610c3ecedf
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-27 00:50:56 +0900

    add abc134_e

--- abc134/src/bin/e.rs
+++ abc134/src/bin/e.rs
@@ -1,8 +1,18 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut set = BTreeSet::new();
+    for i in 0..n {
+        if let Some(&num) = set.range(..(a[i], 0)).last() {
+            set.remove(&num);
+        }
+        set.insert((a[i], i));
+    }
+    println!("{}", set.len());
 }
