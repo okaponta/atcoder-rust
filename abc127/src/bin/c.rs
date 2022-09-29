0eed6b4cbdd9c36eeb2fd5d1cc5a72542bd87dc4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-29 22:55:29 +0900

    add abc127_c

--- abc127/src/bin/c.rs
+++ abc127/src/bin/c.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        lr:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut min = 0;
+    let mut max = n;
+    for (l, r) in lr {
+        min = min.max(l);
+        max = max.min(r);
+    }
+    if max < min {
+        println!("0");
+        return;
+    }
+    println!("{}", max + 1 - min);
 }
