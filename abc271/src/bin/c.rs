72a0555bdf94b871a7c9932366264da801c5ad4d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-01 22:50:43 +0900

    add abc271_c

--- abc271/src/bin/c.rs
+++ abc271/src/bin/c.rs
@@ -1,8 +1,25 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let set = a.into_iter().collect::<HashSet<usize>>();
+    let mut remain = n as i64;
+    let mut next = 1;
+    loop {
+        if set.contains(&next) {
+            remain -= 1;
+        } else {
+            remain -= 2;
+        }
+        if remain < 0 {
+            break;
+        }
+        next += 1;
+    }
+    println!("{}", next - 1);
 }
