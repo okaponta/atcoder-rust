d32f94edb60bac3e75dcef9060ddeb17d9f1bf39
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-03 02:33:07 +0900

    add abc133_d

--- abc133/src/bin/d.rs
+++ abc133/src/bin/d.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut sum = 0;
+    let mut ex_last = 0;
+    for i in 0..n {
+        sum += a[i];
+        if i % 2 == 0 && i != n - 1 {
+            ex_last += 2 * a[i];
+        }
+    }
+    let mut ans = vec![0; n];
+    ans[n - 1] = sum - ex_last;
+    for i in 0..n {
+        ans[i] = 2 * a[(n + i - 1) % n] - ans[(n + i - 1) % n];
+    }
+    println!("{}", ans.iter().join(" "));
 }
