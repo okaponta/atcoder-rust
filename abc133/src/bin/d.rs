347f447cd6cb68f6047ee0bc9c69c772fdc88493
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-03 02:34:45 +0900

    add abc133_d

--- abc133/src/bin/d.rs
+++ abc133/src/bin/d.rs
@@ -8,17 +8,17 @@ fn main() {
         a:[usize;n],
     }
     let mut sum = 0;
-    let mut ex_last = 0;
+    let mut ex_first = 0;
     for i in 0..n {
         sum += a[i];
-        if i % 2 == 0 && i != n - 1 {
-            ex_last += 2 * a[i];
+        if i % 2 == 1 {
+            ex_first += 2 * a[i];
         }
     }
     let mut ans = vec![0; n];
-    ans[n - 1] = sum - ex_last;
-    for i in 0..n {
-        ans[i] = 2 * a[(n + i - 1) % n] - ans[(n + i - 1) % n];
+    ans[0] = sum - ex_first;
+    for i in 1..n {
+        ans[i] = 2 * a[i - 1] - ans[i - 1];
     }
     println!("{}", ans.iter().join(" "));
 }
