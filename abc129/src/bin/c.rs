6255ad652e06e168424250012624123e0d66776c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-04 00:56:36 +0900

    add abc129_c

--- abc129/src/bin/c.rs
+++ abc129/src/bin/c.rs
@@ -1,8 +1,25 @@
+use std::collections::HashSet;
+
 use proconio::input;
+
+const MOD: usize = 1_000_000_007;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;m],
     }
-    println!("{}", n);
+    let set = a.into_iter().collect::<HashSet<usize>>();
+    let mut dp = vec![0; n + 2];
+    dp[0] = 1;
+    for i in 0..n {
+        for j in 1..3 {
+            if !set.contains(&(i + j)) {
+                dp[i + j] += dp[i];
+                dp[i + j] %= MOD;
+            }
+        }
+    }
+    println!("{}", dp[n]);
 }
