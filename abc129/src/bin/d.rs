335c6a2b3bec17d9700fae6fc811df04c1919516
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-05 02:46:45 +0900

    add abc129_d

--- abc129/src/bin/d.rs
+++ abc129/src/bin/d.rs
@@ -1,8 +1,61 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    let mut ans = vec![vec![0; w]; h];
+    for i in 0..h {
+        let mut count = 0;
+        for j in 0..w {
+            if s[i][j] == '.' {
+                count += 1;
+                ans[i][j] += count;
+            } else {
+                count = 0;
+            }
+        }
+    }
+    for i in 0..h {
+        let mut count = 0;
+        for j in (0..w).rev() {
+            if s[i][j] == '.' {
+                count += 1;
+                ans[i][j] += count;
+            } else {
+                count = 0;
+            }
+        }
+    }
+    for j in 0..w {
+        let mut count = 0;
+        for i in 0..h {
+            if s[i][j] == '.' {
+                count += 1;
+                ans[i][j] += count;
+            } else {
+                count = 0;
+            }
+        }
+    }
+    for j in 0..w {
+        let mut count = 0;
+        for i in (0..h).rev() {
+            if s[i][j] == '.' {
+                count += 1;
+                ans[i][j] += count;
+            } else {
+                count = 0;
+            }
+        }
+    }
+    let mut a = 0;
+    for i in 0..h {
+        for j in 0..w {
+            a = a.max(ans[i][j] - 3);
+        }
+    }
+    println!("{}", a);
 }
