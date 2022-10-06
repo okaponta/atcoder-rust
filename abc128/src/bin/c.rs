c0bda6036508e3dd91b2550300fb2bcb5b487d11
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-06 15:13:31 +0900

    add abc128_c

--- abc128/src/bin/c.rs
+++ abc128/src/bin/c.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut switches = vec![];
+    for _ in 0..m {
+        input! {k:usize,s:[Usize1;k]}
+        switches.push(s.into_iter().collect::<HashSet<_>>());
+    }
+    input! {p:[usize;m]}
+    let mut ans = 0;
+    for i in 0..1 << n {
+        let mut count = vec![0; m];
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                for k in 0..m {
+                    if switches[k].contains(&j) {
+                        count[k] += 1;
+                    }
+                }
+            }
+        }
+        if (0..m).into_iter().all(|k| count[k] % 2 == p[k]) {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
