2750368b68dbf0df82724558554dc80addf8cd6a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-08 22:15:17 +0900

    add abc128_e

--- abc128/src/bin/e.rs
+++ abc128/src/bin/e.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use std::collections::BTreeSet;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        mut stx:[(usize,usize,usize);n],
+        d:[usize;q],
     }
-    println!("{}", n);
+    let mut set = BTreeSet::new();
+    for i in 0..q {
+        set.insert((d[i], i));
+    }
+    let mut ans = vec![-1; q];
+    stx.sort_by_key(|k| k.2);
+    for (s, t, x) in stx {
+        let from = s.saturating_sub(x);
+        let to = t.saturating_sub(x);
+        while let Some(&(d, i)) = set.range((from, 0)..(to, 0)).next() {
+            set.remove(&(d, i));
+            ans[i] = x as i64;
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
