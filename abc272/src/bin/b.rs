d802e6a51a02756b02a60ef8e97fcab2f8b5f2ea
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-10 10:39:35 +0900

    add abc272_b

--- abc272/src/bin/b.rs
+++ abc272/src/bin/b.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut ans = vec![vec![false; n]; n];
+    for _ in 0..m {
+        input! {
+            k:usize,
+            x:[Usize1;k],
+        }
+        for (&a, &b) in x.iter().tuple_combinations() {
+            ans[a][b] = true;
+        }
+    }
+    for i in 0..n {
+        for j in i + 1..n {
+            if !ans[i][j] {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
