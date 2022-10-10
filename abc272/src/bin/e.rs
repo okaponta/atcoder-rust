efc4806a16d7d98a567eaf1d515663b28b346e63
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-10 14:04:55 +0900

    add abc272_e

--- abc272/src/bin/e.rs
+++ abc272/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use proconio::{fastout, input};
 
 #[fastout]
@@ -8,39 +10,26 @@ fn main() {
         a:[i64;n],
     }
     let max = n as i64;
-    let mut ans = vec![vec![]; m];
+    let mut ans = vec![HashSet::new(); m];
     for i in 0..n {
         let m = m as i64;
         let diff = (i + 1) as i64;
         let first = a[i] + diff;
         let last = a[i] + diff * m;
-        // 一回の操作で対象外
-        if max < first {
-            continue;
-        }
-        // m回操作しても対象外
-        if last < 0 {
+        // 対象外
+        if max < first || last < 0 {
             continue;
         }
         let start = 0.max((-first + diff - 1) / diff);
         let end = ((n as i64 - first + diff - 1) / diff).min(m - 1);
         for j in start..=end {
-            ans[j as usize].push(a[i] + diff * (j + 1) as i64);
+            ans[j as usize].insert(a[i] + diff * (j + 1) as i64);
         }
     }
-    for mut ans in ans {
-        if ans.len() == 0 {
-            println!("0");
-            continue;
-        }
-        ans.sort();
-        ans.dedup();
+    for ans in ans {
         let mut mex = 0;
-        while ans[mex] == mex as i64 {
+        while ans.contains(&mex) {
             mex += 1;
-            if mex == ans.len() {
-                break;
-            }
         }
         println!("{}", mex);
     }
