718339709e7456a30edbe008b42dc47b2a4105f4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-13 15:13:15 +0900

    add abc126_b

--- abc126/src/bin/b.rs
+++ abc126/src/bin/b.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        s: String,
     }
-    println!("{}", n);
+    let first = s[..2].parse().unwrap();
+    let last = s[2..].parse().unwrap();
+    if is_mm(first) {
+        println!("{}", if is_mm(last) { "AMBIGUOUS" } else { "MMYY" });
+    } else {
+        println!("{}", if is_mm(last) { "YYMM" } else { "NA" });
+    }
+}
+
+fn is_mm(i: i32) -> bool {
+    0 < i && i < 13
 }
