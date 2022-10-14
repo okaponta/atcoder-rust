2c78dfc885f74b40d7d74cdddecf45ef422f36fe
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-14 15:53:06 +0900

    add abc126_c

--- abc126/src/bin/c.rs
+++ abc126/src/bin/c.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut k:usize,
     }
-    println!("{}", n);
+    k -= 1;
+    let mut prev = k.min(n);
+    let mut faces = vec![n.saturating_sub(k)];
+    while 0 < k {
+        k /= 2;
+        let s = k.min(n);
+        faces.push(prev - s);
+        prev = s;
+    }
+    let mut a = faces[0];
+    let mut b = n;
+    for i in 1..faces.len() {
+        a <<= 1;
+        b <<= 1;
+        a += faces[i];
+    }
+    println!("{}", a as f64 / b as f64);
 }
