1ea7ddb961f992e0d6f1c7c7e8df7533e79c79db
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-16 00:33:49 +0900

    add abc273_e

--- abc273/src/bin/e.rs
+++ abc273/src/bin/e.rs
@@ -1,48 +1,39 @@
 use std::collections::HashMap;
 
 use itertools::Itertools;
-use proconio::{input, marker::Chars};
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         q:usize,
     }
-    let mut qs = vec![];
-    for _ in 0..q {
-        input! {
-            s: Chars
-        }
-        if s[0] == 'D' {
-            qs.push((s[0], 0));
-        } else {
-            input! {i:i64}
-            qs.push((s[0], i));
-        }
-    }
     let mut cur = 0;
-    let mut child = vec![0; q + 1];
     let mut parent = vec![0; q + 1];
     let mut value = vec![0; q + 1];
     let mut note = HashMap::new();
     value[0] = -1;
-    let mut ans = vec![];
-    for (idx, (c, i)) in qs.into_iter().enumerate() {
-        if c == 'A' {
-            child[cur] = idx + 1;
-            parent[idx + 1] = cur;
-            cur = child[cur];
-            value[cur] = i;
+    let mut ans = vec![0; q];
+    for i in 0..q {
+        input! {s: String}
+        match s.as_str() {
+            "ADD" => {
+                input! {x: i32}
+                parent[i + 1] = cur;
+                cur = i + 1;
+                value[cur] = x;
+            }
+            "DELETE" => cur = parent[cur],
+            "SAVE" => {
+                input! {y: i32}
+                note.insert(y, cur);
+            }
+            _ => {
+                input! {z: i32}
+                cur = *note.get(&z).unwrap_or(&0);
+            }
         }
-        if c == 'D' {
-            cur = parent[cur];
-        }
-        if c == 'S' {
-            note.insert(i, cur);
-        }
-        if c == 'L' {
-            cur = *note.get(&i).unwrap_or(&0);
-        }
-        ans.push(value[cur]);
+        ans[i] = value[cur];
     }
     println!("{}", ans.iter().join(" "));
 }
