1cec9c7fd3065bca3ab37576d7b7d143c2e5eab9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-15 22:51:24 +0900

    add abc273_b

--- abc273/src/bin/b.rs
+++ abc273/src/bin/b.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        mut s:Chars,
+        k:usize,
     }
-    println!("{}", n);
+    s.reverse();
+    for _ in 0..16 {
+        s.push('0');
+    }
+    let mut ans = 0;
+    let mut dig = 1;
+    let mut kuriagari = 0;
+    for i in 0..16 {
+        let d = s[i].to_digit(10).unwrap() as usize + kuriagari;
+        if i < k {
+            if 5 <= d {
+                kuriagari = 1;
+            } else {
+                kuriagari = 0;
+            }
+        } else {
+            kuriagari = 0;
+            ans += d * dig;
+        }
+        dig *= 10;
+    }
+    println!("{}", ans);
 }
