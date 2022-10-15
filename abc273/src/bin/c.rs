cecc6c4cc08b3c6fb41d02ae7c61024ec8504dab
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-15 22:51:26 +0900

    add abc273_c

--- abc273/src/bin/c.rs
+++ abc273/src/bin/c.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use std::collections::BTreeMap;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = BTreeMap::new();
+    for a in a {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    let mut ans = vec![0; n];
+    for (i, (_, v)) in map.into_iter().rev().enumerate() {
+        ans[i] = v;
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
