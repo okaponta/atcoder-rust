45e2ee41267915f919581b4bbc3c4f431fbeffb0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-18 21:44:27 +0900

    add abc126_e

--- abc126/src/bin/e.rs
+++ abc126/src/bin/e.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        xyz:[(Usize1,Usize1,usize);m],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for (x, y, _) in xyz {
+        uf.union(x, y);
+    }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        set.insert(uf.find(i));
+    }
+    println!("{}", set.len());
 }
