d97f5c979c2cab8ae980e7855c96d3cfc71abe01
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-20 02:12:47 +0900

    add abc125_b

--- abc125/src/bin/b.rs
+++ abc125/src/bin/b.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        v:[usize;n],
+        c:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        v.iter()
+            .zip(c.iter())
+            .map(|(&v, &c)| v.saturating_sub(c))
+            .sum::<usize>()
+    )
 }
