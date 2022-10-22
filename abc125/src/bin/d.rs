dfa16924c76e5e2680df7bc3fb959a4c643b8f68
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-22 17:11:40 +0900

    add abc125_d

--- abc125/src/bin/d.rs
+++ abc125/src/bin/d.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut dp = (-a[0], a[0]);
+    for i in 1..n - 1 {
+        let a = a[i];
+        dp = ((dp.0 + a).max(dp.1 - a), (dp.0 - a).max(dp.1 + a));
+    }
+    println!("{}", (dp.0 - a[n - 1]).max(dp.1 + a[n - 1]));
 }
