297031308e4752039c64dc234130bf4564e28c78
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-25 22:14:49 +0900

    add abc274_c

--- abc274/src/bin/c.rs
+++ abc274/src/bin/c.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut ans = vec![0; 2 * n + 1];
+    for i in 0..n {
+        ans[2 * i + 1] = ans[a[i]] + 1;
+        ans[2 * i + 2] = ans[a[i]] + 1;
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
