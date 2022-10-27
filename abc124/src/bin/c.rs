27fb5276f005a1fe2e9da0d030d62697fbe4f9ea
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-28 00:08:13 +0900

    add abc124_c

--- abc124/src/bin/c.rs
+++ abc124/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let mut count = 0;
+    for i in 0..n {
+        if i % 2 == 0 && s[i] == '0' {
+            count += 1;
+        }
+        if i % 2 == 1 && s[i] == '1' {
+            count += 1;
+        }
+    }
+    println!("{}", count.min(n - count));
 }
