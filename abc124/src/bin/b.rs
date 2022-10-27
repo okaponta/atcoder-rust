92b095cdf7113c8fe57df0680416bf1bfe25f8c3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-27 21:17:57 +0900

    add abc124_b

--- abc124/src/bin/b.rs
+++ abc124/src/bin/b.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        h:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut max = 0;
+    for h in h {
+        max = max.max(h);
+        if max <= h {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
