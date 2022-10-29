e669d8ca69ad794ae947c33b5c52794c888ae3f9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-29 22:45:04 +0900

    add abc275_d

--- abc275/src/bin/d.rs
+++ abc275/src/bin/d.rs
@@ -1,8 +1,23 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    println!("{}", f(n, &mut map));
+}
+
+fn f(k: usize, map: &mut HashMap<usize, usize>) -> usize {
+    if k == 0 {
+        return 1;
+    }
+    if map.contains_key(&k) {
+        return map[&k];
+    }
+    let res = f(k / 2, map) + f(k / 3, map);
+    map.insert(k, res);
+    res
 }
