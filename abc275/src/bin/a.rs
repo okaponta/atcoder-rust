22af5f8582d0b594c1fb7aa287163fa84ca8a60d
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-29 22:45:00 +0900

    add abc275_a

--- abc275/src/bin/a.rs
+++ abc275/src/bin/a.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        h:[usize;n],
     }
-    println!("{}", n);
+    let mut s = vec![];
+    for i in 0..n {
+        s.push((h[i], i));
+    }
+    s.sort();
+    s.reverse();
+    println!("{}", s[0].1 + 1);
 }
