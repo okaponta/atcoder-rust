1b3ef1f96bafa4573066c15ab7bbc9d02bcd89a9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-10-31 22:08:04 +0900

    add abc123_b

--- abc123/src/bin/b.rs
+++ abc123/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:[usize;5],
     }
-    println!("{}", n);
+    let ans1 = a.iter().map(|i| (i + 9) / 10 * 10).sum::<usize>();
+    let ans2 = a
+        .iter()
+        .map(|i| i % 10)
+        .filter(|i| i != &0)
+        .min()
+        .unwrap_or(10);
+    println!("{}", ans1 + ans2 - 10);
 }
