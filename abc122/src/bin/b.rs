e3b48b44ff143f0116ee39f5f0106fb0274d771e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-03 16:00:42 +0900

    add abc122_b

--- abc122/src/bin/b.rs
+++ abc122/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let atcg = vec!['A', 'T', 'C', 'G'].into_iter().collect::<HashSet<_>>();
+    let mut ans = 0;
+    let mut temp = 0;
+    for c in s {
+        if atcg.contains(&c) {
+            temp += 1;
+        } else {
+            ans = ans.max(temp);
+            temp = 0;
+        }
+    }
+    println!("{}", ans.max(temp));
 }
