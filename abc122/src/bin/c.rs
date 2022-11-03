5fb286464acaa38832794ef14bc5cb32fe813f34
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-04 00:24:47 +0900

    add abc122_c

--- abc122/src/bin/c.rs
+++ abc122/src/bin/c.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        s:Chars,
+        lr:[(Usize1,Usize1);q],
     }
-    println!("{}", n);
+    let mut ac = vec![0; n];
+    for i in 1..n {
+        ac[i] = ac[i - 1];
+        if s[i - 1] == 'A' && s[i] == 'C' {
+            ac[i] += 1;
+        }
+    }
+    for (l, r) in lr {
+        println!("{}", ac[r] - ac[l]);
+    }
 }
