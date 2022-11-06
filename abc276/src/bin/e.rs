341ab7e3064471e84c9741b9a6ddf58c84137974
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-07 00:17:11 +0900

    add abc276_e

--- abc276/src/bin/e.rs
+++ abc276/src/bin/e.rs
@@ -1,8 +1,57 @@
-use proconio::input;
+use std::collections::{HashSet, VecDeque};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        c:[Chars;h],
     }
-    println!("{}", n);
+    let mut start = (0, 0);
+    for i in 0..h {
+        for j in 0..w {
+            if c[i][j] == 'S' {
+                start = (i, j);
+            }
+        }
+    }
+    let mut cand = vec![];
+    for (dx, dy) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+        let x = start.0.wrapping_add(dx);
+        let y = start.1.wrapping_add(dy);
+        if h <= x || w <= y {
+            continue;
+        } else if c[x][y] == '.' {
+            cand.push((x, y));
+        }
+    }
+    while 1 < cand.len() {
+        let (x, y) = cand.pop().unwrap();
+        let goal = cand.clone().into_iter().collect::<HashSet<_>>();
+        let mut q = VecDeque::new();
+        let mut visited = vec![vec![false; w]; h];
+        q.push_back((x, y));
+        while let Some((x, y)) = q.pop_front() {
+            if visited[x][y] {
+                continue;
+            }
+            visited[x][y] = true;
+            if goal.contains(&(x, y)) {
+                println!("Yes");
+                return;
+            }
+            for (dx, dy) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+                let xi = x.wrapping_add(dx);
+                let yi = y.wrapping_add(dy);
+                if h <= xi || w <= yi {
+                    continue;
+                }
+                if !visited[xi][yi] && c[xi][yi] == '.' {
+                    q.push_back((xi, yi));
+                }
+            }
+        }
+    }
+    println!("No");
 }
