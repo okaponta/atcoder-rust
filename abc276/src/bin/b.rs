d7c981e19fa2d21ec58be1cc910274d450a84757
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-06 19:51:56 +0900

    add abc276_b

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b + 1);
+        edges[b].push(a + 1);
+    }
+    for i in 0..n {
+        if edges[i].len() == 0 {
+            println!("0");
+            continue;
+        }
+        edges[i].sort();
+        println!("{} {}", edges[i].len(), edges[i].iter().join(" "));
+    }
 }
