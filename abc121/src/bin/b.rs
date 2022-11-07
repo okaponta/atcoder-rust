d1489dfb44ebbe7a2171ce6508e7d197622bf5ec
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-08 00:51:30 +0900

    add abc121_b

--- abc121/src/bin/b.rs
+++ abc121/src/bin/b.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        c:i64,
+        b:[i64;m],
+        a:[[i64;m];n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        a.into_iter()
+            .filter(|a| 0 < (0..m).into_iter().fold(c, |s, i| s + a[i] * b[i]))
+            .count()
+    );
 }
