4af52734e737f6135554f1aad0ad669ac4f92df9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-09 00:44:44 +0900

    add abc124_d

--- abc124/src/bin/d.rs
+++ abc124/src/bin/d.rs
@@ -1,8 +1,47 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        k:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut a = s.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a.push(('0', 0));
+    // 半開区間
+    let mut left = 0;
+    let mut right = 0;
+    let mut flip = 0;
+    let mut count = 0;
+    let mut ans = 0;
+    loop {
+        while flip <= k && right < a.len() {
+            if flip == k && a[right].0 == '0' {
+                break;
+            }
+            if a[right].0 == '0' {
+                flip += 1;
+            }
+            count += a[right].1;
+            right += 1;
+        }
+        ans = ans.max(count);
+        if right == a.len() {
+            break;
+        }
+        count -= a[left].1;
+        left += 1;
+        flip -= 1;
+        if a[left].0 == '0' {
+            count -= a[left].1;
+            left += 1;
+        }
+    }
+    println!("{}", ans);
 }
