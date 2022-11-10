d11d357c237c3098df6ba5bdf1ecdda0922014ba
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-10 23:41:53 +0900

    add abc274_d

--- abc274/src/bin/d.rs
+++ abc274/src/bin/d.rs
@@ -1,8 +1,48 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        x:i64,
+        y:i64,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut xset = HashSet::new();
+    let mut yset = HashSet::new();
+    xset.insert(a[0]);
+    yset.insert(0);
+    for i in 1..n {
+        let mut new_set = HashSet::new();
+        if i % 2 == 0 {
+            for x in xset {
+                if x + a[i] <= 10000 {
+                    new_set.insert(x + a[i]);
+                }
+                if -10000 <= x - a[i] {
+                    new_set.insert(x - a[i]);
+                }
+            }
+            xset = new_set;
+        } else {
+            for y in yset {
+                if y + a[i] <= 10000 {
+                    new_set.insert(y + a[i]);
+                }
+                if -10000 <= y - a[i] {
+                    new_set.insert(y - a[i]);
+                }
+            }
+            yset = new_set;
+        }
+    }
+    println!(
+        "{}",
+        if xset.contains(&x) && yset.contains(&y) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
