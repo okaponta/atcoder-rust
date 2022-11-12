7887376d13518b1d57254dc4dac810a9c93e6703
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-12 22:46:38 +0900

    add abc277_d

--- abc277/src/bin/d.rs
+++ abc277/src/bin/d.rs
@@ -3,6 +3,30 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    let mut sum = a[0];
+    let mut clus = vec![];
+    let mut temp = a[0];
+    for i in 1..n {
+        sum += a[i];
+        if a[i] <= a[i - 1] + 1 {
+            temp += a[i];
+        } else {
+            clus.push(temp);
+            temp = a[i];
+        }
+    }
+    clus.push(temp);
+    if a[0] == 0 && a[n - 1] == m - 1 {
+        let last = clus.pop().unwrap();
+        if clus.len() == 0 {
+            clus.push(last);
+        } else {
+            clus[0] += last;
+        }
+    }
+    println!("{}", sum - clus.into_iter().max().unwrap());
 }
