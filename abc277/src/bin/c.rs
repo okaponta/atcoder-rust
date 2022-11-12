fd4db8bf96207672a6fd47b0b457551a339e1dc1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-12 22:46:37 +0900

    add abc277_c

--- abc277/src/bin/c.rs
+++ abc277/src/bin/c.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        ab:[(Usize1,Usize1);n],
     }
-    println!("{}", n);
+    let mut ladders = HashMap::new();
+    for (a, b) in ab {
+        (*ladders.entry(a).or_insert(vec![])).push(b);
+        (*ladders.entry(b).or_insert(vec![])).push(a);
+    }
+    let mut q = VecDeque::new();
+    q.push_back(0);
+    let mut ans = 0;
+    let mut visited = HashSet::new();
+    while let Some(floor) = q.pop_front() {
+        if visited.contains(&floor) {
+            continue;
+        }
+        ans = ans.max(floor);
+        visited.insert(floor);
+        if !ladders.contains_key(&floor) {
+            continue;
+        }
+        for &next in &ladders[&floor] {
+            if visited.contains(&next) {
+                continue;
+            }
+            q.push_back(next);
+        }
+    }
+    println!("{}", ans + 1);
 }
