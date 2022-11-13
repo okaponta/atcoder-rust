0aac6f8048ee455615a7bf6543799731fcf152e2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-14 01:28:36 +0900

    add abc121_c

--- abc121/src/bin/c.rs
+++ abc121/src/bin/c.rs
@@ -3,6 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut m:usize,
+        mut ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    ab.sort();
+    let mut ans = 0;
+    for (a, b) in ab {
+        if m < b {
+            ans += a * m;
+            break;
+        } else {
+            ans += a * b;
+            m -= b;
+        }
+    }
+    println!("{}", ans);
 }
