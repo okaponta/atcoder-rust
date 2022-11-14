427258a9e45b552b047b6b5be6e83f047c0047b7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-15 02:54:37 +0900

    add abc274_e

--- abc274/src/bin/e.rs
+++ abc274/src/bin/e.rs
@@ -2,7 +2,43 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:usize,
+        m:usize,
+        xy:[(f64,f64);n],
+        pq:[(f64,f64);m],
     }
-    println!("{}", n);
+    let mut p = xy.iter().chain(pq.iter()).collect::<Vec<_>>();
+    p.insert(0, &(0.0, 0.0));
+    n += 1;
+    let l = p.len();
+    let inf = 1e15;
+    let mut dp = vec![vec![inf; l]; 1 << l];
+    dp[0][0] = 0.0;
+    let guide = (1 << l) - (1 << n);
+    for i in 0usize..1 << l {
+        for j in 0..l {
+            for k in 0..l {
+                if i >> k & 1 == 1 {
+                    // 訪問済み
+                    continue;
+                }
+                let cost = (p[j].0 - p[k].0).hypot(p[j].1 - p[k].1)
+                    / (1 << (i & guide).count_ones()) as f64;
+                dp[i | 1 << k][k] = min(dp[i | 1 << k][k], dp[i][j] + cost);
+            }
+        }
+    }
+    let mut ans = inf;
+    for i in (((1 << n) - 1)..1 << l).step_by(1 << n) {
+        ans = min(ans, dp[i][0]);
+    }
+    println!("{}", ans);
+}
+
+fn min(a: f64, b: f64) -> f64 {
+    if a < b {
+        a
+    } else {
+        b
+    }
 }
