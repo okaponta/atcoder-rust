df209c56ba4e0e8be2200596e9377d723c7fe856
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-15 03:05:14 +0900

    add abc274_e

--- abc274/src/bin/e.rs
+++ abc274/src/bin/e.rs
@@ -11,10 +11,17 @@ fn main() {
     p.insert(0, &(0.0, 0.0));
     n += 1;
     let l = p.len();
+    let mut dist = vec![vec![0.0; l]; l];
+    for i in 0..l {
+        for j in 0..l {
+            dist[i][j] = (p[i].0 - p[j].0).hypot(p[i].1 - p[j].1)
+        }
+    }
+
     let inf = 1e15;
     let mut dp = vec![vec![inf; l]; 1 << l];
     dp[0][0] = 0.0;
-    let guide = (1 << l) - (1 << n);
+    let mguide = (1 << l) - (1 << n);
     for i in 0usize..1 << l {
         for j in 0..l {
             for k in 0..l {
@@ -22,8 +29,7 @@ fn main() {
                     // 訪問済み
                     continue;
                 }
-                let cost = (p[j].0 - p[k].0).hypot(p[j].1 - p[k].1)
-                    / (1 << (i & guide).count_ones()) as f64;
+                let cost = dist[j][k] / (1 << (i & mguide).count_ones()) as f64;
                 dp[i | 1 << k][k] = min(dp[i | 1 << k][k], dp[i][j] + cost);
             }
         }
