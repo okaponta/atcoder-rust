ed594ecb82db24684093b0be21a79d6550c46ad3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-16 23:33:50 +0900

    add abc123_d

--- abc123/src/bin/d.rs
+++ abc123/src/bin/d.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use std::collections::BinaryHeap;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        x:usize,
+        y:usize,
+        z:usize,
+        k:usize,
+        a:[i64;x],
+        b:[i64;y],
+        c:[i64;z],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for i in 0..x {
+        for j in 0..y {
+            heap.push(-(a[i] + b[j]));
+            if k < heap.len() {
+                heap.pop();
+            }
+        }
+    }
+    let d = heap.into_iter().collect::<Vec<_>>();
+    let mut heap = BinaryHeap::new();
+    for i in 0..z {
+        for j in 0..d.len() {
+            heap.push(-(c[i] - d[j]));
+            if k < heap.len() {
+                heap.pop();
+            }
+        }
+    }
+    let mut ans = heap.into_iter().rev().map(|i| -i).collect::<Vec<_>>();
+    ans.sort_by(|a, b| b.cmp(a));
+    for ans in ans {
+        println!("{}", ans);
+    }
 }
