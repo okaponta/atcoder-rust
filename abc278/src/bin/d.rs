a1b1baee6835e546c70ac46a1a5c29db9ede4b79
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-19 22:43:48 +0900

    add abc278_d

--- abc278/src/bin/d.rs
+++ abc278/src/bin/d.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use std::collections::HashMap;
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
+        q:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    let mut base = 0;
+    for i in 0..n {
+        map.insert(i, a[i]);
+    }
+    for _ in 0..q {
+        input! {
+            i:u8
+        }
+        if i == 1 {
+            input! { x:usize }
+            base = x;
+            map = HashMap::new();
+        } else if i == 2 {
+            input! { i:Usize1, x:usize }
+            *map.entry(i).or_insert(0) += x;
+        } else {
+            input! { i:Usize1 }
+            println!("{}", map.get(&i).unwrap_or(&0) + base);
+        }
+    }
 }
