2b0215e077546b16ed28552dc8de65fc7c7e1cfb
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-19 22:43:44 +0900

    add abc278_a

--- abc278/src/bin/a.rs
+++ abc278/src/bin/a.rs
@@ -1,8 +1,15 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    for _ in 0..k {
+        a.remove(0);
+        a.push(0);
+    }
+    println!("{}", a.iter().join(" "));
 }
