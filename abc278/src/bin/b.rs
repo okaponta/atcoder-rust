33fa323b1d8e1a221d16629317d21ad90f51e1c7
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-19 22:43:46 +0900

    add abc278_b

--- abc278/src/bin/b.rs
+++ abc278/src/bin/b.rs
@@ -2,7 +2,30 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut min = h * 60 + m;
+    loop {
+        if is_ans(min) {
+            println!("{} {}", min / 60, min % 60);
+            return;
+        }
+        min += 1;
+        if min == 24 * 60 {
+            min = 0;
+        }
+    }
+}
+
+fn is_ans(min: usize) -> bool {
+    let h = min / 60;
+    let m = min % 60;
+    let a = h / 10;
+    let b = h % 10;
+    let c = m / 10;
+    if a == 2 {
+        return c < 4;
+    }
+    return b < 6;
 }
