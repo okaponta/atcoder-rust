99a1a7f9e6935be804f05c1db0496e555ac0911a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-22 11:46:21 +0900

    add abc119_b

--- abc119/src/bin/b.rs
+++ abc119/src/bin/b.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xu:[(f64,String);n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        xu.iter().fold(0.0, |s, (x, u)| {
+            s + if u == "JPY" { *x } else { x * 380000.0 }
+        })
+    );
 }
