495cdf4b187a6ef40eb120492cc79312f7c5ecd9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-24 21:33:40 +0900

    add abc118_b

--- abc118/src/bin/b.rs
+++ abc118/src/bin/b.rs
@@ -1,8 +1,21 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        _:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for _ in 0..n {
+        input! {
+            k:usize,
+            a:[usize;k],
+        }
+        for a in a {
+            *map.entry(a).or_insert(0) += 1;
+        }
+    }
+    println!("{}", map.values().filter(|&v| v == &n).count());
 }
