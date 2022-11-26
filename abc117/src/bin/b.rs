0d1cf9f02566e6ca160d8d2fe6b12ce48e1129d3
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-26 14:54:45 +0900

    add abc117_b

--- abc117/src/bin/b.rs
+++ abc117/src/bin/b.rs
@@ -3,6 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut l:[usize;n],
     }
-    println!("{}", n);
+    l.sort();
+    let max = l.remove(n - 1);
+    let sum = l.into_iter().sum::<usize>();
+    println!("{}", if max < sum { "Yes" } else { "No" });
 }
