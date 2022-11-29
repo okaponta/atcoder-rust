4aa305cb15a9450e97b4c7e5c862bf3e0bcca718
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-30 02:40:42 +0900

    add abc116_c

--- abc116/src/bin/c.rs
+++ abc116/src/bin/c.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut h:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..n {
+        while h[i] != 0 {
+            ans += 1;
+            let mut j = i;
+            while j < n && h[j] != 0 {
+                h[j] -= 1;
+                j += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
