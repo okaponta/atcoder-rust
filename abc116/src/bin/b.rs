ca5f953ddb4d543844bafa91b2438384573fca3e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-11-29 22:25:54 +0900

    add abc116_b

--- abc116/src/bin/b.rs
+++ abc116/src/bin/b.rs
@@ -1,8 +1,21 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        s:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    let mut i = 1;
+    let mut a = s;
+    loop {
+        if let Some(_) = map.get(&a) {
+            println!("{}", i);
+            return;
+        }
+        map.insert(a, i);
+        a = if a % 2 == 0 { a / 2 } else { 3 * a + 1 };
+        i += 1;
+    }
 }
