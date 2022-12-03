4d1c96e8fdee6af6c1e9756ecba549374a5c6818
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-03 22:47:42 +0900

    add abc280_e

--- abc280/src/bin/e.rs
+++ abc280/src/bin/e.rs
@@ -3,6 +3,37 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        p:i64,
     }
-    println!("{}", n);
+    let mut dp = vec![0; n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    let unit = modinv(100, 998244353);
+    let normal = (100 - p) * unit % 998244353;
+    let critical = p * unit % 998244353;
+    for i in 2..=n {
+        dp[i] = normal * (dp[i - 1] + 1);
+        dp[i] %= 998244353;
+        dp[i] += critical * (dp[i - 2] + 1);
+        dp[i] %= 998244353;
+    }
+    println!("{}", dp[n]);
+}
+
+fn modinv(mut a: i64, modulo: i64) -> i64 {
+    let mut b = modulo;
+    let mut u = 1;
+    let mut v = 0;
+    while b > 0 {
+        let t = a / b;
+        a -= t * b;
+        std::mem::swap(&mut a, &mut b);
+        u -= t * v;
+        std::mem::swap(&mut u, &mut v);
+    }
+    u %= modulo;
+    if u < 0 {
+        u += modulo;
+    }
+    u
 }
