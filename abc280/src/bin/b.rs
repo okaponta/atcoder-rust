12c7689decfbaf9b7cd9143e4c9f6fef3bdd2ec1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-03 22:42:04 +0900

    add abc280_b

--- abc280/src/bin/b.rs
+++ abc280/src/bin/b.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        mut s:[i64;n],
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    s.insert(0, 0);
+    for i in 1..=n {
+        ans.push(s[i] - s[i - 1]);
+    }
+    println!("{}", ans.into_iter().join(" "));
 }
