d1fa292b3d4d1f42c8d6054311a050e2bba75b5f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-06 01:16:01 +0900

    add abc114_b

--- abc114/src/bin/b.rs
+++ abc114/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        s.windows(3)
+            .map(|v| diff(dig(v[0]) * 100 + dig(v[1]) * 10 + dig(v[2]), 753))
+            .min()
+            .unwrap()
+    );
+}
+
+fn dig(c: char) -> usize {
+    c.to_digit(10).unwrap() as usize
+}
+
+fn diff(a: usize, b: usize) -> usize {
+    if a > b {
+        a - b
+    } else {
+        b - a
+    }
 }
