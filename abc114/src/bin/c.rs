285e082bd065639d7541824871f684077a894174
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-07 21:57:33 +0900

    add abc114_c

--- abc114/src/bin/c.rs
+++ abc114/src/bin/c.rs
@@ -4,5 +4,24 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for mut i in 0..1 << 20 {
+        let mut tmp = 0;
+        let mut ten = 1;
+        let mut flg = 0;
+        while 0 < i {
+            flg |= 1 << (i % 4);
+            tmp += ((i % 4) * 2 + 1) * ten;
+            i /= 4;
+            ten *= 10;
+        }
+        if flg != 14 {
+            continue;
+        }
+        if n < tmp {
+            println!("{}", ans);
+            return;
+        }
+        ans += 1;
+    }
 }
