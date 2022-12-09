a71f6ef1ecb576fd92e7e1709d2e13095c4359cb
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-09 22:41:47 +0900

    add abc113_b

--- abc113/src/bin/b.rs
+++ abc113/src/bin/b.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:i64,
+        a:i64,
+        h:[i64;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        h.into_iter()
+            .enumerate()
+            .map(|(i, h)| ((t * 1000 - h * 6 - a * 1000).abs(), i + 1))
+            .min()
+            .unwrap()
+            .1
+    );
 }
