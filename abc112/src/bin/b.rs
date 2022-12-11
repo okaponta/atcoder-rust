f9e9c651415ce241e51d9cd1ad1dd1716e82d967
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-11 23:02:16 +0900

    add abc112_b

--- abc112/src/bin/b.rs
+++ abc112/src/bin/b.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        ct:[(usize,usize);n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if let Some(c) = ct.into_iter().filter(|ct| ct.1 <= t).min() {
+            c.0.to_string()
+        } else {
+            "TLE".to_string()
+        }
+    );
 }
