dae042ab13ce38c5f287f790fb9a5cd935f77f04
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-15 23:03:34 +0900

    add abc111_c

--- abc111/src/bin/c.rs
+++ abc111/src/bin/c.rs
@@ -1,8 +1,44 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        v:[usize;n],
     }
-    println!("{}", n);
+    let mut odd = HashMap::new();
+    let mut even = HashMap::new();
+    for i in 0..n {
+        if i % 2 == 1 {
+            *odd.entry(v[i]).or_insert(0) += 1;
+        } else {
+            *even.entry(v[i]).or_insert(0) += 1;
+        }
+    }
+    odd.insert(0, 0);
+    even.insert(0, 0);
+    let mut odds = odd.into_iter().map(|(k, v)| (v, k)).collect::<Vec<_>>();
+    let mut evens = even.into_iter().map(|(k, v)| (v, k)).collect::<Vec<_>>();
+    odds.sort();
+    odds.reverse();
+    evens.sort();
+    evens.reverse();
+    println!(
+        "{}",
+        n - (odds[0].0
+            + if odds[0].1 == evens[0].1 {
+                evens[1].0
+            } else {
+                evens[0].0
+            })
+        .max(
+            odds[1].0
+                + if odds[1].1 == evens[0].1 {
+                    evens[1].0
+                } else {
+                    evens[0].0
+                },
+        )
+    );
 }
