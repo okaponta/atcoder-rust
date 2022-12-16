564887284741d4ef91d8842e38bddc1c5894b21f
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-16 19:14:20 +0900

    add abc115_c

--- abc115/src/bin/c.rs
+++ abc115/src/bin/c.rs
@@ -3,6 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        mut h:[usize;n],
     }
-    println!("{}", n);
+    h.sort();
+    let mut ans = h[n - 1];
+    for i in 0..=n - k {
+        ans = ans.min(h[i + k - 1] - h[i]);
+    }
+    println!(
+        "{}",
+        (0..=n - k)
+            .into_iter()
+            .map(|i| h[i + k - 1] - h[i])
+            .min()
+            .unwrap()
+    );
 }
