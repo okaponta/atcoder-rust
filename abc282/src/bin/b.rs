3a647d6300f8e9b5f054caa47c8b1989ca644403
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-17 23:13:56 +0900

    add abc282_b

--- abc282/src/bin/b.rs
+++ abc282/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for (a, b) in (0..n).into_iter().tuple_combinations() {
+        let mut count = 0;
+        for i in 0..m {
+            if s[a][i] == 'x' && s[b][i] == 'x' {
+                continue;
+            }
+            count += 1;
+        }
+        if count == m {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
