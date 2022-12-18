673225c76f1d75e30537021359e2c0e85d893ea4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-19 00:45:13 +0900

    add abc110_b

--- abc110/src/bin/b.rs
+++ abc110/src/bin/b.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        x:i32,
+        y:i32,
+        mut xi:[i32;n],
+        mut yi:[i32;m]
     }
-    println!("{}", n);
+    xi.push(x);
+    yi.push(y);
+    let diff = yi.into_iter().min().unwrap() - xi.into_iter().max().unwrap();
+    println!("{}", if 0 < diff { "No War" } else { "War" });
 }
