ea5c30fa3dd34b5b9362998fd28b67b818947319
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-20 23:08:56 +0900

    add abc110_c

--- abc110/src/bin/c.rs
+++ abc110/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let mut map = HashMap::new();
+    for i in 0..n {
+        if map.contains_key(&s[i]) {
+            if t[i] != map[&s[i]] {
+                println!("No");
+                return;
+            }
+        }
+        map.insert(s[i], t[i]);
+    }
+    let key_num = map.keys().len();
+    let value_num = map.values().unique_by(|&v| v).count();
+    println!("{}", if key_num == value_num { "Yes" } else { "No" });
 }
