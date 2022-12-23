10763980ba721b4df893f1a1ec1b97b7e21a755a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-23 23:52:06 +0900

    add abc109_b

--- abc109/src/bin/b.rs
+++ abc109/src/bin/b.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        w:[Chars;n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    set.insert(w[0].iter().collect::<String>());
+    for i in 1..n {
+        if w[i][0] != *w[i - 1].last().unwrap() {
+            println!("No");
+            return;
+        }
+        set.insert(w[i].iter().collect::<String>());
+    }
+    println!("{}", if set.len() == n { "Yes" } else { "No" });
 }
