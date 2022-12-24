64b2b91fdc8354d0a95cdde94f476d3f3cd99a5c
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-24 23:07:12 +0900

    add abc109_c

--- abc109/src/bin/c.rs
+++ abc109/src/bin/c.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xx:i64,
+        x:[i64;n],
     }
-    println!("{}", n);
+    println!("{}", x.into_iter().fold(0, |a, x| gcd(a, (x - xx).abs())));
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
