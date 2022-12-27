f89a51ba4634b42ab6b0a0106d24729f8a910c49
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-27 21:34:51 +0900

    add abc107_b

--- abc107/src/bin/b.rs
+++ abc107/src/bin/b.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use proconio::{fastout, input, marker::Chars};
+
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        a:[Chars;h],
     }
-    println!("{}", n);
+    let mut skip_row = HashSet::new();
+    for i in 0..h {
+        if a[i].iter().all(|c| c == &'.') {
+            skip_row.insert(i);
+        }
+    }
+    let mut skip_col = HashSet::new();
+    for j in 0..w {
+        if a.iter().all(|v| v[j] == '.') {
+            skip_col.insert(j);
+        }
+    }
+    for i in 0..h {
+        if skip_row.contains(&i) {
+            continue;
+        }
+        for j in 0..w {
+            if skip_col.contains(&j) {
+                continue;
+            }
+            print!("{}", a[i][j]);
+        }
+        println!();
+    }
 }
