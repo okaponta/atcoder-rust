22bc3a446a6aafaa67d489a15dc5b63d06ee9847
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-28 12:16:45 +0900

    add abc279_c

--- abc279/src/bin/c.rs
+++ abc279/src/bin/c.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
+        t:[Chars;h],
     }
-    println!("{}", n);
+    let mut st = vec![vec![]; w];
+    let mut tt = vec![vec![]; w];
+    for i in 0..h {
+        for j in 0..w {
+            st[j].push(s[i][j]);
+            tt[j].push(t[i][j]);
+        }
+    }
+    st.sort();
+    tt.sort();
+    for i in 0..w {
+        if st[i] != tt[i] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
