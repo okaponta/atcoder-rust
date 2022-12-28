7049d21de103648b55c1bdf8d5cac1a33010f020
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-28 15:31:12 +0900

    add abc279_d

--- abc279/src/bin/d.rs
+++ abc279/src/bin/d.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:f64,
+        b:f64,
     }
-    println!("{}", n);
+    let t = (a / (2.0 * b)).powf(2.0 / 3.0);
+    let t1 = t.floor();
+    let t2 = t.ceil();
+    let a1 = t1 * b + a / (t1 + 1.0).sqrt();
+    let a2 = t2 * b + a / (t2 + 1.0).sqrt();
+    println!("{}", a1.min(a2));
 }
