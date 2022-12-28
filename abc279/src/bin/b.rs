d1e98e28bc9dcfb53a436816151a05b139a151c6
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-28 12:09:32 +0900

    add abc279_b

--- abc279/src/bin/b.rs
+++ abc279/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let sl = s.len();
+    let tl = t.len();
+    if sl < tl {
+        println!("No");
+        return;
+    }
+    for i in 0..=sl - tl {
+        if t.iter().enumerate().all(|(j, c)| c == &s[i + j]) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
