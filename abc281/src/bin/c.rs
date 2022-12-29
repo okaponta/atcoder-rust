4f43a0d1e1ead2f7701017cc8741f88f5171f0fc
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-30 01:05:04 +0900

    add abc281_c

--- abc281/src/bin/c.rs
+++ abc281/src/bin/c.rs
@@ -1,8 +1,17 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut s = vec![0];
+    for i in 0..n {
+        s.push(s[i] + a[i]);
+    }
+    let rem = t % s[n];
+    let i = s.lower_bound(&rem);
+    println!("{} {}", i, rem - s[i - 1]);
 }
