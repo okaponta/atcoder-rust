157c4e38fe04302ad34a6dae5c1f77815fbd70ec
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-30 00:58:44 +0900

    add abc281_b

--- abc281/src/bin/b.rs
+++ abc281/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    if s.len() != 8 {
+        println!("No");
+        return;
+    }
+    if !(s[0].is_alphabetic() && s[7].is_alphabetic()) {
+        println!("No");
+        return;
+    }
+    if !(s[1].is_numeric() && s[1] != '0') {
+        println!("No");
+        return;
+    }
+    for i in 2..7 {
+        if !s[i].is_numeric() {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
