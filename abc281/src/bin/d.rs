9e54f32703969fe244f70c03cc1f0cacd1e3bbba
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-30 01:30:00 +0900

    add abc281_d

--- abc281/src/bin/d.rs
+++ abc281/src/bin/d.rs
@@ -3,6 +3,31 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        d:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut dp = vec![vec![None; d]; n + 1];
+    dp[0][0] = Some(0);
+    for i in 0..n {
+        for j in (0..=i).rev() {
+            for k in 0..d {
+                if let Some(bef) = dp[j][k] {
+                    if let Some(mx) = dp[j + 1][(k + a[i]) % d] {
+                        dp[j + 1][(k + a[i]) % d] = Some(mx.max(bef + a[i]));
+                    } else {
+                        dp[j + 1][(k + a[i]) % d] = Some(bef + a[i]);
+                    }
+                }
+            }
+        }
+    }
+    println!(
+        "{}",
+        if let Some(ans) = dp[k][0] {
+            ans as i64
+        } else {
+            -1
+        }
+    );
 }
