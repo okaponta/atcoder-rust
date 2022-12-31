67f4f5bde1983f1d5f1f94fedaa046c0595e65b2
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-12-31 16:53:59 +0900

    add abc283_b

--- abc283/src/bin/b.rs
+++ abc283/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
+        q:usize,
     }
-    println!("{}", n);
+    for _ in 0..q {
+        input! {q: u8, k:Usize1}
+        if q == 1 {
+            input! {x:usize}
+            a[k] = x;
+        } else {
+            println!("{}", a[k]);
+        }
+    }
 }
