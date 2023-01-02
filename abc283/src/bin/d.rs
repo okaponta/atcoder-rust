5da6767b7ea80e3c82e422957eda6ec4c2fd9ba7
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-02 18:47:34 +0900

    add abc283_d

--- abc283/src/bin/d.rs
+++ abc283/src/bin/d.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut used = vec![false; 26];
+    let mut tmp = vec![vec![false; 26]];
+    let mut lev = 0;
+    for c in s {
+        if c == '(' {
+            lev += 1;
+            if tmp.len() == lev {
+                tmp.push(vec![false; 26]);
+            }
+        } else if c == ')' {
+            for i in 0..26 {
+                if tmp[lev][i] {
+                    used[i] = false;
+                    tmp[lev][i] = false;
+                }
+            }
+            lev -= 1;
+        } else {
+            let idx = (c as u8 - b'a') as usize;
+            if used[idx] {
+                println!("No");
+                return;
+            } else {
+                used[idx] = true;
+                tmp[lev][idx] = true;
+            }
+        }
+    }
+    println!("Yes");
 }
