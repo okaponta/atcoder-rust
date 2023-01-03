ddc78e0d1dab9436952057bd767cd81a15902cc6
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-03 16:35:13 +0900

    add abc108_b

--- abc108/src/bin/b.rs
+++ abc108/src/bin/b.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        x1:i32,
+        y1:i32,
+        x2:i32,
+        y2:i32,
     }
-    println!("{}", n);
+    let x = x2 - x1;
+    let y = y2 - y1;
+    let x3 = x2 - y;
+    let y3 = y2 + x;
+    let x4 = x3 - x;
+    let y4 = y3 - y;
+    println!("{} {} {} {}", x3, y3, x4, y4);
 }
