8783518b98119a3c5ac6c2e4034b918f6faa7f21
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-04 01:27:47 +0900

    add abc108_c

--- abc108/src/bin/c.rs
+++ abc108/src/bin/c.rs
@@ -3,6 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut ans = (n / k).pow(3);
+    if k % 2 == 0 {
+        ans += ((n + k / 2) / k).pow(3);
+    }
+    println!("{}", ans);
 }
