c0dd1d7e59989bedc088b661edb282785ce9e4ff
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-07 23:49:22 +0900

    add abc284_c

--- abc284/src/bin/c.rs
+++ abc284/src/bin/c.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for (u, v) in uv {
+        uf.union(u, v);
+    }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        set.insert(uf.find(i));
+    }
+    println!("{}", set.len());
 }
