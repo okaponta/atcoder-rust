55c49a03fc7b4fcf4110668906c689d5b1c42e4a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-13 23:57:33 +0900

    add abc106_c

--- abc106/src/bin/c.rs
+++ abc106/src/bin/c.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        k:usize,
     }
-    println!("{}", n);
+    let mut one = 0;
+    let mut num = '0';
+    for i in 0..s.len() {
+        if s[i] == '1' {
+            one += 1;
+        } else {
+            num = s[i];
+            break;
+        }
+    }
+    if k <= one {
+        println!("1");
+    } else {
+        println!("{}", num);
+    }
 }
