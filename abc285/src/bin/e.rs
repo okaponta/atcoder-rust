c0fe55d56e8e9f82a173b7702a4a6308c63b2a56
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-16 00:18:26 +0900

    add abc285_e

--- abc285/src/bin/e.rs
+++ abc285/src/bin/e.rs
@@ -9,15 +9,14 @@ fn main() {
     for i in 0..n {
         s.push(s[i] + a[i / 2]);
     }
-    let mut dp = vec![0; n];
-    for i in 0..n {
-        let mut next = vec![0; n];
+    let mut dp = vec![0; n + 1];
+    for i in 0..=n {
+        let mut next = vec![0; n + 1];
         for j in 0..i {
             next[j + 1] = dp[j];
             next[0] = next[0].max(dp[j] + s[j]);
         }
         dp = next;
     }
-    let ans = (0..n).into_iter().map(|i| dp[i] + s[i]).max().unwrap_or(0);
-    println!("{}", ans);
+    println!("{}", dp[0]);
 }
