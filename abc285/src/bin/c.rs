8313bfd5b99b365d0c8631021a5d8d3e66403e1c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-15 23:18:40 +0900

    add abc285_c

--- abc285/src/bin/c.rs
+++ abc285/src/bin/c.rs
@@ -1,8 +1,12 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        s.into_iter()
+            .fold(0, |s, c| s * 26 + (c as u8 - b'@') as usize)
+    );
 }
