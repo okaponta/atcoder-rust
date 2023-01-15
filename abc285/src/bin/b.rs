a058f1ad20a7bd533e14909b67e89960bfd85e58
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-15 23:09:20 +0900

    add abc285_b

--- abc285/src/bin/b.rs
+++ abc285/src/bin/b.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    for i in 1..n {
+        for j in 0..=n - i {
+            if j == n - i || s[j] == s[j + i] {
+                println!("{}", j);
+                break;
+            }
+        }
+    }
 }
