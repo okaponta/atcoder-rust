88fccd314115fb31c17b20998d7c62a93145d2af
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-17 22:28:47 +0900

    add abc105_b

--- abc105/src/bin/b.rs
+++ abc105/src/bin/b.rs
@@ -1,8 +1,8 @@
 use proconio::input;
 
+const NG: [usize; 9] = [1, 2, 3, 5, 6, 9, 10, 13, 17];
+
 fn main() {
-    input! {
-        n:usize,
-    }
-    println!("{}", n);
+    input! {n:usize}
+    println!("{}", if NG.contains(&n) { "No" } else { "Yes" });
 }
