2bbae192367241e790be6495f65b4c7518f89950
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-19 21:41:28 +0900

    add abc104_b

--- abc104/src/bin/b.rs
+++ abc104/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if s[0] != 'A'
+            || (2..s.len() - 1)
+                .into_iter()
+                .filter(|&i| s[i] == 'C')
+                .count()
+                != 1
+            || (0..s.len())
+                .into_iter()
+                .filter(|&i| b'a' <= s[i] as u8 && s[i] as u8 <= b'z')
+                .count()
+                != s.len() - 2
+        {
+            "WA"
+        } else {
+            "AC"
+        }
+    )
 }
