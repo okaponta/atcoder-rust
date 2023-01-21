973025fb6c9ed533779ecf27d83255ad3838a064
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-21 22:54:31 +0900

    add abc286_a

--- abc286/src/bin/a.rs
+++ abc286/src/bin/a.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        pqrs:[Usize1;4],
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    for i in 0..=pqrs[1] - pqrs[0] {
+        a.swap(pqrs[0] + i, pqrs[2] + i);
+    }
+    println!("{}", a.iter().join(" "));
 }
