926a97763411cefcd81d8ad1592aefd540316dd9
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-21 22:54:35 +0900

    add abc286_d

--- abc286/src/bin/d.rs
+++ abc286/src/bin/d.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut ans = vec![false; x + 1];
+    ans[0] = true;
+    for (a, b) in ab {
+        for i in (0..x).rev() {
+            if !ans[i] {
+                continue;
+            }
+            for j in 1..=b {
+                if x < i + j * a {
+                    break;
+                }
+                ans[i + j * a] = true;
+            }
+        }
+    }
+    println!("{}", if ans[x] { "Yes" } else { "No" });
 }
