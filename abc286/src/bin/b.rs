38223c896d3b95e51d4be8843a1dd271bb46ba43
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-21 22:54:33 +0900

    add abc286_b

--- abc286/src/bin/b.rs
+++ abc286/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    let mut i = 0;
+    loop {
+        if s.len() - 1 <= i {
+            break;
+        }
+        if s[i] == 'n' && s[i + 1] == 'a' {
+            s.insert(i + 1, 'y');
+            i += 2;
+        } else {
+            i += 1;
+        }
+    }
+    println!("{}", s.iter().collect::<String>());
 }
