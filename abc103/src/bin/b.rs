b770696e870ca5bd62f9b98e27659ee2c8bede45
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-22 13:23:41 +0900

    add abc103_b

--- abc103/src/bin/b.rs
+++ abc103/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        mut s:Chars,
+        t:String,
     }
-    println!("{}", n);
+    let n = s.len();
+    for _ in 0..n {
+        if s.iter().collect::<String>() == t {
+            println!("Yes");
+            return;
+        }
+        s.push(s[0]);
+        s.remove(0);
+    }
+    println!("No");
 }
