6f38dcdd47ec47f2be2ab0a3d8699a0bf041c457
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-27 23:38:12 +0900

    add abc102_c

--- abc102/src/bin/c.rs
+++ abc102/src/bin/c.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut first = (0..n)
+        .into_iter()
+        .map(|i| a[i] - (i + 1) as i64)
+        .collect::<Vec<_>>();
+    first.sort();
+    let b = first[n / 2];
+    println!("{}", first.into_iter().fold(0, |s, ai| s + (ai - b).abs()));
 }
