aed07fd1de4b9fed77b5772cd102f197ac760711
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-01-28 22:48:36 +0900

    add abc287_d

--- abc287/src/bin/d.rs
+++ abc287/src/bin/d.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let m = t.len();
+    let mut ss = vec![];
+    for i in (0..m).rev() {
+        ss.push(s[n - 1 - i]);
+    }
+    let mut count = 0;
+    let mut is_ok = vec![false; m];
+    for i in 0..m {
+        if ss[i] == '?' || t[i] == '?' || ss[i] == t[i] {
+            is_ok[i] = true;
+            count += 1;
+        }
+    }
+    println!("{}", if count == m { "Yes" } else { "No" });
+    for i in 0..m {
+        if is_ok[i] {
+            count -= 1;
+        }
+        if s[i] == '?' || t[i] == '?' || s[i] == t[i] {
+            is_ok[i] = true;
+            count += 1;
+        }
+        println!("{}", if count == m { "Yes" } else { "No" });
+    }
 }
