9a17797e71041de24db045faf62c786382cf5282
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-05 00:43:29 +0900

    add abc288_d

--- abc288/src/bin/d.rs
+++ abc288/src/bin/d.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[i64;n],
+        q:usize,
+        lr:[(Usize1,usize);q],
     }
-    println!("{}", n);
+    let mut s = vec![vec![0; k]];
+    for i in 0..n {
+        let mut next = s[i].clone();
+        next[i % k] += a[i];
+        s.push(next);
+    }
+    for (l, r) in lr {
+        let ok = (0..k).into_iter().map(|i| s[l][i] - s[r][i]).all_equal();
+        println!("{}", if ok { "Yes" } else { "No" });
+    }
 }
