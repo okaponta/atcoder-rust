229b82832fb9beeaf9ff3ba1fddcae0407f5bde7
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-05 00:16:28 +0900

    add abc288_f

--- abc288/src/bin/f.rs
+++ abc288/src/bin/f.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        x:Chars,
     }
-    println!("{}", n);
+    let m = 998244353;
+    let mut ans = cv(x[0]);
+    let mut sum = 1 + cv(x[0]);
+    for i in 1..n {
+        ans = (10 * ans + sum * cv(x[i])) % m;
+        sum = (sum + ans) % m;
+    }
+    println!("{}", ans);
+}
+
+fn cv(c: char) -> usize {
+    c.to_digit(10).unwrap() as usize
 }
