ea398a8876ccfab88dfecfbc91258926f99c5acf
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-05 00:16:23 +0900

    add abc288_b

--- abc288/src/bin/b.rs
+++ abc288/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        s:[String;n],
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 0..k {
+        ans.push(&s[i]);
+    }
+    ans.sort();
+    for s in ans {
+        println!("{}", s);
+    }
 }
