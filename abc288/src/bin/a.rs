dfe7b6c70a5c2fe547d0324aae0b40c11311146a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-05 00:16:21 +0900

    add abc288_a

--- abc288/src/bin/a.rs
+++ abc288/src/bin/a.rs
@@ -1,8 +1,12 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        ab:[(i64,i64);n],
     }
-    println!("{}", n);
+    for (a, b) in ab {
+        println!("{}", a + b);
+    }
 }
