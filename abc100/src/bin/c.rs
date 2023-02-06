a989310f865b01b8eb3f251b9bd626d1c64bb8ff
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-07 02:02:18 +0900

    add abc100_c

--- abc100/src/bin/c.rs
+++ abc100/src/bin/c.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let ans = a.into_iter().fold(0, |s, a| s + pow2(a));
+    println!("{}", ans);
+}
+
+fn pow2(mut a: usize) -> usize {
+    let mut res = 0;
+    while a & 1 == 0 {
+        a >>= 1;
+        res += 1;
+    }
+    res
 }
