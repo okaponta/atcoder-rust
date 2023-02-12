1c07cfa3ed4b7d8989db3440e2e0b2a2d27d7e03
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-12 23:13:10 +0900

    add abc120_c

--- abc120/src/bin/c.rs
+++ abc120/src/bin/c.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    let mut count = 0;
+    for c in s {
+        if let Some(top) = q.pop_front() {
+            if top == c {
+                q.push_back(top);
+                q.push_back(c);
+            } else {
+                count += 2;
+            }
+        } else {
+            q.push_back(c);
+        }
+    }
+    println!("{}", count);
 }
