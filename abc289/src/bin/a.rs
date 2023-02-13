c58e4bbdd406bc5d42a467eebe48d4a4b830ecb2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-13 14:04:38 +0900

    add abc289_a

--- abc289/src/bin/a.rs
+++ abc289/src/bin/a.rs
@@ -1,8 +1,12 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    for c in s {
+        print!("{}", if c == '0' { '1' } else { '0' });
+    }
+    println!();
 }
