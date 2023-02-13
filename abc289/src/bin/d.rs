ed0c30c338fbe4bbdde102a675749afc6e8aaaa8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-13 14:26:45 +0900

    add abc289_d

--- abc289/src/bin/d.rs
+++ abc289/src/bin/d.rs
@@ -1,8 +1,29 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
+        m:usize,
+        b:[usize;m],
+        x:usize,
     }
-    println!("{}", n);
+    let ng = b.into_iter().collect::<HashSet<_>>();
+    let mut ok = vec![false; x + 1];
+    ok[0] = true;
+    for i in 0..x {
+        if !ok[i] {
+            continue;
+        }
+        for j in 0..n {
+            let next = i + a[j];
+            if ng.contains(&next) || x < next {
+                continue;
+            }
+            ok[next] = true;
+        }
+    }
+    println!("{}", if ok[x] { "Yes" } else { "No" });
 }
