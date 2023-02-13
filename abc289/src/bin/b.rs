c191d92a7426f230c5e6ec2d5335cf328d33a2f9
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-13 14:13:11 +0900

    add abc289_b

--- abc289/src/bin/b.rs
+++ abc289/src/bin/b.rs
@@ -1,8 +1,24 @@
+use std::collections::{HashSet, VecDeque};
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;m],
     }
-    println!("{}", n);
+    let set = a.into_iter().collect::<HashSet<_>>();
+    let mut q = VecDeque::new();
+    let mut ans = vec![];
+    for i in 1..=n {
+        q.push_front(i);
+        if !set.contains(&i) {
+            while let Some(j) = q.pop_front() {
+                ans.push(j);
+            }
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
