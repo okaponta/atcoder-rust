e446d2572b1fbb12f867f8a83aa8c915638565ce
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-14 22:42:49 +0900

    add abc118_c

--- abc118/src/bin/c.rs
+++ abc118/src/bin/c.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for a in a {
+        ans = gcd(ans, a);
+    }
+    println!("{}", ans);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
