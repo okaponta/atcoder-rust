6d3323ec29f65b7f7860f458862e7372139a089b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-15 23:16:25 +0900

    add abc117_c

--- abc117/src/bin/c.rs
+++ abc117/src/bin/c.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut x:[i64;m],
     }
-    println!("{}", n);
+    x.sort();
+    let mut diff = vec![];
+    for i in 1..m {
+        diff.push(x[i] - x[i - 1]);
+    }
+    diff.sort();
+    let mut ans = 0;
+    for i in 0..m.saturating_sub(n) {
+        ans += diff[i];
+    }
+    println!("{}", ans);
 }
