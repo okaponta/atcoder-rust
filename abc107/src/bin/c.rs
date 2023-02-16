29f951aee5300c1955e8c01b29a360105f25428c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-16 22:47:36 +0900

    add abc107_c

--- abc107/src/bin/c.rs
+++ abc107/src/bin/c.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        x:[i64;n],
     }
-    println!("{}", n);
+    let mut x1 = vec![1 << 60; n];
+    let mut x2 = vec![1 << 60; n];
+    for i in 0..n {
+        if x[i] < 0 {
+            x1[i] = -x[i];
+        } else {
+            x2[i] = x[i];
+        }
+    }
+    x1.push(0);
+    x2.push(0);
+    x1.sort();
+    x2.sort();
+    let ans = (0..=k)
+        .into_iter()
+        .map(|i| (x1[i] * 2 + x2[k - i]).min(x1[i] + x2[k - i] * 2))
+        .min()
+        .unwrap();
+    println!("{}", ans);
 }
