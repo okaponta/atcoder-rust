ed8521eb03af7a094227d2d21da9ffc102b32ff8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-19 22:40:15 +0900

    add abc290_d

--- abc290/src/bin/d.rs
+++ abc290/src/bin/d.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        t:usize,
     }
-    println!("{}", n);
+    for _ in 0..t {
+        input! {
+            n:usize,
+            d:usize,
+            k:Usize1,
+        }
+        let g = gcd(n, d);
+        if g == 1 {
+            println!("{}", k * d % n);
+        } else {
+            let cycle = n / g;
+            let mut ans = k * d % n;
+            ans += k / cycle;
+            println!("{}", ans % n);
+        }
+    }
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
