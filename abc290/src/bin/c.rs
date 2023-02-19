4e00bda42a15f8a80533898475d367bddfcddd1f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-19 22:40:14 +0900

    add abc290_c

--- abc290/src/bin/c.rs
+++ abc290/src/bin/c.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    let mut ans = 0;
+    for i in 0..n {
+        if a[i] == ans && ans < k {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
