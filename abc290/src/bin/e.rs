28aea4aec50801676ec767ac5b5fab8920449377
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-20 01:49:09 +0900

    add abc290_e

--- abc290/src/bin/e.rs
+++ abc290/src/bin/e.rs
@@ -1,8 +1,38 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    let mut ans = 0;
+    for i in 0..n {
+        (*map.entry(a[i]).or_insert(vec![])).push(i);
+        ans += ((i + 1) / 2) * (n - i);
+    }
+    for (_, v) in map {
+        ans -= calc(v, n);
+    }
+    println!("{}", ans);
+}
+
+fn calc(v: Vec<usize>, n: usize) -> usize {
+    let mut res = 0;
+    let mut l = 0;
+    let mut r = v.len() - 1;
+    while l != r {
+        let posl = v[l] + 1;
+        let posr = n - v[r];
+        if posl < posr {
+            res += posl * (r - l);
+            l += 1;
+        } else {
+            res += posr * (r - l);
+            r -= 1;
+        }
+    }
+    res
 }
