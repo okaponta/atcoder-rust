b0f0132a37742af21b1bd4c894de12edacc7ba6f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-19 22:40:11 +0900

    add abc290_a

--- abc290/src/bin/a.rs
+++ abc290/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
+        b:[Usize1;m],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..m {
+        ans += a[b[i]];
+    }
+    println!("{}", ans);
 }
