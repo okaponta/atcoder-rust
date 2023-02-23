169d307f42d07377119fa21f51285203276001f6
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-23 15:57:51 +0900

    add abc029_c

--- abc029/src/bin/c.rs
+++ abc029/src/bin/c.rs
@@ -1,9 +1,22 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-       a:i32,
+       n:usize,
     }
-    println!("{}", a);
-    todo!();
+    dfs(&mut vec!['a'; n], 0);
+}
+
+fn dfs(s: &mut Vec<char>, d: usize) {
+    if d == s.len() {
+        println!("{}", s.iter().collect::<String>());
+        return;
+    }
+    s[d] = 'a';
+    dfs(s, d + 1);
+    s[d] = 'b';
+    dfs(s, d + 1);
+    s[d] = 'c';
+    dfs(s, d + 1);
 }
