f18a2992a8ac3c8f17d903fd7a6bdda38b6d973f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-01 22:42:16 +0900

    add abc291_b

--- abc291/src/bin/b.rs
+++ abc291/src/bin/b.rs
@@ -3,6 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut x:[usize;5*n],
     }
-    println!("{}", n);
+    x.sort();
+    let sum = (n..4 * n).into_iter().fold(0, |s, e| s + x[e]);
+    println!("{}", sum as f64 / (3 * n) as f64);
 }
