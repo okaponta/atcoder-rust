c78ee97a5eb17a2f3cc8fd38553638cd48d350ab
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-04 22:53:54 +0900

    add abc292_c

--- abc292/src/bin/c.rs
+++ abc292/src/bin/c.rs
@@ -1,8 +1,36 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut fac = vec![1; n + 1];
+    for i in 2..=n {
+        fac[i] = factorize(i as i64);
+    }
+    let mut ans = 0;
+    for i in 1..n {
+        ans += fac[i] * fac[n - i];
+    }
+    println!("{}", ans);
+}
+
+fn factorize(mut n: i64) -> usize {
+    let mut res = 1;
+    for i in 2..=n.sqrt() {
+        if n % i != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % i == 0 {
+            ex += 1;
+            n /= i;
+        }
+        res *= ex + 1;
+    }
+    if n != 1 {
+        res *= 2;
+    }
+    res
 }
