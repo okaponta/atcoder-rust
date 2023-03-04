3a0fd8ef892ee7603142703aa5d55a9fc54527bd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-04 22:53:53 +0900

    add abc292_b

--- abc292/src/bin/b.rs
+++ abc292/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        ix:[(u8,Usize1);q],
     }
-    println!("{}", n);
+    let mut count = vec![0; n];
+    for (i, x) in ix {
+        if i == 1 {
+            count[x] += 1;
+        }
+        if i == 2 {
+            count[x] += 2;
+        }
+        if i == 3 {
+            println!("{}", if count[x] > 1 { "Yes" } else { "No" });
+        }
+    }
 }
