92a9df0f7fcea80214b3bddbed00188757fd76dc
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-04 22:53:55 +0900

    add abc292_d

--- abc292/src/bin/d.rs
+++ abc292/src/bin/d.rs
@@ -1,8 +1,96 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for i in 0..m {
+        uf.union(uv[i].0, uv[i].1);
+    }
+    let mut count = vec![0; n];
+    for i in 0..m {
+        count[uf.root(uv[i].0)] += 1;
+    }
+    let mut target = HashSet::new();
+    for i in 0..n {
+        target.insert(uf.root(i));
+    }
+    for i in target {
+        if count[i] != uf.size(i) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
+}
+
+pub struct UnionFind {
+    parent: Vec<usize>,
+    size: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+            rank: vec![0; n],
+        }
+    }
+
+    // 根を返却
+    pub fn root(&mut self, x: usize) -> usize {
+        // parentが自分自身の場合は根
+        if self.parent[x] == x {
+            return x;
+        }
+        // 経路圧縮
+        self.parent[x] = self.root(self.parent[x]);
+        self.parent[x]
+    }
+
+    // xとyが同じ根か判定
+    pub fn equiv(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    // xとyを合体
+    pub fn union(&mut self, x: usize, y: usize) -> bool {
+        let mut rx = self.root(x);
+        let mut ry = self.root(y);
+        // 既に同じ
+        if rx == ry {
+            return false;
+        }
+
+        // ryのrankが小さくなるように調整
+        if self.rank[rx] < self.rank[ry] {
+            std::mem::swap(&mut rx, &mut ry);
+        }
+        // ryの根をrxにする
+        self.parent[ry] = rx;
+        // rxのrank調整
+        if self.rank[rx] == self.rank[ry] {
+            self.rank[rx] += 1;
+        }
+        self.size[rx] += self.size[ry];
+        true
+    }
+
+    // xのグループの要素数
+    pub fn size(&mut self, x: usize) -> usize {
+        let root = self.root(x);
+        self.size[root]
+    }
+
+    // 連結かどうかを返却する
+    pub fn is_linked(&mut self) -> bool {
+        self.size(0) == self.size.len()
+    }
 }
