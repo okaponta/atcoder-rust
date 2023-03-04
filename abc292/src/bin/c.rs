efecb8ac113d2f9b6aee2995e75c2cb8ba00e09c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-05 01:07:05 +0900

    add abc292_c

--- abc292/src/bin/c.rs
+++ abc292/src/bin/c.rs
@@ -1,36 +1,15 @@
-use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    let mut fac = vec![1; n + 1];
-    for i in 2..=n {
-        fac[i] = factorize(i as i64);
+    let mut div = vec![1i64; n];
+    for i in 2..n {
+        for j in (i..n).step_by(i) {
+            div[j] += 1;
+        }
     }
-    let mut ans = 0;
-    for i in 1..n {
-        ans += fac[i] * fac[n - i];
-    }
+    let ans = (1..n).into_iter().fold(0, |s, i| s + div[i] * div[n - i]);
     println!("{}", ans);
-}
-
-fn factorize(mut n: i64) -> usize {
-    let mut res = 1;
-    for i in 2..=n.sqrt() {
-        if n % i != 0 {
-            continue;
-        }
-        let mut ex = 0;
-        while n % i == 0 {
-            ex += 1;
-            n /= i;
-        }
-        res *= ex + 1;
-    }
-    if n != 1 {
-        res *= 2;
-    }
-    res
 }
