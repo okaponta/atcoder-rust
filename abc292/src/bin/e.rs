c65d906bce75954ae2f74a102806c6b8add32b36
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-04 22:53:56 +0900

    add abc292_e

--- abc292/src/bin/e.rs
+++ abc292/src/bin/e.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut g = vec![HashSet::new(); n];
+    let mut rev_g = vec![HashSet::new(); n];
+    for (u, v) in uv {
+        g[u].insert(v);
+        rev_g[v].insert(u);
+    }
+    let mut count = 0;
+    for i in 0..n {
+        let mut tmp = vec![];
+        for &j in &g[i] {
+            for &k in &rev_g[i] {
+                if j != k && !g[k].contains(&j) {
+                    tmp.push((k, j));
+                }
+            }
+        }
+        for (k, j) in tmp {
+            count += 1;
+            if !g[k].contains(&j) {
+                g[k].insert(j);
+                rev_g[j].insert(k);
+            }
+        }
+    }
+    println!("{}", count);
 }
