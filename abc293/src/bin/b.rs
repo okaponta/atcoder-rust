a75424c461a63b4d751234db7ebbfc6bb1da100e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-11 22:58:41 +0900

    add abc293_b

--- abc293/src/bin/b.rs
+++ abc293/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut used = vec![false; n];
+    for i in 0..n {
+        if used[i] {
+            continue;
+        }
+        used[a[i]] = true;
+    }
+    let mut ans = vec![];
+    for i in 0..n {
+        if !used[i] {
+            ans.push(i + 1);
+        }
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
