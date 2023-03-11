8d2bbfad73ed3d76bfeaa98d3a27f42f31fec6c2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-11 22:58:42 +0900

    add abc293_c

--- abc293/src/bin/c.rs
+++ abc293/src/bin/c.rs
@@ -1,8 +1,32 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        a:[[usize;w];h],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for v in (0..h + w - 2).combinations(h - 1) {
+        let mut set = HashSet::new();
+        let mut cur = vec![0, 0];
+        let mut step = 0;
+        while cur != vec![h - 1, w - 1] {
+            set.insert(a[cur[0]][cur[1]]);
+            if v.contains(&step) {
+                cur[0] += 1;
+            } else {
+                cur[1] += 1;
+            }
+            step += 1;
+        }
+        set.insert(a[cur[0]][cur[1]]);
+        if set.len() == h + w - 1 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
