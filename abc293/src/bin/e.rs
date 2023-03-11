3f8e856778e7fefa2f9b5bddf67191325ae1c049
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-11 22:58:45 +0900

    add abc293_e

--- abc293/src/bin/e.rs
+++ abc293/src/bin/e.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        x:usize,
+        m:usize,
     }
-    println!("{}", n);
+    // aの2^i乗
+    let mut pow = vec![1, a % m];
+    // 2^i-1までの和
+    let mut s = vec![1, (a + 1) % m];
+    for i in 2..45 {
+        pow.push((pow[i - 1] * pow[i - 1]) % m);
+        s.push((s[i - 1] + s[i - 1] * pow[i]) % m);
+    }
+    let mut ans = 0;
+    let mut cur = 1;
+    for i in (0..45).rev() {
+        if x >> i & 1 == 1 {
+            ans += s[i] * cur;
+            cur *= pow[i + 1];
+            ans %= m;
+            cur %= m;
+        }
+    }
+    println!("{}", ans);
 }
