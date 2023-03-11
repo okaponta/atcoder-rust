7d57f3bb0493f5f32cf11f736581b1b15555ad06
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-11 22:58:39 +0900

    add abc293_a

--- abc293/src/bin/a.rs
+++ abc293/src/bin/a.rs
@@ -1,8 +1,11 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    for i in 0..s.len() / 2 {
+        s.swap(2 * i, 2 * i + 1);
+    }
+    println!("{}", s.into_iter().collect::<String>());
 }
