d489496acc9140a298690647e590242f9c961c3e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-13 02:26:24 +0900

    add abc098_b

--- abc098/src/bin/b.rs
+++ abc098/src/bin/b.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut x_count = vec![0; 26];
+    let mut y_count = vec![0; 26];
+    for i in 0..n {
+        y_count[(s[i] as u8 - b'a') as usize] += 1;
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        let c = (s[i] as u8 - b'a') as usize;
+        x_count[c] += 1;
+        y_count[c] -= 1;
+        let mut tmp = 0;
+        for j in 0..26 {
+            if 0 < x_count[j] && 0 < y_count[j] {
+                tmp += 1;
+            }
+        }
+        ans = ans.max(tmp);
+    }
+    println!("{}", ans);
 }
