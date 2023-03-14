4fbc694007563f8b2494d9a2e7288cc5a724fc47
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-14 23:12:41 +0900

    add abc098_c

--- abc098/src/bin/c.rs
+++ abc098/src/bin/c.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    s.insert(0, 'E');
+    s.push('W');
+    let e = s.iter().filter(|&&c| c == 'E').count() - 1;
+    let mut ans = e;
+    let mut tmp = e;
+    for i in 0..=n {
+        if s[i] == 'W' {
+            tmp += 1;
+        }
+        if s[i + 1] == 'E' {
+            tmp -= 1;
+        }
+        ans = ans.min(tmp);
+    }
+    println!("{}", ans);
 }
