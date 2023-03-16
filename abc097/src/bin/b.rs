0ee06224f76fac99f872251efabfb172bfd99a08
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-16 22:59:08 +0900

    add abc097_b

--- abc097/src/bin/b.rs
+++ abc097/src/bin/b.rs
@@ -1,8 +1,17 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        x:usize,
     }
-    println!("{}", n);
+    let mut ans = 1;
+    for b in 2..=x.sqrt() {
+        let mut tmp = b * b;
+        while tmp <= x {
+            ans = ans.max(tmp);
+            tmp *= b;
+        }
+    }
+    println!("{}", ans);
 }
