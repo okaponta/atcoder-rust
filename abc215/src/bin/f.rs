e4567273e96db76a87ae482a832e969bbf743c5c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-17 22:44:39 +0900

    add abc215_f

--- abc215/src/bin/f.rs
+++ abc215/src/bin/f.rs
@@ -1,9 +1,43 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut xy:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    xy.sort();
+    let mut lower = 0;
+    let mut upper = 1 << 30;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_over(med, &xy) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", lower);
+}
+
+fn is_over(k: i64, xy: &Vec<(i64, i64)>) -> bool {
+    let mut min = 1 << 30;
+    let mut max = 0;
+    let mut q = VecDeque::new();
+    for &(x, y) in xy {
+        while let Some((px, py)) = q.pop_front() {
+            if x < px + k {
+                q.push_front((px, py));
+                break;
+            }
+            min = min.min(py);
+            max = max.max(py);
+        }
+        if min + k <= y || y <= max - k {
+            return true;
+        }
+        q.push_back((x, y));
+    }
+    false
 }
