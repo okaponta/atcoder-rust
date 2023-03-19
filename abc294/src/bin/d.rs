3d1103623284c80f72c8a9e914d7fc58b66b220e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-19 22:41:31 +0900

    add abc294_d

--- abc294/src/bin/d.rs
+++ abc294/src/bin/d.rs
@@ -1,8 +1,24 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        q:usize,
     }
-    println!("{}", n);
+    let mut next = 1;
+    let mut set = BTreeSet::new();
+    for _ in 0..q {
+        input! {qi: u8}
+        if qi == 1 {
+            set.insert(next);
+            next += 1;
+        } else if qi == 2 {
+            input! {x:usize}
+            set.remove(&x);
+        } else {
+            println!("{}", set.iter().next().unwrap());
+        }
+    }
 }
