40a0e940c781be2e6b71a379e0d9f56267300940
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-19 22:41:30 +0900

    add abc294_c

--- abc294/src/bin/c.rs
+++ abc294/src/bin/c.rs
@@ -1,8 +1,35 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
+        b:[usize;m],
     }
-    println!("{}", n);
+    let mut c = vec![];
+    for i in 0..n {
+        c.push(a[i]);
+    }
+    for i in 0..m {
+        c.push(b[i]);
+    }
+    let mut map = HashMap::new();
+    c.sort();
+    for i in 0..n + m {
+        map.insert(c[i], i + 1);
+    }
+    let mut ansa = vec![];
+    for i in 0..n {
+        ansa.push(map.get(&a[i]).unwrap());
+    }
+    println!("{}", ansa.iter().join(" "));
+    let mut ansb = vec![];
+    for i in 0..m {
+        ansb.push(map.get(&b[i]).unwrap());
+    }
+    println!("{}", ansb.iter().join(" "));
 }
