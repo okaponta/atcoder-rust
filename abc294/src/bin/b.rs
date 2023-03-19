924b0a732d77bdcaa936c025dc49c6d2fc67084d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-19 22:41:29 +0900

    add abc294_b

--- abc294/src/bin/b.rs
+++ abc294/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        a:[[u8;w];h],
     }
-    println!("{}", n);
+    for a in a {
+        println!(
+            "{}",
+            a.into_iter()
+                .map(|i| if i == 0 { '.' } else { (b'A' + i - 1) as char })
+                .collect::<String>()
+        )
+    }
 }
