452d4f98c6e03d01b352324cde4d02f772062f61
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-21 23:03:40 +0900

    add abc206_e

--- abc206/src/bin/e.rs
+++ abc206/src/bin/e.rs
@@ -1,8 +1,45 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       l:Usize1,
+       r:usize,
     }
-    println!("{}", a);
+    let fact_num = eratosthenes(r);
+    let mut ans = 0;
+    for i in 2..=r {
+        if fact_num[i] < 0 {
+            continue;
+        }
+        let mut div = nc2(r / i - l / i) as i64;
+        if fact_num[i] % 2 == 0 {
+            div = -div;
+        }
+        ans += div;
+    }
+    for i in 2.max(l + 1)..=r {
+        ans -= (r / i) as i64 - 1;
+    }
+    println!("{}", ans * 2);
+}
+
+fn nc2(n: usize) -> usize {
+    n * (n - 1) / 2
+}
+
+fn eratosthenes(n: usize) -> Vec<i64> {
+    let mut res = vec![0; n + 1];
+    for i in 2..=n {
+        if res[i] != 0 {
+            continue;
+        }
+        for j in (i..=n).step_by(i) {
+            res[j] += 1;
+        }
+        for j in (i * i..=n).step_by(i * i) {
+            // iのときにカウント済なのでとばす
+            res[j] = -100000000;
+        }
+    }
+    res
 }
