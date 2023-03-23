164d95307b4b43a8a35810f6eb76c40b3ab7aba1
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-24 01:53:23 +0900

    add abc166_f

--- abc166/src/bin/f.rs
+++ abc166/src/bin/f.rs
@@ -10,55 +10,23 @@ fn main() {
     let all = count[0] + count[1] + count[2];
     let mut ans = vec![];
     for i in 0..n {
-        let mut target = vec![];
-        if &s[i] == "AB" {
-            target.push((count[0], 0));
-            target.push((count[1], 1));
-        } else if &s[i] == "AC" {
-            target.push((count[0], 0));
-            target.push((count[2], 2));
-        } else {
-            target.push((count[1], 1));
-            target.push((count[2], 2));
-        }
-        target.sort();
-        if target[0].0 == 0 && target[1].0 == 0 {
+        let mut tmp = vec![0; 3];
+        add_target(&s[i], &mut tmp);
+        let mut t = get_index(&tmp, &count);
+        if count[t[0]] == 0 && count[t[1]] == 0 {
             println!("No");
             return;
         }
-        if target[0].0 == 1 && target[1].0 == 1 && all == 2 {
-            if i == n - 1 {
-                ans.push((b'A' + target[0].1 as u8) as char);
-            } else {
-                let mut next = vec![0; 3];
-                next[target[0].1] += 1;
-                next[target[1].1] += 1;
-                if &s[i + 1] == "AB" {
-                    next[0] += 1;
-                    next[1] += 1;
-                } else if &s[i + 1] == "AC" {
-                    next[0] += 1;
-                    next[2] += 1;
-                } else {
-                    next[1] += 1;
-                    next[2] += 1;
-                }
-                if next[0] == 2 {
-                    f(0, &mut count, target);
-                    ans.push((b'A' + 0) as char);
-                } else if next[1] == 2 {
-                    f(1, &mut count, target);
-                    ans.push((b'A' + 1) as char);
-                } else {
-                    f(2, &mut count, target);
-                    ans.push((b'A' + 2) as char);
-                }
+        if count[t[0]] == 1 && count[t[1]] == 1 && all == 2 && i != n - 1 {
+            add_target(&s[i + 1], &mut tmp);
+            let inc = tmp.iter().enumerate().find(|(_, i)| **i == 2).unwrap().0;
+            if t[0] != inc {
+                t.swap(0, 1);
             }
-        } else {
-            count[target[0].1] += 1;
-            count[target[1].1] -= 1;
-            ans.push((b'A' + target[0].1 as u8) as char);
         }
+        count[t[0]] += 1;
+        count[t[1]] -= 1;
+        ans.push((b'A' + t[0] as u8) as char);
     }
     println!("Yes");
     for a in ans {
@@ -66,12 +34,23 @@ fn main() {
     }
 }
 
-fn f(a: usize, count: &mut Vec<usize>, target: Vec<(usize, usize)>) {
-    count[a] += 1;
-    for (_, i) in target {
-        if a == i {
-            continue;
+fn add_target(s: &String, tmp: &mut Vec<usize>) {
+    for i in 0..3 {
+        if s.contains((b'A' + i as u8) as char) {
+            tmp[i] += 1;
         }
-        count[i] -= 1;
     }
+}
+
+fn get_index(tmp: &Vec<usize>, count: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![];
+    for i in 0..3 {
+        if tmp[i] != 0 {
+            res.push(i);
+        }
+    }
+    if count[res[1]] < count[res[0]] {
+        res.swap(0, 1);
+    }
+    res
 }
