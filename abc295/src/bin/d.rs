b6df04b7b1cdb54abd8200cc1acfb3f07857c8c2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-25 22:47:42 +0900

    add abc295_d

--- abc295/src/bin/d.rs
+++ abc295/src/bin/d.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut dp = vec![0; 1 << 10];
+    let mut ans = 0usize;
+    for c in s {
+        let c = c.to_digit(10).unwrap() as usize;
+        let mut next = vec![0; 1 << 10];
+        for i in 0..1 << 10 {
+            next[i ^ 1 << c] += dp[i];
+        }
+        next[1 << c] += 1;
+        ans += next[0];
+        dp = next;
+    }
+    println!("{}", ans);
 }
