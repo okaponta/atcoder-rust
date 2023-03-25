a66f2dba7ae14759c809003cf6cc934862cbb468
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-25 22:47:41 +0900

    add abc295_c

--- abc295/src/bin/c.rs
+++ abc295/src/bin/c.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for a in a {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    let mut ans = 0usize;
+    for (_, v) in map {
+        ans += v / 2;
+    }
+    println!("{}", ans);
 }
