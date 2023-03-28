490697ae3c3bf2db1afa9f6261d3a43ebb725ff4
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-28 23:19:42 +0900

    add abc096_c

--- abc096/src/bin/c.rs
+++ abc096/src/bin/c.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        mut s:[Chars;h],
     }
-    println!("{}", n);
+    s.insert(0, vec!['.'; w + 2]);
+    s.push(vec!['.'; w + 2]);
+    for i in 1..=h {
+        s[i].insert(0, '.');
+        s[i].push('.');
+    }
+    for i in 1..=h {
+        for j in 1..=w {
+            if s[i][j] == '.' {
+                continue;
+            }
+            if s[i - 1][j] == '.' && s[i][j - 1] == '.' && s[i + 1][j] == '.' && s[i][j + 1] == '.'
+            {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
