361ceeb065f2dc62f6636109ab416b078af82c84
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-29 22:17:21 +0900

    add abc096_d

--- abc096/src/bin/d.rs
+++ abc096/src/bin/d.rs
@@ -1,8 +1,38 @@
-use proconio::input;
+use num_integer::Roots;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let primes = prime_lists_one(3100000);
+    for i in 0..n {
+        print!("{} ", primes[i]);
+    }
+    println!();
+}
+
+fn prime_lists_one(n: usize) -> Vec<usize> {
+    judge_primes(n)
+        .iter()
+        .enumerate()
+        .filter(|(i, &prime)| prime && i % 5 == 1)
+        .map(|(i, _)| i)
+        .collect()
+}
+
+fn judge_primes(n: usize) -> Vec<bool> {
+    let mut res = vec![true; n + 1];
+    res[0] = false;
+    res[1] = false;
+    for i in 2..=n.sqrt() {
+        if !res[i] {
+            continue;
+        }
+        for j in (i * i..=n).step_by(i) {
+            res[j] = false;
+        }
+    }
+    res
 }
