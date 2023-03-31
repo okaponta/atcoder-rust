3f368a20718831f2e4b8f07b0b70c4493659d7c8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-03-31 23:46:45 +0900

    add abc097_c

--- abc097/src/bin/c.rs
+++ abc097/src/bin/c.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        k:Usize1,
     }
-    println!("{}", n);
+    let mut hist: Vec<Vec<char>> = vec![];
+    let mut best: Vec<String> = vec![];
+    for c in s {
+        for i in 0..hist.len() {
+            hist[i].push(c);
+            best.push(hist[i].iter().collect::<String>());
+        }
+        hist.push(vec![c]);
+        hist.sort();
+        hist.truncate(5);
+        best.push(c.to_string());
+        best.sort();
+        best.dedup();
+        best.truncate(5);
+    }
+    println!("{}", best[k]);
 }
