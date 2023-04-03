c3b5c9c5b8eb01b15ed6cd2211ca049abd717d5b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-03 23:32:04 +0900

    add abc296_d

--- abc296/src/bin/d.rs
+++ abc296/src/bin/d.rs
@@ -1,8 +1,22 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut ans = 1 << 60;
+    for i in 1..=(m.sqrt() + 1).min(n) {
+        let div = (m + i - 1) / i;
+        if n < div {
+            continue;
+        }
+        ans = ans.min(div * i);
+    }
+    if ans == 1 << 60 {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
