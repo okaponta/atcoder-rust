97ec75977e4998f95070c1d44188ed2507cd9661
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-03 22:58:14 +0900

    add abc296_b

--- abc296/src/bin/b.rs
+++ abc296/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:[Chars;8],
     }
-    println!("{}", n);
+    for i in 0..8 {
+        for j in 0..8 {
+            if s[i][j] == '*' {
+                println!("{}{}", (b'a' + j as u8) as char, 8 - i);
+                return;
+            }
+        }
+    }
 }
