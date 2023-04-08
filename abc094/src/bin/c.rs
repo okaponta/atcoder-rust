f860ff4589a9d7c8ab88b1473030907f9b99cd81
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-08 19:13:28 +0900

    add abc094_c

--- abc094/src/bin/c.rs
+++ abc094/src/bin/c.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        x:[usize;n],
     }
-    println!("{}", n);
+    let mut y = x.clone();
+    y.sort();
+    let a = y[n / 2 - 1];
+    let b = y[n / 2];
+    for x in x {
+        println!("{}", if x <= a { b } else { a });
+    }
 }
