74447881d87d5fdcdc6d7cd9feb92bb712846b3c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-09 22:43:33 +0900

    add abc297_a

--- abc297/src/bin/a.rs
+++ abc297/src/bin/a.rs
@@ -2,7 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n: usize,
+        d: i32,
+        t: [i32; n],
     }
-    println!("{}", n);
+
+    let mut last_click_time = -1;
+    let mut double_click_time = -1;
+
+    for i in 1..n {
+        if t[i] - t[i - 1] <= d {
+            if last_click_time == -1 {
+                last_click_time = t[i - 1];
+            }
+            double_click_time = t[i];
+        } else {
+            last_click_time = -1;
+        }
+        if double_click_time != -1 && double_click_time - last_click_time <= d {
+            println!("{}", double_click_time);
+            return;
+        }
+    }
+
+    println!("-1");
 }
