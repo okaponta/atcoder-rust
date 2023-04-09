ff3a947104d8b3e8142396a165ad4ebf660908a9
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-09 22:43:38 +0900

    add abc297_e

--- abc297/src/bin/e.rs
+++ abc297/src/bin/e.rs
@@ -1,8 +1,38 @@
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet},
+};
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    let mut set = HashSet::new();
+    set.insert(0);
+    heap.push(Reverse(0));
+    for a in a {
+        let mut next = BinaryHeap::new();
+        next.push(Reverse(0));
+        let mut n_set = HashSet::new();
+        n_set.insert(0);
+        while next.len() <= k {
+            let Reverse(min) = heap.pop().unwrap();
+            if !n_set.contains(&min) {
+                next.push(Reverse(min));
+                n_set.insert(min);
+            }
+            if !set.contains(&(min + a)) {
+                heap.push(Reverse(min + a));
+                set.insert(min + a);
+            }
+        }
+        heap = next;
+    }
+    let Reverse(ans) = heap.into_iter().last().unwrap();
+    println!("{}", ans);
 }
