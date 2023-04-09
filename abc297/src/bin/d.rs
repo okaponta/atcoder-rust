756a169809315792dda52436f5613043c40996b1
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-09 22:43:37 +0900

    add abc297_d

--- abc297/src/bin/d.rs
+++ abc297/src/bin/d.rs
@@ -1,8 +1,24 @@
+use std::mem::swap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut a:usize,
+        mut b:usize,
     }
-    println!("{}", n);
+    if a < b {
+        swap(&mut a, &mut b);
+    }
+    let mut ans = 0;
+    while a != b {
+        ans += a / b;
+        a = a % b;
+        if a == 0 {
+            break;
+        } else {
+            swap(&mut a, &mut b);
+        }
+    }
+    println!("{}", ans.saturating_sub(1));
 }
