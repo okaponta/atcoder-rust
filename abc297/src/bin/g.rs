366828eb2119503d1b55c0fde2e4214ff5394b44
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-10 23:03:03 +0900

    add abc297_g

--- abc297/src/bin/g.rs
+++ abc297/src/bin/g.rs
@@ -3,6 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        l:usize,
+        r:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let g = a.into_iter().fold(0, |g, a| g ^ ((a % (l + r)) / l));
+    println!("{}", if g == 0 { "Second" } else { "First" });
 }
