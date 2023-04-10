4e14b5a2811f8eb7c21336d0878004c051dc326d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-11 00:45:56 +0900

    add abc094_d

--- abc094/src/bin/d.rs
+++ abc094/src/bin/d.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let i = *a.iter().max().unwrap();
+    let d = |a: &i64, b: &i64| f(*a, i).cmp(&f(*b, i));
+    let j = a.into_iter().filter(|a| a != &i).min_by(d).unwrap();
+    println!("{} {}", i, j);
+}
+
+fn f(a: i64, i: i64) -> i64 {
+    ((i + 1) / 2 - a).abs()
 }
