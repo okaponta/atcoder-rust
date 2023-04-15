3c674cb1dce6d031b2044a503cd581aac5084116
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-15 22:41:33 +0900

    add abc298_c

--- abc298/src/bin/c.rs
+++ abc298/src/bin/c.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use std::collections::BTreeSet;
 
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
     }
-    println!("{}", n);
+    let mut num = vec![BTreeSet::new(); 200_001];
+    let mut boxes = vec![BTreeSet::new(); n];
+    for qi in 0..q {
+        input! {q: u8}
+        if q == 1 {
+            input! {i:usize, j:Usize1}
+            boxes[j].insert((i, qi));
+            num[i].insert(j + 1);
+        } else if q == 2 {
+            input! {i:Usize1}
+            println!("{}", boxes[i].iter().map(|(i, _)| i).join(" "));
+        } else {
+            input! {i:usize}
+            println!("{}", num[i].iter().join(" "));
+        }
+    }
 }
