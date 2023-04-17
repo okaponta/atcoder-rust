3b0c0e0fd08d7471dcde0705c77d8ad4bb0841de
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-18 00:52:28 +0900

    add abc092_b

--- abc092/src/bin/b.rs
+++ abc092/src/bin/b.rs
@@ -3,6 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        d:usize,
+        x:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    println!("{}", a.into_iter().fold(x + n, |s, i| s + (d - 1) / i));
 }
