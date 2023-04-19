fb5e5c88151b90690504b036f1f59c6a537ccb08
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-19 22:06:59 +0900

    add abc092_c

--- abc092/src/bin/c.rs
+++ abc092/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        mut a:[i64;n],
     }
-    println!("{}", n);
+    a.insert(0, 0);
+    a.push(0);
+    let s = (0..=n)
+        .into_iter()
+        .fold(0, |s, i| s + (a[i + 1] - a[i]).abs());
+    for i in 1..=n {
+        let diff = (a[i] - a[i - 1]).abs() + (a[i + 1] - a[i]).abs() - (a[i + 1] - a[i - 1]).abs();
+        println!("{}", s - diff);
+    }
 }
