dfb51e3320512a73207f3de2b24584f1e4f93ecf
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-21 01:09:05 +0900

    add abc097_d

--- abc097/src/bin/d.rs
+++ abc097/src/bin/d.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
+
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        p:[Usize1;n],
+        xy:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for (x, y) in xy {
+        uf.union(x, y);
+    }
+    let mut map = HashMap::new();
+    for i in 0..n {
+        (*map.entry(uf.find(i)).or_insert(HashSet::new())).insert(i);
+    }
+    println!(
+        "{}",
+        (0..n)
+            .into_iter()
+            .filter(|&i| map[&uf.find(i)].contains(&p[i]))
+            .count()
+    );
 }
