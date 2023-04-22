0cbd4139d05f6af11bc6b78901a6199faf3bc3a8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-22 21:41:11 +0900

    add abc299_a

--- abc299/src/bin/a.rs
+++ abc299/src/bin/a.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s: Chars,
     }
-    println!("{}", n);
+    let mut pipe = vec![];
+    let mut aster = 0;
+    for i in 0..n {
+        if s[i] == '|' {
+            pipe.push(i);
+        }
+        if s[i] == '*' {
+            aster = i;
+        }
+    }
+    pipe.sort();
+    if pipe[0] < aster && aster < pipe[1] {
+        println!("in");
+    } else {
+        println!("out");
+    }
 }
