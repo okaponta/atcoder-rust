a69549ff32afe9aba54da4ef637c8b9277924dbc
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-22 21:41:13 +0900

    add abc299_c

--- abc299/src/bin/c.rs
+++ abc299/src/bin/c.rs
@@ -1,8 +1,37 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    let mut count = 0i64;
+    let mut tmp = 0;
+    let mut flg = false;
+    for i in 0..n {
+        if s[i] == '-' {
+            flg = true;
+            tmp = 0;
+        }
+        if flg && s[i] == 'o' {
+            tmp += 1;
+            count = count.max(tmp);
+        }
+    }
+    s.reverse();
+    tmp = 0;
+    for i in 0..n {
+        if s[i] == '-' {
+            flg = true;
+            tmp = 0;
+        }
+        if flg && s[i] == 'o' {
+            tmp += 1;
+            count = count.max(tmp);
+        }
+    }
+    if count == 0 {
+        count = -1;
+    }
+    println!("{}", count);
 }
