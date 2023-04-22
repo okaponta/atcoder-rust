3412ca46fa80c42767184f9a444ed2820efd53c7
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-22 21:41:14 +0900

    add abc299_d

--- abc299/src/bin/d.rs
+++ abc299/src/bin/d.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use std::io::{stdin, BufReader};
+
+use proconio::{input, source::line::LineSource};
 
 fn main() {
+    let stdin = stdin();
+    let mut source = LineSource::new(BufReader::new(stdin.lock()));
+
     input! {
-        n:usize,
+        from &mut source,
+        n: usize
     }
-    println!("{}", n);
+    let mut lower = 0;
+    let mut upper = n - 1;
+    while upper - lower != 1 {
+        let mid = (lower + upper) / 2;
+        println!("? {}", mid + 1);
+        input! {
+            from &mut source,
+            a: u8
+        }
+        if a == 0 {
+            lower = mid;
+        } else {
+            upper = mid;
+        }
+    }
+    println!("! {}", upper);
 }
