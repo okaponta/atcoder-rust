765128574df23fd7d5d6bb8f3487e96d55ad5cf2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-28 11:38:23 +0900

    add abc090_b

--- abc090/src/bin/b.rs
+++ abc090/src/bin/b.rs
@@ -9,7 +9,12 @@ fn main() {
         "{}",
         (a..=b)
             .into_iter()
-            .filter(|i| i / 10000 == i % 10 && i % 100 / 10 == i % 10000 / 1000)
+            .filter(|&i| get_digit(i, 1) == get_digit(i, 5) && get_digit(i, 2) == get_digit(i, 4))
             .count()
     );
+}
+
+// 下からi桁目の数字を返却する
+fn get_digit(n: usize, i: usize) -> usize {
+    n % 10usize.pow(i as u32) / 10usize.pow(i as u32 - 1)
 }
