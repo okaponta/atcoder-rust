038d9e78bd5a8436b3784d8b4ad27ae06e0f45eb
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-28 10:52:15 +0900

    add abc090_b

--- abc090/src/bin/b.rs
+++ abc090/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        b:usize,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        (a..=b)
+            .into_iter()
+            .filter(|i| i / 10000 == i % 10 && i % 100 / 10 == i % 10000 / 1000)
+            .count()
+    );
 }
