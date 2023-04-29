3f7377f82d9249e8d82c91cc19be3478ec2de32e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-29 22:52:42 +0900

    add abc300_a

--- abc300/src/bin/a.rs
+++ abc300/src/bin/a.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize,
+        c:[usize;n],
     }
-    println!("{}", n);
+    let s = a + b;
+    for i in 0..n {
+        if s == c[i] {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
