3e27c1bff14c5af38ac2972e72a72320853fca10
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-29 22:52:44 +0900

    add abc300_c

--- abc300/src/bin/c.rs
+++ abc300/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        c:[Chars;h],
     }
-    println!("{}", n);
+    let mut ans = vec![0; h.min(w)];
+    for i in 1..h - 1 {
+        for j in 1..w - 1 {
+            if c[i][j] == '#'
+                && c[i + 1][j + 1] == '#'
+                && c[i + 1][j - 1] == '#'
+                && c[i - 1][j + 1] == '#'
+                && c[i - 1][j - 1] == '#'
+            {
+                // 中心
+                let mut size = 0;
+                while i + size + 1 < h && j + size + 1 < w && c[i + size + 1][j + size + 1] == '#' {
+                    size += 1;
+                }
+                ans[size - 1] += 1;
+            }
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
