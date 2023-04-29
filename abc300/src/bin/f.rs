e4e17afd52f30ca16f54e052509bba1f0e41c669
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-29 23:42:46 +0900

    add abc300_f

--- abc300/src/bin/f.rs
+++ abc300/src/bin/f.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        k:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut x_c = 0;
+    let mut cum = vec![0];
+    for i in 0..n {
+        if s[i] == 'x' {
+            x_c += 1;
+        }
+        cum.push(x_c);
+    }
+    println!(
+        "{}",
+        (0..n)
+            .into_iter()
+            .map(|i| max_right(i, m, x_c, n, k, &cum) - i)
+            .max()
+            .unwrap_or(0)
+    );
+}
+
+fn max_right(start: usize, m: usize, count: usize, n: usize, k: usize, s: &Vec<usize>) -> usize {
+    let mut lower = start;
+    let mut upper = n * m;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if med / n * count + s[med % n + 1] - s[start] <= k {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    upper
 }
