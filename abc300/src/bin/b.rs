e707e0c3c817f7ad1846741cc5e0c0622c65e81e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-29 22:52:43 +0900

    add abc300_b

--- abc300/src/bin/b.rs
+++ abc300/src/bin/b.rs
@@ -1,8 +1,36 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        h: usize,
+        w: usize,
+        a: [String; h],
+        b: [String; h],
     }
-    println!("{}", n);
+
+    let mut ans = "No";
+
+    // s回の縦方向のシフトとt回の横方向のシフトをすべて試す
+    for s in 0..h {
+        for t in 0..w {
+            let mut match_flag = true;
+            // AをシフトしてBと一致するかどうか調べる
+            for i in 0..h {
+                for j in 0..w {
+                    let x = (i + s) % h;
+                    let y = (j + t) % w;
+                    if a[x].chars().nth(y).unwrap() != b[i].chars().nth(j).unwrap() {
+                        match_flag = false;
+                    }
+                }
+            }
+            if match_flag {
+                ans = "Yes";
+                break;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
