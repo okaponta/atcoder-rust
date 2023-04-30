8303f493307a8c58da79ffc689e7c945a0519d92
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-04-30 19:12:24 +0900

    add abc142_e

--- abc142/src/bin/e.rs
+++ abc142/src/bin/e.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut keys = vec![];
+    for _ in 0..m {
+        input! {
+            a:usize,
+            b:usize,
+            c:[Usize1;b],
+        }
+        let mut target = 0;
+        for c in c {
+            target += 1 << c;
+        }
+        keys.push((a, target));
+    }
+    let mut dp = vec![1 << 60; 1 << n];
+    dp[0] = 0;
+    for i in 0..1 << n {
+        for j in 0..m {
+            dp[i | keys[j].1] = dp[i | keys[j].1].min(dp[i] + keys[j].0);
+        }
+    }
+    if dp[(1 << n) - 1] == 1 << 60 {
+        println!("-1");
+    } else {
+        println!("{}", dp[(1 << n) - 1]);
+    }
 }
