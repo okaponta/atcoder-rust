b107cdbc377e055682f5386b49eb475e76a92d17
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-03 00:35:45 +0900

    add abc112_c

--- abc112/src/bin/c.rs
+++ abc112/src/bin/c.rs
@@ -1,8 +1,24 @@
+use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        xyh:[(i64,i64,i64);n],
     }
-    println!("{}", n);
+    for (x, y) in iproduct!(0..101, 0..101) {
+        let mut h = 0;
+        for &(xi, yi, hi) in &xyh {
+            if hi != 0 {
+                h = hi + (x - xi).abs() + (y - yi).abs();
+            }
+        }
+        if xyh
+            .iter()
+            .all(|&(xi, yi, hi)| (h - (x - xi).abs() - (y - yi).abs()).max(0) == hi)
+        {
+            println!("{} {} {}", x, y, h);
+            return;
+        }
+    }
 }
