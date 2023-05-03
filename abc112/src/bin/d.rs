710db288722da5ca0edbc5804bfec2ca3aa452eb
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-04 01:10:08 +0900

    add abc112_d

--- abc112/src/bin/d.rs
+++ abc112/src/bin/d.rs
@@ -1,8 +1,33 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        divisor(m)
+            .into_iter()
+            .filter(|&i| n <= m / i)
+            .max()
+            .unwrap()
+    );
+}
+
+fn divisor(n: usize) -> Vec<usize> {
+    let mut res = vec![];
+    let mut upper = vec![];
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            res.push(i);
+            if i != n / i {
+                upper.push(n / i);
+            }
+        }
+    }
+    upper.reverse();
+    res.append(&mut upper);
+    res
 }
