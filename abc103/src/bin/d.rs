263d4d72a61d4e3bbeb27dae8c8b3ba36b6204ff
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-07 18:04:03 +0900

    add abc103_d

--- abc103/src/bin/d.rs
+++ abc103/src/bin/d.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut ab:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    ab.sort_by(|a, b| a.1.cmp(&b.1));
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[b].push(a);
+    }
+    let mut min = 0;
+    let mut ans = 0;
+    for i in 1..n {
+        for &prev in &g[i] {
+            if min <= prev {
+                ans += 1;
+                min = i;
+                break;
+            }
+        }
+    }
+    println!("{}", ans);
 }
