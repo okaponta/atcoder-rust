1a0d013ffb8030734b65f3dec8f34c8624c81f97
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-08 02:26:48 +0900

    add abc090_c

--- abc090/src/bin/c.rs
+++ abc090/src/bin/c.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut ans = n.saturating_sub(2) * m.saturating_sub(2);
+    if n == 1 {
+        ans = m.saturating_sub(2);
+    }
+    if m == 1 {
+        ans = n.saturating_sub(2);
+    }
+    if n == 1 && m == 1 {
+        ans = 1;
+    }
+    println!("{}", ans);
 }
