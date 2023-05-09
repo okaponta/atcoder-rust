afffc4d1caea1acc933a3a59e6a6767e66ecc472
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-09 21:16:49 +0900

    add abc105_c

--- abc105/src/bin/c.rs
+++ abc105/src/bin/c.rs
@@ -1,8 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:i64,
     }
-    println!("{}", n);
+    if n == 0 {
+        println!("0");
+        return;
+    }
+    let mut ans = Vec::new();
+    while n.abs() > 0 {
+        let b = n.abs() % 2;
+        ans.push(b);
+        if b == 1 {
+            n -= 1;
+        }
+        n /= -2;
+    }
+    println!("{}", ans.into_iter().rev().join(""));
 }
