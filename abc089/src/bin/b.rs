f8650a9ceea985f150698c272e53c5938766ab3b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-12 16:03:06 +0900

    add abc089_b

--- abc089/src/bin/b.rs
+++ abc089/src/bin/b.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        s:[char;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if s.into_iter().any(|c| c == 'Y') {
+            "Four"
+        } else {
+            "Three"
+        }
+    );
 }
