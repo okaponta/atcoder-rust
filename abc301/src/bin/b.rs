6962e49b9c74750e8ac2ac1d8005ba3f6574def6
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-13 22:59:49 +0900

    add abc301_b

--- abc301/src/bin/b.rs
+++ abc301/src/bin/b.rs
@@ -1,8 +1,29 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n: usize,
+        a: [i32; n],
     }
-    println!("{}", n);
+
+    let mut result = Vec::new();
+
+    for i in 0..n - 1 {
+        result.push(a[i]);
+        if (a[i] - a[i + 1]).abs() != 1 {
+            if a[i] < a[i + 1] {
+                for j in (a[i] + 1)..a[i + 1] {
+                    result.push(j);
+                }
+            } else {
+                for j in (a[i + 1] + 1..(a[i])).rev() {
+                    result.push(j);
+                }
+            }
+        }
+    }
+
+    result.push(a[n - 1]);
+    println!("{}", result.iter().join(" "));
 }
