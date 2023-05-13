bde961e7b4bb547ffa0205045363765d5e7869d4
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-13 22:59:48 +0900

    add abc301_a

--- abc301/src/bin/a.rs
+++ abc301/src/bin/a.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ta = 0;
+    let mut ao = 0;
+    for i in 0..n {
+        if s[i] == 'T' {
+            ta += 1;
+        } else {
+            ao += 1;
+        }
+    }
+    if ta == ao {
+        println!("{}", if s[n - 1] == 'T' { 'A' } else { 'T' });
+        return;
+    }
+    println!("{}", if ao < ta { 'T' } else { 'A' });
 }
