313a8c0511c9df4d9fb2d986845e2fc320b5d059
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-13 22:59:51 +0900

    add abc301_d

--- abc301/src/bin/d.rs
+++ abc301/src/bin/d.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
+        s:Chars,
         n:usize,
     }
-    println!("{}", n);
+    let l = s.len();
+    let mut masks = vec![];
+    let mut num = 0;
+    for i in 0..l {
+        if s[i] == '?' {
+            masks.push(1 << (l - 1 - i));
+        } else if s[i] == '1' {
+            num += 1 << (l - 1 - i);
+        }
+    }
+    if n < num {
+        println!("-1");
+        return;
+    }
+    for i in masks {
+        if n < num + i {
+            continue;
+        }
+        num += i;
+    }
+    println!("{}", num);
 }
