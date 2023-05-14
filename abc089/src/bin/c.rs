663f4ddb453a7cc28330c9882e117dec99846aab
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-15 00:09:21 +0900

    add abc089_c

--- abc089/src/bin/c.rs
+++ abc089/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut cnt = vec![0; 5];
+    for s in s {
+        match s[0] {
+            'M' => cnt[0] += 1,
+            'A' => cnt[1] += 1,
+            'R' => cnt[2] += 1,
+            'C' => cnt[3] += 1,
+            'H' => cnt[4] += 1,
+            _ => (),
+        }
+    }
+    let mut ans = 0usize;
+    for (a, b, c) in (0..5).tuple_combinations() {
+        ans += cnt[a] * cnt[b] * cnt[c];
+    }
+    println!("{}", ans);
 }
