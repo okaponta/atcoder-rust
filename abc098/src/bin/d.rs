c88759e61b33a5eae0a61c36114d2b0882d1d035
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-14 23:43:41 +0900

    add abc098_d

--- abc098/src/bin/d.rs
+++ abc098/src/bin/d.rs
@@ -3,6 +3,33 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut left = 0;
+    let mut right = 0;
+    let mut xor = 0;
+    let mut sum = 0;
+    let mut ans = 0;
+    loop {
+        while sum == xor && right < n {
+            sum += a[right];
+            xor ^= a[right];
+            if sum == xor {
+                right += 1;
+            } else {
+                sum -= a[right];
+                xor ^= a[right];
+                break;
+            }
+        }
+        ans += right - left;
+        sum -= a[left];
+        xor ^= a[left];
+        if left == n - 1 {
+            // しゃくとり終了
+            break;
+        }
+        left += 1;
+    }
+    println!("{}", ans);
 }
