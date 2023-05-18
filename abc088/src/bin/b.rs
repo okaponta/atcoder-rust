b63c5506d865646aacb84878c7c2b2390472ff98
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-18 21:10:58 +0900

    add abc088_b

--- abc088/src/bin/b.rs
+++ abc088/src/bin/b.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    a.reverse();
+    let mut ans = 0;
+    for i in 0..n {
+        if i % 2 == 0 {
+            ans += a[i];
+        } else {
+            ans -= a[i];
+        }
+    }
+    println!("{}", ans);
 }
