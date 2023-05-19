c606e4ddba7bd2899467b8d4a2dbed0e27106f91
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-19 21:35:47 +0900

    add abc088_c

--- abc088/src/bin/c.rs
+++ abc088/src/bin/c.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        c:[[i64;3];3],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if (1..3)
+            .into_iter()
+            .any(|i| c[1][i] - c[0][i] != c[1][0] - c[0][0]
+                || c[2][i] - c[1][i] != c[2][0] - c[1][0]
+                || c[i][1] - c[i][0] != c[0][1] - c[0][0]
+                || c[i][2] - c[i][1] != c[0][2] - c[0][1])
+        {
+            "No"
+        } else {
+            "Yes"
+        }
+    );
 }
