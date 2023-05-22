0b2ffd93d750c5d42398521843a71a1302d6eaef
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-22 22:37:28 +0900

    add abc302_b

--- abc302/src/bin/b.rs
+++ abc302/src/bin/b.rs
@@ -1,8 +1,32 @@
-use proconio::input;
+use itertools::iproduct;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    for (i, j) in iproduct!(0..h, 0..w) {
+        if s[i][j] != 's' {
+            continue;
+        }
+        'a: for (k, l) in iproduct!(vec![!0, 0, 1], vec![!0, 0, 1]) {
+            let mut tmpi = i;
+            let mut tmpj = j;
+            let mut ans = vec![(i, j)];
+            for c in vec!['n', 'u', 'k', 'e'] {
+                tmpi = tmpi.wrapping_add(k);
+                tmpj = tmpj.wrapping_add(l);
+                if h <= tmpi || w <= tmpj || s[tmpi][tmpj] != c {
+                    continue 'a;
+                }
+                ans.push((tmpi, tmpj));
+            }
+            for (i, j) in ans {
+                println!("{} {}", i + 1, j + 1);
+            }
+            return;
+        }
+    }
 }
