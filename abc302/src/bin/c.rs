18d7f7636fa1dd064d6c9c377374935564e0afd8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-23 14:57:29 +0900

    add abc302_c

--- abc302/src/bin/c.rs
+++ abc302/src/bin/c.rs
@@ -1,8 +1,43 @@
-use proconio::input;
+use std::process::exit;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut used = vec![false; n];
+    dfs(&mut used, 0, 0, n, m, &s);
+    println!("No");
+}
+
+fn dfs(used: &mut Vec<bool>, prev: usize, depth: usize, n: usize, m: usize, s: &Vec<Vec<char>>) {
+    if depth == n {
+        println!("Yes");
+        exit(0);
+    }
+    for i in 0..n {
+        if used[i] {
+            continue;
+        }
+        if depth == 0 {
+            used[i] = true;
+            dfs(used, i, 1, n, m, s);
+            used[i] = false;
+        } else {
+            let mut count = 0;
+            for j in 0..m {
+                if s[prev][j] != s[i][j] {
+                    count += 1;
+                }
+            }
+            if count == 1 {
+                used[i] = true;
+                dfs(used, i, depth + 1, n, m, s);
+                used[i] = false;
+            }
+        }
+    }
 }
