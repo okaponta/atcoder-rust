5b164c8378ca598ac8d2a9de403a15070e32e27b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-24 23:02:40 +0900

    add abc302_d

--- abc302/src/bin/d.rs
+++ abc302/src/bin/d.rs
@@ -1,8 +1,23 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        d:usize,
+        mut a:[usize;n],
+        b:[usize;m],
     }
-    println!("{}", n);
+    a.sort();
+    a.reverse();
+    let set = b.into_iter().collect::<BTreeSet<_>>();
+    for a in a {
+        if let Some(b) = set.range(a.saturating_sub(d)..=a + d).last() {
+            println!("{}", a + b);
+            return;
+        }
+    }
+    println!("-1");
 }
