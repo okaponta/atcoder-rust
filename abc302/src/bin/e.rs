3f1bda7950d9971dc1c0f788aa9038fbe876149b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-25 01:06:36 +0900

    add abc302_e

--- abc302/src/bin/e.rs
+++ abc302/src/bin/e.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for _ in 0..q {
+        input! {q:u8}
+        if q == 1 {
+            input! {u:Usize1,v:Usize1}
+            (*map.entry(u).or_insert(HashSet::new())).insert(v);
+            (*map.entry(v).or_insert(HashSet::new())).insert(u);
+        } else {
+            input! {v:Usize1}
+            let rem = map.remove(&v).unwrap_or(HashSet::new());
+            for i in rem {
+                if map[&i].len() == 1 {
+                    map.remove(&i);
+                } else {
+                    map.entry(i).or_default().remove(&v);
+                }
+            }
+        }
+        println!("{}", n - map.len());
+    }
 }
