29565c3f9c43d68329689c7144f8a1f06f1991c9
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-27 22:47:22 +0900

    add abc303_b

--- abc303/src/bin/b.rs
+++ abc303/src/bin/b.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[[Usize1;n];m],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 0..m {
+        for j in 1..n {
+            let k = a[i][j - 1];
+            let l = a[i][j];
+            set.insert((k.min(l), k.max(l)));
+        }
+    }
+    println!("{}", n * (n - 1) / 2 - set.len());
 }
