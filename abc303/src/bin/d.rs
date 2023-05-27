3dd012b97ac9cc8d8e80d66a23fc3ab0752867b3
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-27 22:47:24 +0900

    add abc303_d

--- abc303/src/bin/d.rs
+++ abc303/src/bin/d.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        x:usize,
+        y:usize,
+        z:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut dp = vec![0, z];
+    for i in 0..s.len() {
+        let mut next = vec![1 << 60; 2];
+        if s[i] == 'a' {
+            // capsOFF
+            next[0] = next[0].min(dp[0] + x);
+            next[0] = next[0].min(dp[1] + z + x);
+            // capsON
+            next[1] = next[1].min(dp[0] + z + y);
+            next[1] = next[1].min(dp[1] + y);
+        } else {
+            // capsOFF
+            next[0] = next[0].min(dp[0] + y);
+            next[0] = next[0].min(dp[1] + z + y);
+            // capsON
+            next[1] = next[1].min(dp[1] + x);
+            next[1] = next[1].min(dp[0] + z + x);
+        }
+        dp = next;
+    }
+    println!("{}", dp[0].min(dp[1]));
 }
