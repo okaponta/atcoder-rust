ad95dd43ddd375612adb7b1d0c6c0ec3114232b6
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-27 22:47:20 +0900

    add abc303_a

--- abc303/src/bin/a.rs
+++ abc303/src/bin/a.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    for i in 0..n {
+        if s[i] == t[i]
+            || (s[i] == '1' && t[i] == 'l')
+            || (t[i] == '1' && s[i] == 'l')
+            || (s[i] == '0' && t[i] == 'o')
+            || (t[i] == '0' && s[i] == 'o')
+        {
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
