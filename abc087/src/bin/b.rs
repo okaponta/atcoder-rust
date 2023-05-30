b1a32268322e3751acdbbcb2aad97d94dad429ab
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-30 22:10:55 +0900

    add abc087_b

--- abc087/src/bin/b.rs
+++ abc087/src/bin/b.rs
@@ -2,7 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        b:usize,
+        c:usize,
+        x:usize,
     }
-    println!("{}", n);
+    let mut count = 0;
+    for i in 0..=a {
+        for j in 0..=b {
+            let tmp = 500 * i + 100 * j;
+            if x < tmp || (x - tmp) % 50 != 0 {
+                continue;
+            }
+            if (x - tmp) / 50 <= c {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
