842e3fd948f055fb44ee3970ccb091bc12d1014d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-03 23:06:01 +0900

    add abc304_e

--- abc304/src/bin/e.rs
+++ abc304/src/bin/e.rs
@@ -1,8 +1,39 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use petgraph::unionfind::UnionFind;
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
+        k:usize,
+        xy:[(Usize1,Usize1);k],
+        q:usize,
+        pq:[(Usize1,Usize1);q]
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for (u, v) in uv {
+        uf.union(u, v);
+    }
+    let mut set = HashSet::new();
+    for (x, y) in xy {
+        let a = uf.find(x);
+        let b = uf.find(y);
+        set.insert((a.min(b), a.max(b)));
+    }
+    for (p, q) in pq {
+        let a = uf.find(p);
+        let b = uf.find(q);
+        println!(
+            "{}",
+            if set.contains(&(a.min(b), a.max(b))) {
+                "No"
+            } else {
+                "Yes"
+            }
+        )
+    }
 }
