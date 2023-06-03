a47668bb81b3d98337482622b37fb41ffee1be1c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-03 23:05:59 +0900

    add abc304_c

--- abc304/src/bin/c.rs
+++ abc304/src/bin/c.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use std::collections::VecDeque;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        d:i64,
+        xy:[(i64,i64);n],
     }
-    println!("{}", n);
+    let mut affected = vec![false; n];
+    affected[0] = true;
+    let mut q = VecDeque::new();
+    q.push_back(0);
+    while let Some(i) = q.pop_front() {
+        for j in 0..n {
+            if affected[j] {
+                continue;
+            }
+            if dist(xy[i], xy[j]) <= d * d {
+                affected[j] = true;
+                q.push_back(j);
+            }
+        }
+    }
+    for i in 0..n {
+        println!("{}", if affected[i] { "Yes" } else { "No" });
+    }
+}
+
+fn dist(x: (i64, i64), y: (i64, i64)) -> i64 {
+    (x.0 - y.0) * (x.0 - y.0) + (x.1 - y.1) * (x.1 - y.1)
 }
