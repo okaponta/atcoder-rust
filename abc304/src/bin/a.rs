2c1f247c234974a8d63d311dc560c7d46c2e6c3c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-03 23:05:55 +0900

    add abc304_a

--- abc304/src/bin/a.rs
+++ abc304/src/bin/a.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        sa:[(String,usize);n],
     }
-    println!("{}", n);
+    let mut min = 1 << 60;
+    let mut min_idx = 0;
+    for i in 0..n {
+        if sa[i].1 < min {
+            min = sa[i].1;
+            min_idx = i;
+        }
+    }
+    for i in min_idx..min_idx + n {
+        println!("{}", sa[i % n].0);
+    }
 }
