fa520a6ba6822399982ae980a903894c09acd377
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-03 23:05:57 +0900

    add abc304_b

--- abc304/src/bin/b.rs
+++ abc304/src/bin/b.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n: usize,
+    }
+    let mut count = 0;
+    while 1000 <= n {
+        n /= 10;
+        count += 1;
+    }
+    for _ in 0..count {
+        n *= 10;
     }
     println!("{}", n);
 }
