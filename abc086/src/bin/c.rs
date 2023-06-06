2021c5dcd872f64fb4b21a0a20ccc587ba9d95c6
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-06 20:22:39 +0900

    add abc086_c

--- abc086/src/bin/c.rs
+++ abc086/src/bin/c.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        txy:[(i64,i64,i64);n],
     }
-    println!("{}", n);
+    let mut before = (0, 0, 0);
+    for (t, x, y) in txy {
+        let d = (x - before.1).abs() + (y - before.2).abs();
+        let p = t - before.0;
+        if p < d || (p - d) % 2 == 1 {
+            println!("No");
+            return;
+        }
+        before = (t, x, y);
+    }
+    println!("Yes");
 }
