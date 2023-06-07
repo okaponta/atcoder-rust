23df7fb019f3e170bd83b1a6b88268b312b45829
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-07 22:53:25 +0900

    add abc099_c

--- abc099/src/bin/c.rs
+++ abc099/src/bin/c.rs
@@ -4,5 +4,18 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut dp = (0..=n).into_iter().collect::<Vec<_>>();
+    ope(n, 6, &mut dp);
+    ope(n, 9, &mut dp);
+    println!("{}", dp[n]);
+}
+
+fn ope(n: usize, i: usize, dp: &mut Vec<usize>) {
+    let mut tmp = i;
+    while tmp <= n {
+        for i in 0..=n - tmp {
+            dp[i + tmp] = dp[i + tmp].min(dp[i] + 1);
+        }
+        tmp *= i;
+    }
 }
