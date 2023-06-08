703803516db47fe41736c990fd1e82259a8272d5
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-08 21:46:00 +0900

    add abc099_d

--- abc099/src/bin/d.rs
+++ abc099/src/bin/d.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::{iproduct, Itertools};
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        c:usize,
+        d:[[usize;c];c],
+        col:[[Usize1;n];n]
     }
-    println!("{}", n);
+    let mut counts = vec![vec![0; c]; 3];
+    for (i, j) in iproduct!(0..n, 0..n) {
+        counts[(i + j) % 3][col[i][j]] += 1;
+    }
+    let mut ans = 1 << 60;
+    for v in (0..c).permutations(3) {
+        let mut cost = 0;
+        for (i, j) in iproduct!(0..3, 0..c) {
+            cost += counts[i][j] * d[j][v[i]];
+        }
+        ans = ans.min(cost);
+    }
+    println!("{}", ans);
 }
