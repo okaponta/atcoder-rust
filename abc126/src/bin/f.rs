fafb5f2038109ef4d317c92686b44e66b398637b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-10 13:41:48 +0900

    add abc126_f

--- abc126/src/bin/f.rs
+++ abc126/src/bin/f.rs
@@ -1,8 +1,35 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        m:usize,
+        k:usize,
     }
-    println!("{}", n);
+    if m == 0 {
+        println!("{}", if k == 0 { "0 0" } else { "-1" });
+        return;
+    }
+    if m == 1 {
+        println!("{}", if k == 0 { "0 0 1 1" } else { "-1" });
+        return;
+    }
+    if (1 << m) <= k {
+        println!("-1");
+        return;
+    }
+    let mut ans = vec![];
+    for i in 0..1 << m {
+        if i != k {
+            ans.push(i);
+        }
+    }
+    ans.push(k);
+    for i in (0..1 << m).rev() {
+        if i != k {
+            ans.push(i);
+        }
+    }
+    ans.push(k);
+    println!("{}", ans.iter().join(" "));
 }
