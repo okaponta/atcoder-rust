d86c45ba50be07b1a36fec5634b67c1e938057a0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-10 22:58:03 +0900

    add abc305_a

--- abc305/src/bin/a.rs
+++ abc305/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:i32,
     }
-    println!("{}", n);
+    let mut min = 100;
+    let mut ans = 0;
+    for i in (0..=100).step_by(5) {
+        if (n - i).abs() < min {
+            min = (n - i).abs();
+            ans = i;
+        }
+    }
+    println!("{}", ans);
 }
