b43c47ccb177a3329b691227624e4e0a9913465f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-10 22:58:06 +0900

    add abc305_c

--- abc305/src/bin/c.rs
+++ abc305/src/bin/c.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    let mut a = h;
+    let mut b = 0;
+    let mut c = w;
+    let mut d = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '#' {
+                a = a.min(i);
+                b = b.max(i);
+                c = c.min(j);
+                d = d.max(j);
+            }
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '.' && a <= i && i <= b && c <= j && j <= d {
+                println!("{} {}", i + 1, j + 1);
+                return;
+            }
+        }
+    }
 }
