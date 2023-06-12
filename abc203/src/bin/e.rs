ec1c8bbef8c29810862565141c06d0fcd8bdcecb
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-13 01:42:12 +0900

    add abc203_e

--- abc203/src/bin/e.rs
+++ abc203/src/bin/e.rs
@@ -1,8 +1,41 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       mut m:usize,
+       xy:[(usize,usize);m],
     }
-    println!("{}", a);
+    if n < m {
+        m = n;
+    }
+    let mut ans = vec![false; 2 * m + 3];
+    ans[m + 1] = true;
+    let mut map = BTreeMap::new();
+    for (x, y) in xy {
+        if n - m <= y && y <= n + m {
+            (*map.entry(x).or_insert(vec![])).push(y + m + 1 - n);
+        }
+    }
+    for (_, v) in map {
+        let mut f_list = vec![];
+        let mut t_list = vec![];
+        for p in v {
+            if ans[p] && !(ans[p - 1] || ans[p + 1]) {
+                f_list.push(p);
+            }
+            if !ans[p] && (ans[p - 1] || ans[p + 1]) {
+                t_list.push(p);
+            }
+        }
+        for f in f_list {
+            ans[f] = false;
+        }
+        for t in t_list {
+            ans[t] = true;
+        }
+    }
+    println!("{}", ans.iter().filter(|&&b| b).count());
 }
