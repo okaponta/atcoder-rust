cf3b60e19913e216bff5b8f0015fbad876435e09
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-16 00:42:26 +0900

    add abc085_b

--- abc085/src/bin/b.rs
+++ abc085/src/bin/b.rs
@@ -3,6 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut d:[usize;n],
     }
-    println!("{}", n);
+    d.sort();
+    d.dedup();
+    println!("{}", d.len());
 }
