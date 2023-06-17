71ac298c8672e0c3269139346184b8b89166632d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-17 22:54:07 +0900

    add abc306_c

--- abc306/src/bin/c.rs
+++ abc306/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;3*n],
     }
-    println!("{}", n);
+    let mut count = vec![0; n];
+    let mut ans = vec![];
+    for i in 0..3 * n {
+        count[a[i]] += 1;
+        if count[a[i]] == 2 {
+            ans.push(a[i] + 1);
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
