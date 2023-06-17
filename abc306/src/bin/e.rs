2907e1f37c0cbd6d6bf05e0aa8556e08f6e571e8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-17 22:54:10 +0900

    add abc306_e

--- abc306/src/bin/e.rs
+++ abc306/src/bin/e.rs
@@ -1,8 +1,54 @@
-use proconio::input;
+use std::collections::BTreeSet;
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        q:usize,
+        xy:[(Usize1,usize);q],
     }
-    println!("{}", n);
+    let mut a = vec![0; n];
+    let mut is_ans = vec![false; n];
+    let mut seta = BTreeSet::new();
+    let mut setb = BTreeSet::new();
+    for i in 0..n {
+        if i < k {
+            seta.insert((0, i));
+            is_ans[i] = true;
+        } else {
+            setb.insert((0, i));
+        }
+    }
+    let mut ans = 0;
+    for (x, y) in xy {
+        let before = a[x];
+        if is_ans[x] {
+            seta.remove(&(before, x));
+            seta.insert((y, x));
+            ans -= before;
+            ans += y;
+        } else {
+            setb.remove(&(before, x));
+            setb.insert((y, x));
+        }
+        a[x] = y;
+        if setb.len() != 0 {
+            let mina = *seta.iter().next().unwrap();
+            let maxb = *setb.iter().last().unwrap();
+            if mina < maxb {
+                seta.remove(&mina);
+                setb.remove(&maxb);
+                ans -= mina.0;
+                seta.insert(maxb);
+                ans += maxb.0;
+                setb.insert(mina);
+                is_ans[mina.1] = false;
+                is_ans[maxb.1] = true;
+            }
+        }
+        println!("{}", ans);
+    }
 }
