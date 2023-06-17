ee26c690bf74e73130aaba7a134e1e61a68a75a8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-18 00:13:37 +0900

    add abc085_c

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -3,6 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        y:usize,
     }
-    println!("{}", n);
+    for i in 0..=n {
+        for j in 0..=n - i {
+            let k = n - i - j;
+            if 10000 * i + 5000 * j + 1000 * k == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
