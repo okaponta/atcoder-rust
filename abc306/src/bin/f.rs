377ea2cce7c28497738b5462b075c245ee14ca78
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-17 22:54:11 +0900

    add abc306_f

--- abc306/src/bin/f.rs
+++ abc306/src/bin/f.rs
@@ -3,6 +3,92 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[[usize;m];n],
     }
-    println!("{}", n);
+    let mut aa = vec![];
+    for i in 0..n {
+        for j in 0..m {
+            aa.push((a[i][j], i));
+        }
+    }
+    aa.sort();
+    let mut fw = FenwickTree::new(n);
+    for i in 0..n {
+        fw.add(i + 1, m as i64);
+    }
+    let mut ans = (m * (m + 1) / 2) * (n * (n - 1) / 2);
+    for (_, i) in aa {
+        fw.add(i + 1, -1);
+        ans += fw.sum(i) as usize;
+    }
+    println!("{}", ans);
+}
+
+pub struct FenwickTree {
+    len: usize,
+    data: Vec<i64>,
+}
+
+impl FenwickTree {
+    // a1~anの配列を作成
+    pub fn new(n: usize) -> Self {
+        Self {
+            len: n + 1,
+            data: vec![0; n + 1],
+        }
+    }
+
+    // aiにvを加算する
+    pub fn add(&mut self, i: usize, v: i64) {
+        assert!(i > 0);
+        assert!(i < self.len);
+        let mut i = i as i64;
+        while (i as usize) < self.len {
+            self.data[i as usize] += v;
+            i += i & -i;
+        }
+    }
+
+    // aiをvで置き換える
+    pub fn update(&mut self, i: usize, v: i64) {
+        assert!(i > 0);
+        assert!(i < self.len);
+        let cur = self.range(i, i);
+        self.add(i, v - cur);
+    }
+
+    // a1+a2+...aiを計算する
+    pub fn sum(&self, i: usize) -> i64 {
+        assert!(i < self.len);
+        let mut i = i as i64;
+        let mut sum = 0;
+        while i > 0 {
+            sum += self.data[i as usize];
+            i -= i & -i;
+        }
+        sum
+    }
+
+    // ai+...+ajを計算する
+    pub fn range(&self, i: usize, j: usize) -> i64 {
+        assert!(i <= j);
+        assert!(j < self.len);
+        self.sum(j) - self.sum(i - 1)
+    }
+
+    // 和がs以下の位置を返却
+    pub fn lower(&self, s: i64) -> usize {
+        let mut lower = 0;
+        let mut upper = self.len;
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if self.sum(med) <= s {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
