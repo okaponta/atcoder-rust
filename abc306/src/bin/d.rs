b43d84e5f12d427806dc81fedc2fea533823b34c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-17 22:54:09 +0900

    add abc306_d

--- abc306/src/bin/d.rs
+++ abc306/src/bin/d.rs
@@ -3,6 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xy:[(u8,i64);n],
     }
-    println!("{}", n);
+    let mut dp = vec![0, -1 << 60];
+    for (x, y) in xy {
+        let mut next = vec![-1 << 60; 2];
+        if x == 0 {
+            next[0] = dp[0].max(dp[0] + y).max(dp[1] + y);
+            next[1] = dp[1];
+        } else {
+            next[0] = dp[0];
+            next[1] = dp[1].max(dp[0] + y);
+        }
+        dp = next;
+    }
+    println!("{}", dp[0].max(dp[1]));
 }
