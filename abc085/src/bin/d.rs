e2ad991b83b26e0c6f1ba65298bada160c751312
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-19 20:27:18 +0900

    add abc085_d

--- abc085/src/bin/d.rs
+++ abc085/src/bin/d.rs
@@ -1,8 +1,32 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        h:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for (a, b) in ab {
+        heap.push((a, false));
+        heap.push((b, true));
+    }
+    let mut count = 0;
+    let mut d = 0;
+    while let Some((damage, throw)) = heap.pop() {
+        if throw {
+            d += damage;
+            count += 1;
+        } else {
+            let c = (h - d + damage - 1) / damage;
+            count += c;
+            d += c * damage;
+        }
+        if h <= d {
+            break;
+        }
+    }
+    println!("{}", count);
 }
