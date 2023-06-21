a528d9eb610d9a410b90cff4a2026b1585ed61d2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-21 22:38:03 +0900

    add abc084_b

--- abc084/src/bin/b.rs
+++ abc084/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        b:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    for i in 0..a {
+        if !s[i].is_numeric() {
+            println!("No");
+            return;
+        }
+    }
+    if s[a] != '-' {
+        println!("No");
+        return;
+    }
+    for i in 0..b {
+        if !s[a + 1 + i].is_numeric() {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
