8df2e0bc8a2326a959f78817239766ecabd7250b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-22 23:38:02 +0900

    add abc084_c

--- abc084/src/bin/c.rs
+++ abc084/src/bin/c.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        csf:[(usize,usize,usize);n-1],
     }
-    println!("{}", n);
+    let mut dp = vec![0; n];
+    for i in 0..n - 1 {
+        for j in 0..=i {
+            dp[j] = (dp[j] + (csf[i].2 - dp[j] % csf[i].2) % csf[i].2).max(csf[i].1) + csf[i].0;
+        }
+    }
+    for ans in dp {
+        println!("{}", ans);
+    }
 }
