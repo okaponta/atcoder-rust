e245e8f0406278651e20096d15919f0b57326026
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-24 22:43:38 +0900

    add abc307_b

--- abc307/src/bin/b.rs
+++ abc307/src/bin/b.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            let s = s[i].iter().chain(s[j].iter()).collect::<Vec<_>>();
+            if is_kaibun(s) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
+}
+
+fn is_kaibun(c: Vec<&char>) -> bool {
+    let n = c.len();
+    for i in 0..n / 2 {
+        if c[i] != c[n - 1 - i] {
+            return false;
+        }
+    }
+    true
 }
