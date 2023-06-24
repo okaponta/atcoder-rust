d0a3cd6cef4e66d3927aafed760d3d975129b43d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-24 23:09:32 +0900

    add abc307_c

--- abc307/src/bin/c.rs
+++ abc307/src/bin/c.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use proconio::{input, marker::Chars};
 
 fn main() {
@@ -12,95 +13,67 @@ fn main() {
         wx:usize,
         x:[Chars;hx],
     }
-    while (0..wa).into_iter().all(|j| a[0][j] == '.') {
-        a.remove(0);
-        ha -= 1;
-    }
-    while (0..wa).into_iter().rev().all(|j| a[ha - 1][j] == '.') {
-        a.remove(ha - 1);
-        ha -= 1;
-    }
-    while (0..ha).into_iter().all(|i| a[i][0] == '.') {
-        for j in 0..ha {
-            a[j].remove(0);
-        }
-        wa -= 1;
-    }
-    while (0..ha).into_iter().rev().all(|i| a[i][wa - 1] == '.') {
-        for j in 0..ha {
-            a[j].remove(wa - 1);
-        }
-        wa -= 1;
-    }
-    while (0..wb).into_iter().all(|j| b[0][j] == '.') {
-        b.remove(0);
-        hb -= 1;
-    }
-    while (0..wb).into_iter().rev().all(|j| b[hb - 1][j] == '.') {
-        b.remove(hb - 1);
-        hb -= 1;
-    }
-    while (0..hb).into_iter().all(|i| b[i][0] == '.') {
-        for j in 0..hb {
-            b[j].remove(0);
-        }
-        wb -= 1;
-    }
-    while (0..hb).into_iter().rev().all(|i| b[i][wb - 1] == '.') {
-        for j in 0..hb {
-            b[j].remove(wb - 1);
-        }
-        wb -= 1;
-    }
+    let (ha, wa, a) = trim_empty_rounds(ha, wa, a);
+    let (hb, wb, b) = trim_empty_rounds(hb, wb, b);
     if hx < ha || hx < hb || wx < wa || wx < wb {
         println!("No");
         return;
     }
-    // for i in 0..ha {
-    //     println!("{:?}", a[i]);
-    // }
-    // for i in 0..hb {
-    //     println!("{:?}", b[i]);
-    // }
-    // aの開始
-    for i in 0..=hx - ha {
-        for j in 0..=wx - wa {
-            // bの開始
-            for k in 0..=hx - hb {
-                for l in 0..=wx - wb {
-                    let mut ans = vec![vec!['.'; wx]; hx];
-                    for m in 0..ha {
-                        for n in 0..wa {
-                            if a[m][n] == '#' {
-                                ans[i + m][j + n] = '#';
-                            }
-                        }
-                    }
-                    for m in 0..hb {
-                        for n in 0..wb {
-                            if b[m][n] == '#' {
-                                ans[k + m][l + n] = '#';
-                            }
-                        }
-                    }
-                    let mut is_ans = true;
-                    for m in 0..hx {
-                        for n in 0..wx {
-                            if ans[m][n] != x[m][n] {
-                                is_ans = false;
-                            }
-                        }
-                    }
-                    // for i in 0..hx {
-                    //     println!("{:?}", ans[i]);
-                    // }
-                    if is_ans {
-                        println!("Yes");
-                        return;
-                    }
-                }
+    for (i, j, k, l) in iproduct!(0..=hx - ha, 0..=wx - wa, 0..=hx - hb, 0..=wx - wb) {
+        let mut ans = vec![vec!['.'; wx]; hx];
+        for (m, n) in iproduct!(0..ha, 0..wa) {
+            if a[m][n] == '#' {
+                ans[i + m][j + n] = '#';
             }
+        }
+        for (m, n) in iproduct!(0..hb, 0..wb) {
+            if b[m][n] == '#' {
+                ans[k + m][l + n] = '#';
+            }
+        }
+        if is_same(hx, wx, &ans, &x) {
+            println!("Yes");
+            return;
         }
     }
     println!("No");
+}
+
+fn trim_empty_rounds(
+    mut h: usize,
+    mut w: usize,
+    mut a: Vec<Vec<char>>,
+) -> (usize, usize, Vec<Vec<char>>) {
+    while (0..w).into_iter().all(|j| a[0][j] == '.') {
+        a.remove(0);
+        h -= 1;
+    }
+    while (0..w).into_iter().rev().all(|j| a[h - 1][j] == '.') {
+        a.remove(h - 1);
+        h -= 1;
+    }
+    while (0..h).into_iter().all(|i| a[i][0] == '.') {
+        for j in 0..h {
+            a[j].remove(0);
+        }
+        w -= 1;
+    }
+    while (0..h).into_iter().rev().all(|i| a[i][w - 1] == '.') {
+        for j in 0..h {
+            a[j].remove(w - 1);
+        }
+        w -= 1;
+    }
+    (h, w, a)
+}
+
+fn is_same(h: usize, w: usize, a: &Vec<Vec<char>>, b: &Vec<Vec<char>>) -> bool {
+    for i in 0..h {
+        for j in 0..w {
+            if a[i][j] != b[i][j] {
+                return false;
+            }
+        }
+    }
+    true
 }
