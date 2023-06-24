4ff6441c92c9ff1598e3505222cf37404618d6a5
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-24 22:43:37 +0900

    add abc307_a

--- abc307/src/bin/a.rs
+++ abc307/src/bin/a.rs
@@ -1,8 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;7*n],
     }
-    println!("{}", n);
+    let mut ans = vec![0; n];
+    for i in 0..7 * n {
+        ans[i / 7] += a[i];
+    }
+    println!("{}", ans.iter().join(" "));
 }
