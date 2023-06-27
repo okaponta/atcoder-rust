dcde15bf7d451752082088d36a4d124ab041e95a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-27 23:51:26 +0900

    add abc083_b

--- abc083/src/bin/b.rs
+++ abc083/src/bin/b.rs
@@ -3,6 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 1..=n {
+        let s = sum_digits(i);
+        if a <= s && s <= b {
+            ans += i;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn sum_digits(mut n: usize) -> usize {
+    let mut res = 0;
+    while 0 < n {
+        res += n % 10;
+        n /= 10;
+    }
+    res
 }
