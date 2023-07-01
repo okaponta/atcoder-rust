5f2bfe592017c35bbefb9065197b9ae53334cbd3
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-01 22:45:52 +0900

    add abc308_c

--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -1,8 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut v = ab
+        .into_iter()
+        .enumerate()
+        .map(|(i, (a, b))| (a, b, i + 1))
+        .collect::<Vec<_>>();
+    v.sort_by(|a, b| ((b.0 * (a.0 + a.1)).cmp(&(a.0 * (b.0 + b.1)))).then(a.2.cmp(&b.2)));
+    println!("{}", v.into_iter().map(|(_, _, i)| i).join(" "));
 }
