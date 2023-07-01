91258f4324e0136a9b5bf5d56fc029e11330d215
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-01 22:45:37 +0900

    add abc308_b

--- abc308/src/bin/b.rs
+++ abc308/src/bin/b.rs
@@ -1,8 +1,22 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        c:[String;n],
+        d:[String;m],
+        p:[usize;m+1],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 0..m {
+        map.insert(&d[i], p[i + 1]);
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        ans += map.get(&c[i]).unwrap_or(&p[0]);
+    }
+    println!("{}", ans);
 }
