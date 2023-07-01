fc10619d19a5da2e62ab4796b9ff010169cbdc04
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-01 22:53:17 +0900

    add abc308_e

--- abc308/src/bin/e.rs
+++ abc308/src/bin/e.rs
@@ -1,8 +1,49 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
+        s:Chars,
     }
-    println!("{}", n);
+    let mut mdp = vec![0; 3];
+    let mut edp = vec![0; 6];
+    let mut ans = 0usize;
+    for i in 0..n {
+        if s[i] == 'M' {
+            mdp[a[i]] += 1;
+        } else if s[i] == 'E' {
+            if a[i] == 0 {
+                edp[0] += mdp[0];
+                edp[1] += mdp[1];
+                edp[2] += mdp[2];
+            } else if a[i] == 1 {
+                edp[1] += mdp[0];
+                edp[3] += mdp[1];
+                edp[4] += mdp[2];
+            } else {
+                edp[2] += mdp[0];
+                edp[4] += mdp[1];
+                edp[5] += mdp[2];
+            }
+        } else {
+            if a[i] == 0 {
+                ans += edp[0];
+                ans += 2 * edp[1];
+                ans += edp[2];
+                ans += 2 * edp[3];
+                ans += 3 * edp[4];
+                ans += edp[5];
+            } else if a[i] == 1 {
+                ans += 2 * edp[0];
+                ans += 2 * edp[1];
+                ans += 3 * edp[2];
+            } else {
+                ans += edp[0];
+                ans += 3 * edp[1];
+                ans += edp[2];
+            }
+        }
+    }
+    println!("{}", ans);
 }
