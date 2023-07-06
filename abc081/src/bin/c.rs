e7eddc6e55fbb499637f2c07f92468d8be9d7afb
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-06 21:55:18 +0900

    add abc081_c

--- abc081/src/bin/c.rs
+++ abc081/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        k:Usize1,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for a in a {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    let mut count = map.into_iter().map(|(_, v)| v).collect::<Vec<_>>();
+    count.sort();
+    count.reverse();
+    let mut ans = 0;
+    for i in 0..count.len() {
+        if k < i {
+            ans += count[i];
+        }
+    }
+    println!("{}", ans);
 }
