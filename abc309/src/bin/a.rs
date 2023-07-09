3b333b8b50dfe064c7587c61feb0b40b8c4aaa2b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-09 23:17:45 +0900

    add abc309_a

--- abc309/src/bin/a.rs
+++ abc309/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i32,
+        b:i32,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if b - a == 1 && a % 3 != 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
 }
