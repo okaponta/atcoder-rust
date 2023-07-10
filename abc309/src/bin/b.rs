616335ac23daa178c675b4f6b4ed3ba65fbea38a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-10 20:22:16 +0900

    add abc309_b

--- abc309/src/bin/b.rs
+++ abc309/src/bin/b.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[Chars;n],
     }
-    println!("{}", n);
+    let mut b = a.clone();
+    for i in 0..n - 1 {
+        b[0][i + 1] = a[0][i];
+        b[i + 1][n - 1] = a[i][n - 1];
+        b[n - 1][n - i - 2] = a[n - 1][n - i - 1];
+        b[n - i - 2][0] = a[n - i - 1][0];
+    }
+    for i in 0..n {
+        for j in 0..n {
+            print!("{}", b[i][j]);
+        }
+        println!();
+    }
 }
