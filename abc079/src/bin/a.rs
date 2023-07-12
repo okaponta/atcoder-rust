ce0a0745ee477eae6db01220f10fa767872e8440
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-12 14:20:13 +0900

    add abc079_a

--- abc079/src/bin/a.rs
+++ abc079/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        n:Chars,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if (n[0] == n[1] || n[2] == n[3]) && n[1] == n[2] {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
