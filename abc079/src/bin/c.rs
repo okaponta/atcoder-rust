ac0593a016c4be882f44a9e9ec7d1ff9330bc83d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-15 23:11:21 +0900

    add abc079_c

--- abc079/src/bin/c.rs
+++ abc079/src/bin/c.rs
@@ -1,8 +1,46 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let a = s[0].to_digit(10).unwrap() as i32;
+    let b = s[1].to_digit(10).unwrap() as i32;
+    let c = s[2].to_digit(10).unwrap() as i32;
+    let d = s[3].to_digit(10).unwrap() as i32;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                let mut ans = a;
+                if i == 0 {
+                    ans += b;
+                } else {
+                    ans -= b;
+                }
+                if j == 0 {
+                    ans += c;
+                } else {
+                    ans -= c;
+                }
+                if k == 0 {
+                    ans += d;
+                } else {
+                    ans -= d;
+                }
+                if ans == 7 {
+                    println!(
+                        "{}{}{}{}{}{}{}=7",
+                        a,
+                        if i == 0 { '+' } else { '-' },
+                        b,
+                        if j == 0 { '+' } else { '-' },
+                        c,
+                        if k == 0 { '+' } else { '-' },
+                        d
+                    );
+                    return;
+                }
+            }
+        }
+    }
 }
