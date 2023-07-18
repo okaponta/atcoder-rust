ca81c3400b23fb1d9f09f8c6e6bf1ef4d9582f62
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-18 20:41:34 +0900

    add abc310_a

--- abc310/src/bin/a.rs
+++ abc310/src/bin/a.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut p:usize,
+        q:usize,
+        d:[usize;n],
     }
-    println!("{}", n);
+    for d in d {
+        p = p.min(q + d);
+    }
+    println!("{}", p);
 }
