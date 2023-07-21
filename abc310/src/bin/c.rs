e6d2bb4475c2d376480c6ce6930e8f89f5ca2214
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-21 13:23:47 +0900

    add abc310_c

--- abc310/src/bin/c.rs
+++ abc310/src/bin/c.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for s in s {
+        let s1 = s.iter().collect::<String>();
+        let s2 = s.iter().rev().collect::<String>();
+        if set.contains(&s1) || set.contains(&s2) {
+            continue;
+        }
+        set.insert(s1);
+    }
+    println!("{}", set.len());
 }
