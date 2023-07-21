63519ebee7a084d90ecc6364f0e68bc2ab6a1a5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-21 15:12:14 +0900

    add abc310_e

--- abc310/src/bin/e.rs
+++ abc310/src/bin/e.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut dp = vec![0, 0];
+    let mut ans = 0usize;
+    for i in 0..n {
+        if s[i] == '0' {
+            dp[1] += dp[0];
+            dp[0] = 1;
+        } else {
+            dp.swap(0, 1);
+            dp[1] += 1;
+        }
+        ans += dp[1];
+    }
+    println!("{}", ans);
 }
