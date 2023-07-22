e59df6fbcff11adf0b383aa8c315e84007662b7e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-22 22:43:00 +0900

    add abc311_a

--- abc311/src/bin/a.rs
+++ abc311/src/bin/a.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut flg = vec![false; 3];
+    for i in 0..n {
+        flg[(s[i] as u8 - b'A') as usize] = true;
+        if flg.iter().all(|&b| b) {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
