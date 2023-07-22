c22744440e641cc6a0e0474f37428f9fd90a7b5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-23 02:07:24 +0900

    add abc311_b

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -6,23 +6,13 @@ fn main() {
         d:usize,
         s:[Chars;n],
     }
-    let mut ok = vec![];
+    let mut ans = 0;
+    let mut tmp = 0;
     for i in 0..d {
         if (0..n).into_iter().all(|j| s[j][i] == 'o') {
-            ok.push(i);
-        }
-    }
-    if ok.len() == 0 {
-        println!("0");
-        return;
-    }
-    let mut ans = 1;
-    let mut tmp = 1;
-    for i in 1..ok.len() {
-        if ok[i] == ok[i - 1] + 1 {
             tmp += 1;
         } else {
-            tmp = 1;
+            tmp = 0;
         }
         ans = ans.max(tmp);
     }
