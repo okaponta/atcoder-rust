7f38c7f01e39c9e9b40e01fef64df96ee845c11e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-22 22:43:01 +0900

    add abc311_b

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        d:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut ok = vec![];
+    for i in 0..d {
+        if (0..n).into_iter().all(|j| s[j][i] == 'o') {
+            ok.push(i);
+        }
+    }
+    if ok.len() == 0 {
+        println!("0");
+        return;
+    }
+    let mut ans = 1;
+    let mut tmp = 1;
+    for i in 1..ok.len() {
+        if ok[i] == ok[i - 1] + 1 {
+            tmp += 1;
+        } else {
+            tmp = 1;
+        }
+        ans = ans.max(tmp);
+    }
+    println!("{}", ans);
 }
