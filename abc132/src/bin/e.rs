f251e68802b3c3dedc11b2506ed2baa57fa0d418
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-27 01:03:08 +0900

    add abc132_e

--- abc132/src/bin/e.rs
+++ abc132/src/bin/e.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
+        s:Usize1,
+        t:Usize1,
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for (u, v) in uv {
+        g[u].push(v);
+    }
+    let mut dist = vec![vec![1usize << 60; 3]; n];
+    let mut q = VecDeque::new();
+    q.push_back((s, 0, 0));
+    while let Some((cur, d, turn)) = q.pop_front() {
+        if dist[cur][turn] <= d {
+            continue;
+        }
+        dist[cur][turn] = d;
+        for &next in &g[cur] {
+            let nextd = if (turn + 1) % 3 == 0 { d + 1 } else { d };
+            if nextd < dist[next][(turn + 1) % 3] {
+                q.push_back((next, nextd, (turn + 1) % 3));
+            }
+        }
+    }
+    let ans = dist[t][0];
+    println!("{}", if ans == 1 << 60 { -1 } else { ans as i64 });
 }
