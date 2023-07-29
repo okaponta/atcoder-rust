229993469a274599838b3ab2a743fa3f3348dccd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-29 22:33:54 +0900

    add abc077_b

--- abc077/src/bin/b.rs
+++ abc077/src/bin/b.rs
@@ -4,5 +4,9 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut tmp = 1;
+    while (tmp + 1) * (tmp + 1) <= n {
+        tmp += 1;
+    }
+    println!("{}", tmp * tmp);
 }
