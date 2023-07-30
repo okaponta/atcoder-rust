d4fbfa6ac413caad73c4948dfc100b8a55bbc7f3
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-07-30 16:03:32 +0900

    add abc312_c

--- abc312/src/bin/c.rs
+++ abc312/src/bin/c.rs
@@ -1,8 +1,30 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut a:[usize;n],
+        mut b:[usize;m],
     }
-    println!("{}", n);
+    a.sort();
+    b.sort();
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(med, &a, &b) {
+            upper = med;
+        } else {
+            lower = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn is_ok(med: usize, a: &Vec<usize>, b: &Vec<usize>) -> bool {
+    let a = a.upper_bound(&med);
+    let b = b.len() - b.lower_bound(&med);
+    b <= a
 }
