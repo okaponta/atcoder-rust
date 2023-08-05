c3ce70ac62cae5440c254cdba3e319f29c90878d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-05 22:57:03 +0900

    add abc313_c

--- abc313/src/bin/c.rs
+++ abc313/src/bin/c.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    a.reverse();
+    let sum: usize = a.iter().sum();
+    let mut ans = 0;
+    for i in 0..n {
+        let mut target = sum / n;
+        if i < sum % n {
+            target += 1;
+        }
+        ans += abs(target, a[i]);
+    }
+    println!("{}", ans / 2);
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
