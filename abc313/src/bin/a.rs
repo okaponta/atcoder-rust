ccb510a1ea5ffe423b40561710ea51c36cdb9475
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-05 22:57:00 +0900

    add abc313_a

--- abc313/src/bin/a.rs
+++ abc313/src/bin/a.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut p:[usize;n],
     }
-    println!("{}", n);
+    let base = p[0];
+    p.sort();
+    p.reverse();
+    if p.len() == 1 {
+        println!("0");
+        return;
+    }
+    if p[0] == p[1] {
+        if base == p[0] {
+            println!("1");
+        } else {
+            println!("{}", p[0] + 1 - base);
+        }
+    } else {
+        if base == p[0] {
+            println!("0");
+        } else {
+            println!("{}", p[0] + 1 - base);
+        }
+    }
 }
