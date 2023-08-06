120e6b8d3801c00af4cf0eab82375d033ac71c77
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-07 01:33:27 +0900

    add abc313_e

--- abc313/src/bin/e.rs
+++ abc313/src/bin/e.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in (0..n - 1).rev() {
+        let prev = s[i + 1].to_digit(10).unwrap() as usize;
+        let cur = s[i].to_digit(10).unwrap() as usize;
+        if 1 < cur && 1 < prev {
+            println!("-1");
+            return;
+        }
+        ans = (ans * prev + prev) % 998244353;
+    }
+    println!("{}", ans);
 }
