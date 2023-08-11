effd9c36e24658e3f3f3f6d2432110874d1aa552
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-11 23:33:30 +0900

    add abc076_c

--- abc076/src/bin/c.rs
+++ abc076/src/bin/c.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let m = t.len();
+    if n < m {
+        println!("UNRESTORABLE");
+        return;
+    }
+    let mut str = vec![];
+    'a: for i in 0..=n - m {
+        let mut tmp = s.clone();
+        for j in 0..m {
+            if s[i + j] == '?' || s[i + j] == t[j] {
+                tmp[i + j] = t[j];
+            } else {
+                continue 'a;
+            }
+        }
+        str.push(tmp);
+    }
+    if str.len() == 0 {
+        println!("UNRESTORABLE");
+        return;
+    }
+    for i in 0..str.len() {
+        for j in 0..n {
+            if str[i][j] == '?' {
+                str[i][j] = 'a';
+            }
+        }
+    }
+    str.sort();
+
+    println!("{}", str[0].iter().collect::<String>());
 }
