96108b7ffed43633e6f5521c356c7b64bdfdef5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-13 00:33:57 +0900

    add abc314_c

--- abc314/src/bin/c.rs
+++ abc314/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        s:Chars,
+        c:[Usize1;n],
     }
-    println!("{}", n);
+    let mut t = s.clone();
+    let mut colors = vec![vec![]; m];
+    for i in 0..n {
+        colors[c[i]].push(i);
+    }
+    let mut index = vec![0; n];
+    for i in 0..m {
+        for j in 1..=colors[i].len() {
+            index[colors[i][j % colors[i].len()]] = colors[i][j - 1];
+        }
+    }
+    for i in 0..n {
+        t[i] = s[index[i]];
+    }
+    println!("{}", t.iter().join(""));
 }
