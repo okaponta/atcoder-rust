f1c6401186fc98f3fc3b9aba68eb27ae47ee5e5d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-15 22:19:01 +0900

    add abc157_e

--- abc157/src/bin/e.rs
+++ abc157/src/bin/e.rs
@@ -1,8 +1,139 @@
-use proconio::input;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        s:Chars,
+        q:usize,
     }
-    println!("{}", n);
+    let mut seg = SegmentTree::new(n, 0usize, |a, b| a | b);
+    for i in 0..n {
+        seg.update_tmp(i, 1 << (s[i] as u8 + 1 - b'a'));
+    }
+    seg.update_all();
+    for _ in 0..q {
+        input! {q:u8}
+        if q == 1 {
+            input! {
+                i:Usize1,
+                c:char
+            }
+            seg.update(i, 1 << (c as u8 + 1 - b'a'));
+        } else {
+            input! {
+                l:Usize1,
+                r:usize,
+            }
+            let sum = seg.query(l..r);
+            println!("{}", sum.count_ones());
+        }
+    }
+}
+
+// segment tree
+// seg[0] -> seg[1]+seg[2]
+// seg[1] -> seg[3]+seg[4] seg[2] -> seg[5]+seg[6]
+// seg[3] -> seg[7]+seg[8] seg[6] -> seg[13]+seg[14]]
+// 必要な要素数は2^n-1
+// 区間上の値を更新する
+// 任意の区間上の最小値や合計値(与えるfuncによって全てのbit or値)などを取得する
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
+
+    // lowerとupperの間でfを満たす最小の値
+    // ng, ng, ng, (ok), ok, ok
+    pub fn max_right<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                upper = med;
+            } else {
+                lower = med;
+            }
+        }
+        upper
+    }
+
+    // lowerとupperの間でfを満たさない最大の値
+    pub fn min_left<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
