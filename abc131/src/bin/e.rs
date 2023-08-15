054eb88103c60685737402733f5be671702f211c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-16 00:19:06 +0900

    add abc131_e

--- abc131/src/bin/e.rs
+++ abc131/src/bin/e.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let max = n * (n - 1) / 2;
+    if max + 1 - n < k {
+        println!("-1");
+        return;
+    }
+    let mut ans = vec![];
+    for i in 0..n {
+        for j in i + 1..n {
+            if k + ans.len() == max {
+                break;
+            }
+            ans.push((i + 1, j + 1));
+        }
+    }
+    println!("{}", ans.len());
+    for i in 0..ans.len() {
+        println!("{} {}", ans[i].0, ans[i].1);
+    }
 }
