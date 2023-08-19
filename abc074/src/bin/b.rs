50aec57da6284b6ca2935104fa7555b3765469f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-19 21:43:57 +0900

    add abc074_b

--- abc074/src/bin/b.rs
+++ abc074/src/bin/b.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:i64,
+        x:[i64;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..n {
+        ans += x[i].min((x[i] - k).abs());
+    }
+    println!("{}", ans * 2);
 }
