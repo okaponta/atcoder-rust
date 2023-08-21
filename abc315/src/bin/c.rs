9ff9b8d3776950d07bbec37b644c1ce74bf1f750
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-21 21:25:29 +0900

    add abc315_c

--- abc315/src/bin/c.rs
+++ abc315/src/bin/c.rs
@@ -3,6 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        fs:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut max = (0, 0);
+    for i in 0..n {
+        if max.0 < fs[i].1 {
+            max = (fs[i].1, i);
+        }
+    }
+    let taste = fs[max.1].0;
+    let mut ans = max.0;
+    for i in 0..n {
+        if i == max.1 {
+            continue;
+        }
+        if fs[i].0 == taste {
+            ans = ans.max(max.0 + fs[i].1 / 2);
+        } else {
+            ans = ans.max(max.0 + fs[i].1);
+        }
+    }
+    println!("{}", ans);
 }
