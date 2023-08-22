d5f4012c50723793f01949a9fd8b75ebcabfc562
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-23 02:26:54 +0900

    add abc315_e

--- abc315/src/bin/e.rs
+++ abc315/src/bin/e.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for i in 0..n {
+        input! {
+            c:usize,
+            p:[Usize1;c],
+        }
+        for p in p {
+            g[i].push(p);
+        }
+    }
+    let mut visited = vec![false; n];
+    let mut ans = vec![];
+    dfs(0, &g, &mut ans, &mut visited);
+    ans.pop();
+    println!("{}", ans.into_iter().join(" "));
+}
+
+fn dfs(cur: usize, g: &Vec<Vec<usize>>, ans: &mut Vec<usize>, visited: &mut Vec<bool>) {
+    if visited[cur] {
+        return;
+    }
+    visited[cur] = true;
+    for &next in &g[cur] {
+        dfs(next, g, ans, visited);
+    }
+    ans.push(cur + 1);
 }
