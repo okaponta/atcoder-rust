1f6087f9391306dc9125fee52586317870685640
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-26 22:42:15 +0900

    add abc317_d

--- abc317/src/bin/d.rs
+++ abc317/src/bin/d.rs
@@ -3,6 +3,37 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xyz:[(usize,usize,usize);n],
     }
-    println!("{}", n);
+    let mut s = 0;
+    for i in 0..n {
+        s += xyz[i].2;
+    }
+    // これより大きい
+    let need = s / 2;
+    let mut win = 0;
+    let mut lose = vec![];
+    for (x, y, z) in xyz {
+        if y < x {
+            win += z;
+        } else {
+            lose.push(((y - x) / 2 + 1, z));
+        }
+    }
+    if need < win {
+        println!("0");
+        return;
+    }
+    let mut dp = vec![!0; 2 + need - win];
+    dp[0] = 0;
+    let l = dp.len();
+    for (ne, giseki) in lose {
+        for i in (0..l).rev() {
+            if dp[i] == !0 {
+                continue;
+            }
+            dp[(i + giseki).min(l - 1)] = dp[(i + giseki).min(l - 1)].min(dp[i] + ne);
+        }
+    }
+    println!("{}", dp[l - 1]);
 }
