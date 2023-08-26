1d55427fad93123e48f0be0fc8b2f9c192ccc5e1
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-26 22:42:12 +0900

    add abc317_b

--- abc317/src/bin/b.rs
+++ abc317/src/bin/b.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    for i in 1..n {
+        if a[i] - a[i - 1] != 1 {
+            println!("{}", a[i - 1] + 1);
+            return;
+        }
+    }
 }
