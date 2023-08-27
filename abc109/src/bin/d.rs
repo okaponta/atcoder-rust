cc19f5e1874e263b9f8138d2104c32236a0af0d1
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-27 22:03:37 +0900

    add abc109_d

--- abc109/src/bin/d.rs
+++ abc109/src/bin/d.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        mut a:[[u8;w];h],
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 0..h {
+        for j in 0..w - 1 {
+            if a[i][j] % 2 == 1 {
+                ans.push((i, j, i, j + 1));
+                a[i][j + 1] += 1;
+            }
+        }
+    }
+    for i in 0..h - 1 {
+        if a[i][w - 1] % 2 == 1 {
+            ans.push((i, w - 1, i + 1, w - 1));
+            a[i + 1][w - 1] += 1;
+        }
+    }
+    println!("{}", ans.len());
+    for (a, b, c, d) in ans {
+        println!("{} {} {} {}", a + 1, b + 1, c + 1, d + 1);
+    }
 }
