38c1543e4272dc3bf1a845e8e8d02c5355ddcf25
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-29 20:07:39 +0900

    add abc077_c

--- abc077/src/bin/c.rs
+++ abc077/src/bin/c.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
+        mut b:[usize;n],
+        mut c:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    b.sort();
+    c.sort();
+    let mut bc = vec![0; n + 1];
+    for i in (0..n).rev() {
+        bc[i] = bc[i + 1] + n - c.upper_bound(&b[i]);
+    }
+    let ans = a.iter().fold(0, |s, a| s + bc[b.upper_bound(a)]);
+    println!("{}", ans);
 }
