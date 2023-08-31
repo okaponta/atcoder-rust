29f29a461b13937e5266130602da13ccedd3d619
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-08-31 21:53:07 +0900

    add abc165_e

--- abc165/src/bin/e.rs
+++ abc165/src/bin/e.rs
@@ -2,8 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       _:usize,
+       m:usize,
     }
-    println!("{}", a);
-    todo!();
+    if m == 1 {
+        println!("1 2");
+        return;
+    }
+    let mut l1 = 1;
+    let mut r1 = 1 + m;
+    let mut l2 = r1 + 1;
+    let mut r2 = l2 + m - 1;
+    let mut ans = vec![];
+    while l1 < r1 {
+        ans.push((l1, r1));
+        l1 += 1;
+        r1 -= 1;
+    }
+    while l2 < r2 {
+        ans.push((l2, r2));
+        l2 += 1;
+        r2 -= 1;
+    }
+    for (l, r) in ans {
+        println!("{} {}", l, r);
+    }
 }
