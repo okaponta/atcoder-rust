65c9d3885a2e220d54d0709ce5f42965c3b36797
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-01 11:13:03 +0900

    add abc073_a

--- abc073/src/bin/a.rs
+++ abc073/src/bin/a.rs
@@ -4,5 +4,12 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        if n % 10 == 9 || n / 10 == 9 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
