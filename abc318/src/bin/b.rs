3692e0d764932838b961ff3c778f4ee6b0ccd0d0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-02 23:07:43 +0900

    add abc318_b

--- abc318/src/bin/b.rs
+++ abc318/src/bin/b.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        abcd:[(usize,usize,usize,usize);n],
     }
-    println!("{}", n);
+    let mut sheet = vec![vec![false; 101]; 101];
+    for (a, b, c, d) in abcd {
+        for i in a..b {
+            for j in c..d {
+                sheet[i][j] = true;
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..101 {
+        for j in 0..101 {
+            if sheet[i][j] {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
