cd345d365cbb221c8f2174987b8b6f65e4fb1c81
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-02 23:07:45 +0900

    add abc318_c

--- abc318/src/bin/c.rs
+++ abc318/src/bin/c.rs
@@ -3,6 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        d:usize,
+        p:usize,
+        mut f:[usize;n],
     }
-    println!("{}", n);
+    let mut s = f.iter().sum::<usize>();
+    let mut i = 0;
+    let mut j = 0;
+    f.sort();
+    f.reverse();
+    let mut ans = s;
+    while j < n {
+        j += d;
+        s += p;
+        while i < j && i < n {
+            s -= f[i];
+            i += 1;
+        }
+        ans = ans.min(s);
+    }
+    println!("{}", ans);
 }
