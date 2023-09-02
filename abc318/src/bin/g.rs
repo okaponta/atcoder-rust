4f8c157b4981c21129b06ea823927afd2104a6f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-03 00:00:53 +0900

    add abc318_g

--- abc318/src/bin/g.rs
+++ abc318/src/bin/g.rs
@@ -3,6 +3,76 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:usize,
+        b:usize,
+        c:usize,
+        uv:[(usize,usize);m],
     }
-    println!("{}", n);
+    let mut d = FordFulkerson::new(2 * n + 2);
+    for i in 0..n {
+        d.add_edges(i + 1, n + i + 1, 1);
+    }
+    for (u, v) in uv {
+        d.add_edges(n + u, v, 1);
+        d.add_edges(n + v, u, 1);
+    }
+    d.add_edges(0, n + b, 2);
+    d.add_edges(n + a, 2 * n + 1, 1);
+    d.add_edges(n + c, 2 * n + 1, 1);
+    println!("{}", if d.max_flow() == 2 { "Yes" } else { "No" });
+}
+
+struct FordFulkerson {
+    n: usize,
+    edge: Vec<Vec<(usize, i64, usize)>>,
+    flow: i64,
+}
+
+impl FordFulkerson {
+    pub fn new(n: usize) -> Self {
+        let flow = 0;
+        let edge = vec![vec![]; n];
+        Self { n, edge, flow }
+    }
+
+    pub fn add_edges(&mut self, from: usize, to: usize, cap: i64) {
+        let idx = self.edge[from].len();
+        let rev_idx = self.edge[to].len();
+        self.edge[from].push((to, cap, rev_idx));
+        self.edge[to].push((from, 0, idx));
+    }
+
+    pub fn max_flow(&mut self) -> i64 {
+        loop {
+            if self.next_flow() == 0 {
+                return self.flow;
+            }
+        }
+    }
+
+    pub fn next_flow(&mut self) -> i64 {
+        let f = self.dfs(0, self.n - 1, 1 << 60, &mut vec![false; self.n]);
+        self.flow += f;
+        f
+    }
+
+    pub fn dfs(&mut self, from: usize, to: usize, flow: i64, used: &mut Vec<bool>) -> i64 {
+        if from == to {
+            return flow;
+        }
+        used[from] = true;
+        for i in 0..self.edge[from].len() {
+            let (next, cap, rev) = self.edge[from][i];
+            if !used[next] && cap > 0 {
+                let d = self.dfs(next, to, flow.min(cap), used);
+                if d > 0 {
+                    self.edge[from][i].1 -= d;
+                    self.edge[next][rev].1 += d;
+                    return d;
+                }
+            }
+        }
+        0
+    }
 }
