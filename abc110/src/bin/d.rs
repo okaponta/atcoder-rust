4b3fdf736a5a4cb2859f89cab577790354db89f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-04 08:52:22 +0900

    add abc110_d

--- abc110/src/bin/d.rs
+++ abc110/src/bin/d.rs
@@ -1,8 +1,72 @@
+use num_integer::Roots;
 use proconio::input;
+
+const MOD: usize = 1_000_000_007;
 
 fn main() {
     input! {
         n:usize,
+        m:i64,
     }
-    println!("{}", n);
+    let mut ans = 1;
+    let comb = Binom::init(n + 40, MOD);
+    for (_, i) in factorize(m) {
+        ans = ans * comb.get(n + i as usize - 1, n - 1);
+        ans %= MOD;
+    }
+    println!("{}", ans);
+}
+
+fn factorize(mut n: i64) -> Vec<(i64, i64)> {
+    let mut res = vec![];
+    for i in 2..=n.sqrt() {
+        if n % i != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % i == 0 {
+            ex += 1;
+            n /= i;
+        }
+        res.push((i, ex));
+    }
+    if n != 1 {
+        res.push((n, 1));
+    }
+    res
+}
+
+pub struct Binom {
+    fact: Vec<usize>,
+    fact_inv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Binom {
+    pub fn init(n: usize, modulo: usize) -> Self {
+        let mut fact = vec![0; n + 1];
+        let mut inv = vec![0; n + 1];
+        let mut fact_inv = vec![0; n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        inv[1] = 1;
+        fact_inv[0] = 1;
+        fact_inv[1] = 1;
+        for i in 2..(n + 1) {
+            fact[i] = fact[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            fact_inv[i] = fact_inv[i - 1] * inv[i] % modulo;
+        }
+        Self {
+            fact,
+            fact_inv,
+            modulo,
+        }
+    }
+
+    // nCk
+    pub fn get(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fact[n] * self.fact_inv[k] % self.modulo * self.fact_inv[n - k] % self.modulo
+    }
 }
