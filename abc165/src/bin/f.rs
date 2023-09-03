c7ff471ee3297efa63fbf9d181ffe89151ef8a71
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-03 21:16:04 +0900

    add abc165_f

--- abc165/src/bin/f.rs
+++ abc165/src/bin/f.rs
@@ -1,9 +1,154 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       a:[usize;n],
+       uv:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut g = vec![vec![]; n];
+    for (u, v) in uv {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let map = compress(&a);
+    let b = (0..n).into_iter().map(|i| map[&a[i]]).collect_vec();
+    let mut seg = SegmentTree::new(map.len(), 0, |a, b| a.max(b));
+    let mut ans = vec![0; n];
+    dfs(&mut seg, &g, 0, 0, &mut ans, &b, map.len());
+    for ans in ans {
+        println!("{}", ans);
+    }
+}
+
+fn dfs(
+    seg: &mut SegmentTree<usize, impl Fn(usize, usize) -> usize>,
+    g: &Vec<Vec<usize>>,
+    cur: usize,
+    prev: usize,
+    ans: &mut Vec<usize>,
+    b: &Vec<usize>,
+    n: usize,
+) {
+    let before = seg.query(b[cur]..b[cur] + 1);
+    let pre = seg.query(0..b[cur]);
+    seg.update(b[cur], pre + 1);
+    ans[cur] = seg.query(0..n);
+    for &next in &g[cur] {
+        if prev == next {
+            continue;
+        }
+        dfs(seg, g, next, cur, ans, b, n);
+    }
+    seg.update(b[cur], before);
+}
+
+fn compress(source: &[usize]) -> HashMap<usize, usize> {
+    let set: HashSet<&usize> = source.iter().collect();
+    let mut result: HashMap<usize, usize> = HashMap::new();
+    for (i, x) in set.into_iter().sorted().enumerate() {
+        result.insert(*x, i);
+    }
+    result
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
+
+    // lowerとupperの間でfを満たす最小の値
+    // ng, ng, ng, (ok), ok, ok
+    pub fn max_right<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                upper = med;
+            } else {
+                lower = med;
+            }
+        }
+        upper
+    }
+
+    // lowerとupperの間でfを満たさない最大の値
+    pub fn min_left<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
