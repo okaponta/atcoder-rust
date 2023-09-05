c3de1a51c3494ab9557a5a2392576545f186632d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-05 22:56:51 +0900

    add abc073_b

--- abc073/src/bin/b.rs
+++ abc073/src/bin/b.rs
@@ -1,8 +1,9 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        lr:[(Usize1,usize);n],
     }
-    println!("{}", n);
+    println!("{}", lr.iter().fold(0, |s, (l, r)| s + r - l));
 }
