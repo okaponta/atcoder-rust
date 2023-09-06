711439aee260bb8d25970839dcea32e4a2712688
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-06 22:29:30 +0900

    add abc199_e

--- abc199/src/bin/e.rs
+++ abc199/src/bin/e.rs
@@ -2,7 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       m:usize,
+       xyz:[(usize,usize,u32);m],
     }
-    println!("{}", a);
+    let mut yz = vec![vec![]; n];
+    for (x, y, z) in xyz {
+        yz[x].push((y, z));
+    }
+    let mut dp = vec![0usize; 1 << n];
+    dp[0] = 1;
+    for i in 0usize..(1 << n) - 1 {
+        if yz[i.count_ones() as usize]
+            .iter()
+            .any(|&(y, z)| z < (i & ((1 << y) - 1)).count_ones())
+        {
+            continue;
+        }
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                continue;
+            }
+            dp[i | 1 << j] += dp[i];
+        }
+    }
+    println!("{}", dp[(1 << n) - 1]);
 }
