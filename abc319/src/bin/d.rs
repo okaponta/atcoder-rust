0f562e618609c19b2530b9211df6faabeb3ca57a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-09 22:49:07 +0900

    add abc319_d

--- abc319/src/bin/d.rs
+++ abc319/src/bin/d.rs
@@ -3,6 +3,36 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        l:[usize;n],
     }
-    println!("{}", n);
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(med, &l, m) {
+            upper = med;
+        } else {
+            lower = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn is_ok(row: usize, l: &Vec<usize>, m: usize) -> bool {
+    let mut ans = 0;
+    let mut cur = row;
+    for &l in l {
+        if row < l {
+            return false;
+        }
+        cur += l + 1;
+        if row < cur {
+            // 改行
+            ans += 1;
+            cur = l;
+        }
+        //cur += 1;
+    }
+    ans <= m
 }
