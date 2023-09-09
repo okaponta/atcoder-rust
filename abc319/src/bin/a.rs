049f235e092f18415955f48f7de48f1b2f70f7cd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-09 22:49:03 +0900

    add abc319_a

--- abc319/src/bin/a.rs
+++ abc319/src/bin/a.rs
@@ -1,8 +1,21 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        s:String,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    map.insert("tourist".to_string(), 3858);
+    map.insert("ksun48".to_string(), 3679);
+    map.insert("Benq".to_string(), 3658);
+    map.insert("Um_nik".to_string(), 3648);
+    map.insert("apiad".to_string(), 3638);
+    map.insert("Stonefeang".to_string(), 3630);
+    map.insert("ecnerwala".to_string(), 3613);
+    map.insert("mnbvmar".to_string(), 3555);
+    map.insert("newbiedmy".to_string(), 3516);
+    map.insert("semiexp".to_string(), 3481);
+    println!("{}", map[&s]);
 }
