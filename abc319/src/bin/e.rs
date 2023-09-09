50c6577057b873954725fbaf69bfbe699ef7edae
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-09 22:49:08 +0900

    add abc319_e

--- abc319/src/bin/e.rs
+++ abc319/src/bin/e.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        y:usize,
+        pt:[(usize,usize);n-1],
+        q:usize,
+        qi:[usize;q],
     }
-    println!("{}", n);
+    let mut bus = vec![0; 840];
+    for i in 0..840 {
+        let mut cur = i;
+        for &(p, t) in &pt {
+            cur = ((cur + p - 1) / p) * p + t;
+        }
+        bus[i] = cur;
+    }
+    for qi in qi {
+        let mut ans = ((qi + x) / 840) * 840;
+        ans += bus[(qi + x) % 840];
+        ans += y;
+        println!("{}", ans);
+    }
 }
