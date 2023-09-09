b6f94fcb0779f452038bde18dc035d4bd9d8c88e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-09 22:49:05 +0900

    add abc319_b

--- abc319/src/bin/b.rs
+++ abc319/src/bin/b.rs
@@ -1,8 +1,45 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let div = divisor(n);
+    let mut js = vec![];
+    for j in div {
+        if j < 10 {
+            js.push(j);
+        }
+    }
+    for i in 0..=n {
+        let mut flg = false;
+        for &j in &js {
+            if i % (n / j) == 0 {
+                flg = true;
+                print!("{}", j);
+                break;
+            }
+        }
+        if !flg {
+            print!("-")
+        }
+    }
+    println!();
+}
+
+fn divisor(n: usize) -> Vec<usize> {
+    let mut res = vec![];
+    let mut upper = vec![];
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            res.push(i);
+            if i != n / i {
+                upper.push(n / i);
+            }
+        }
+    }
+    upper.reverse();
+    res.append(&mut upper);
+    res
 }
