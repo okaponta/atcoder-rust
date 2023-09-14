d2ba65fb1ebaed80d374fbc7644e8d20b7ed0ed0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-15 00:14:31 +0900

    add abc129_e

--- abc129/src/bin/e.rs
+++ abc129/src/bin/e.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        l:Chars,
     }
-    println!("{}", n);
+    const MOD: usize = 1_000_000_007;
+    let n = l.len();
+    let mut mag = 1;
+    let mut tri = vec![1];
+    for i in 0..n {
+        tri.push((tri[i] * 3) % MOD);
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        if l[i] == '1' {
+            ans = (ans + mag * tri[n - 1 - i]) % MOD;
+            mag = (mag * 2) % MOD;
+        }
+    }
+    println!("{}", (ans + mag) % MOD);
 }
