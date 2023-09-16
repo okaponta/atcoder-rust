1c9ae94924fe2c608abc6e17fb8dda921b11cc2a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-16 12:33:28 +0900

    add abc145_e

--- abc145/src/bin/e.rs
+++ abc145/src/bin/e.rs
@@ -3,6 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        mut ab:[(usize,i64);n],
     }
-    println!("{}", n);
+    ab.sort();
+    let mut dp = vec![-1 << 60; t + 1];
+    dp[0] = 0;
+    for (a, b) in ab {
+        for i in (0..t).rev() {
+            dp[(i + a).min(t)] = dp[(i + a).min(t)].max(dp[i] + b);
+        }
+    }
+    let mut ans = 0;
+    for i in 0..=t {
+        ans = ans.max(dp[i]);
+    }
+    println!("{}", ans);
 }
