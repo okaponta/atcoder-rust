e47c0b7c291d70397e7654c60342876aa33c1e26
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-18 22:07:57 +0900

    add abc320_c

--- abc320/src/bin/c.rs
+++ abc320/src/bin/c.rs
@@ -1,8 +1,32 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        m:usize,
+        s:[Chars;3],
     }
-    println!("{}", n);
+    let mut ans = 4 * m;
+    for i in 0..10 {
+        for p in (0..3).permutations(3) {
+            let mut flg = vec![false; 3];
+            for t in 0..4 * m {
+                for &j in &p {
+                    if !flg[j] && s[j][t % m] == (b'0' + i) as char {
+                        flg[j] = true;
+                        break;
+                    }
+                }
+                if flg.iter().all(|b| *b) {
+                    ans = ans.min(t);
+                    break;
+                }
+            }
+        }
+    }
+    if ans == 4 * m {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
