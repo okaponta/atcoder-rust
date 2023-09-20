1f7a289d5a9489ea9cdb67ac9a737866e3e19dff
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-20 13:49:31 +0900

    add abc320_f

--- abc320/src/bin/f.rs
+++ abc320/src/bin/f.rs
@@ -12,30 +12,32 @@ fn main() {
     pf.push((0, 0));
     // dp[i][j][k]...往路でj、復路でkの燃料をもっているときに支払う金額の合計の最小値
     // 往路では使用後、復路では使用前
-    let mut dp = vec![vec![vec![1 << 60; h + 1]; h + 1]; n + 1];
+    let mut dp = vec![vec![1 << 60; h + 1]; h + 1];
     for i in 0..=h {
-        dp[0][h][i] = 0;
+        dp[h][i] = 0;
     }
     for i in 1..=n {
+        let mut next = vec![vec![1 << 60; h + 1]; h + 1];
         let d = x[i] - x[i - 1];
         for j in d..=h {
             for k in 0..=h - d {
                 // 往路使わない復路使わない
-                dp[i][j - d][k + d] = dp[i][j - d][k + d].min(dp[i - 1][j][k]);
+                next[j - d][k + d] = next[j - d][k + d].min(dp[j][k]);
                 // 往路使う
-                dp[i][(j - d + pf[i].1).min(h)][k + d] =
-                    dp[i][(j - d + pf[i].1).min(h)][k + d].min(dp[i - 1][j][k] + pf[i].0);
-
+                next[(j - d + pf[i].1).min(h)][k + d] =
+                    next[(j - d + pf[i].1).min(h)][k + d].min(dp[j][k] + pf[i].0);
+                // 復路使う
                 let k2 = (k + pf[i].1).min(h);
                 if d <= k2 {
-                    dp[i][j - d][k] = dp[i][j - d][k].min(dp[i - 1][j][k2 - d] + pf[i].0);
+                    next[j - d][k] = next[j - d][k].min(dp[j][k2 - d] + pf[i].0);
                 }
             }
         }
+        dp = next;
     }
     let mut ans = 1 << 60;
     for i in 0..=h {
-        ans = ans.min(dp[n][i][i]);
+        ans = ans.min(dp[i][i]);
     }
     if ans == 1 << 60 {
         println!("-1");
