651e39b483c31e4ebd57ad15cd1035966c31122c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-21 09:44:23 +0900

    add abc320_b

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    for i in (0..n).rev() {
+        for j in 0..n - i {
+            if (0..i).into_iter().all(|k| s[j + k] == s[j + i - k]) {
+                println!("{}", i + 1);
+                return;
+            }
+        }
+    }
 }
