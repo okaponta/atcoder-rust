689ffdd606de4f2a4e07e520af1e8ba839d602db
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-23 22:47:22 +0900

    add abc321_c

--- abc321/src/bin/c.rs
+++ abc321/src/bin/c.rs
@@ -1,8 +1,22 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    for i in 1usize..10 {
+        q.push_back(i);
+    }
+    let mut ans = vec![];
+    while let Some(i) = q.pop_front() {
+        ans.push(i);
+        for j in 0..i % 10 {
+            q.push_back(i * 10 + j);
+        }
+    }
+    ans.sort();
+    println!("{}", ans[k - 1]);
 }
