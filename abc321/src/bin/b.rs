bb2797610204d22db6f9938e7dd2231bf6216fea
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-23 22:47:21 +0900

    add abc321_b

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        a:[usize;n-1],
     }
-    println!("{}", n);
+    for i in 0..=100 {
+        let mut b = a.clone();
+        b.push(i);
+        b.sort();
+        let mut sum = 0;
+        for j in 1..n - 1 {
+            sum += b[j];
+        }
+        if x <= sum {
+            println!("{}", i);
+            return;
+        }
+    }
+    println!("-1");
 }
