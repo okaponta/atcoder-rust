1c2c00fcdb88e4fbcb03f4b28a3e87f5834baa48
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-23 22:47:23 +0900

    add abc321_d

--- abc321/src/bin/d.rs
+++ abc321/src/bin/d.rs
@@ -1,8 +1,24 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        p:usize,
+        a:[usize;n],
+        mut b:[usize;m],
     }
-    println!("{}", n);
+    b.sort();
+    let mut bs = vec![0];
+    for i in 0..m {
+        bs.push(bs[i] + b[i]);
+    }
+    let mut ans = 0;
+    for a in a {
+        let sat = b.upper_bound(&(p.saturating_sub(a)));
+        ans += a * sat + bs[sat];
+        ans += (m - sat) * p;
+    }
+    println!("{:?}", ans);
 }
