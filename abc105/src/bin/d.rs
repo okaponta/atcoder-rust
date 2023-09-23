cd168e550e9ce9f159599f8d10bb485ca4ba1421
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-24 08:38:32 +0900

    add abc105_d

--- abc105/src/bin/d.rs
+++ abc105/src/bin/d.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::from([(0, 1)]);
+    let mut s = 0;
+    for a in a {
+        s += a;
+        *map.entry(s % m).or_insert(0usize) += 1;
+    }
+    let ans = map.into_iter().fold(0, |s, (_, v)| s + v * (v - 1) / 2);
+    println!("{}", ans);
 }
