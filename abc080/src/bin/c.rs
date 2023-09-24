373fec40e1bf72efc13acfd35207078f860f121a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-25 00:03:02 +0900

    add abc080_c

--- abc080/src/bin/c.rs
+++ abc080/src/bin/c.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        f:[[u8;10];n],
+        p:[[i64;11];n],
     }
-    println!("{}", n);
+    let mut ans = -1 << 60;
+    for i in 1..1 << 10 {
+        let mut inter = vec![0; n];
+        for j in 0..10 {
+            if i >> j & 1 == 1 {
+                for k in 0..n {
+                    if f[k][j] == 1 {
+                        inter[k] += 1;
+                    }
+                }
+            }
+        }
+        let mut tmp = 0;
+        for j in 0..n {
+            tmp += p[j][inter[j]];
+        }
+        ans = ans.max(tmp);
+    }
+    println!("{}", ans);
 }
