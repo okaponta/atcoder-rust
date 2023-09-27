40467925bf3572a1cd7e2f1f45f21e6aadb72d6f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-27 20:42:54 +0900

    add abc083_d

--- abc083/src/bin/d.rs
+++ abc083/src/bin/d.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let r = run_length_encode(s);
+    let mut ans = 1;
+    let mut sum = 0;
+    for (_, i) in r {
+        sum += i;
+        ans = ans.max(sum.min(n + i - sum));
+    }
+    println!("{}", ans);
+}
+
+fn run_length_encode<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
 }
