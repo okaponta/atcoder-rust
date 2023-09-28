a4138d75c417f365743a6941ab5563a7e99c5dd4
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-28 10:11:51 +0900

    add abc104_c

--- abc104/src/bin/c.rs
+++ abc104/src/bin/c.rs
@@ -2,7 +2,39 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        d:usize,
+        g:usize,
+        pc:[(usize,usize);d],
     }
-    println!("{}", n);
+    let mut comp = vec![0; d];
+    for i in 0..d {
+        comp[i] = pc[i].0 * ((i + 1) * 100) + pc[i].1;
+    }
+    let mut ans = 1usize << 60;
+    for i in 0..1 << d {
+        let mut tmp = 0;
+        let mut score = 0;
+        for j in 0..d {
+            if i >> j & 1 == 1 {
+                tmp += pc[j].0;
+                score += comp[j];
+            }
+        }
+        if g <= score {
+            ans = ans.min(tmp);
+            continue;
+        }
+        let mut max = 0;
+        for j in (0..d).rev() {
+            if i >> j & 1 != 1 {
+                max = j;
+                break;
+            }
+        }
+        if pc[max].0 <= (g - score + max * 100 + 99) / ((max + 1) * 100) {
+            continue;
+        }
+        ans = ans.min(tmp + (g - score + max * 100 + 99) / ((max + 1) * 100));
+    }
+    println!("{}", ans);
 }
