c816a1e761cf2c3c203d016d01c85276c73b81cd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-30 23:12:22 +0900

    add abc322_e

--- abc322/src/bin/e.rs
+++ abc322/src/bin/e.rs
@@ -1,8 +1,33 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        p:usize,
+        ca:[(usize,[usize;k]);n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    map.insert(vec![0; k], 0);
+    for (c, a) in ca {
+        let mut new = map.clone();
+        for (mut key, v) in map {
+            for i in 0..k {
+                key[i] = (key[i] + a[i]).min(p);
+            }
+            if !new.contains_key(&key) {
+                new.insert(key, v + c);
+            } else if v + c < new[&key] {
+                new.insert(key, v + c);
+            }
+        }
+        map = new;
+    }
+    if map.contains_key(&vec![p; k]) {
+        println!("{}", map[&vec![p; k]]);
+    } else {
+        println!("-1");
+    }
 }
