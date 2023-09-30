c094ae258a1ce9ae947fc21bf5fce35bf48f630a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-30 23:12:17 +0900

    add abc322_b

--- abc322/src/bin/b.rs
+++ abc322/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let mut ans = 3;
+    if (0..n).into_iter().all(|i| s[i] == t[i]) {
+        ans -= 2;
+    }
+    if (0..n).into_iter().all(|i| s[i] == t[m - n + i]) {
+        ans -= 1;
+    }
+    println!("{}", ans);
 }
