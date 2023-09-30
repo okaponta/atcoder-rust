5f326e5dac752f368473cf2fa6103a22848ab172
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-09-30 23:12:19 +0900

    add abc322_c

--- abc322/src/bin/c.rs
+++ abc322/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;m],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 0..m {
+        set.insert(a[i]);
+    }
+    let mut ans = vec![0; n];
+    let mut tmp = 0;
+    for i in (1..=n).rev() {
+        if set.contains(&i) {
+            tmp = 0;
+        } else {
+            tmp += 1;
+            ans[i - 1] = tmp;
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
