5831644906d940461a01c8ebb77be3c4aab5954c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-02 23:47:09 +0900

    add abc092_d

--- abc092/src/bin/d.rs
+++ abc092/src/bin/d.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        mut a:Usize1,
+        mut b:Usize1,
     }
-    println!("{}", n);
+    let mut grid = vec![vec!['#'; 100]; 100];
+    for i in 50..100 {
+        for j in 0..100 {
+            grid[i][j] = '.';
+        }
+    }
+    let mut tmp = 0;
+    while 0 < a {
+        grid[tmp / 100][tmp % 100] = '.';
+        tmp = next(tmp);
+        a -= 1;
+    }
+    tmp = 5100;
+    while 0 < b {
+        grid[tmp / 100][tmp % 100] = '#';
+        tmp = next(tmp);
+        b -= 1;
+    }
+    println!("100 100");
+    for i in 0..100 {
+        println!("{}", grid[i].iter().join(""));
+    }
+}
+
+fn next(mut tmp: usize) -> usize {
+    tmp += 2;
+    if tmp % 100 == 0 {
+        tmp += 100;
+    }
+    tmp
 }
