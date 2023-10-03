bdd2db3651b439de0a9541e4f71a0c2447f66d00
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-03 23:01:52 +0900

    add abc072_c

--- abc072/src/bin/c.rs
+++ abc072/src/bin/c.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut map = vec![0; 100000];
+    for a in a {
+        map[a] += 1;
+    }
+    let mut ans = 0;
+    for i in 1..99999 {
+        ans = ans.max(map[i - 1] + map[i] + map[i + 1]);
+    }
+    println!("{}", ans);
 }
