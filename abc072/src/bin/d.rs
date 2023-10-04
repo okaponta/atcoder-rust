58423b0e20e30822d880e42432c98fc4b1685a47
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-04 19:54:11 +0900

    add abc072_d

--- abc072/src/bin/d.rs
+++ abc072/src/bin/d.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        mut a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut count = 0;
+    for i in 0..n - 1 {
+        if a[i] == i {
+            a.swap(i, i + 1);
+            count += 1;
+        }
+    }
+    if a[n - 1] == n - 1 {
+        count += 1;
+    }
+    println!("{}", count);
 }
