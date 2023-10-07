9e9b355a2d28365bd55fe72c9df806691f5a326e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-07 23:00:33 +0900

    add abc323_b

--- abc323/src/bin/b.rs
+++ abc323/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut win = vec![0; n];
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == 'o' {
+                win[i] += 1;
+            }
+        }
+    }
+    let mut ans = vec![];
+    for i in (0..n).rev() {
+        for j in 0..n {
+            if win[j] == i {
+                ans.push(j + 1);
+            }
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
