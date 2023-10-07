cb3da822f66e2bddaf468a2a5efe8753e81b29e0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-07 23:06:32 +0900

    add abc323_d

--- abc323/src/bin/d.rs
+++ abc323/src/bin/d.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeSet, HashMap};
+use std::collections::BTreeMap;
 
 use proconio::input;
 
@@ -7,21 +7,17 @@ fn main() {
         n:usize,
         sc:[(usize,usize);n],
     }
-    let mut set = BTreeSet::new();
-    let mut map = HashMap::new();
+    let mut map = BTreeMap::new();
     for (s, c) in sc {
-        set.insert(s);
         map.insert(s, c);
     }
     let mut cur = 0;
-    while let Some(&next) = set.range(cur + 1..).next() {
-        let v = map[&next];
-        map.insert(next, v % 2);
-        if 1 < v {
-            set.insert(next * 2);
-            *map.entry(next * 2).or_insert(0) += v / 2;
+    while let Some((&s, &c)) = map.range(cur + 1..).next() {
+        map.insert(s, c % 2);
+        if 1 < c {
+            *map.entry(s * 2).or_insert(0) += c / 2;
         }
-        cur = next;
+        cur = s;
     }
     let mut ans = 0;
     for (_, v) in map {
