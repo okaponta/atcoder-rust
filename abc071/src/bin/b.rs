b91d650b47dd908a3ed6198a01cf3cb9cf816daf
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-10 20:25:36 +0900

    add abc071_b

--- abc071/src/bin/b.rs
+++ abc071/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for c in s {
+        set.insert(c);
+    }
+    for i in 0..26 {
+        let c = (b'a' + i) as char;
+        if !set.contains(&c) {
+            println!("{}", c);
+            return;
+        }
+    }
+    println!("None");
 }
