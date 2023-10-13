8f64cfb3ad44269a64c95b74b69254421c1078db
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-13 10:47:57 +0900

    add abc071_c

--- abc071/src/bin/c.rs
+++ abc071/src/bin/c.rs
@@ -1,8 +1,27 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    let mut pair = vec![];
+    for a in a {
+        if set.contains(&a) {
+            set.remove(&a);
+            pair.push(a);
+        } else {
+            set.insert(a);
+        }
+    }
+    if pair.len() < 2 {
+        println!("0");
+        return;
+    }
+    pair.sort();
+    pair.reverse();
+    println!("{}", pair[0] * pair[1]);
 }
