b506db4b1a300177520da139e299ba496d8e655b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-14 22:48:23 +0900

    add abc324_e

--- abc324/src/bin/e.rs
+++ abc324/src/bin/e.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        t:Chars,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut head = vec![];
+    let mut tail = vec![];
+    for i in 0..n {
+        let mut k = 0;
+        for j in 0..s[i].len() {
+            if k == t.len() {
+                break;
+            }
+            if s[i][j] == t[k] {
+                k += 1;
+            }
+        }
+        head.push(k);
+        k = 0;
+        for j in (0..s[i].len()).rev() {
+            if k == t.len() {
+                break;
+            }
+            if s[i][j] == t[t.len() - 1 - k] {
+                k += 1;
+            }
+        }
+        tail.push(k);
+    }
+    tail.sort();
+    let mut ans = 0;
+    for i in head {
+        ans += tail.len() - tail.lower_bound(&(t.len() - i));
+    }
+    println!("{}", ans);
 }
