055420aa04f8d9998d80697af4b60169d88c9882
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-14 22:48:21 +0900

    add abc324_d

--- abc324/src/bin/d.rs
+++ abc324/src/bin/d.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    s.sort();
+    let mut ans = 0;
+    let mut root = 0;
+    while root * root < 10usize.pow(n as u32) {
+        let mut cs = vec![];
+        let mut tmp = root * root;
+        while 0 < tmp {
+            cs.push(std::char::from_digit((tmp % 10) as u32, 10).unwrap());
+            tmp /= 10;
+        }
+        while cs.len() < s.len() {
+            cs.push('0');
+        }
+        cs.sort();
+        if cs == s {
+            ans += 1;
+        }
+        root += 1;
+    }
+    println!("{}", ans);
 }
