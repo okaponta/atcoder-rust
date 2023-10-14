9c6f02d116e79b59e66699f0f16fcda30b1f1e3b
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-14 22:48:19 +0900

    add abc324_b

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -4,5 +4,20 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let xn = n.next_power_of_two().trailing_zeros() as usize;
+    for x in 0..=xn {
+        let mut y = 0;
+        while calc(x, y) <= n {
+            if calc(x, y) == n {
+                println!("Yes");
+                return;
+            }
+            y += 1;
+        }
+    }
+    println!("No");
+}
+
+fn calc(x: usize, y: usize) -> usize {
+    2usize.pow(x as u32) * 3usize.pow(y as u32)
 }
