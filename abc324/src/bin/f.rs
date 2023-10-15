0019de5da37039d317d85d11f3f27a9b30253c13
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-15 17:54:26 +0900

    add abc324_f

--- abc324/src/bin/f.rs
+++ abc324/src/bin/f.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut uvbc:[(Usize1,Usize1,usize,usize);m],
     }
-    println!("{}", n);
+    uvbc.sort();
+    let mut lower = 0.0;
+    let mut upper = 1e5;
+    while upper - lower > 1e-10 {
+        let med = (lower + upper) / 2.0;
+        if is_ok(med, &uvbc, n) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", lower);
+}
+
+fn is_ok(m: f64, uvbc: &Vec<(usize, usize, usize, usize)>, n: usize) -> bool {
+    let mut dp = vec![-1e10; n];
+    dp[0] = 0.0;
+    for &(u, v, b, c) in uvbc {
+        let tmp = dp[u] + b as f64 - c as f64 * m;
+        if dp[v] < tmp {
+            dp[v] = tmp;
+        }
+    }
+    0.0 < dp[n - 1]
 }
