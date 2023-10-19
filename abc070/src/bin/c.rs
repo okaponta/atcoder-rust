6d11063fa5f7b8c791dd4a2e06ffc5ae7aff82d2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-19 22:56:13 +0900

    add abc070_c

--- abc070/src/bin/c.rs
+++ abc070/src/bin/c.rs
@@ -3,6 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:[u128;n],
     }
-    println!("{}", n);
+    let ans = t.into_iter().fold(1, |s, i| lcm(s, i));
+    println!("{}", ans);
+}
+
+fn gcd(a: u128, b: u128) -> u128 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
+}
+
+fn lcm(a: u128, b: u128) -> u128 {
+    a * b / gcd(a, b)
 }
