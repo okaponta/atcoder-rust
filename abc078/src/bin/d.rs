b4e7fd84df7d7406a648060431cce07185870f7e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-25 23:11:11 +0900

    add abc078_d

--- abc078/src/bin/d.rs
+++ abc078/src/bin/d.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        _:i64,
+        w:i64,
+        a:[i64;n],
     }
-    println!("{}", n);
+    if n == 1 {
+        println!("{}", (w - a[0]).abs());
+    } else {
+        println!("{}", (w - a[n - 1]).abs().max((a[n - 1] - a[n - 2]).abs()));
+    }
 }
