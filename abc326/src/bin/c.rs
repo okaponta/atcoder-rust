b7b88abc97d61beafcbf66e30017382c6eb50f39
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-28 22:46:42 +0900

    add abc326_c

--- abc326/src/bin/c.rs
+++ abc326/src/bin/c.rs
@@ -1,8 +1,17 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    let mut ans = 0;
+    for i in 0..n {
+        let tmp = a.lower_bound(&(a[i] + m));
+        ans = ans.max(tmp - i);
+    }
+    println!("{}", ans);
 }
