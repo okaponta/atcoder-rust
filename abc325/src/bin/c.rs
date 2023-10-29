3ad4d58f380b3f6ea6587b031216009fcc890f0d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-30 00:35:45 +0900

    add abc325_c

--- abc325/src/bin/c.rs
+++ abc325/src/bin/c.rs
@@ -1,8 +1,42 @@
-use proconio::input;
+use std::collections::{HashSet, VecDeque};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        mut s:[Chars;h],
     }
-    println!("{}", n);
+    s.insert(0, vec!['.'; w]);
+    s.push(vec!['.'; w]);
+    for i in 0..h + 2 {
+        s[i].insert(0, '.');
+        s[i].push('.');
+    }
+    let mut set = HashSet::new();
+    let mut q = VecDeque::new();
+    let mut count = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if s[i + 1][j + 1] == '#' && !set.contains(&(i + 1, j + 1)) {
+                set.insert((i + 1, j + 1));
+                count += 1;
+                q.push_back((i + 1, j + 1));
+                while let Some((i, j)) = q.pop_front() {
+                    for k in 0..3 {
+                        for l in 0..3 {
+                            if s[i - 1 + k][j - 1 + l] == '#'
+                                && !set.contains(&(i - 1 + k, j - 1 + l))
+                            {
+                                set.insert((i - 1 + k, j - 1 + l));
+                                q.push_back((i - 1 + k, j - 1 + l));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", count);
 }
