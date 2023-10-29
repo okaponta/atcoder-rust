f1acd12f46f3f721553b09dd4e0438615781f72c
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-30 00:22:31 +0900

    add abc325_b

--- abc325/src/bin/b.rs
+++ abc325/src/bin/b.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        wx:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut count = vec![0; 24];
+    for (w, x) in wx {
+        for i in 9..18 {
+            count[(x + i) % 24] += w;
+        }
+    }
+    println!("{}", count.into_iter().max().unwrap());
 }
