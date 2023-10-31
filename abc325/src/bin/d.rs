518cbaccd32e70edb4ddedfcd32f76e8b4df7e7f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-10-31 22:08:17 +0900

    add abc325_d

--- abc325/src/bin/d.rs
+++ abc325/src/bin/d.rs
@@ -1,8 +1,42 @@
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, BinaryHeap},
+};
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        td:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut map = BTreeMap::new();
+    for (t, d) in td {
+        (*map.entry(t).or_insert(vec![])).push(d);
+    }
+    let mut heap = BinaryHeap::new();
+    let mut ans = 0;
+    let mut t = 0;
+    loop {
+        if heap.is_empty() {
+            if let Some(t_next) = map.range(t..).next() {
+                t = *t_next.0;
+            } else {
+                break;
+            }
+        }
+        if map.contains_key(&t) {
+            for v in map[&t].iter() {
+                heap.push(Reverse(t + v));
+            }
+        }
+        while let Some(Reverse(ti)) = heap.pop() {
+            if t <= ti {
+                ans += 1;
+                break;
+            }
+        }
+        t += 1;
+    }
+    println!("{}", ans);
 }
