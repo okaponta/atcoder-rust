a7c5a21d23284ded4ae719e78937ba068d19c40f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-04 22:46:21 +0900

    add abc327_a

--- abc327/src/bin/a.rs
+++ abc327/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    for i in 0..n - 1 {
+        if (s[i] == 'b' && s[i + 1] == 'a') || (s[i] == 'a' && s[i + 1] == 'b') {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
