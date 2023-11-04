d4b9bb2411edcef43e79fbd8ba5e0092e47801fc
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-04 23:10:52 +0900

    add abc327_c

--- abc327/src/bin/c.rs
+++ abc327/src/bin/c.rs
@@ -4,35 +4,22 @@ fn main() {
     input! {
         a:[[usize;9];9],
     }
-    let mut row = vec![vec![]; 9];
-    let mut col = vec![vec![]; 9];
-    let mut small = vec![vec![]; 9];
+    let mut check = vec![vec![vec![]; 9]; 3];
     for i in 0..9 {
         for j in 0..9 {
-            row[i].push(a[i][j]);
-            col[j].push(a[i][j]);
-            small[(i / 3) * 3 + (j / 3)].push(a[i][j]);
+            check[0][i].push(a[i][j]);
+            check[1][j].push(a[i][j]);
+            check[2][(i / 3) * 3 + (j / 3)].push(a[i][j]);
         }
     }
     for i in 0..9 {
-        row[i].sort();
-        row[i].dedup();
-        if row[i].len() != 9 {
-            println!("No");
-            return;
-        }
-        col[i].sort();
-        col[i].dedup();
-        if col[i].len() != 9 {
-            println!("No");
-            return;
-        }
-
-        small[i].sort();
-        small[i].dedup();
-        if small[i].len() != 9 {
-            println!("No");
-            return;
+        for j in 0..3 {
+            check[j][i].sort();
+            check[j][i].dedup();
+            if check[j][i].len() != 9 {
+                println!("No");
+                return;
+            }
         }
     }
     println!("Yes");
