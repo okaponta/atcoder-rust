8f550dd81f97536c5b69f25d49fa632d57cd6c79
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-04 22:46:25 +0900

    add abc327_e

--- abc327/src/bin/e.rs
+++ abc327/src/bin/e.rs
@@ -3,6 +3,28 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        p:[f64;n],
     }
-    println!("{}", n);
+    let mut dp = vec![-1500.0; n + 1];
+    dp[0] = 0.0;
+    for i in 0..n {
+        for j in (0..=i).rev() {
+            let tmp = dp[j] * 0.9 + p[i];
+            if dp[j + 1] < tmp {
+                dp[j + 1] = tmp;
+            }
+        }
+    }
+    let mut ans = -1500.0;
+    let mut div = vec![1.0; n + 1];
+    for i in 2..=n {
+        div[i] = div[i - 1] * 0.9 + 1.0;
+    }
+    for i in 1..=n {
+        let tmp = dp[i] / div[i] - 1200.0 / (i as f64).sqrt();
+        if ans < tmp {
+            ans = tmp;
+        }
+    }
+    println!("{}", ans);
 }
