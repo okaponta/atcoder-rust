18241cce119dad6b6eec851d0be0ff41b8a38353
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-09 00:49:47 +0900

    add abc161_e

--- abc161/src/bin/e.rs
+++ abc161/src/bin/e.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        c:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut min = vec![];
+    for i in 0..n {
+        if s[i] == 'o' {
+            if min.len() == 0 {
+                min.push(i);
+            } else if min[min.len() - 1] + c < i {
+                min.push(i);
+            }
+        }
+    }
+    if min.len() != k {
+        return;
+    }
+    let mut max = vec![];
+    for i in (0..n).rev() {
+        if s[i] == 'o' {
+            if max.len() == 0 {
+                max.push(i);
+            } else if max[max.len() - 1].saturating_sub(c) > i {
+                max.push(i);
+            }
+        }
+    }
+    max.reverse();
+    for i in 0..k {
+        if min[i] == max[i] {
+            println!("{}", min[i] + 1);
+        }
+    }
 }
