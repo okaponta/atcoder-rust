a8734fb8db2ace8e5feb40529ff84fce66360f36
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-10 00:09:30 +0900

    add abc068_c

--- abc068/src/bin/c.rs
+++ abc068/src/bin/c.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    let mut rev_set = HashSet::new();
+    for (a, b) in ab {
+        if a.min(b) == 0 {
+            set.insert(a.max(b));
+        }
+        if a.max(b) == n - 1 {
+            rev_set.insert(a.min(b));
+        }
+    }
+    for e in set {
+        if rev_set.contains(&e) {
+            println!("POSSIBLE");
+            return;
+        }
+    }
+    println!("IMPOSSIBLE");
 }
