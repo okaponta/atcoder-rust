ba8e6b984a48a277bd4b906263b875ff6e65827a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-11 22:51:12 +0900

    add abc328_a

--- abc328/src/bin/a.rs
+++ abc328/src/bin/a.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        s:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for s in s {
+        if s <= x {
+            ans += s;
+        }
+    }
+    println!("{}", ans);
 }
