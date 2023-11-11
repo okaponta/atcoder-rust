0d4e5df6c392937c74163b0805f4f8bd31b9f6c0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-11 22:51:13 +0900

    add abc328_b

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -3,6 +3,30 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        d:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 1..=n {
+        let a = is_zoro(i);
+        if a == 0 {
+            continue;
+        }
+        for j in 1..=d[i - 1] {
+            let b = is_zoro(j);
+            if a == b {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
+}
+
+fn is_zoro(n: usize) -> usize {
+    if n < 10 {
+        return n;
+    }
+    if n % 11 == 0 {
+        return n / 11;
+    }
+    0
 }
