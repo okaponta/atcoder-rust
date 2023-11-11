83eed92a4ef9f59a066f5d4fc9e84a5c5b1e4ef9
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-11 22:51:14 +0900

    add abc328_c

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        s:Chars,
+        lr:[(Usize1,Usize1);q],
     }
-    println!("{}", n);
+    let mut sum = vec![0; n];
+    for i in 1..n {
+        if s[i - 1] == s[i] {
+            sum[i] = sum[i - 1] + 1;
+        } else {
+            sum[i] = sum[i - 1]
+        }
+    }
+    for (l, r) in lr {
+        println!("{}", sum[r] - sum[l]);
+    }
 }
