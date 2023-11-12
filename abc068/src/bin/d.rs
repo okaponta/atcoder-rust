2027d7f4dbd59bd331d7115e6101c2603d8556fd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-13 00:31:04 +0900

    add abc068_d

--- abc068/src/bin/d.rs
+++ abc068/src/bin/d.rs
@@ -1,8 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut ans = vec![k / 50; 50];
+    for i in 0..50 {
+        if i < k % 50 {
+            ans[i] += 50;
+        } else {
+            ans[i] += 49 - k % 50;
+        }
+    }
+    println!("50");
+    println!("{}", ans.iter().join(" "));
 }
