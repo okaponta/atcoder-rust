52734be7de2a399aa4e085eddc4a617aec8a9a15
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-14 20:37:01 +0900

    add abc067_b

--- abc067/src/bin/b.rs
+++ abc067/src/bin/b.rs
@@ -3,6 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        mut l:[usize;n],
     }
-    println!("{}", n);
+    l.sort();
+    l.reverse();
+    println!("{}", (0..k).into_iter().fold(0, |s, i| s + l[i]));
 }
