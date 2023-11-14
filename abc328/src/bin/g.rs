3364f9c1d01f717912d345ca2488348ff87297fe
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-15 08:51:01 +0900

    add abc328_g

--- abc328/src/bin/g.rs
+++ abc328/src/bin/g.rs
@@ -3,6 +3,36 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        c:i64,
+        a:[i64;n],
+        b:[i64;n],
     }
-    println!("{}", n);
+    let mut dp = vec![1 << 60; 1 << n];
+    dp[0] = 0;
+    for i in 0usize..(1 << n) {
+        let s = i.count_ones() as usize;
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                continue;
+            }
+            let mut val = dp[i];
+            let mut next = i;
+            if j > 0 && next >> (j - 1) & 1 == 0 {
+                val += c;
+            }
+            for k in j..n {
+                if next >> k & 1 == 1 {
+                    break;
+                }
+                val += (a[k] - b[s + k - j]).abs();
+                next |= 1 << k;
+                dp[next] = dp[next].min(if k + 1 < n && next >> (k + 1) & 1 == 0 {
+                    val + c
+                } else {
+                    val
+                });
+            }
+        }
+    }
+    println!("{}", dp[(1 << n) - 1]);
 }
