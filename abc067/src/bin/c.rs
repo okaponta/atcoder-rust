310d1ba894279e8ec4c0203682db74a13e1575bd
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-17 00:31:40 +0900

    add abc067_c

--- abc067/src/bin/c.rs
+++ abc067/src/bin/c.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let s = a.iter().sum::<i64>();
+    let mut tmp = 0;
+    let mut ans = 1 << 60;
+    for i in 0..n - 1 {
+        tmp += a[i];
+        ans = ans.min((2 * tmp - s).abs());
+    }
+    println!("{}", ans);
 }
