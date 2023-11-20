73fcf9076736d30b2808bb8201ea75ff259eb12f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-20 09:45:26 +0900

    add abc066_c

--- abc066/src/bin/c.rs
+++ abc066/src/bin/c.rs
@@ -1,8 +1,24 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    for i in 0..n {
+        if i % 2 == 0 {
+            q.push_back(a[i]);
+        } else {
+            q.push_front(a[i]);
+        }
+    }
+    let mut ans = q.into_iter().collect::<Vec<_>>();
+    if n % 2 == 1 {
+        ans.reverse();
+    }
+    println!("{}", ans.iter().join(" "));
 }
