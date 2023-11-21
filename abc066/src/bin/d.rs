7eab68edc3eb124b2396937ae112c2253d6f26e4
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-21 20:56:04 +0900

    add abc066_d

--- abc066/src/bin/d.rs
+++ abc066/src/bin/d.rs
@@ -1,8 +1,66 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n+1],
     }
-    println!("{}", n);
+    let mut used = vec![n + 1; n];
+    let mut l = 0;
+    let mut r = 0;
+    for i in 0..n + 1 {
+        if used[a[i]] != n + 1 {
+            l = used[a[i]];
+            r = i;
+            break;
+        }
+        used[a[i]] = i;
+    }
+    let modulo = 1_000_000_007;
+    let c = Binom::init(n + 1, modulo);
+    for i in 0..n + 1 {
+        let ans = (modulo + c.get(n + 1, i + 1) - c.get(n + l - r, i)) % modulo;
+        println!("{}", ans);
+    }
+}
+
+// 二項定理 nまでのiCjを計算して返却する。
+// MODが素数である必要あり。
+// 計算量はO(N)
+pub struct Binom {
+    fact: Vec<usize>,
+    fact_inv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Binom {
+    pub fn init(n: usize, modulo: usize) -> Self {
+        let mut fact = vec![0; n + 1];
+        let mut inv = vec![0; n + 1];
+        let mut fact_inv = vec![0; n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        inv[1] = 1;
+        fact_inv[0] = 1;
+        fact_inv[1] = 1;
+        for i in 2..(n + 1) {
+            fact[i] = fact[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            fact_inv[i] = fact_inv[i - 1] * inv[i] % modulo;
+        }
+        Self {
+            fact,
+            fact_inv,
+            modulo,
+        }
+    }
+
+    // nCk
+    pub fn get(&self, n: usize, k: usize) -> usize {
+        if n < k {
+            return 0;
+        }
+        self.fact[n] * self.fact_inv[k] % self.modulo * self.fact_inv[n - k] % self.modulo
+    }
 }
