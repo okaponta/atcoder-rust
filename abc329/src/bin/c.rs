c503ac21d7b7c58521304efd78e45bcb976303e3
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-22 21:14:28 +0900

    add abc329_c

--- abc329/src/bin/c.rs
+++ abc329/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut cnt = vec![0; 26];
+    let rl = run_length_encode(s);
+    for (c, ct) in rl {
+        cnt[(c as u8 - b'a') as usize] = cnt[(c as u8 - b'a') as usize].max(ct);
+    }
+    let ans = cnt.into_iter().sum::<usize>();
+    println!("{}", ans);
+}
+
+fn run_length_encode<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
 }
