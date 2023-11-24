d2416ccf930ce7988bed35f3dee12f643862a7f5
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-24 09:35:35 +0900

    add abc329_f

--- abc329/src/bin/f.rs
+++ abc329/src/bin/f.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use std::{collections::HashSet, mem};
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        c:[Usize1;n],
+        ab:[(Usize1,Usize1);q],
     }
-    println!("{}", n);
+    let mut sets = c.iter().map(|i| HashSet::from([i])).collect::<Vec<_>>();
+    for (a, b) in ab {
+        if sets[b].len() < sets[a].len() {
+            sets.swap(a, b);
+        }
+        let aa = mem::take(&mut sets[a]);
+        sets[b].extend(aa);
+        println!("{}", sets[b].len());
+    }
 }
