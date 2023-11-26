803df2c5f3c99a643b48224a51170111c3a92106
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-26 21:47:17 +0900

    add abc330_a

--- abc330/src/bin/a.rs
+++ abc330/src/bin/a.rs
@@ -3,6 +3,8 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        l:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    println!("{}", a.into_iter().filter(|a| &l <= a).count());
 }
