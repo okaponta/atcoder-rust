95656e5d9ea589f28a56b2c5c36ea421a8084414
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-27 22:00:03 +0900

    add abc330_d

--- abc330/src/bin/d.rs
+++ abc330/src/bin/d.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut row = vec![0usize; n];
+    let mut col = vec![0usize; n];
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == 'o' {
+                row[i] += 1;
+                col[j] += 1;
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == 'o' {
+                ans += row[i].saturating_sub(1) * col[j].saturating_sub(1);
+            }
+        }
+    }
+    println!("{}", ans);
 }
