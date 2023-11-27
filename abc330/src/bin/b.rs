a52011293736afdd24face4345e05f2eaebb9758
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-27 22:00:01 +0900

    add abc330_b

--- abc330/src/bin/b.rs
+++ abc330/src/bin/b.rs
@@ -1,8 +1,12 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        l:usize,
+        r:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    println!("{}", a.into_iter().map(|a| a.min(r).max(l)).join(" "));
 }
