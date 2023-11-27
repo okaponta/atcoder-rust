28f405ab3fd5591389770faa7eaa0e8341146c42
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-27 22:00:02 +0900

    add abc330_c

--- abc330/src/bin/c.rs
+++ abc330/src/bin/c.rs
@@ -1,8 +1,29 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
-        n:usize,
+        d:usize,
     }
-    println!("{}", n);
+    let mut ans = d;
+    let sq = (0..2_000_000)
+        .into_iter()
+        .map(|i| i * i)
+        .collect::<Vec<_>>();
+    let mut x = 0usize;
+    while sq[x.saturating_sub(1)] <= d {
+        let a = sq[sq.lower_bound(&(d.saturating_sub(sq[x])))];
+        let b = sq[sq.lower_bound(&(d.saturating_sub(sq[x]))).saturating_sub(1)];
+        ans = ans.min(abs(sq[x] + a, d)).min(abs(sq[x] + b, d));
+        x += 1;
+    }
+    println!("{}", ans);
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
