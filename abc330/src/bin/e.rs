3abca7c11713ac244096882a93fb8499b27ca355
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-28 22:42:18 +0900

    add abc330_e

--- abc330/src/bin/e.rs
+++ abc330/src/bin/e.rs
@@ -1,8 +1,153 @@
-use proconio::input;
+use std::collections::{HashMap, HashSet};
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        mut a:[usize;n],
+        ix:[(Usize1,usize);q],
     }
-    println!("{}", n);
+    let mut count_map = HashMap::new();
+    let mut set = HashSet::new();
+    for i in 0..n {
+        *count_map.entry(a[i]).or_insert(0) += 1;
+        set.insert(a[i]);
+    }
+    for i in 0..q {
+        set.insert(ix[i].1);
+    }
+    let mut max = 0;
+    while let Some(_) = set.get(&max) {
+        max += 1;
+    }
+    max += 1;
+
+    let mut count = vec![0; max];
+    for (k, v) in count_map {
+        if k < max {
+            count[k] += v;
+        }
+    }
+
+    let mut seg = SegmentTree::new(max, 1usize << 60, |a, b| a.min(b));
+    for i in 0..max {
+        seg.update_tmp(i, count[i]);
+    }
+    seg.update_all();
+
+    for (i, x) in ix {
+        if a[i] < max {
+            seg.update(a[i], seg.get(a[i]) - 1);
+        }
+        if x < max {
+            seg.update(x, seg.get(x) + 1);
+        }
+        println!("{}", seg.min_left(0, max, |i| 0 < i));
+        a[i] = x;
+    }
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    // 半開区間なので注意
+    pub fn get(&self, k: usize) -> T {
+        self.query(k..k + 1)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
+
+    // lowerとupperの間でfを満たす最小の値
+    // ng, ng, ng, (ok), ok, ok
+    pub fn max_right<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                upper = med;
+            } else {
+                lower = med;
+            }
+        }
+        upper
+    }
+
+    // lowerとupperの間でfを満たさない最大の値
+    pub fn min_left<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
