091c765698154dedf2418d947a7c415c30122805
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-29 22:09:34 +0900

    add abc065_b

--- abc065/src/bin/b.rs
+++ abc065/src/bin/b.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut used = vec![false; n];
+    used[0] = true;
+    let mut tmp = 0;
+    let mut count = 0;
+    loop {
+        count += 1;
+        tmp = a[tmp];
+        if used[tmp] {
+            println!("-1");
+            return;
+        }
+        if tmp == 1 {
+            println!("{}", count);
+            return;
+        }
+        used[tmp] = true;
+    }
 }
