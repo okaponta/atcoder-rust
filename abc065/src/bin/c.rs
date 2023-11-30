3987c094df90653996778d9a71157bfc5f5ff71d
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-11-30 21:00:02 +0900

    add abc065_c

--- abc065/src/bin/c.rs
+++ abc065/src/bin/c.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:[usize;2],
     }
-    println!("{}", n);
+    n.sort();
+    let m = 1_000_000_007;
+    let mut fact = vec![1];
+    for i in 1..=n[1] {
+        fact.push(fact[i - 1] * i % m);
+    }
+    if n[1] - n[0] == 0 {
+        println!("{}", fact[n[0]] * fact[n[1]] * 2 % m);
+    } else if n[1] - n[0] == 1 {
+        println!("{}", fact[n[0]] * fact[n[1]] % m);
+    } else {
+        println!("0");
+    }
 }
