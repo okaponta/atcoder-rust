4b86f56b0041c1d30a861ac8f1e1c786db2e74f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-03 00:21:43 +0900

    add abc331_e

--- abc331/src/bin/e.rs
+++ abc331/src/bin/e.rs
@@ -1,8 +1,35 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        l:usize,
+        a:[usize;n],
+        b:[usize;m],
+        cd:[(Usize1,Usize1);l],
     }
-    println!("{}", n);
+    let mut bb = vec![];
+    for i in 0..m {
+        bb.push((b[i], i));
+    }
+    let mut set = HashSet::new();
+    for (c, d) in cd {
+        set.insert((c, d));
+    }
+    bb.sort();
+    bb.reverse();
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..m {
+            if set.contains(&(i, bb[j].1)) {
+                continue;
+            }
+            ans = ans.max(a[i] + bb[j].0);
+            break;
+        }
+    }
+    println!("{}", ans);
 }
