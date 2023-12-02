90ba97a68fdcba21402d8ec2a3cfb27169d39000
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-02 22:44:06 +0900

    add abc331_c

--- abc331/src/bin/c.rs
+++ abc331/src/bin/c.rs
@@ -1,8 +1,30 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut b = vec![];
+    for i in 0..n {
+        b.push(a[i]);
+    }
+    b.sort();
+    b.reverse();
+    let mut map = HashMap::new();
+    let mut tmp = 0;
+    for i in 0..n {
+        if !map.contains_key(&b[i]) {
+            map.insert(b[i], tmp);
+        }
+        tmp += b[i];
+    }
+    let mut ans = vec![];
+    for i in 0..n {
+        ans.push(map[&a[i]]);
+    }
+    println!("{}", ans.iter().join(" "));
 }
