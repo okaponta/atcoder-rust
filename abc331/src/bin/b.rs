9299b7de8794ac3190a96223e935ef8e54af899f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-02 22:42:30 +0900

    add abc331_b

--- abc331/src/bin/b.rs
+++ abc331/src/bin/b.rs
@@ -3,6 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        s:usize,
+        m:usize,
+        l:usize,
     }
-    println!("{}", n);
+    let mut ans = 1 << 60;
+    for i in 0..20 {
+        for j in 0..15 {
+            for k in 0..10 {
+                if n <= 6 * i + 8 * j + 12 * k {
+                    ans = ans.min(s * i + m * j + l * k);
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
