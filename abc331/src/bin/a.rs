5346efbd52a43cb21ee6f2d574530702b2f892d8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-02 22:42:13 +0900

    add abc331_a

--- abc331/src/bin/a.rs
+++ abc331/src/bin/a.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mm:usize,
+        dd:usize,
+        mut y:usize,
+        mut m:usize,
+        mut d:usize,
     }
-    println!("{}", n);
+    d += 1;
+    if dd < d {
+        m += 1;
+        d = 1;
+    }
+    if mm < m {
+        y += 1;
+        m = 1;
+    }
+    println!("{} {} {}", y, m, d);
 }
