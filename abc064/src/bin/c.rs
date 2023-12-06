4e219fb4ddf832ab96f8f0baa68e06af1fe39d26
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-06 23:19:00 +0900

    add abc064_c

--- abc064/src/bin/c.rs
+++ abc064/src/bin/c.rs
@@ -1,8 +1,20 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    let mut ast = 0;
+    for a in a {
+        if 7 < a / 400 {
+            ast += 1;
+        } else {
+            set.insert(a / 400);
+        }
+    }
+    println!("{} {}", set.len().max(1), set.len() + ast);
 }
