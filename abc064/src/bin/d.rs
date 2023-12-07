c170f469dea27a109c8c4f9d2928f47362c6d50f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-07 21:26:33 +0900

    add abc064_d

--- abc064/src/bin/d.rs
+++ abc064/src/bin/d.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        mut s:Chars,
     }
-    println!("{}", n);
+    for _ in 0..count_bracket(&s, '(') {
+        s.push(')');
+    }
+    s.reverse();
+    for _ in 0..count_bracket(&s, ')') {
+        s.push('(');
+    }
+    s.reverse();
+    println!("{}", s.iter().join(""));
+}
+
+fn count_bracket(s: &Vec<char>, target: char) -> i32 {
+    let mut i = 0;
+    let mut count = 0;
+    while i < s.len() {
+        let b = if s[i] == target { 1 } else { -1 };
+        count = (count + b).max(0);
+        i += 1;
+    }
+    count
 }
