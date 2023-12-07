b48c854a4b925c41c370862ccd392823bf785618
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-08 00:29:44 +0900

    add abc331_f

--- abc331/src/bin/f.rs
+++ abc331/src/bin/f.rs
@@ -1,8 +1,147 @@
-use proconio::input;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let base = 1000001137;
+    let mut h = vec![1u128];
+    for i in 0..n {
+        h.push(h[i].wrapping_mul(base));
+    }
+    let mut seg = SegmentTree::new(n, (0u128, 0u128), |a, b| merge(a, b));
+    for i in 0..n {
+        let c = s[i] as u128;
+        seg.update_tmp(i, (c.wrapping_mul(h[i]), c.wrapping_mul(h[n - 1 - i])));
+    }
+    seg.update_all();
+    for _ in 0..q {
+        input! {q: u8}
+        if q == 1 {
+            input! {x:Usize1,c:char}
+            let c = c as u128;
+            seg.update(x, (c.wrapping_mul(h[x]), c.wrapping_mul(h[n - 1 - x])));
+        } else {
+            input! {l:Usize1,r:usize}
+            let (mut a, mut b) = seg.query(l..r);
+            if l < n - r {
+                a = a.wrapping_mul(h[n - r - l]);
+            } else {
+                b = b.wrapping_mul(h[l + r - n]);
+            }
+            println!("{}", if a == b { "Yes" } else { "No" })
+        }
+    }
+}
+
+fn merge(a: (u128, u128), b: (u128, u128)) -> (u128, u128) {
+    (a.0.wrapping_add(b.0), a.1.wrapping_add(b.1))
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    // 半開区間なので注意
+    pub fn get(&self, k: usize) -> T {
+        self.query(k..k + 1)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
+
+    // lowerとupperの間でfを満たす最小の値
+    // ng, ng, ng, (ok), ok, ok
+    pub fn max_right<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                upper = med;
+            } else {
+                lower = med;
+            }
+        }
+        upper
+    }
+
+    // lowerとupperの間でfを満たさない最大の値
+    pub fn min_left<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
