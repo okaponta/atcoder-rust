b2edb89898718c64a991089ede77c06500e4ed7e
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-10 18:05:15 +0900

    add arc169_b

--- arc169/src/bin/b.rs
+++ arc169/src/bin/b.rs
@@ -1,8 +1,30 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        s:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let ss = ruiseki(&a);
+    let mut p = vec![n; n + 1];
+    for i in 0..n {
+        p[i] = ss.lower_bound(&(ss[i] + s + 1)) - 1;
+    }
+    let mut ans = 0;
+    let mut dp = vec![0; n + 2];
+    for i in (0..n).rev() {
+        dp[i] = dp[p[i]] + n - i;
+        ans += dp[i];
+    }
+    println!("{}", ans);
+}
+
+fn ruiseki(a: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
