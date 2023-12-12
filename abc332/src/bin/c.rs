7702a33f665dc7b5ba67cb627e76f88770c1c487
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-12 22:21:50 +0900

    add abc332_c

--- abc332/src/bin/c.rs
+++ abc332/src/bin/c.rs
@@ -1,8 +1,34 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _:usize,
+        m:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut t = vec![m, 0];
+    for c in s {
+        if c == '0' {
+            t[0] = m;
+            t[1] = ans;
+        } else if c == '1' {
+            if 0 < t[0] {
+                t[0] -= 1;
+            } else {
+                if 0 < t[1] {
+                    t[1] -= 1;
+                } else {
+                    ans += 1;
+                }
+            }
+        } else {
+            if 0 < t[1] {
+                t[1] -= 1;
+            } else {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
