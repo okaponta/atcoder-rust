7fd7e4a533fbcd956fe8e804dfcbe2e08a791ec8
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-12 22:21:48 +0900

    add abc332_b

--- abc332/src/bin/b.rs
+++ abc332/src/bin/b.rs
@@ -2,7 +2,25 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        k:usize,
+        g:usize,
+        m:usize,
     }
-    println!("{}", n);
+    let mut state = vec![0; 2];
+    for _ in 0..k {
+        if state[0] == g {
+            state[0] = 0;
+        } else if state[1] == 0 {
+            state[1] = m;
+        } else {
+            if g < state[1] {
+                state[1] -= g;
+                state[0] = g;
+            } else {
+                state[0] = state[1];
+                state[1] = 0;
+            }
+        }
+    }
+    println!("{} {}", state[0], state[1]);
 }
