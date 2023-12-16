5596a5f0fa66b72fbe27195d45cd49b067e8a183
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-16 22:46:58 +0900

    add abc333_d

--- abc333/src/bin/d.rs
+++ abc333/src/bin/d.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        uv:[(Usize1,Usize1);n-1]
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for (u, v) in uv {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    if g[0].len() == 1 {
+        println!("1");
+        return;
+    }
+    let mut ans = 0;
+    for &next in &g[0] {
+        ans = ans.max(dfs(next, 0, &g));
+    }
+    println!("{}", n - ans);
+}
+
+fn dfs(cur: usize, prev: usize, g: &Vec<Vec<usize>>) -> usize {
+    let mut res = 1;
+    for &next in &g[cur] {
+        if next == prev {
+            continue;
+        }
+        res += dfs(next, cur, g);
+    }
+    res
 }
