4416944668437e1ac8e4aba9920ad5d83dcff7a0
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-16 22:46:57 +0900

    add abc333_c

--- abc333/src/bin/c.rs
+++ abc333/src/bin/c.rs
@@ -4,5 +4,19 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut rep = vec![1usize];
+    for i in 0..15 {
+        rep.push(rep[i] * 10 + 1);
+    }
+    let mut ans = vec![];
+    for i in 0..15 {
+        for j in 0..15 {
+            for k in 0..15 {
+                ans.push(rep[i] + rep[j] + rep[k]);
+            }
+        }
+    }
+    ans.sort();
+    ans.dedup();
+    println!("{}", ans[n - 1]);
 }
