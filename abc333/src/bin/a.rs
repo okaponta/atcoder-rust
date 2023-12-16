50ada56f7ff0c79e702d5633d98745bf0dd29e5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-16 22:46:55 +0900

    add abc333_a

--- abc333/src/bin/a.rs
+++ abc333/src/bin/a.rs
@@ -1,8 +1,11 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n:usize,
+        n:Usize1,
     }
-    println!("{}", n);
+    let ans = vec![
+        1usize, 22, 333, 4444, 55555, 666666, 7777777, 88888888, 999999999,
+    ];
+    println!("{}", ans[n]);
 }
