9171eb29c1db282ec7e8ed8c4a87e6df8750e9a2
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-16 22:46:52 +0900

    add abc333_b

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let a = diff(s[0], s[1]);
+    let b = diff(t[0], t[1]);
+    if a && b || !a && !b {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+}
+
+fn diff(a: char, b: char) -> bool {
+    let a = a as u8;
+    let b = b as u8;
+    let d = if a < b { b - a } else { a - b };
+    d == 1 || d == 4
 }
