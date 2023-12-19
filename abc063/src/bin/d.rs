e23e13a394b4059831225f391a7b345cf9908198
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-19 21:50:48 +0900

    add abc063_d

--- abc063/src/bin/d.rs
+++ abc063/src/bin/d.rs
@@ -3,6 +3,31 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize,
+        h:[usize;n],
     }
-    println!("{}", n);
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(med, &h, a, b) {
+            upper = med;
+        } else {
+            lower = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+// たおせる
+fn is_ok(med: usize, h: &Vec<usize>, a: usize, b: usize) -> bool {
+    let mut count = 0;
+    for &h in h {
+        if h <= med * b {
+            continue;
+        }
+        count += ((h - med * b) + (a - b) - 1) / (a - b);
+    }
+    count <= med
 }
