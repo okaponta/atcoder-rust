86cc5255e75fe46bf28e50eaec08b296fa011b5f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-22 04:07:59 +0900

    add abc062_a

--- abc062/src/bin/a.rs
+++ abc062/src/bin/a.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        x:usize,
+        y:usize,
     }
-    println!("{}", n);
+    let a = vec![1, 3, 5, 7, 8, 10, 12];
+    let b = vec![4, 6, 9, 11];
+    println!(
+        "{}",
+        if a.contains(&x) && a.contains(&y) || (b.contains(&x) && b.contains(&y)) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
