dced9168d04c638912118703984944c4e5b20ad4
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-23 22:52:18 +0900

    add abc334_b

--- abc334/src/bin/b.rs
+++ abc334/src/bin/b.rs
@@ -2,7 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:i64,
+        m:i64,
+        mut l:i64,
+        mut r:i64,
     }
-    println!("{}", n);
+    l -= a;
+    r -= a;
+    let mut k1 = l / m;
+    let mut k2 = r / m;
+    k1 -= 2;
+    k2 += 2;
+    while k1 * m < l {
+        k1 += 1;
+    }
+    while r < k2 * m {
+        k2 -= 1;
+    }
+    println!("{}", k2 - k1 + 1);
 }
