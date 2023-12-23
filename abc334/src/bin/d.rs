668bdfb2f9a13b18f687d736d31758f397904023
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-23 22:53:33 +0900

    add abc334_d

--- abc334/src/bin/d.rs
+++ abc334/src/bin/d.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{fastout, input};
+use superslice::Ext;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        mut r:[usize;n],
+        x:[usize;q],
     }
-    println!("{}", n);
+    r.sort();
+    let mut s = vec![0];
+    for i in 0..n {
+        s.push(s[i] + r[i]);
+    }
+    s.remove(0);
+    for x in x {
+        println!("{}", s.upper_bound(&x));
+    }
 }
