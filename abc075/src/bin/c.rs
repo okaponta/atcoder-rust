b5c7bfbab1bcc2eb250d77ca8649a35955c65759
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-24 00:45:34 +0900

    add abc075_c

--- abc075/src/bin/c.rs
+++ abc075/src/bin/c.rs
@@ -16,6 +16,7 @@ fn main() {
 pub struct LowLink {
     n: usize,
     g: Vec<Vec<usize>>,
+    articulation_points: Vec<usize>,
     bridges: Vec<(usize, usize)>,
 }
 
@@ -24,6 +25,7 @@ impl LowLink {
         Self {
             n,
             g: vec![vec![]; n],
+            articulation_points: vec![],
             bridges: vec![],
         }
     }
@@ -34,39 +36,68 @@ impl LowLink {
     }
 
     pub fn execute(&mut self) -> usize {
-        let mut pre = vec![!0; self.n];
+        let mut ord = vec![!0; self.n];
         let mut low = vec![!0; self.n];
-        let mut res = vec![];
+        let mut bridges = vec![];
+        let mut articulation = vec![];
         fn dfs(
             cur: usize,
             prev: usize,
             mut count: usize,
             g: &Vec<Vec<usize>>,
-            pre: &mut Vec<usize>,
+            used: &mut Vec<bool>,
+            ord: &mut Vec<usize>,
             low: &mut Vec<usize>,
-            res: &mut Vec<(usize, usize)>,
+            articulation: &mut Vec<usize>,
+            bridges: &mut Vec<(usize, usize)>,
         ) -> usize {
+            used[cur] = true;
             count += 1;
-            pre[cur] = count;
-            low[cur] = pre[cur];
+            ord[cur] = count;
+            low[cur] = ord[cur];
+            let mut is_articulation = false;
+            let mut childs = 0;
             for &next in &g[cur] {
-                if pre[next] == !0 {
-                    // 未到達
-                    low[cur] = low[cur].min(dfs(next, cur, count, g, pre, low, res));
-                    if low[next] == pre[next] {
-                        res.push((cur, next));
-                    }
-                } else {
-                    if prev == next {
-                        continue;
-                    }
+                if !used[next] {
+                    childs += 1;
+                    count = dfs(next, cur, count, g, used, ord, low, articulation, bridges);
                     low[cur] = low[cur].min(low[next]);
+                    if prev != !0 && ord[cur] <= low[next] {
+                        is_articulation = true;
+                    }
+                    if ord[cur] < low[next] {
+                        bridges.push((cur, next));
+                    }
+                } else if next != prev {
+                    low[cur] = low[cur].min(ord[next]);
                 }
             }
-            low[cur]
+            if prev == !0 && 2 <= childs {
+                is_articulation = true;
+            }
+            if is_articulation {
+                articulation.push(cur);
+            }
+            count
         }
-        dfs(0, !0, 0, &self.g, &mut pre, &mut low, &mut res);
-        self.bridges = res;
+        let mut used = vec![false; self.n];
+        for i in 0..self.n {
+            if !used[i] {
+                dfs(
+                    i,
+                    !0,
+                    0,
+                    &self.g,
+                    &mut used,
+                    &mut ord,
+                    &mut low,
+                    &mut articulation,
+                    &mut bridges,
+                );
+            }
+        }
+        self.bridges = bridges;
+        self.articulation_points = articulation;
         self.bridges.len()
     }
 }
