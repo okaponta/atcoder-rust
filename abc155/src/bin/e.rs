790a02bd946a83fa7e78fb7a347dc54cdbb8f10f
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-25 10:24:43 +0900

    add abc155_e

--- abc155/src/bin/e.rs
+++ abc155/src/bin/e.rs
@@ -1,9 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let coins = vec![0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
+    let mut dp = vec![0usize, 10];
+    for c in s {
+        let c = c.to_digit(10).unwrap() as usize;
+        let a = (dp[0] + coins[c]).min(dp[1] + coins[10 - c]);
+        let mut b = dp[0] + coins[c + 1];
+        if c != 0 {
+            b = b.min(dp[1] + coins[10 - c - 1]);
+        }
+        dp = vec![a, b];
+    }
+    println!("{}", dp[0]);
 }
