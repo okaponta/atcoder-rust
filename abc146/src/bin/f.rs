e1cbdbf386b8d73a96babfcf5c50e624093b6e50
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-12-30 13:33:21 +0900

    add abc146_f

--- abc146/src/bin/f.rs
+++ abc146/src/bin/f.rs
@@ -1,9 +1,30 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        m:usize,
+        mut s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    s.reverse();
+    let mut ans = vec![];
+    let mut i = 0;
+    while i != n {
+        for j in (0..=m).rev() {
+            if j == 0 {
+                println!("-1");
+                return;
+            }
+            if n < i + j {
+                continue;
+            }
+            if s[i + j] == '0' {
+                ans.push(j);
+                i += j;
+                break;
+            }
+        }
+    }
+    println!("{}", ans.iter().rev().join(" "));
 }
