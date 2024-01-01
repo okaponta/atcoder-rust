b830e886f9b339dc18fe8bcee8501a463b4103eb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-01 15:11:27 +0900

    add abc062_b

--- abc062/src/bin/b.rs
+++ abc062/src/bin/b.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        s:[String;h],
     }
-    println!("{}", n);
+    let header = (0..w + 2).into_iter().map(|_| '#').collect::<String>();
+    println!("{}", header);
+    for s in s {
+        println!("#{}#", s);
+    }
+    println!("{}", header);
 }
