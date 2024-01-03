c7602b7d77911ca7ffd3acb1dded5f3c2a39ccbf
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-03 22:12:20 +0900

    add abc061_b

--- abc061/src/bin/b.rs
+++ abc061/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut ans = vec![0; n];
+    for (a, b) in ab {
+        ans[a] += 1;
+        ans[b] += 1;
+    }
+    for ans in ans {
+        println!("{}", ans);
+    }
 }
