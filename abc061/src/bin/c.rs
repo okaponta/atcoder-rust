1290ea504d88cf2b3f85e55aeeae8a3e5d845988
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-04 10:45:54 +0900

    add abc061_c

--- abc061/src/bin/c.rs
+++ abc061/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        ab:[(Usize1,usize);n],
     }
-    println!("{}", n);
+    let mut count = vec![0; 100_010];
+    for (a, b) in ab {
+        count[a] += b;
+    }
+    let s = ruiseki(&count);
+    let ans = s.lower_bound(&k);
+    println!("{}", ans);
+}
+
+// 累積和
+fn ruiseki(a: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
