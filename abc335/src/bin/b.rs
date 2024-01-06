a8f042acc3b2d87869681eab6693340132a7f5ad
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-06 22:48:06 +0900

    add abc335_b

--- abc335/src/bin/b.rs
+++ abc335/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    for x in 0..=n {
+        for y in 0..=n {
+            for z in 0..=n {
+                if x + y + z <= n {
+                    println!("{} {} {}", x, y, z);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
