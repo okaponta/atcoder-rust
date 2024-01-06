c5b075b6efa469155e9dce5926903064e28fe03f
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-06 20:50:26 +0900

    add typical90_003

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,9 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        ab:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let (_, _, d) = tree_diameter(&g);
+    println!("{}", d + 1);
+}
+
+fn tree_diameter(edges: &Vec<Vec<usize>>) -> (usize, usize, usize) {
+    let l = tree_diameter_dfs(edges, 0, !0);
+    let r = tree_diameter_dfs(edges, l.1, !0);
+    (l.1, r.1, r.0)
+}
+
+// (距離, to)の最大値を返却する
+fn tree_diameter_dfs(edges: &Vec<Vec<usize>>, cur: usize, parent: usize) -> (usize, usize) {
+    let mut ret = (0, cur);
+    for &to in &edges[cur] {
+        if to == parent {
+            continue;
+        }
+        let mut next = tree_diameter_dfs(edges, to, cur);
+        next.0 += 1;
+        ret = ret.max(next);
+    }
+    ret
 }
