b20abb182e402202859bda5dfafbfd826d8e0ac8
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-07 11:38:36 +0900

    add typical90_004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use itertools::{iproduct, Itertools};
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        a:i32,
+        h:usize,
+        w:usize,
+        a:[[usize;w];h],
     }
-    println!("{}", a);
-    todo!();
+    let mut row = vec![0; h];
+    let mut col = vec![0; w];
+    for (i, j) in iproduct!(0..h, 0..w) {
+        row[i] += a[i][j];
+        col[j] += a[i][j];
+    }
+    let mut ans = vec![vec![0; w]; h];
+    for (i, j) in iproduct!(0..h, 0..w) {
+        ans[i][j] = row[i] + col[j] - a[i][j];
+    }
+    for i in 0..h {
+        println!("{}", ans[i].iter().join(" "));
+    }
 }
