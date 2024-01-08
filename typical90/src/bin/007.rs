207d63d246af4c0fc0f05e8acddd5b6b6c76a559
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-09 02:56:27 +0900

    add typical90_007

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,9 +1,26 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut a:[i64;n],
+        q:usize,
+        b:[i64;q],
     }
-    println!("{}", a);
-    todo!();
+    a.sort();
+    for b in b {
+        let mut lower = 0;
+        let mut upper = n - 1;
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if a[med] < b {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        let ans = (a[upper] - b).abs().min((a[lower] - b).abs());
+        println!("{}", ans);
+    }
 }
