f3014bfb29dca11de69b1595db188c96bac4ea49
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-08 13:08:07 +0900

    add typical90_006

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,9 +1,28 @@
-use proconio::input;
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:usize,
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut heap = BinaryHeap::new();
+    let mut ans = vec![];
+    let mut index = 0;
+    for i in 0..n {
+        heap.push(Reverse((s[i], i)));
+        if n - k <= i {
+            while let Some(Reverse((c, i))) = heap.pop() {
+                if index <= i {
+                    index = i;
+                    ans.push(c);
+                    break;
+                }
+            }
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
