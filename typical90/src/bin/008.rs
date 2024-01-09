99cc3b9f3c2d96abcc2e7d3b0d3ceab995a0359a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-09 21:22:06 +0900

    add typical90_008

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        _:usize,
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let map = vec!['a', 't', 'c', 'o', 'd', 'e', 'r']
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (c, i + 1))
+        .collect::<HashMap<char, usize>>();
+    let mut count = vec![0; 8];
+    count[0] = 1usize;
+    for c in s {
+        if map.contains_key(&c) {
+            count[map[&c]] += count[map[&c] - 1];
+            count[map[&c]] %= 1_000_000_007;
+        }
+    }
+    println!("{}", count[7]);
 }
