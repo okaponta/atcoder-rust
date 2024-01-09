7dd64444df5b44b411c70b837636d9f254a4badf
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-10 00:13:31 +0900

    add typical90_010

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -1,9 +1,34 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        cp:[(Usize1,usize);n],
+        q:usize,
+        ab:[(Usize1,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut one = vec![0; n];
+    let mut two = vec![0; n];
+    for i in 0..n {
+        if cp[i].0 == 0 {
+            one[i] += cp[i].1;
+        } else {
+            two[i] += cp[i].1;
+        }
+    }
+    let s1 = ruiseki(&one);
+    let s2 = ruiseki(&two);
+    for (a, b) in ab {
+        println!("{} {}", s1[b] - s1[a], s2[b] - s2[a]);
+    }
+}
+
+// 累積和
+fn ruiseki(a: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
