1a4c376706d482d65c79ddd12c3d243d3e5eb35d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-11 00:40:02 +0900

    add typical90_011

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut dcs:[(usize,usize,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    dcs.sort();
+    let m = dcs[n - 1].0;
+    // dp[i][j] i番目の仕事までで、j日目の仕事を見た場合の最大
+    let mut dp = vec![-1; m + 1];
+    dp[0] = 0;
+    for i in 0..n {
+        let (d, c, s) = dcs[i];
+        for j in (0..m).rev() {
+            if dp[j] == -1 || d < j + c {
+                continue;
+            }
+            dp[j + c] = dp[j + c].max(dp[j] + s);
+        }
+    }
+    println!("{}", dp.iter().max().unwrap());
 }
