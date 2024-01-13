734db114dc9cad6540117c547a08655b647c48b0
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-14 02:33:44 +0900

    add typical90_016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        a:usize,
+        b:usize,
+        c:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 10000;
+    for i in 0..10000 {
+        let tmpa = a * i;
+        if n < tmpa {
+            break;
+        }
+        for j in 0..10000 - i {
+            let tmpb = tmpa + b * j;
+            if n < tmpb {
+                break;
+            }
+            if (n - tmpb) % c == 0 {
+                ans = ans.min(i + j + (n - tmpb) / c);
+            }
+        }
+    }
+    println!("{}", ans);
 }
