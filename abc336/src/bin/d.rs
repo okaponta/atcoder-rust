0a8babb411330313cd16306570c6f80cedf7941c
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-15 13:48:23 +0900

    add abc336_d

--- abc336/src/bin/d.rs
+++ abc336/src/bin/d.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    let mut left = vec![1];
+    for i in 1..n {
+        left.push((left[i - 1] + 1).min(a[i]));
+    }
+    a.reverse();
+    let mut right = vec![1];
+    for i in 1..n {
+        right.push((right[i - 1] + 1).min(a[i]));
+    }
+    right.reverse();
+    let mut ans = 0;
+    for i in 0..n {
+        ans = ans.max(left[i].min(right[i]));
+    }
+    println!("{}", ans);
 }
