420aae1c29a81afda1c5d68a6a9dc9a13179d826
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-16 20:59:59 +0900

    add typical90_018

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,9 +1,20 @@
+use std::f64::consts::PI;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        t:f64,
+        l:f64,
+        x:f64,
+        y:f64,
+        q:usize,
+        e:[f64;q],
     }
-    println!("{}", a);
-    todo!();
+    for e in e {
+        let ang = e / t * 2.0 * PI;
+        let z = l / 2.0 - ang.cos() * l / 2.0;
+        let w = ((y + ang.sin() * l / 2.0).powi(2) + x.powi(2)).sqrt();
+        println!("{}", (z / w).atan() * 180.0 / PI)
+    }
 }
