4860c65b5708851cfd43586fc0342ca6d5b14d1e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-19 03:39:56 +0900

    add abc060_c

--- abc060/src/bin/c.rs
+++ abc060/src/bin/c.rs
@@ -3,6 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        tn:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        (0..n - 1).fold(t, |s, i| s + (tn[i + 1] - tn[i]).min(t))
+    );
 }
