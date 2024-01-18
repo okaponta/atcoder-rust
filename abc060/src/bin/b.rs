cd1c03b15faca17f077f37dc4e87ccef5bccd1b1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-18 21:54:50 +0900

    add abc060_b

--- abc060/src/bin/b.rs
+++ abc060/src/bin/b.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:usize,
+        b:usize,
+        c:usize,
     }
-    println!("{}", n);
+    let g = gcd(a, b);
+    println!("{}", if c % g == 0 { "YES" } else { "NO" });
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
