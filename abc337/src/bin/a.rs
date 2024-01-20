3d9f238441fcadb92f83c6a96766617b9beaf8b4
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-20 23:01:43 +0900

    add abc337_a

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        xy:[(i64,i64);n],
     }
-    println!("{}", n);
+    let s = xy.into_iter().fold(0, |s, i| s + i.0 - i.1);
+    println!(
+        "{}",
+        if s < 0 {
+            "Aoki"
+        } else if s == 0 {
+            "Draw"
+        } else {
+            "Takahashi"
+        }
+    );
 }
