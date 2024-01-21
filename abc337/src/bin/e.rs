d378a553f8165be43d20c3cd36e97f6db734b091
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-21 22:57:43 +0900

    add abc337_e

--- abc337/src/bin/e.rs
+++ abc337/src/bin/e.rs
@@ -1,8 +1,33 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input_interactive, marker::Chars};
 
 fn main() {
-    input! {
+    input_interactive! {
         n:usize,
     }
-    println!("{}", n);
+    let m = n.next_power_of_two().trailing_zeros() as usize;
+    println!("{}", m);
+    let mut res = vec![vec![]; m];
+    for i in 0..n {
+        for j in 0..m {
+            if i >> j & 1 == 1 {
+                res[j].push(i + 1);
+            }
+        }
+    }
+    for i in 0..m {
+        println!("{} {}", res[i].len(), res[i].iter().join(" "));
+    }
+    input_interactive! {
+        mut s: Chars,
+    }
+    let mut ans = 1;
+    let mut d = 1;
+    for i in 0..m {
+        if s[i] == '1' {
+            ans += d;
+        }
+        d *= 2;
+    }
+    println!("{}", ans);
 }
