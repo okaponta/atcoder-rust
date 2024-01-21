9aa55a18ff7ba5aac35de52bf11455df794ee1fb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-22 00:13:30 +0900

    add abc337_b

--- abc337/src/bin/b.rs
+++ abc337/src/bin/b.rs
@@ -1,8 +1,9 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let b = (1..s.len()).all(|i| s[i - 1] as u8 <= s[i] as u8);
+    println!("{}", if b { "Yes" } else { "No" });
 }
