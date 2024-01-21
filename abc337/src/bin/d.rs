57be4631302f214933f9b79999739e09f894662e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-22 00:13:33 +0900

    add abc337_d

--- abc337/src/bin/d.rs
+++ abc337/src/bin/d.rs
@@ -1,8 +1,60 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        h:usize,
+        w:usize,
+        k:usize,
+        s:[Chars;h],
     }
-    println!("{}", n);
+    let mut ans = h * w + 1;
+    for i in 0..h {
+        let mut count = 0;
+        let mut point = 0;
+        for j in 0..w {
+            if s[i][j] == 'o' {
+                count += 1;
+            } else if s[i][j] == '.' {
+                count += 1;
+                point += 1;
+            } else {
+                count = 0;
+                point = 0;
+            }
+            if count == k {
+                ans = ans.min(point);
+                count -= 1;
+                if s[i][j + 1 - k] == '.' {
+                    point -= 1;
+                }
+            }
+        }
+    }
+    for j in 0..w {
+        let mut count = 0;
+        let mut point = 0;
+        for i in 0..h {
+            if s[i][j] == 'o' {
+                count += 1;
+            } else if s[i][j] == '.' {
+                count += 1;
+                point += 1;
+            } else {
+                count = 0;
+                point = 0;
+            }
+            if count == k {
+                ans = ans.min(point);
+                count -= 1;
+                if s[i + 1 - k][j] == '.' {
+                    point -= 1;
+                }
+            }
+        }
+    }
+    if ans == h * w + 1 {
+        println!("-1");
+        return;
+    }
+    println!("{}", ans);
 }
