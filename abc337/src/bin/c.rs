ed6d2e7cb0a7971ad257966f92d9f5c447339248
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-22 00:13:32 +0900

    add abc337_c

--- abc337/src/bin/c.rs
+++ abc337/src/bin/c.rs
@@ -1,8 +1,25 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut g = vec![0; n];
+    let mut ans = vec![];
+    let mut prev = 0;
+    for i in 0..n {
+        if a[i] < 0 {
+            prev = i;
+            ans.push(i + 1);
+        } else {
+            g[(a[i] - 1) as usize] = i;
+        }
+    }
+    for _ in 1..n {
+        ans.push(g[prev] + 1);
+        prev = g[prev];
+    }
+    println!("{}", ans.iter().join(" "));
 }
