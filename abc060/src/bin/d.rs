baf7aefd2eb2e6262fee96a0154e1f0b4802c7f0
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-23 23:15:04 +0900

    add abc060_d

--- abc060/src/bin/d.rs
+++ abc060/src/bin/d.rs
@@ -1,8 +1,23 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        w:usize,
+        wv:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut map: HashMap<usize, usize> = HashMap::from([(0, 0)]);
+    for (wi, vi) in wv {
+        let mut nmap = map.clone();
+        for (k, v) in map {
+            if k + wi <= w {
+                let bef = nmap.get(&(k + wi)).unwrap_or(&0);
+                nmap.insert(k + wi, (v + vi).max(*bef));
+            }
+        }
+        map = nmap;
+    }
+    println!("{}", map.into_iter().map(|(_, v)| v).max().unwrap());
 }
