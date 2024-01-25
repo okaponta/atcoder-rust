73a05d58edbbbb31c17f0cc94a3a7b8e1249bc1d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-25 21:29:08 +0900

    add abc059_b

--- abc059/src/bin/b.rs
+++ abc059/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        a:Chars,
+        b:Chars,
     }
-    println!("{}", n);
+    if a.len() < b.len() {
+        println!("LESS");
+    } else if a.len() == b.len() {
+        for i in 0..a.len() {
+            if a[i] < b[i] {
+                println!("LESS");
+                return;
+            } else if a[i] > b[i] {
+                println!("GREATER");
+                return;
+            }
+        }
+        println!("EQUAL");
+    } else {
+        println!("GREATER");
+    }
 }
