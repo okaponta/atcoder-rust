f1012c3f00a6ddb3878c705ddbb8a6216c1ed9d0
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-27 22:58:08 +0900

    add abc338_c

--- abc338/src/bin/c.rs
+++ abc338/src/bin/c.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        q:[usize;n],
+        a:[usize;n],
+        b:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..=1_000_000 {
+        if (0..n).into_iter().any(|j| q[j] < a[j] * i) {
+            break;
+        }
+        let mut nb = 1_000_000;
+        for j in 0..n {
+            if 0 < b[j] {
+                let tmp = (q[j] - a[j] * i) / b[j];
+                nb = nb.min(tmp);
+            }
+        }
+        ans = ans.max(i + nb);
+    }
+    println!("{}", ans);
 }
