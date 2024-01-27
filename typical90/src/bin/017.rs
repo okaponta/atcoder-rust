ef99f176359bdbc9e074ab41df3c707e97a3e196
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-28 00:09:14 +0900

    add typical90_017

--- typical90/src/bin/017.rs
+++ typical90/src/bin/017.rs
@@ -1,9 +1,91 @@
+use std::cmp::Reverse;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        m:usize,
+        mut lr:[(usize,usize);m],
     }
-    println!("{}", a);
-    todo!();
+    lr.sort_by_key(|&(l, r)| (r, Reverse(l)));
+    let mut fw = FenwickTree::new(n);
+    let mut ans = 0;
+    for (l, r) in lr {
+        ans += fw.sum(l);
+        fw.add(l + 1, 1);
+        fw.add(r, -1);
+    }
+    println!("{}", ans);
+}
+
+// フェニック木。以下2つができる。1-indexedなので注意
+// 1. ai に v を加算する
+// 2. a1+a2+...+aiを求める
+pub struct FenwickTree {
+    len: usize,
+    data: Vec<i64>,
+}
+
+impl FenwickTree {
+    // a1~anの配列を作成
+    pub fn new(n: usize) -> Self {
+        Self {
+            len: n + 1,
+            data: vec![0; n + 1],
+        }
+    }
+
+    // aiにvを加算する
+    pub fn add(&mut self, i: usize, v: i64) {
+        assert!(i > 0);
+        assert!(i < self.len);
+        let mut i = i as i64;
+        while (i as usize) < self.len {
+            self.data[i as usize] += v;
+            i += i & -i;
+        }
+    }
+
+    // aiをvで置き換える
+    pub fn update(&mut self, i: usize, v: i64) {
+        assert!(i > 0);
+        assert!(i < self.len);
+        let cur = self.range(i, i);
+        self.add(i, v - cur);
+    }
+
+    // a1+a2+...aiを計算する
+    pub fn sum(&self, i: usize) -> i64 {
+        assert!(i < self.len);
+        let mut i = i as i64;
+        let mut sum = 0;
+        while i > 0 {
+            sum += self.data[i as usize];
+            i -= i & -i;
+        }
+        sum
+    }
+
+    // ai+...+ajを計算する
+    pub fn range(&self, i: usize, j: usize) -> i64 {
+        assert!(i <= j);
+        assert!(j < self.len);
+        self.sum(j) - self.sum(i - 1)
+    }
+
+    // 和がs以下の位置を返却
+    pub fn lower(&self, s: i64) -> usize {
+        let mut lower = 0;
+        let mut upper = self.len;
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if self.sum(med) <= s {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
 }
