531755f8672c78aeb4f12d89db74743bd186852d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-27 22:58:06 +0900

    add abc338_a

--- abc338/src/bin/a.rs
+++ abc338/src/bin/a.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    if s[0].is_ascii_uppercase() {
+        if (1..s.len()).all(|i| s[i].is_ascii_lowercase()) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
