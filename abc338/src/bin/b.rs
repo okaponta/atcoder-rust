0c4dd88e5ceee4a0c4206584f69f66985f9a1849
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-27 22:58:08 +0900

    add abc338_b

--- abc338/src/bin/b.rs
+++ abc338/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut c = vec![0; 26];
+    for s in s {
+        c[s as usize - 'a' as usize] += 1;
+    }
+    let max = c.iter().max().unwrap();
+    for i in 0..26 {
+        if c[i] == *max {
+            println!("{}", (b'a' + i as u8) as char);
+            return;
+        }
+    }
 }
