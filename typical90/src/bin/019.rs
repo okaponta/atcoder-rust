4b592fb51422d337bb3c71917398d5838f896010
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-29 00:37:15 +0900

    add typical90_019

--- typical90/src/bin/019.rs
+++ typical90/src/bin/019.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        mut n:usize,
+        a:[i64;2*n],
     }
-    println!("{}", a);
-    todo!();
+    n *= 2;
+    let mut dp = vec![vec![1 << 60; n]; n];
+    for i in 1..n {
+        dp[i - 1][i] = (a[i - 1] - a[i]).abs();
+    }
+    for l in (0..n).rev() {
+        for r in (l + 1..n).step_by(2) {
+            dp[l][r] = dp[l][r].min(dp[l + 1][r - 1] + (a[l] - a[r]).abs());
+            for i in (l + 1..r).step_by(2) {
+                dp[l][r] = dp[l][r].min(dp[l][i] + dp[i + 1][r]);
+            }
+        }
+    }
+    println!("{}", dp[0][n - 1]);
 }
