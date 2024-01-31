7b59709ca744a37bf4e89c54afd2c91fd5049ee5
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-01-31 21:21:31 +0900

    add typical90_022

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -2,8 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        a:usize,
+        b:usize,
+        c:usize,
     }
-    println!("{}", a);
-    todo!();
+    let g = gcd(a, gcd(b, c));
+    let ans = a / g + b / g + c / g;
+    println!("{}", ans - 3);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
