87a2ad9d4c7ac33ff7f4ebc90f8fd622ff5f80a1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-02 01:51:22 +0900

    add typical90_024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -2,8 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:i64,
+        a:[i64;n],
+        b:[i64;n],
     }
-    println!("{}", a);
-    todo!();
+    let min = (0..n).fold(0, |s, i| s + (a[i] - b[i]).abs());
+    println!(
+        "{}",
+        if min <= k && (k - min) % 2 == 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
