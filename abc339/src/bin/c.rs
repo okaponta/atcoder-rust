4118deec51c02e31edcd217a7401fce21bcb6848
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-03 23:16:50 +0900

    add abc339_c

--- abc339/src/bin/c.rs
+++ abc339/src/bin/c.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:[i64;n],
     }
-    println!("{}", n);
+    let mut cur = 0;
+    let mut ans = 0;
+    let mut sum = 0;
+    for a in a {
+        cur += a;
+        sum += a;
+        if cur < 0 {
+            ans += -cur;
+            cur = 0;
+        }
+    }
+    println!("{}", ans + sum);
 }
