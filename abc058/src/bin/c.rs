1d43d4331ea978ee7708c0a557e9d66c7b488715
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-06 22:10:16 +0900

    add abc058_c

--- abc058/src/bin/c.rs
+++ abc058/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut count = vec![100; 26];
+    for i in 0..n {
+        let mut tmp = vec![0; 26];
+        for j in 0..s[i].len() {
+            tmp[(s[i][j] as u8 - b'a') as usize] += 1;
+        }
+        for j in 0..26 {
+            count[j] = count[j].min(tmp[j]);
+        }
+    }
+    let mut ans = vec![];
+    for i in 0..26 {
+        for _ in 0..count[i] {
+            ans.push((b'a' + i as u8) as char);
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
