86b55b96f6a5a203ec4592c16e788a6b1dcd8712
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-07 13:44:28 +0900

    add abc339_f

--- abc339/src/bin/f.rs
+++ abc339/src/bin/f.rs
@@ -1,8 +1,30 @@
+use std::collections::HashMap;
+
+use num_bigint::BigInt;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[BigInt;n],
     }
-    println!("{}", n);
+    a.sort();
+    let mut map = HashMap::new();
+    for i in 0..n {
+        *map.entry(a[i].clone()).or_insert(0) += 1;
+    }
+    let max = a[n - 1].clone();
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..n {
+            let mul = &a[i] * &a[j];
+            if max < mul {
+                break;
+            }
+            if let Some(&c) = map.get(&mul) {
+                ans += c;
+            }
+        }
+    }
+    println!("{}", ans);
 }
