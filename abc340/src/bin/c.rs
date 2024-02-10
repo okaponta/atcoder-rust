46f7dd7f10236ac09e91cc39d5ada45f2e6c0659
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-10 22:48:26 +0900

    add abc340_c

--- abc340/src/bin/c.rs
+++ abc340/src/bin/c.rs
@@ -1,8 +1,27 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut memo = HashMap::new();
+    memo.insert(1, 0);
+    println!("{}", dfs(n, &mut memo));
+}
+
+fn dfs(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
+    if memo.contains_key(&n) {
+        return memo[&n];
+    }
+    let mut res = n;
+    if n % 2 == 0 {
+        res += 2 * dfs(n / 2, memo);
+    } else {
+        res += dfs(n / 2, memo);
+        res += dfs((n / 2) + 1, memo);
+    }
+    memo.insert(n, res);
+    res
 }
