5ac0585602437bd69cdb87f2ced212c9ba6eab5a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-12 17:18:08 +0900

    add typical90_025

--- typical90/src/bin/025.rs
+++ typical90/src/bin/025.rs
@@ -1,9 +1,31 @@
+use itertools::Itertools;
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-        a:i32,
+        n: usize,
+        b: usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut set = HashSet::new();
+    for digits in (0..=9).combinations_with_replacement(11) {
+        set.insert(digits.iter().product());
+    }
+    let mut ans = 0;
+    for x in set {
+        let m = x + b;
+        if m <= n && product(m) == x {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn product(mut x: usize) -> usize {
+    let mut res = 1;
+    while x > 0 {
+        res *= x % 10;
+        x /= 10;
+    }
+    res
 }
