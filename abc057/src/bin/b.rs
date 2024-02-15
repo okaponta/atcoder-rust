718f87084d59567d7952e09140d3f126a3422a40
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-16 00:50:34 +0900

    add abc057_b

--- abc057/src/bin/b.rs
+++ abc057/src/bin/b.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(i64,i64);n],
+        cd:[(i64,i64);m],
     }
-    println!("{}", n);
+    for (a, b) in ab {
+        let mut ans = 0;
+        let mut min = 1 << 60;
+        for i in 0..m {
+            let d = (a - cd[i].0).abs() + (b - cd[i].1).abs();
+            if d < min {
+                min = d;
+                ans = i + 1;
+            }
+        }
+        println!("{}", ans);
+    }
 }
