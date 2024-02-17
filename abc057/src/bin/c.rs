9331ee7476b418c55a572ea88fa8effa0de35c66
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-17 11:44:48 +0900

    add abc057_c

--- abc057/src/bin/c.rs
+++ abc057/src/bin/c.rs
@@ -1,8 +1,25 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut ans = 1 << 10;
+    for a in divisor(n) {
+        let b = n / a;
+        let len = a.to_string().len().max(b.to_string().len());
+        ans = ans.min(len);
+    }
+    println!("{}", ans);
+}
+
+fn divisor(n: usize) -> Vec<usize> {
+    let mut res = vec![];
+    for i in 1..=n.sqrt() {
+        if n % i == 0 {
+            res.push(i);
+        }
+    }
+    res
 }
