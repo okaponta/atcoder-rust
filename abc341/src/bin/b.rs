6165688f1ae7f057e8b4ce91b2c31d34fd80cb41
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-19 23:20:59 +0900

    add abc341_b

--- abc341/src/bin/b.rs
+++ abc341/src/bin/b.rs
@@ -3,6 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
+        st:[(usize,usize);n-1],
     }
-    println!("{}", n);
+    for i in 0..n - 1 {
+        a[i + 1] += a[i] / st[i].0 * st[i].1;
+    }
+    println!("{}", a[n - 1]);
 }
