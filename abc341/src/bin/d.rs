a3181c171f56d1e7ab4605df24758cbc3113dbb8
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-02-21 22:34:24 +0900

    add abc341_d

--- abc341/src/bin/d.rs
+++ abc341/src/bin/d.rs
@@ -3,6 +3,37 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let l = lcm(n, m);
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if rank(med, n, m, l) < k {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn rank(med: usize, n: usize, m: usize, l: usize) -> usize {
+    let nn = med / n;
+    let nm = med / m;
+    let nl = med / l;
+    nn + nm - 2 * nl
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
