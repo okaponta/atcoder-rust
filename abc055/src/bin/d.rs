b85a73c37fafbf02e2a7959ebf7036711ebe734d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-01 00:55:40 +0900

    add abc055_d

--- abc055/src/bin/d.rs
+++ abc055/src/bin/d.rs
@@ -1,8 +1,46 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let first = vec![('S', 'S'), ('S', 'W'), ('W', 'S'), ('W', 'W')];
+    for (one, two) in first {
+        let mut ans = vec![one, two];
+        for i in 1..n - 1 {
+            ans.push(nx(ans[i], ans[i - 1], s[i]));
+        }
+        if nx(ans[n - 2], ans[n - 1], s[n - 1]) == ans[0] && nx(ans[n - 1], ans[0], s[0]) == ans[1]
+        {
+            println!("{}", ans.iter().join(""));
+            return;
+        }
+    }
+    println!("-1");
+}
+
+fn nx(now: char, prev: char, c: char) -> char {
+    if now == 'S' {
+        if c == 'o' {
+            return prev;
+        } else {
+            return nt(prev);
+        }
+    } else {
+        if c == 'o' {
+            return nt(prev);
+        } else {
+            return prev;
+        }
+    }
+}
+
+fn nt(c: char) -> char {
+    if c == 'S' {
+        'W'
+    } else {
+        'S'
+    }
 }
