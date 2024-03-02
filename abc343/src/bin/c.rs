a67b72960c4993286cec49509eefbce272e5fa06
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-02 22:52:02 +0900

    add abc343_c

--- abc343/src/bin/c.rs
+++ abc343/src/bin/c.rs
@@ -4,5 +4,16 @@ fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut max = 1;
+    let mut i = 1;
+    while i * i * i <= n {
+        let k = i * i * i;
+        let s = k.to_string();
+        let t = s.chars().rev().collect::<String>();
+        if s == t {
+            max = k;
+        }
+        i += 1;
+    }
+    println!("{}", max);
 }
