5d7b40de75a2b6315974b82d5357524eeffb3a9a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-02 22:52:03 +0900

    add abc343_d

--- abc343/src/bin/d.rs
+++ abc343/src/bin/d.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashMap;
 
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        ab:[(Usize1,usize);t],
     }
-    println!("{}", n);
+    let mut points = vec![0; n];
+    let mut map = HashMap::new();
+    map.insert(0, n);
+    for (a, b) in ab {
+        let bef = points[a];
+        *map.entry(bef).or_insert(0) -= 1;
+        if map[&bef] == 0 {
+            map.remove(&bef);
+        }
+        *map.entry(bef + b).or_insert(0) += 1;
+        points[a] += b;
+        println!("{}", map.len());
+    }
 }
