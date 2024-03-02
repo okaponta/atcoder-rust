a7c3d920db729ffeabf69b3c4d15a1a8c56b7b59
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-02 22:52:00 +0900

    add abc343_b

--- abc343/src/bin/b.rs
+++ abc343/src/bin/b.rs
@@ -1,8 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[[usize;n];n],
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for i in 0..n {
+        for j in 0..n {
+            if a[i][j] == 1 {
+                g[i].push(j + 1);
+            }
+        }
+    }
+    for i in 0..n {
+        println!("{}", g[i].iter().join(" "));
+    }
 }
