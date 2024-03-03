67bc81d98c5d541e3cc1e6e146f1c5d04ed93c92
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-03 22:13:09 +0900

    add abc342_b

--- abc342/src/bin/b.rs
+++ abc342/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        p:[Usize1;n],
+        q:usize,
+        ab:[(Usize1,Usize1);q],
     }
-    println!("{}", n);
+    let mut ord = vec![0; n];
+    for i in 0..n {
+        ord[p[i]] = i;
+    }
+    for (a, b) in ab {
+        println!("{}", if ord[a] < ord[b] { a + 1 } else { b + 1 });
+    }
 }
