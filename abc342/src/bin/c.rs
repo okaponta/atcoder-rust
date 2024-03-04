d8c82507f88d673d704413317d43168da8c92979
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-05 08:30:38 +0900

    add abc342_c

--- abc342/src/bin/c.rs
+++ abc342/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _n:usize,
+        s:Chars,
+        q:usize,
+        cd:[(char,char);q],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    ('a'..='z').into_iter().for_each(|c| {
+        map.insert(c, c);
+    });
+    for (c, d) in cd {
+        ('a'..='z').into_iter().for_each(|b| {
+            if map[&b] == c {
+                map.insert(b, d);
+            }
+        });
+    }
+    println!("{}", s.into_iter().map(|c| map[&c]).join(""));
 }
