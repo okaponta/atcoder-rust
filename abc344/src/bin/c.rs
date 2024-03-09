a530fd600c0a1cc1fb30edf466f737e9b75aa774
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-09 22:44:20 +0900

    add abc344_c

--- abc344/src/bin/c.rs
+++ abc344/src/bin/c.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use std::collections::HashSet;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
+        m:usize,
+        b:[usize;m],
+        l:usize,
+        c:[usize;l],
+        q:usize,
+        x:[usize;q],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 0..n {
+        for j in 0..m {
+            for k in 0..l {
+                set.insert(a[i] + b[j] + c[k]);
+            }
+        }
+    }
+    for x in x {
+        println!("{}", if set.contains(&x) { "Yes" } else { "No" });
+    }
 }
