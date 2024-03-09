99981e7bfb6ade0a4cf9005e4e69d0531ccdba00
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-09 22:44:17 +0900

    add abc344_a

--- abc344/src/bin/a.rs
+++ abc344/src/bin/a.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut del = false;
+    let mut ans = vec![];
+    for c in s {
+        if c == '|' {
+            del = !del;
+            continue;
+        }
+        if !del {
+            ans.push(c);
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
