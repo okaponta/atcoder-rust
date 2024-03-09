d6f8ab3ca59b200f019e7ecdfd3823e491d3c27a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-09 22:44:21 +0900

    add abc344_d

--- abc344/src/bin/d.rs
+++ abc344/src/bin/d.rs
@@ -1,8 +1,42 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
+        t:Chars,
         n:usize,
     }
-    println!("{}", n);
+    let mut check = vec![];
+    let m = t.len();
+    for i in 0..m {
+        let mut set = HashSet::new();
+        let mut tmp = vec![];
+        set.insert(tmp.clone());
+        for i in i..t.len() {
+            tmp.push(t[i]);
+            set.insert(tmp.clone());
+        }
+        check.push(set);
+    }
+
+    let mut dp = vec![1 << 30; m + 1];
+    dp[0] = 0;
+    for _ in 0..n {
+        input! {a:usize, s:[Chars;a]}
+        let mut next = dp.clone();
+        for i in 0..m {
+            for j in 0..a {
+                if check[i].contains(&s[j]) {
+                    next[i + s[j].len()] = next[i + s[j].len()].min(dp[i] + 1);
+                }
+            }
+        }
+        dp = next;
+    }
+    if dp[m] == 1 << 30 {
+        println!("-1");
+        return;
+    }
+    println!("{}", dp[m]);
 }
