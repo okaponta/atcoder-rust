30dd6d5490aefd44a0fd3ae148e2ba6cd93a14af
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-12 02:27:43 +0900

    add abc341_f

--- abc341/src/bin/f.rs
+++ abc341/src/bin/f.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
+        w:[usize;n],
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for (u, v) in uv {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let ord = (0..n)
+        .into_iter()
+        .sorted_by_key(|&i| w[i])
+        .collect::<Vec<_>>();
+    let mut c = vec![0; n];
+    for &i in &ord {
+        let mut dp = vec![0; w[i]];
+        dp[0] = 1;
+        for &next in &g[i] {
+            if w[i] <= w[next] {
+                continue;
+            }
+            for j in (0..w[i] - w[next]).rev() {
+                dp[j + w[next]] = dp[j + w[next]].max(dp[j] + c[next]);
+            }
+        }
+        c[i] = dp.into_iter().max().unwrap();
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        ans += c[i] * a[i];
+    }
+    println!("{}", ans);
 }
