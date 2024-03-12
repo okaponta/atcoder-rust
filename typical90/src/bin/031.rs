d72d93cb1c2b4703bcdb6e2d40613a81a588de09
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-13 00:05:05 +0900

    add typical90_031

--- typical90/src/bin/031.rs
+++ typical90/src/bin/031.rs
@@ -2,8 +2,34 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        w:[usize;n],
+        b:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut grundy = vec![vec![0; 1500]; 51];
+    for i in 0..51 {
+        for j in 0..1300 {
+            let mut mex = vec![false; 1500];
+            if 0 < i {
+                mex[grundy[i - 1][j + i]] = true;
+            }
+            if 0 < j {
+                for k in 1..=(j / 2) {
+                    mex[grundy[i][j - k]] = true;
+                }
+            }
+            grundy[i][j] = calc_mex(mex);
+        }
+    }
+    let xor = (0..n).fold(0, |s, i| s ^ grundy[w[i]][b[i]]);
+    println!("{}", if xor == 0 { "Second" } else { "First" });
+}
+
+fn calc_mex(vec: Vec<bool>) -> usize {
+    for k in 0..1500 {
+        if !vec[k] {
+            return k;
+        }
+    }
+    return 1500;
 }
