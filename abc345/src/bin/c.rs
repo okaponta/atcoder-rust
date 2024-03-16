9eeaf0a75d1ddca237de98685d5bc821dd6faec6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-16 22:48:12 +0900

    add abc345_c

--- abc345/src/bin/c.rs
+++ abc345/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let mut map = HashMap::new();
+    for i in 0..n {
+        *map.entry(s[i]).or_insert(0) += 1;
+    }
+    let mut ans = n * (n - 1) / 2;
+    let mut flg = false;
+    for (_, v) in map {
+        if 1 < v {
+            flg = true;
+        }
+        ans -= v * (v - 1) / 2;
+    }
+    if flg {
+        ans += 1;
+    }
+    println!("{}", ans);
 }
