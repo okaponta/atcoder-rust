15e95254238ab6a94b8bad811c6f3b2b325bf1ef
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-18 01:35:21 +0900

    add typical90_034

--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -1,9 +1,32 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    // 半開区間
+    let mut left = 0;
+    let mut right = 0;
+    let mut map = HashMap::new();
+    let mut ans = 0;
+    loop {
+        while right < n && (map.len() < k || map.contains_key(&a[right])) {
+            *map.entry(a[right]).or_insert(0) += 1;
+            right += 1;
+        }
+        ans = ans.max(right - left);
+        if right == n {
+            break;
+        }
+        *map.entry(a[left]).or_insert(0) -= 1;
+        if map[&a[left]] == 0 {
+            map.remove(&a[left]);
+        }
+        left += 1;
+    }
+    println!("{}", ans);
 }
