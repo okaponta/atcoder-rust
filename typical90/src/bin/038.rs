1179c5e2181c4a259a2626c7d69abd66efcbe819
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-21 23:54:50 +0900

    add typical90_038

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        a:i128,
+        b:i128,
     }
-    println!("{}", a);
-    todo!();
+    let l = lcm(a, b);
+    if 10i128.pow(18u32) < l {
+        println!("Large");
+    } else {
+        println!("{}", l);
+    }
+}
+
+fn gcd(a: i128, b: i128) -> i128 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
+}
+
+// 最小公倍数
+// 計算量はO(log min(a,b))
+fn lcm(a: i128, b: i128) -> i128 {
+    a * b / gcd(a, b)
 }
