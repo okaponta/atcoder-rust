802a399925c567b15be91640facb0e41470263d6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-23 22:48:24 +0900

    add abc346_a

--- abc346/src/bin/a.rs
+++ abc346/src/bin/a.rs
@@ -1,8 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 1..n {
+        ans.push(a[i] * a[i - 1]);
+    }
+    println!("{}", ans.iter().join(" "));
 }
