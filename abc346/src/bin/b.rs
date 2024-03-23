3835434e8a89e06ade9c26376f0aa848aa21abb6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-23 22:48:37 +0900

    add abc346_b

--- abc346/src/bin/b.rs
+++ abc346/src/bin/b.rs
@@ -2,7 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        w:usize,
+        b:usize,
     }
-    println!("{}", n);
+    let p = "wbwbwwbwbwbw".to_string().chars().collect::<Vec<char>>();
+    for i in 0..12 {
+        let mut cw = 0;
+        let mut cb = 0;
+        for j in 0..w + b {
+            if p[(i + j) % 12] == 'w' {
+                cw += 1;
+            } else {
+                cb += 1;
+            }
+        }
+        if w == cw && b == cb {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
