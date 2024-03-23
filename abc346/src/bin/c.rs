263365267d78abab85b44cd683e9df6de3ae92d6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-23 22:48:45 +0900

    add abc346_c

--- abc346/src/bin/c.rs
+++ abc346/src/bin/c.rs
@@ -1,8 +1,22 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = k * (k + 1) / 2;
+    let mut set = HashSet::new();
+    for a in a {
+        if a <= k {
+            set.insert(a);
+        }
+    }
+    for i in set {
+        ans -= i;
+    }
+    println!("{}", ans);
 }
