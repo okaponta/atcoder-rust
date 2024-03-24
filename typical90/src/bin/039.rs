b7198762717d863bb5df9c15c1ec72e226b1ee8d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-25 06:22:16 +0900

    add typical90_039

--- typical90/src/bin/039.rs
+++ typical90/src/bin/039.rs
@@ -1,9 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        ab:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut dp = vec![1; n];
+    dfs(0, 0, &mut dp, &g);
+    let ans = (0..n).fold(0, |s, i| s + dp[i] * (n - dp[i]));
+    println!("{}", ans);
+}
+
+fn dfs(cur: usize, prev: usize, dp: &mut Vec<usize>, g: &Vec<Vec<usize>>) {
+    for &next in &g[cur] {
+        if prev == next {
+            continue;
+        }
+        dfs(next, cur, dp, g);
+        dp[cur] += dp[next];
+    }
 }
