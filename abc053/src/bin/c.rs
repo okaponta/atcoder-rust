9f80ecade78183c6a0378f3e3d93069915a935de
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-27 20:56:17 +0900

    add abc053_c

--- abc053/src/bin/c.rs
+++ abc053/src/bin/c.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        x:usize,
     }
-    println!("{}", n);
+    let mut ans = (x / 11) * 2;
+    if x % 11 != 0 {
+        ans += 1;
+        if 6 < x % 11 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
