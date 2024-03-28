e73056377b62f3896591826cd537d09db113a42a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-28 22:14:37 +0900

    add abc053_b

--- abc053/src/bin/b.rs
+++ abc053/src/bin/b.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut a = 0;
+    let mut z = 0;
+    for i in 0..s.len() {
+        if s[i] == 'Z' {
+            z = i;
+        }
+    }
+    for i in (0..s.len()).rev() {
+        if s[i] == 'A' {
+            a = i;
+        }
+    }
+    println!("{}", z - a + 1);
 }
