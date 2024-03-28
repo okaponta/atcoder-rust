4bd2c01dc0888331cd4c5a67b0c6e362975a6270
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-29 02:08:35 +0900

    add abc054_b

--- abc054/src/bin/b.rs
+++ abc054/src/bin/b.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use itertools::iproduct;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[Chars;n],
+        b:[Chars;m],
     }
-    println!("{}", n);
+    for (i, j) in iproduct!(0..=n - m, 0..=n - m) {
+        if is_ok(&a, &b, m, i, j) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
+}
+
+fn is_ok(a: &Vec<Vec<char>>, b: &Vec<Vec<char>>, m: usize, i: usize, j: usize) -> bool {
+    for (k, l) in iproduct!(0..m, 0..m) {
+        if a[i + k][j + l] != b[k][l] {
+            return false;
+        }
+    }
+    true
 }
