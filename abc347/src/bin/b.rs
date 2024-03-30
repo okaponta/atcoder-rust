fd8bb50490c02c9e7c9c23f36b8e0123522a1e25
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-03-30 23:41:13 +0900

    add abc347_b

--- abc347/src/bin/b.rs
+++ abc347/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 0..s.len() {
+        let mut tmp = vec![];
+        for j in i..s.len() {
+            tmp.push(s[j]);
+            set.insert(tmp.clone());
+        }
+    }
+    println!("{}", set.len());
 }
