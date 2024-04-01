f0f41bcc4aeec2e604f5c555cdd9080db80e25d3
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-02 00:04:28 +0900

    add abc347_e

--- abc347/src/bin/e.rs
+++ abc347/src/bin/e.rs
@@ -1,8 +1,112 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        x:[Usize1;q],
     }
-    println!("{}", n);
+    let mut seg = SegmentTree::new(q + 1, 0, |a, b| a + b);
+    let mut query = vec![vec![]; n];
+    let mut set = HashSet::new();
+    for i in 0..q {
+        query[x[i]].push(i);
+        if set.contains(&x[i]) {
+            set.remove(&x[i]);
+        } else {
+            set.insert(x[i]);
+        }
+        seg.update_tmp(i, set.len());
+    }
+    seg.update_all();
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        if query[i].len() % 2 == 1 {
+            query[i].push(q);
+        }
+        for j in (0..query[i].len()).step_by(2) {
+            ans[i] += seg.query(query[i][j]..query[i][j + 1]);
+        }
+    }
+    println!("{}", ans.iter().join(" "));
+}
+
+// segment tree
+// seg[0] -> seg[1]+seg[2]
+// seg[1] -> seg[3]+seg[4] seg[2] -> seg[5]+seg[6]
+// seg[3] -> seg[7]+seg[8] seg[6] -> seg[13]+seg[14]]
+// 必要な要素数は2^n-1
+// 区間上の値を更新する
+// 任意の区間上の最小値や合計値(与えるfuncによって全てのbit or値)などを取得する
+// let mut seg = SegmentTree::new(9, 0, |a, b| a + b);
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    // 半開区間なので注意
+    pub fn get(&self, k: usize) -> T {
+        self.query(k..k + 1)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
 }
