592e51a08a7292a7c8298e57c2667dd2afdae0f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-06 23:00:25 +0900

    add abc348_c

--- abc348/src/bin/c.rs
+++ abc348/src/bin/c.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        ac:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for (a, c) in ac {
+        map.insert(c, *map.get(&c).unwrap_or(&(1 << 60)).min(&a));
+    }
+    let mut ans = 0;
+    for (_, v) in map {
+        ans = ans.max(v);
+    }
+    println!("{}", ans);
 }
