0f6d4e18f698dda0070fd24684095297e71077ab
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-06 23:00:24 +0900

    add abc348_b

--- abc348/src/bin/b.rs
+++ abc348/src/bin/b.rs
@@ -1,8 +1,22 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        xy:[(i64,i64);n],
     }
-    println!("{}", n);
+    for i in 0..n {
+        let mut ans = 0;
+        let mut d = 0;
+        for j in 0..n {
+            let tmp = (xy[i].0 - xy[j].0) * (xy[i].0 - xy[j].0)
+                + (xy[i].1 - xy[j].1) * (xy[i].1 - xy[j].1);
+            if d < tmp {
+                ans = j + 1;
+                d = tmp;
+            }
+        }
+        println!("{}", ans);
+    }
 }
