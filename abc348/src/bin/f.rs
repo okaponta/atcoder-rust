bcc128baa5264e0faa776278f75c36be1bee1fd7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-06 23:05:22 +0900

    add abc348_f

--- abc348/src/bin/f.rs
+++ abc348/src/bin/f.rs
@@ -3,6 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[[u16;m];n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            if judge(&a[i], &a[j], m) {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
+}
+
+fn judge(a: &Vec<u16>, b: &Vec<u16>, m: usize) -> bool {
+    let mut res = false;
+    for k in 0..m {
+        res ^= a[k] == b[k];
+    }
+    res
 }
