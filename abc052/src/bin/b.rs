bb9e3c782f6ab4c6eebefe5ea64a04b1e4b5f35b
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-07 16:09:19 +0900

    add abc052_b

--- abc052/src/bin/b.rs
+++ abc052/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        _n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut x = 0;
+    for c in s {
+        if c == 'I' {
+            x += 1;
+        } else {
+            x -= 1;
+        }
+        ans = ans.max(x);
+    }
+    println!("{}", ans);
 }
