df2119ce6cc30b6d8a14cc9061f7b51cc0861218
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-09 01:20:36 +0900

    add typical90_042

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        x:usize,
     }
-    println!("{}", a);
-    todo!();
+    if x % 9 != 0 {
+        println!("0");
+        return;
+    }
+    let mut dp = vec![0usize; x + 1];
+    for i in 1..=x {
+        if i < 10 {
+            dp[i] += 1;
+        }
+        for j in i.saturating_sub(9)..i {
+            dp[i] += dp[j];
+        }
+        dp[i] %= 1_000_000_007;
+    }
+    println!("{}", dp[x]);
 }
