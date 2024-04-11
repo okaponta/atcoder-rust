3a754e5a05a1d2a59bde68f5b2b97bcc73abb1f1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-11 13:34:16 +0900

    add typical90_044

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -1,9 +1,25 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        q:usize,
+        mut a:[usize;n],
+        txy:[(u8,usize,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut offset = 0;
+    for (t, x, y) in txy {
+        if t == 1 {
+            // swap
+            a.swap((n + x - offset - 1) % n, (n + y - offset - 1) % n);
+        } else if t == 2 {
+            // shift
+            offset += 1;
+            offset %= n;
+        } else {
+            // print
+            println!("{}", a[(n + x - offset - 1) % n]);
+        }
+    }
 }
