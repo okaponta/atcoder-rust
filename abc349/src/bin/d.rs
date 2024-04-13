e55da976077e19d07fc6eaac30557843cf6b504e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-13 23:12:11 +0900

    add abc349_d

--- abc349/src/bin/d.rs
+++ abc349/src/bin/d.rs
@@ -3,38 +3,27 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        mut l:usize,
+        l:usize,
         r:usize,
     }
     let mut ans = vec![];
-    if l == 0 {
-        let mut tmp = 1;
-        while tmp * 2 <= r {
-            tmp *= 2;
-        }
-        ans.push((0, tmp));
-        l = tmp;
-    }
-    while l < r {
-        // max
-        for i in 0..62 {
-            if l >> i & 1 == 1 {
-                // min
-                let mut j = 0;
-                while j < i {
-                    if r < (l + (1 << (j + 1))) {
-                        break;
-                    }
-                    j += 1;
-                }
-                ans.push((l, l + (1 << j)));
-                l += 1 << j;
-                break;
-            }
-        }
-    }
+    dfs(l, r, 0, 1 << 60, &mut ans);
     println!("{}", ans.len());
     for (l, r) in ans {
         println!("{} {}", l, r);
+    }
+}
+
+fn dfs(l: usize, r: usize, rl: usize, rr: usize, ans: &mut Vec<(usize, usize)>) {
+    let rm = (rl + rr) / 2;
+    if rl == l && rr == r {
+        ans.push((rl, rr));
+    } else if l < rm && rm < r {
+        dfs(l, rm, rl, rm, ans);
+        dfs(rm, r, rm, rr, ans);
+    } else if r <= rm {
+        dfs(l, r, rl, rm, ans);
+    } else if rm <= l {
+        dfs(l, r, rm, rr, ans);
     }
 }
