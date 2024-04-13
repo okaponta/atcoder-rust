6afad0af71eed24609fc5ad97df7b3d835ed40e6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-13 22:49:33 +0900

    add abc349_c

--- abc349/src/bin/c.rs
+++ abc349/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        mut t:Chars,
     }
-    println!("{}", n);
+    let mut i = 0;
+    for c in s {
+        if c == t[i].to_ascii_lowercase() {
+            i += 1;
+        }
+        if i == 3 {
+            break;
+        }
+    }
+    if i == 3 {
+        println!("Yes");
+        return;
+    }
+    if i == 2 && t[2] == 'X' {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
