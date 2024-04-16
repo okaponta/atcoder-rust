9516bcd547df468702781dacd5aff3f0e2460e9d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-16 14:30:48 +0900

    add typical90_048

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,9 +1,24 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut heap = BinaryHeap::new();
+    for i in 0..n {
+        heap.push((ab[i].1, i, 0));
+    }
+    let mut ans = 0;
+    for _ in 0..k {
+        let (v, i, j) = heap.pop().unwrap();
+        ans += v;
+        if j == 0 {
+            heap.push((ab[i].0 - ab[i].1, i, 1));
+        }
+    }
+    println!("{}", ans);
 }
