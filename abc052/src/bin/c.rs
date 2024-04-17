64a758f9cd629817ad37a993615f3717847bf316
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-17 20:31:02 +0900

    add abc052_c

--- abc052/src/bin/c.rs
+++ abc052/src/bin/c.rs
@@ -1,8 +1,39 @@
+use std::collections::HashMap;
+
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 1..=n {
+        for (j, k) in factorize(i) {
+            *map.entry(j).or_insert(0) += k;
+        }
+    }
+    let ans = map
+        .into_iter()
+        .fold(1, |s, (_, v)| (s * (v + 1)) % 1_000_000_007);
+    println!("{}", ans);
+}
+
+fn factorize(mut n: usize) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    for i in 2..=n.sqrt() {
+        if n % i != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % i == 0 {
+            ex += 1;
+            n /= i;
+        }
+        res.push((i, ex));
+    }
+    if n != 1 {
+        res.push((n, 1));
+    }
+    res
 }
