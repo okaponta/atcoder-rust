c8f3ce1649eb85b01a367b37fff4591b09d48317
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-19 11:35:16 +0900

    add typical90_049

--- typical90/src/bin/049.rs
+++ typical90/src/bin/049.rs
@@ -1,9 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        m:usize,
+        clr:[(i64,Usize1,usize);m],
     }
-    println!("{}", a);
-    todo!();
+    let g = clr
+        .into_iter()
+        .map(|(c, l, r)| (l, r, c))
+        .collect::<Vec<_>>();
+    println!("{}", kruskal(n + 1, g));
+}
+
+fn kruskal(n: usize, mut edges: Vec<(usize, usize, i64)>) -> i64 {
+    edges.sort_by_key(|e| e.2);
+    let mut uf = petgraph::unionfind::UnionFind::new(n);
+    let mut res = 0;
+    let mut count = 0;
+    for (u, v, cost) in edges {
+        if uf.union(u, v) {
+            res += cost;
+            count += 1;
+        }
+    }
+    if count != n - 1 {
+        return -1;
+    }
+    res
 }
