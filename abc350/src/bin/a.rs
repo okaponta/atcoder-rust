40b9a01bef418ae2ca99edb5a4c81ab7c0182ea4
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-20 22:56:41 +0900

    add abc350_a

--- abc350/src/bin/a.rs
+++ abc350/src/bin/a.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 1..350 {
+        if i == 316 {
+            continue;
+        }
+        let mut tmp = vec!['A', 'B', 'C'];
+        tmp.push(f(i / 100));
+        tmp.push(f((i % 100) / 10));
+        tmp.push(f(i % 10));
+        set.insert(tmp);
+    }
+    println!("{}", if set.contains(&s) { "Yes" } else { "No" });
+}
+
+fn f(i: usize) -> char {
+    std::char::from_digit(i as u32, 10).unwrap()
 }
