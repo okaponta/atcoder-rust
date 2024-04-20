14ef01f64a1153eedaac251ea4e8a098dbecb807
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-20 22:56:43 +0900

    add abc350_b

--- abc350/src/bin/b.rs
+++ abc350/src/bin/b.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        t:[Usize1;q],
     }
-    println!("{}", n);
+    let mut ans = vec![true; n];
+    for t in t {
+        ans[t] = !ans[t];
+    }
+    println!("{}", ans.iter().filter(|i| **i).count());
 }
