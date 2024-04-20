c4e4615f75a972cc2c37cb11ad10c181feb2fa1f
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-20 22:56:44 +0900

    add abc350_c

--- abc350/src/bin/c.rs
+++ abc350/src/bin/c.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        mut a:[Usize1;n],
     }
-    println!("{}", n);
+    let mut b = vec![0; n];
+    for i in 0..n {
+        b[a[i]] = i;
+    }
+    let mut ans = vec![];
+    for i in 0..n {
+        if a[i] == i {
+            continue;
+        }
+        let j = b[i];
+        let k = a[j];
+        b.swap(a[i], k);
+        a.swap(i, j);
+        ans.push((i + 1, j + 1));
+    }
+    println!("{}", ans.len());
+    for i in 0..ans.len() {
+        println!("{} {}", ans[i].0, ans[i].1);
+    }
 }
