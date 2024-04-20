c10f88d2c6034f3ceca501901353b82d63b3b844
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-20 22:56:45 +0900

    add abc350_d

--- abc350/src/bin/d.rs
+++ abc350/src/bin/d.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", n);
+    let mut uf = UnionFind::new(n);
+    for &(a, b) in &ab {
+        uf.union(a, b);
+    }
+    let mut size = HashMap::new();
+    for i in 0..n {
+        *size.entry(uf.find(i)).or_insert(0) += 1;
+    }
+    let mut ans = 0;
+    for (_, v) in size {
+        ans += v * (v - 1) / 2;
+    }
+    println!("{}", ans - m);
 }
