74424663d295f5870a5cb2a7bffe020382491941
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-23 01:14:55 +0900

    add typical90_050

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,9 +1,21 @@
 use proconio::input;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        l:usize,
     }
-    println!("{}", a);
-    todo!();
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    for i in 0..n {
+        dp[i + 1] += dp[i];
+        dp[i + 1] %= MOD;
+        if i + l <= n {
+            dp[i + l] += dp[i];
+            dp[i + l] %= MOD;
+        }
+    }
+    println!("{}", dp[n]);
 }
