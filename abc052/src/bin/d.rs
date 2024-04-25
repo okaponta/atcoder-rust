fb829e09670921f9a902e95b8c4d4c35da35ec21
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-25 09:06:44 +0900

    add abc052_d

--- abc052/src/bin/d.rs
+++ abc052/src/bin/d.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize,
+        x:[usize;n],
     }
-    println!("{}", n);
+    println!(
+        "{}",
+        (1..n).fold(0, |s, i| s + ((x[i] - x[i - 1]) * a).min(b))
+    );
 }
