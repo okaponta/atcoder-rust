ee680be42177d848cb705c91878ac5505c94e65e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-27 22:57:42 +0900

    add abc351_b

--- abc351/src/bin/b.rs
+++ abc351/src/bin/b.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        a:[Chars;n],
+        b:[Chars;n],
     }
-    println!("{}", n);
+    for i in 0..n {
+        for j in 0..n {
+            if a[i][j] != b[i][j] {
+                println!("{} {}", i + 1, j + 1);
+            }
+        }
+    }
 }
