b600776d6a5b8a7cdc9317611df5524987961419
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-04-27 22:57:44 +0900

    add abc351_c

--- abc351/src/bin/c.rs
+++ abc351/src/bin/c.rs
@@ -1,8 +1,26 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    for a in a {
+        q.push_front(a);
+        while 1 < q.len() {
+            let b = q.pop_front().unwrap();
+            let c = q.pop_front().unwrap();
+            if b == c {
+                q.push_front(b + 1);
+            } else {
+                q.push_front(c);
+                q.push_front(b);
+                break;
+            }
+        }
+    }
+    println!("{}", q.len());
 }
