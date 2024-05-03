a710c80e393c0d603c30e03b76c50de8124c43de
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-03 15:32:27 +0900

    add typical90_058

--- typical90/src/bin/058.rs
+++ typical90/src/bin/058.rs
@@ -1,9 +1,37 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:usize,
     }
-    println!("{}", a);
-    todo!();
+    println!("{}", index_with_loop(n, k));
+}
+
+fn index_with_loop(init: usize, k: usize) -> usize {
+    fn next(mut n: usize) -> usize {
+        let mut res = n;
+        for i in vec![10000, 1000, 100, 10, 1] {
+            res += n / i;
+            n %= i;
+        }
+        res % 100000
+    }
+
+    let mut v = vec![];
+    let mut set = HashSet::new();
+    let mut tmp = init;
+    while !set.contains(&tmp) {
+        v.push(tmp);
+        set.insert(tmp);
+        tmp = next(tmp);
+    }
+    if k < v.len() {
+        return v[k];
+    }
+    let offset = v.iter().position(|&x| x == tmp).unwrap();
+    let l = v.len() - offset;
+    v[offset + (k - offset) % l]
 }
