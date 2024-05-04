1e3c4c5e2bff46c25eacc1ffd2835ecd7e6079a7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-05 01:40:14 +0900

    add typical90_061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,9 +1,19 @@
-use proconio::input;
+use std::collections::VecDeque;
 
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
     input! {
-        a:i32,
+        q:usize,
+        tx:[(u8,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut q = VecDeque::new();
+    for (t, x) in tx {
+        match t {
+            1 => q.push_front(x),
+            2 => q.push_back(x),
+            _ => println!("{}", q[x - 1]),
+        }
+    }
 }
