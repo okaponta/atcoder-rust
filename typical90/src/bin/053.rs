3dcb13fb77affdddb5f1b1c91b08e1654ab71bfc
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-05 23:02:35 +0900

    add typical90_053

--- typical90/src/bin/053.rs
+++ typical90/src/bin/053.rs
@@ -1,9 +1,57 @@
-use proconio::input;
+use proconio::input_interactive;
+use std::collections::HashMap;
 
 fn main() {
-    input! {
-        a:i32,
+    input_interactive! {
+        t: usize,
     }
-    println!("{}", a);
-    todo!();
+    for _ in 0..t {
+        test();
+    }
+}
+
+fn test() {
+    input_interactive! {
+        n: usize,
+    }
+    let fibo = build_fibo(n + 1);
+
+    let mut map = HashMap::new();
+    // 左端をlで管理し、黄金分割探索で範囲を狭めていく
+    let mut l = 0;
+    for v in fibo.windows(3).rev() {
+        let l0 = l + v[0];
+        let r0 = l + v[1];
+        if f(r0, n, &mut map) > f(l0, n, &mut map) {
+            l = l0;
+        }
+    }
+    println!("! {}", f(l + 1, n, &mut map));
+}
+
+// フィボナッチ数列を n 以上の値が現れるまで組み立てる
+fn build_fibo(n: usize) -> Vec<usize> {
+    if n == 1 {
+        return vec![1];
+    }
+    let mut v = vec![1, 2];
+    while v[v.len() - 1] < n {
+        let x = v[v.len() - 2] + v[v.len() - 1];
+        v.push(x);
+    }
+    v
+}
+
+fn f(i: usize, n: usize, map: &mut HashMap<usize, usize>) -> usize {
+    let i = i.min(n);
+    if let Some(&x) = map.get(&i) {
+        return x;
+    }
+
+    println!("? {}", i);
+    input_interactive! {
+        x: usize,
+    }
+    map.insert(i, x);
+    x
 }
