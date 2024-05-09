6518785fcef60938ec94ceb488f045911b72d866
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-10 02:12:25 +0900

    add typical90_067

--- typical90/src/bin/067.rs
+++ typical90/src/bin/067.rs
@@ -1,9 +1,38 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        mut n:Chars,
+        k:usize,
     }
-    println!("{}", a);
-    todo!();
+    for _ in 0..k {
+        n = f(n);
+    }
+    if n.len() == 0 {
+        println!("0");
+        return;
+    }
+    println!("{}", n.into_iter().join(""));
+}
+
+fn f(n: Vec<char>) -> Vec<char> {
+    let mut m = 0;
+    let mut r = 1;
+    for i in (0..n.len()).rev() {
+        m += n[i].to_digit(10).unwrap() as usize * r;
+        r *= 8;
+    }
+    let mut res = vec![];
+    while 0 < m {
+        res.push(g(m % 9));
+        m /= 9;
+    }
+    res.reverse();
+    res
+}
+
+fn g(n: usize) -> char {
+    let c = if n % 9 == 8 { 5 } else { n % 9 };
+    std::char::from_digit(c as u32, 10).unwrap()
 }
