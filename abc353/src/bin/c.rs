3871be5b8e98f9de1048092706b3a9b0fba92e43
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-11 22:48:51 +0900

    add abc353_c

--- abc353/src/bin/c.rs
+++ abc353/src/bin/c.rs
@@ -1,8 +1,28 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    let modu = 100_000_000;
+    let s = ruiseki(&a);
+    let mut ans = 0;
+    for i in 0..n {
+        let ov = (n - a.lower_bound(&(modu - a[i]))).min(n - 1 - i);
+        ans += s[n] - s[i + 1] + a[i] * (n - 1 - i);
+        ans -= ov * modu;
+    }
+    println!("{}", ans);
+}
+
+// 累積和
+fn ruiseki(a: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
