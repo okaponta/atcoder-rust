0a0cf88a98648c765bbf73ff109420aff19733d0
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-11 22:48:50 +0900

    add abc353_b

--- abc353/src/bin/b.rs
+++ abc353/src/bin/b.rs
@@ -3,6 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    let mut tmp = 0;
+    for a in a {
+        tmp += a;
+        if k < tmp {
+            ans += 1;
+            tmp = a;
+        }
+    }
+    println!("{}", ans + 1);
 }
