c37c2ff6630d5fefaff5ab4bb9e999d1730c8aca
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-16 23:13:44 +0900

    add typical90_069

--- typical90/src/bin/069.rs
+++ typical90/src/bin/069.rs
@@ -2,8 +2,31 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        k:usize,
     }
-    println!("{}", a);
-    todo!();
+    if n == 1 {
+        println!("{k}");
+        return;
+    }
+    if n == 2 && k < 3 {
+        println!("{}", k * (k - 1));
+        return;
+    }
+    let modulo = 1_000_000_007;
+    let ans = (((k * (k - 1)) % modulo) * pow(k - 2, n - 2, modulo)) % modulo;
+    println!("{}", ans);
+}
+
+fn pow(mut x: usize, mut n: usize, modulo: usize) -> usize {
+    x %= modulo;
+    let mut ret = if x == 0 { 0 } else { 1 };
+    while n > 0 {
+        if n & 1 == 1 {
+            ret = ret * x % modulo;
+        }
+        x = x * x % modulo;
+        n >>= 1;
+    }
+    ret
 }
