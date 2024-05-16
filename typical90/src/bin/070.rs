b6cea558f1469169228783b4d6ade1acdb0c26bc
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-17 08:06:30 +0900

    add typical90_070

--- typical90/src/bin/070.rs
+++ typical90/src/bin/070.rs
@@ -2,8 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        xy:[(i64,i64);n],
     }
-    println!("{}", a);
-    todo!();
+    let mut x = vec![];
+    let mut y = vec![];
+    for i in 0..n {
+        x.push(xy[i].0);
+        y.push(xy[i].1);
+    }
+    x.sort();
+    y.sort();
+    let midx = x[n / 2];
+    let midy = y[n / 2];
+    let mut ans = 0;
+    for i in 0..n {
+        ans += (midx - x[i]).abs();
+        ans += (midy - y[i]).abs();
+    }
+    println!("{}", ans);
 }
