bf21c9d027fe2a38acefcd7cc8ec15246144f23d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-18 01:21:20 +0900

    add typical90_075

--- typical90/src/bin/075.rs
+++ typical90/src/bin/075.rs
@@ -1,9 +1,36 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
     }
-    println!("{}", a);
-    todo!();
+    println!(
+        "{}",
+        factorize(n)
+            .into_iter()
+            .map(|(_, v)| v)
+            .sum::<usize>()
+            .next_power_of_two()
+            .trailing_zeros()
+    );
+}
+
+fn factorize(mut n: usize) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    for i in 2..=n.sqrt() {
+        if n % i != 0 {
+            continue;
+        }
+        let mut ex = 0;
+        while n % i == 0 {
+            ex += 1;
+            n /= i;
+        }
+        res.push((i, ex));
+    }
+    if n != 1 {
+        res.push((n, 1));
+    }
+    res
 }
