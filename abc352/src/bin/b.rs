48d08674873792647512bb0fe8ba24da54f6e9a6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-19 18:03:47 +0900

    add abc352_b

--- abc352/src/bin/b.rs
+++ abc352/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    let mut i = 0;
+    for j in 0..s.len() {
+        while s[j] != t[i] {
+            i += 1;
+        }
+        ans.push(i + 1);
+        i += 1;
+    }
+    println!("{}", ans.iter().join(" "));
 }
