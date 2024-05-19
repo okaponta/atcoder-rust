8196218390d166955df5dc80ace5ae80a51c8a67
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-19 18:03:48 +0900

    add abc352_c

--- abc352/src/bin/c.rs
+++ abc352/src/bin/c.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut head = 0;
+    let mut ans = 0;
+    for (a, b) in ab {
+        ans += a;
+        head = head.max(b - a);
+    }
+    ans += head;
+    println!("{}", ans);
 }
