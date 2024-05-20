bca88e5bf338b3305717229a718140f41f4e861c
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-21 03:26:05 +0900

    add typical90_076

--- typical90/src/bin/076.rs
+++ typical90/src/bin/076.rs
@@ -2,8 +2,36 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let s = a.iter().sum::<usize>();
+    if s % 10 != 0 {
+        println!("No");
+        return;
+    }
+    for i in 0..n {
+        a.push(a[i]);
+    }
+    // 半開区間
+    let mut left = 0;
+    let mut right = 0;
+    let mut sum = 0;
+    loop {
+        while sum < s / 10 && left < n {
+            sum += a[right];
+            right += 1;
+        }
+        if left == n {
+            // しゃくとり終了
+            break;
+        }
+        if sum == s / 10 {
+            println!("Yes");
+            return;
+        }
+        sum -= a[left];
+        left += 1;
+    }
+    println!("No");
 }
