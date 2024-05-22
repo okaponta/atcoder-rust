3bf25c2627280fcae07ee733b64c1d63ebdc1332
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-22 21:37:10 +0900

    add typical90_073

--- typical90/src/bin/073.rs
+++ typical90/src/bin/073.rs
@@ -1,9 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
+
+const MOD: usize = 1_000_000_007;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        c:[char;n],
+        ab:[(Usize1,Usize1);n-1],
     }
-    println!("{}", a);
-    todo!();
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    println!("{}", dfs(0, 0, &g, &c).2);
+}
+
+fn dfs(cur: usize, prev: usize, g: &Vec<Vec<usize>>, c: &Vec<char>) -> (usize, usize, usize) {
+    let mut res = if c[cur] == 'a' { (1, 0, 1) } else { (0, 1, 1) };
+    for &nxt in &g[cur] {
+        if nxt == prev {
+            continue;
+        }
+        let ret = dfs(nxt, cur, g, c);
+        if c[cur] == 'a' {
+            res.0 = res.0 * (ret.0 + ret.2) % MOD;
+        } else {
+            res.1 = res.1 * (ret.1 + ret.2) % MOD;
+        }
+        res.2 = res.2 * (ret.0 + ret.1 + 2 * ret.2) % MOD;
+    }
+    res.2 = (res.2 + MOD - if c[cur] == 'a' { res.0 } else { res.1 }) % MOD;
+    res
 }
