6780418b976dbf42ac5c0fc73d4dd1674f10b239
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-23 22:47:36 +0900

    add abc354_c

--- abc354/src/bin/c.rs
+++ abc354/src/bin/c.rs
@@ -1,8 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut ac:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut aci = vec![];
+    for (i, (a, c)) in ac.into_iter().enumerate() {
+        aci.push((a, c, i));
+    }
+    aci.sort_by(|a, b| a.1.cmp(&b.1).then(a.0.cmp(&b.0)));
+    let mut max = 0;
+    let mut ans = vec![];
+    for (a, _, i) in aci.into_iter() {
+        if a < max {
+            continue;
+        }
+        max = max.max(a);
+        ans.push(i + 1);
+    }
+    ans.sort();
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
