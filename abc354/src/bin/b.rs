4efd57611bb5b25a08f6ba276769f538afafe53c
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-23 22:30:41 +0900

    add abc354_b

--- abc354/src/bin/b.rs
+++ abc354/src/bin/b.rs
@@ -3,6 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut sc:[(String, usize);n],
     }
-    println!("{}", n);
+    sc.sort();
+    let ord = sc.iter().map(|(_, c)| c).sum::<usize>() % n;
+    println!("{}", sc[ord].0);
 }
