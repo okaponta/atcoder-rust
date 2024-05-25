a8e395402462a93fa55fabdadb205dd10d4144e4
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-25 22:54:47 +0900

    add abc355_d

--- abc355/src/bin/d.rs
+++ abc355/src/bin/d.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        lr:[(usize,usize);n],
     }
-    println!("{}", n);
+    let mut q = vec![];
+    for (l, r) in lr {
+        q.push((l, 0));
+        q.push((r, 1));
+    }
+    q.sort();
+    let mut ans = 0i64;
+    let mut count = 0;
+    for (_i, q) in q {
+        if q == 0 {
+            ans += count;
+            count += 1;
+        } else {
+            count -= 1;
+        }
+    }
+    println!("{}", ans);
 }
