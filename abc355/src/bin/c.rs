ec35761ed46d32a3c01dd2455dbd2a2a27e5c498
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-25 22:54:46 +0900

    add abc355_c

--- abc355/src/bin/c.rs
+++ abc355/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        t:usize,
+        a:[Usize1;t],
     }
-    println!("{}", n);
+    let mut row = vec![0; n];
+    let mut col = vec![0; n];
+    let mut x = vec![0; 2];
+    for (i, a) in a.into_iter().enumerate() {
+        let ri = a % n;
+        let ci = a / n;
+        row[ri] += 1;
+        col[ci] += 1;
+        if ri == ci {
+            x[0] += 1;
+        }
+        if ri + ci == n - 1 {
+            x[1] += 1;
+        }
+        if row[ri] == n || col[ci] == n || x[0] == n || x[1] == n {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("-1");
 }
