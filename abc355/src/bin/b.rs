2f157181724ac9a266cbd0443edcdb76a16e98c9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-25 22:54:45 +0900

    add abc355_b

--- abc355/src/bin/b.rs
+++ abc355/src/bin/b.rs
@@ -1,8 +1,29 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
+        b:[usize;m],
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    let mut c = vec![];
+    for i in 0..n {
+        c.push(a[i]);
+        set.insert(a[i]);
+    }
+    for i in 0..m {
+        c.push(b[i]);
+    }
+    c.sort();
+    for i in 1..c.len() {
+        if set.contains(&c[i - 1]) && set.contains(&c[i]) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
