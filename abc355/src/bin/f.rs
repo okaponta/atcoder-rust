c9ee18188df57bffa96c8e7ef601f1e702ab0509
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-27 01:10:59 +0900

    add abc355_f

--- abc355/src/bin/f.rs
+++ abc355/src/bin/f.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        q:usize,
+        abc:[(Usize1,Usize1,Usize1);n-1],
+        uvw:[(Usize1,Usize1,Usize1);q],
     }
-    println!("{}", n);
+    let mut cost = 0;
+    let mut g = vec![UnionFind::new(n); 10];
+    for (a, b, c) in abc {
+        cost += c + 1;
+        for i in c..10 {
+            g[i].union(a, b);
+        }
+    }
+    for (u, v, w) in uvw {
+        for i in w..10 {
+            if !g[i].union(u, v) {
+                cost -= i - w;
+                break;
+            }
+        }
+        println!("{}", cost);
+    }
 }
