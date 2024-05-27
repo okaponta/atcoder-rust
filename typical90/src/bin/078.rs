726b1e62617c5cdf83d2bd07edbf9a733de1a046
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-28 00:34:12 +0900

    add typical90_078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -1,9 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    println!("{}", a);
-    todo!();
+    let mut c = vec![0; n];
+    for (a, b) in ab {
+        c[a.max(b)] += 1;
+    }
+    let ans = c.into_iter().filter(|v| v == &1).count();
+    println!("{}", ans);
 }
