c86d26b2194018968b890c68e2b6b1ca7962aced
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-31 00:17:55 +0900

    add abc051_b

--- abc051/src/bin/b.rs
+++ abc051/src/bin/b.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        k:usize,
+        s:usize,
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for x in 0..=k {
+        for y in 0..=k {
+            if x + y <= s && s - x - y <= k {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
