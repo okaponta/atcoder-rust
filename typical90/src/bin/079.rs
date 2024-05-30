3059e42dcbb0d5d50c1282bb87e13e8bb2461814
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-05-30 23:43:46 +0900

    add typical90_079

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -1,9 +1,27 @@
+use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        h:usize,
+        w:usize,
+        mut a:[[i64;w];h],
+        b:[[i64;w];h],
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 0;
+    for (i, j) in iproduct!(0..h - 1, 0..w - 1) {
+        let diff = b[i][j] - a[i][j];
+        for (k, l) in iproduct!(0..2, 0..2) {
+            a[i + k][j + l] += diff;
+        }
+        ans += diff.abs();
+    }
+    for (i, j) in iproduct!(1..3, 1..3) {
+        if a[h - i][w - j] != b[h - i][w - j] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
+    println!("{ans}")
 }
