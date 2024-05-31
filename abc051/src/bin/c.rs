990b1210d7816aa5756e20fcf8749fb80cf20af8
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-01 02:43:41 +0900

    add abc051_c

--- abc051/src/bin/c.rs
+++ abc051/src/bin/c.rs
@@ -1,8 +1,31 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        sx:i64,
+        sy:i64,
+        tx:i64,
+        ty:i64,
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    f((sx, sy), (tx, ty), &mut ans);
+    f((tx, ty), (sx, sy), &mut ans);
+    ans.push('L');
+    f((sx - 1, sy), (tx, ty + 1), &mut ans);
+    ans.push('D');
+    ans.push('R');
+    f((tx + 1, ty), (sx, sy - 1), &mut ans);
+    ans.push('U');
+    println!("{}", ans.iter().join(""));
+}
+
+fn f(s: (i64, i64), t: (i64, i64), ans: &mut Vec<char>) {
+    if s.0 + s.1 < t.0 + t.1 {
+        (0..(t.1 - s.1)).into_iter().for_each(|_i| ans.push('U'));
+        (0..(t.0 - s.0)).into_iter().for_each(|_i| ans.push('R'));
+    } else {
+        (0..(s.1 - t.1)).into_iter().for_each(|_i| ans.push('D'));
+        (0..(s.0 - t.0)).into_iter().for_each(|_i| ans.push('L'));
+    }
 }
