214c6c1bc665484067e470ccb645180b847d3325
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-02 17:58:31 +0900

    add abc356_c

--- abc356/src/bin/c.rs
+++ abc356/src/bin/c.rs
@@ -1,8 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut ok = vec![true; 1 << n];
+    for _ in 0..m {
+        input! {
+            c:usize,
+            a:[Usize1;c],
+            r:char,
+        }
+        for i in 0..1 << n {
+            let mut count = 0;
+            for &j in &a {
+                if i >> j & 1 == 1 {
+                    count += 1;
+                }
+            }
+            if r == 'o' {
+                if count < k {
+                    ok[i] = false;
+                }
+            } else {
+                if k <= count {
+                    ok[i] = false;
+                }
+            }
+        }
+    }
+    println!("{}", ok.into_iter().filter(|b| *b).count());
 }
