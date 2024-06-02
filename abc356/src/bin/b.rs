1cc90fe338f6a5f82853e9607d52c3eb2bba4bcb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-02 17:58:29 +0900

    add abc356_b

--- abc356/src/bin/b.rs
+++ abc356/src/bin/b.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;m],
+        x:[[usize;m];n],
     }
-    println!("{}", n);
+    let mut s = vec![0; m];
+    for i in 0..n {
+        for j in 0..m {
+            s[j] += x[i][j];
+        }
+    }
+    for i in 0..m {
+        if s[i] < a[i] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
