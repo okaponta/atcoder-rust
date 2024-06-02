f92fa1b606cb10a71b543ee58c794250e71607b3
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-02 17:58:28 +0900

    add abc356_a

--- abc356/src/bin/a.rs
+++ abc356/src/bin/a.rs
@@ -1,8 +1,21 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        l:usize,
+        r:usize,
     }
-    println!("{}", n);
+    let mut ans = vec![];
+    for i in 1..l {
+        ans.push(i);
+    }
+    for i in (l..=r).rev() {
+        ans.push(i);
+    }
+    for i in r + 1..=n {
+        ans.push(i)
+    }
+    println!("{}", ans.iter().join(" "));
 }
