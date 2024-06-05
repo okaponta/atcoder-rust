b43cd6b55ff2fca76dc3cd61c71420b1bbfa90cf
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-05 14:00:41 +0900

    add typical90_080

--- typical90/src/bin/080.rs
+++ typical90/src/bin/080.rs
@@ -2,8 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        d:usize,
+        a:[usize;n],
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 1usize << d;
+    for i in 1_usize..1 << n {
+        let mut bit = usize::max_value() - ((1 << d) - 1);
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                bit |= a[j];
+            }
+        }
+        let s = 1 << bit.count_zeros();
+        if i.count_ones() & 1 == 1 {
+            ans -= s;
+        } else {
+            ans += s;
+        }
+    }
+    println!("{}", ans);
 }
