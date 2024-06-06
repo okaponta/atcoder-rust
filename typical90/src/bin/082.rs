41ef27f6bda8ad4c9a8554432a8b01aa6fa91c6b
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-07 00:48:00 +0900

    add typical90_082

--- typical90/src/bin/082.rs
+++ typical90/src/bin/082.rs
@@ -1,9 +1,29 @@
 use proconio::input;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
-        a:i32,
+        l:usize,
+        r:usize,
     }
-    println!("{}", a);
-    todo!();
+    println!("{}", (f(r) + MOD - f(l - 1)) % MOD);
+}
+
+fn f(a: usize) -> usize {
+    if a == 0 {
+        return 0;
+    }
+    let mut tmp = 1;
+    let mut num = 1;
+    while tmp * 10 <= a {
+        tmp *= 10;
+        num += 1;
+    }
+    let c = (kai(a) + MOD - kai(tmp - 1)) % MOD;
+    ((num % MOD) * c + f(tmp - 1)) % MOD
+}
+
+fn kai(a: usize) -> usize {
+    ((a % MOD) * ((a + 1) % MOD) / 2) % MOD
 }
