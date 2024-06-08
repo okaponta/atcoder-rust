d91a3508401e8b92597dd14d6019517bfcd60038
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-08 22:52:21 +0900

    add abc357_c

--- abc357/src/bin/c.rs
+++ abc357/src/bin/c.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
+        n:u32,
     }
-    println!("{}", n);
+    let m = 3usize.pow(n);
+    let mut ans = vec![vec!['.'; m]; m];
+    f(n as usize, 0, 0, m / 3, &mut ans);
+    for i in 0..m {
+        println!("{}", ans[i].iter().join(""));
+    }
+}
+
+fn f(k: usize, i1: usize, j1: usize, m: usize, ans: &mut Vec<Vec<char>>) {
+    if k == 0 {
+        ans[i1][j1] = '#';
+        return;
+    }
+    for i in 0..3 {
+        for j in 0..3 {
+            if i == 1 && j == 1 {
+                continue;
+            }
+            f(k - 1, i1 + i * m, j1 + j * m, m / 3, ans);
+        }
+    }
 }
