ce64f2d9bcd459c44de9a4a9a81dd84a523871b4
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-08 22:52:19 +0900

    add abc357_b

--- abc357/src/bin/b.rs
+++ abc357/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let small = s.iter().filter(|c| c.is_ascii_lowercase()).count();
+    let big = s.iter().filter(|c| c.is_ascii_uppercase()).count();
+    if small < big {
+        println!("{}", s.iter().map(|c| c.to_ascii_uppercase()).join(""));
+    } else {
+        println!("{}", s.iter().map(|c| c.to_ascii_lowercase()).join(""));
+    }
 }
