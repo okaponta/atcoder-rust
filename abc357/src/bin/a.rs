b4853cd3ddec6c31dbbea6ee8f7b044ee1199343
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-08 22:52:18 +0900

    add abc357_a

--- abc357/src/bin/a.rs
+++ abc357/src/bin/a.rs
@@ -3,6 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        h:[usize;n],
+    }
+    let s = ruiseki(&h);
+    for i in 0..=n {
+        if m < s[i] {
+            println!("{}", i - 1);
+            return;
+        }
     }
     println!("{}", n);
+}
+
+fn ruiseki(a: &Vec<usize>) -> Vec<usize> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
