794846f75c9b84712a97711f26a02d3a1f221c69
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-09 10:31:23 +0900

    add typical90_084

--- typical90/src/bin/084.rs
+++ typical90/src/bin/084.rs
@@ -1,9 +1,37 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        s:Chars,
     }
-    println!("{}", a);
-    todo!();
+    let mut ans = 0;
+    let mut o = 0;
+    let mut x = 0;
+    let mut r = 0;
+    if s[r] == 'o' {
+        o += 1;
+    } else {
+        x += 1;
+    }
+    for l in 0..n {
+        while r < n && (o == 0 || x == 0) {
+            r += 1;
+            if r == n {
+                break;
+            }
+            if s[r] == 'o' {
+                o += 1;
+            } else {
+                x += 1;
+            }
+        }
+        ans += n - r;
+        if s[l] == 'o' {
+            o -= 1;
+        } else {
+            x -= 1;
+        }
+    }
+    println!("{}", ans);
 }
