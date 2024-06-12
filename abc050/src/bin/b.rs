b43777cba983e2232e91ed2cc81a3a0ae6e2ff50
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-12 22:18:19 +0900

    add abc050_b

--- abc050/src/bin/b.rs
+++ abc050/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{fastout, input, marker::Usize1};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
+        t:[i64;n],
+        m:usize,
+        px:[(Usize1,i64);m],
     }
-    println!("{}", n);
+    let s = t.iter().sum::<i64>();
+    for (p, x) in px {
+        println!("{}", s - t[p] + x);
+    }
 }
