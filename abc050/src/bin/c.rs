842dfda30ed850fe95c8969767c822311039f753
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-13 21:47:30 +0900

    add abc050_c

--- abc050/src/bin/c.rs
+++ abc050/src/bin/c.rs
@@ -3,6 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        mut a:[i64;n],
     }
-    println!("{}", n);
+    if n % 2 == 0 {
+        a.push(-1);
+        a.push(-1);
+    } else {
+        a.push(-2);
+        a.push(0);
+    }
+    a.sort();
+    let mut ans = 1;
+    for i in 0..n {
+        if i % 2 != 0 {
+            ans *= 2;
+            ans %= 1_000_000_007;
+        }
+        if a[i + 2] - a[i] != 2 {
+            println!("0");
+            return;
+        }
+    }
+    println!("{}", ans);
 }
