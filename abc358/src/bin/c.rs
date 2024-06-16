94c1cd1fc3d93dc9ece63127069c9a20bcb89d41
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-16 18:43:00 +0900

    add abc358_c

--- abc358/src/bin/c.rs
+++ abc358/src/bin/c.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        s:[Chars;n],
     }
-    println!("{}", n);
+    let mut ans = 100;
+    for i in 1..1 << n {
+        let mut pop = vec![false; m];
+        let mut count = 0;
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                count += 1;
+                for k in 0..m {
+                    if s[j][k] == 'o' {
+                        pop[k] = true;
+                    }
+                }
+            }
+        }
+        if pop.iter().all(|&b| b) {
+            ans = ans.min(count);
+        }
+    }
+    println!("{}", ans);
 }
