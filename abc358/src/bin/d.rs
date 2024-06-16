02c82337d33f3d4d28fb982986e943435d6b3a3a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-16 18:43:01 +0900

    add abc358_d

--- abc358/src/bin/d.rs
+++ abc358/src/bin/d.rs
@@ -1,8 +1,28 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        a:[usize;n],
+        mut b:[usize;m],
     }
-    println!("{}", n);
+    let mut set = BTreeSet::new();
+    for i in 0..n {
+        set.insert((a[i], i));
+    }
+    b.sort();
+    let mut ans = 0;
+    for b in b {
+        if let Some(&(a, i)) = set.range((b, 0)..).next() {
+            ans += a;
+            set.remove(&(a, i));
+        } else {
+            println!("-1");
+            return;
+        }
+    }
+    println!("{}", ans);
 }
