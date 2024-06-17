7104eb24c5b340793ba0b700f12e23faa1a5ce94
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-18 01:47:13 +0900

    add typical90_086

--- typical90/src/bin/086.rs
+++ typical90/src/bin/086.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        q:usize,
+        xyzw:[(Usize1,Usize1,Usize1,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    println!(
+        "{}",
+        (0..60).fold(1, |s, i| {
+            s * (0..1 << n).filter(|&j| ok(i, j, &xyzw)).count() % 1000000007
+        })
+    );
+}
+
+fn ok(i: usize, j: usize, xyzw: &Vec<(usize, usize, usize, usize)>) -> bool {
+    for &(x, y, z, w) in xyzw {
+        if j >> x & 1 | j >> y & 1 | j >> z & 1 != w >> i & 1 {
+            return false;
+        }
+    }
+    true
 }
