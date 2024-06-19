eac125f18bcaf0649a577c326a1c92c9e3d36d54
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-20 00:00:53 +0900

    add abc049_c

--- abc049/src/bin/c.rs
+++ abc049/src/bin/c.rs
@@ -1,8 +1,30 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    let target = vec![
+        vec!['d', 'r', 'e', 'a', 'm'],
+        vec!['d', 'r', 'e', 'a', 'm', 'e', 'r'],
+        vec!['e', 'r', 'a', 's', 'e'],
+        vec!['e', 'r', 'a', 's', 'e', 'r'],
+    ];
+    let mut q = VecDeque::new();
+    q.push_back(0);
+    while let Some(i) = q.pop_front() {
+        if i == n {
+            println!("YES");
+            return;
+        }
+        for t in &target {
+            if i + t.len() <= n && (0..t.len()).into_iter().all(|j| s[i + j] == t[j]) {
+                q.push_back(i + t.len());
+            }
+        }
+    }
+    println!("NO");
 }
