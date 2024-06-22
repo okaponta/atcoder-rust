ba17fb6191209e29dd14488a6b7e7a440ceb7019
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-22 22:51:40 +0900

    add abc359_b

--- abc359/src/bin/b.rs
+++ abc359/src/bin/b.rs
@@ -1,8 +1,29 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;2*n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 0..2 * n {
+        map.entry(a[i]).or_insert(vec![]).push(i);
+    }
+    let mut ans = 0;
+    for (_, v) in map {
+        if abs(v[0], v[1]) == 2 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
