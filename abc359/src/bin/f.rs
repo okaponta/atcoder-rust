87028fbecdff92f4c0844d0535d76d40f8f16073
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-22 22:51:44 +0900

    add abc359_f

--- abc359/src/bin/f.rs
+++ abc359/src/bin/f.rs
@@ -1,8 +1,23 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    a.sort();
+    heap.push(Reverse((a[0], a[0], 0)));
+    let mut ans = 0;
+    for i in 1..n {
+        if let Some(Reverse((cj, aj, j))) = heap.pop() {
+            ans += cj;
+            heap.push(Reverse(((2 * j + 3) * aj, aj, j + 1)));
+            ans += a[i];
+            heap.push(Reverse((3 * a[i], a[i], 1)));
+        }
+    }
+    println!("{}", ans);
 }
