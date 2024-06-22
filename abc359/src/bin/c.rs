0373c1982b9a07e5feef3e6160e7670370ca79fb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-22 22:51:41 +0900

    add abc359_c

--- abc359/src/bin/c.rs
+++ abc359/src/bin/c.rs
@@ -2,7 +2,30 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        s:(i64,i64),
+        t:(i64,i64),
     }
-    println!("{}", n);
+    let y = (s.1 - t.1).abs();
+    if (s.0 + s.1) % 2 == 0 {
+        // 左
+        let min = s.0 - y;
+        let max = s.0 + 1 + y;
+        if min <= t.0 && t.0 <= max {
+            println!("{}", y);
+        } else if t.0 < min {
+            println!("{}", y + (min + 1 - t.0) / 2);
+        } else {
+            println!("{}", y + (t.0 - max + 1) / 2);
+        }
+    } else {
+        let min = s.0 - y - 1;
+        let max = s.0 + y;
+        if min <= t.0 && t.0 <= max {
+            println!("{}", y);
+        } else if t.0 < min {
+            println!("{}", y + (min + 1 - t.0) / 2);
+        } else {
+            println!("{}", y + (t.0 - max + 1) / 2);
+        }
+    }
 }
