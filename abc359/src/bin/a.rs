56a6dba0602b902b00e797abb8b36741589f176e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-22 22:51:39 +0900

    add abc359_a

--- abc359/src/bin/a.rs
+++ abc359/src/bin/a.rs
@@ -3,6 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        s:[String;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    for s in s {
+        if &s == "Takahashi" {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
