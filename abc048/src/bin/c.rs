10be5d045a4e26ebe133ed4653e13fc117ddf77b
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-25 06:06:07 +0900

    add abc048_c

--- abc048/src/bin/c.rs
+++ abc048/src/bin/c.rs
@@ -3,6 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        x:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    let mut ans = 0;
+    if x < a[0] {
+        ans += a[0] - x;
+        a[0] = x;
+    }
+    for i in 1..n {
+        if x < a[i - 1] + a[i] {
+            let t = a[i - 1] + a[i] - x;
+            ans += t;
+            a[i] -= t;
+        }
+    }
+    println!("{}", ans);
 }
