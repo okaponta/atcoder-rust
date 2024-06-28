a52f0027660b2eb540f39db9575ce68ce9fbc506
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-28 12:12:29 +0900

    add abc047_c

--- abc047/src/bin/c.rs
+++ abc047/src/bin/c.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut num = 0;
+    for i in 1..s.len() {
+        if s[i] != s[i - 1] {
+            num += 1;
+        }
+    }
+    println!("{}", num);
 }
