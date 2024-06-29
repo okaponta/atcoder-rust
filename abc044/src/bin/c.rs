dbce024e595663ee44253ec66892fe7f4dd35987
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-30 01:31:36 +0900

    add abc044_c

--- abc044/src/bin/c.rs
+++ abc044/src/bin/c.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        x:[usize;n],
     }
-    println!("{}", n);
+    let mut dp = vec![vec![0usize; 2501]; n + 1];
+    dp[0][0] = 1;
+    for i in 0..n {
+        for j in (0..=i).rev() {
+            for k in 0..=j * 50 {
+                dp[j + 1][k + x[i]] += dp[j][k];
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 1..=n {
+        ans += dp[i][i * a];
+    }
+    println!("{}", ans);
 }
