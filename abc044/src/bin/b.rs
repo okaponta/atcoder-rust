b8ff47092c27821433825f1bbfbd20032c7247ea
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-29 22:51:23 +0900

    add abc044_b

--- abc044/src/bin/b.rs
+++ abc044/src/bin/b.rs
@@ -1,8 +1,18 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        w:Chars,
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for c in w {
+        *map.entry(c).or_insert(0) += 1;
+    }
+    if map.into_iter().all(|(_, v)| v % 2 == 0) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
