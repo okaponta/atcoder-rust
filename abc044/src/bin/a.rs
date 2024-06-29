7228c83b48c0e580ed33b8ede2ff3708969b6292
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-06-29 22:54:53 +0900

    add abc044_a

--- abc044/src/bin/a.rs
+++ abc044/src/bin/a.rs
@@ -3,6 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        x:usize,
+        y:usize,
     }
-    println!("{}", n);
+    let ans = if n <= k { x * n } else { x * k + y * (n - k) };
+    println!("{}", ans);
 }
