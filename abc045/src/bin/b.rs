cca1e339c2e59b508eb86ed2b719fa984486cc1d
kohei.okamoto <59508678+okaponta@users.noreply.github.com>
2024-07-02 17:38:31 +0900

    b.rs を更新

--- abc045/src/bin/b.rs
+++ abc045/src/bin/b.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        a:Chars,
+        b:Chars,
+        c:Chars,
     }
-    println!("{}", n);
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+    let mut next = 'a';
+    loop {
+        if next == 'a' {
+            if i == a.len() {
+                println!("A");
+                return;
+            }
+            
+            next = a[i];
+            i += 1;
+        } else if next == 'b' {
+            if j == b.len() {
+                println!("B");
+                return;
+            }
+            
+            next = b[j];
+            j += 1;
+        } else {
+            if k == c.len() {
+                println!("C");
+                return;
+            }
+            
+            next = c[k];
+            k += 1;
+        }
+    }
 }
