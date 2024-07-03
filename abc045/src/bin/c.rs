b91fc35be2f717ca29cf910c1039f130f429b85f
kohei.okamoto <59508678+okaponta@users.noreply.github.com>
2024-07-03 16:24:06 +0900

    c.rs を更新

--- abc045/src/bin/c.rs
+++ abc045/src/bin/c.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+ use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut dp = vec![];
+    dp.push((0, 0));
+    for c in s {
+        let c = c.to_digit(10).unwrap() as usize;
+        let mut next = vec![];
+        for (s, t) in dp {
+            next.push((s + t, c));
+            next.push((s, t * 10 + c));
+        }
+        dp = next;
+    }
+    let ans = dp.into_iter().map(|(s, t)| s + t).sum::<usize>();
+    println!("{}", ans / 2);
 }
