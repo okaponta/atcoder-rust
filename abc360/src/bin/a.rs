0edc4f86c53596e94d2189ec3da34a31600040ca
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-04 23:08:05 +0900

    add abc360_a

--- abc360/src/bin/a.rs
+++ abc360/src/bin/a.rs
@@ -1,8 +1,10 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let r = s.iter().position(|&c| c == 'R').unwrap();
+    let m = s.iter().position(|&c| c == 'M').unwrap();
+    println!("{}", if r < m { "Yes" } else { "No" });
 }
