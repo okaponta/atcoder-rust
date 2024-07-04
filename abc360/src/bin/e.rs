1c4dd97d281dd7595fc8bacf2467213aca54cc3a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-04 23:48:12 +0900

    add abc360_e

--- abc360/src/bin/e.rs
+++ abc360/src/bin/e.rs
@@ -3,6 +3,117 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
     }
-    println!("{}", n);
+    let mut dp = (ModInt::one(), ModInt::zero());
+    let m1 = ModInt::new(n * n - 2 * n + 2) * ModInt::new(n * n).inv();
+    let m2 = ModInt::new(2) * ModInt::new(n * n).inv();
+    for _ in 0..k {
+        let n1 = dp.0 * m1 + dp.1 * m2;
+        let n2 = ModInt::one() - n1;
+        dp = (n1, n2);
+    }
+    let ans = dp.0 + ModInt::new(n * (n + 1) / 2 - 1) * dp.1 * ModInt::new(n - 1).inv();
+    println!("{}", ans.val);
+}
+
+const MOD: usize = 998244353;
+
+#[derive(Clone, Copy)]
+pub struct ModInt {
+    val: usize,
+}
+
+impl ModInt {
+    pub const fn zero() -> Self {
+        Self { val: 0 }
+    }
+
+    pub const fn one() -> Self {
+        Self { val: 1 }
+    }
+
+    pub fn new(i: usize) -> Self {
+        ModInt { val: i % MOD }
+    }
+
+    pub fn inv(&self) -> Self {
+        let mut a = self.val as i64;
+        let mut b = MOD as i64;
+        let modulo = MOD as i64;
+        let mut u = 1;
+        let mut v = 0;
+        while b > 0 {
+            let t = a / b;
+            a -= t * b;
+            std::mem::swap(&mut a, &mut b);
+            u -= t * v;
+            std::mem::swap(&mut u, &mut v);
+        }
+        u %= modulo;
+        if u < 0 {
+            u += modulo;
+        }
+        ModInt { val: u as usize }
+    }
+
+    pub fn pow(&self, mut n: usize) -> Self {
+        let mut x = self.val;
+        // let mut ret = if x == 0 { 0 } else { 1 };
+        let mut ret = 1;
+        while n > 0 {
+            if n & 1 == 1 {
+                ret = ret * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
+        }
+        ModInt { val: ret }
+    }
+}
+
+impl std::ops::Add for ModInt {
+    type Output = ModInt;
+    fn add(self, other: Self) -> Self {
+        ModInt::new(self.val + other.val)
+    }
+}
+
+impl std::ops::AddAssign for ModInt {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl std::ops::Sub for ModInt {
+    type Output = ModInt;
+    fn sub(self, other: Self) -> Self {
+        ModInt::new(MOD + self.val - other.val)
+    }
+}
+
+impl std::ops::SubAssign for ModInt {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl std::ops::Mul for ModInt {
+    type Output = ModInt;
+    fn mul(self, other: Self) -> Self {
+        ModInt::new(self.val * other.val)
+    }
+}
+
+impl std::ops::MulAssign for ModInt {
+    fn mul_assign(&mut self, other: Self) {
+        *self = *self * other;
+    }
+}
+
+impl std::fmt::Debug for ModInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.val)?;
+        Ok(())
+    }
 }
