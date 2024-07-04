4ec09314cc1c9528d5e21e0f0b2410ba43caf8e9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-04 23:21:58 +0900

    add abc360_d

--- abc360/src/bin/d.rs
+++ abc360/src/bin/d.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use superslice::Ext;
 
 fn main() {
     input! {
         n:usize,
+        t:i64,
+        s:Chars,
+        x:[i64;n],
     }
-    println!("{}", n);
+    let mut plus = vec![];
+    let mut minus = vec![];
+    for i in 0..n {
+        if s[i] == '0' {
+            minus.push(x[i]);
+        } else {
+            plus.push(x[i]);
+        }
+    }
+    minus.sort();
+    let mut ans = 0;
+    for i in plus {
+        let bef = minus.upper_bound(&i);
+        let after = minus.upper_bound(&(i + 2 * t));
+        ans += after - bef;
+    }
+    println!("{}", ans);
 }
