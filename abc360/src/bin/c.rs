1a04c5e4bcc7d7f9e3ecd3aa095821e2538ea25a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-04 23:13:10 +0900

    add abc360_c

--- abc360/src/bin/c.rs
+++ abc360/src/bin/c.rs
@@ -1,8 +1,23 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        a:[usize;n],
+        w:[usize;n],
     }
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 0..n {
+        map.entry(a[i]).or_insert(vec![]).push(w[i]);
+    }
+    let mut ans = 0;
+    for (_, mut v) in map {
+        if 1 < v.len() {
+            v.sort();
+            ans += (0..v.len() - 1).fold(0, |s, i| s + v[i]);
+        }
+    }
+    println!("{}", ans);
 }
