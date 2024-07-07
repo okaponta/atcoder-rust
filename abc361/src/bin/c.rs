5dcbf1f38d168f6a7e2b7bb09d5338ae79e0dfc9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-08 00:13:33 +0900

    add abc361_c

--- abc361/src/bin/c.rs
+++ abc361/src/bin/c.rs
@@ -3,6 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.sort();
+    let mut ans = a[n - 1] - a[0];
+    for i in 0..=k {
+        if a[i + n - 1 - k] - a[i] < ans {
+            ans = a[i + n - 1 - k] - a[i];
+        }
+    }
+    println!("{}", ans);
 }
