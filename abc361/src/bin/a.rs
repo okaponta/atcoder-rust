aff0deacfb29cdfd857cc7485c6cc3b3ba254ccb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-07 23:04:26 +0900

    add abc361_a

--- abc361/src/bin/a.rs
+++ abc361/src/bin/a.rs
@@ -1,8 +1,13 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        x:usize,
+        mut a:[usize;n],
     }
-    println!("{}", n);
+    a.insert(k, x);
+    println!("{}", a.iter().join(" "));
 }
