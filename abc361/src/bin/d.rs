f2d973fd76e52d2a66d8037f170a23dfc298003d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-08 10:07:24 +0900

    add abc361_d

--- abc361/src/bin/d.rs
+++ abc361/src/bin/d.rs
@@ -1,8 +1,44 @@
-use proconio::input;
+use std::collections::{HashSet, VecDeque};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        mut s:Chars,
+        mut t:Chars,
     }
-    println!("{}", n);
+    if s == t {
+        println!("0");
+        return;
+    }
+    s.push('.');
+    s.push('.');
+    t.push('.');
+    t.push('.');
+    let mut q = VecDeque::new();
+    q.push_back((s.clone(), 0));
+    let mut set = HashSet::new();
+    set.insert(s.clone());
+    while let Some((s, i)) = q.pop_front() {
+        let dot = s.iter().position(|&c| c == '.').unwrap();
+        for j in 0..=n {
+            if j == dot || j == dot + 1 || j + 1 == dot {
+                continue;
+            }
+            let mut ss = s.clone();
+            ss.swap(j, dot);
+            ss.swap(j + 1, dot + 1);
+            if set.contains(&ss) {
+                continue;
+            }
+            if ss == t {
+                println!("{}", i + 1);
+                return;
+            }
+            set.insert(ss.clone());
+            q.push_back((ss, i + 1));
+        }
+    }
+    println!("{}", -1);
 }
