201a4607edef42638b6fd185411c0a2262cbada9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-09 10:16:02 +0900

    add abc361_f

--- abc361/src/bin/f.rs
+++ abc361/src/bin/f.rs
@@ -1,8 +1,25 @@
+use std::collections::HashSet;
+
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        n:u128,
     }
-    println!("{}", n);
+    let mut ans = n.sqrt();
+    let mut used = HashSet::new();
+    let mut i = 2;
+    while i * i * i <= n {
+        let mut tmp = i * i * i;
+        while tmp <= n {
+            if !used.contains(&tmp) && tmp.sqrt() * tmp.sqrt() != tmp {
+                ans += 1;
+                used.insert(tmp);
+            }
+            tmp *= i * i;
+        }
+        i += 1;
+    }
+    println!("{}", ans);
 }
