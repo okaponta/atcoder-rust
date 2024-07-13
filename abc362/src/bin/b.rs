d79936ee8a53a52d618a27f42cc0777d9203ea44
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-13 22:56:07 +0900

    add abc362_b

--- abc362/src/bin/b.rs
+++ abc362/src/bin/b.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        a:(i64,i64),
+        b:(i64,i64),
+        c:(i64,i64),
     }
-    println!("{}", n);
+    let ab = (b.0 - a.0, b.1 - a.1);
+    let bc = (c.0 - b.0, c.1 - b.1);
+    let ca = (a.0 - c.0, a.1 - c.1);
+    if is_ra(ab, bc) || is_ra(bc, ca) || is_ra(ca, ab) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn is_ra(a: (i64, i64), b: (i64, i64)) -> bool {
+    a.0 * b.0 + a.1 * b.1 == 0
 }
