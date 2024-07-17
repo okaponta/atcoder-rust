f0d089eebcf793f0464cc952eb55376cd8fe57ea
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-17 23:23:45 +0900

    add abc047_d

--- abc047/src/bin/d.rs
+++ abc047/src/bin/d.rs
@@ -1,8 +1,26 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n:usize,
+        _t:usize,
+        a:[usize;n],
     }
-    println!("{}", n);
+    let mut min = 0;
+    let mut ans = 0;
+    let mut heap = BinaryHeap::new();
+    heap.push(a[n - 1]);
+    for i in (0..n - 1).rev() {
+        let tmp = heap.peek().unwrap().saturating_sub(a[i]);
+        if min < tmp {
+            min = tmp;
+            ans = 1;
+        } else if min == tmp {
+            ans += 1;
+        }
+        heap.push(a[i]);
+    }
+    println!("{}", ans);
 }
