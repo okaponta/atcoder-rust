cb81cf44aa5433d7df16d371e5f9c2f004dc9176
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-20 22:49:49 +0900

    add abc363_c

--- abc363/src/bin/c.rs
+++ abc363/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for v in (0..n).into_iter().permutations(n) {
+        let target = v.iter().map(|&i| s[i]).collect::<Vec<_>>();
+        if set.contains(&target) {
+            continue;
+        }
+        let mut flg = true;
+        for j in 0..=n - k {
+            if (0..k).all(|i| target[j + i] == target[j + k - 1 - i]) {
+                flg = false;
+            }
+        }
+        if flg {
+            set.insert(target);
+        }
+    }
+    println!("{}", set.len());
 }
