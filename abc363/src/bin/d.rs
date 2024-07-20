9a00d138dc25c633aee96512920b901a2a593dd9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-20 22:49:50 +0900

    add abc363_d

--- abc363/src/bin/d.rs
+++ abc363/src/bin/d.rs
@@ -1,8 +1,37 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:usize,
     }
-    println!("{}", n);
+    if n <= 10 {
+        println!("{}", n - 1);
+        return;
+    }
+    n -= 10;
+    if n <= 9 {
+        println!("{}{}", n, n);
+        return;
+    }
+    let mut digit = 2;
+    let mut tmp = 9;
+    let mut ten = 1;
+    while tmp < n {
+        n -= tmp;
+        if digit % 2 == 0 {
+            tmp *= 10;
+            ten *= 10;
+        }
+        digit += 1;
+    }
+    ten -= 1;
+    n += ten;
+    let mut ans = vec!['0'; digit];
+    let nn = n.to_string().chars().collect::<Vec<_>>();
+    for i in 0..nn.len() {
+        ans[i] = nn[i];
+        ans[digit - 1 - i] = nn[i];
+    }
+    println!("{}", ans.iter().join(""));
 }
