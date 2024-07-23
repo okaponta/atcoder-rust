21d9f7c70a780612bb3b41b027ecf6c57afb6222
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-23 21:09:41 +0900

    add abc043_d

--- abc043/src/bin/d.rs
+++ abc043/src/bin/d.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n:usize,
+        s:Chars,
     }
-    println!("{}", n);
+    let n = s.len();
+    for i in 0..n - 1 {
+        if s[i] == s[i + 1] {
+            println!("{} {}", i + 1, i + 2);
+            return;
+        }
+    }
+    for i in 0..n - 2 {
+        if s[i] == s[i + 2] {
+            println!("{} {}", i + 1, i + 3);
+            return;
+        }
+    }
+    println!("-1 -1");
 }
