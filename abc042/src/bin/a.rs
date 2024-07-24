0937f941802e21cc159808bd4cddd933ae6c6409
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-24 18:04:40 +0900

    add abc042_a

--- abc042/src/bin/a.rs
+++ abc042/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:[usize;3],
     }
-    println!("{}", n);
+    n.sort();
+    println!(
+        "{}",
+        if n[0] == 5 && n[1] == 5 && n[2] == 7 {
+            "YES"
+        } else {
+            "NO"
+        }
+    );
 }
