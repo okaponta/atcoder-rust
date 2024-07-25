799f57f7cfc166e3a51119b0102466ca0bbc986d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-26 07:40:28 +0900

    add abc042_c

--- abc042/src/bin/c.rs
+++ abc042/src/bin/c.rs
@@ -1,8 +1,23 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n:usize,
+        mut n:usize,
+        k:usize,
+        d:[char;k],
+    }
+    let mut ngs = HashSet::new();
+    for d in d {
+        ngs.insert(d);
+    }
+    while ng(n, &ngs) {
+        n += 1;
     }
     println!("{}", n);
+}
+
+fn ng(n: usize, ngs: &HashSet<char>) -> bool {
+    n.to_string().chars().into_iter().any(|c| ngs.contains(&c))
 }
