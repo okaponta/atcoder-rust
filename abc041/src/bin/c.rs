2bdf0460361aa4facd85c9045cab3b1b75bee6af
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-07-27 00:38:52 +0900

    join abc041

---
+++ abc041/src/bin/c.rs
@@ -0,0 +1,12 @@
+use itertools::*;
+use proconio::{marker::*, *};
+use superslice::*;
+
+fn main() {
+    input! {
+        n:usize,
+        s:Chars,
+    }
+    let mut ans = 0;
+    println!("{}", ans);
+}
