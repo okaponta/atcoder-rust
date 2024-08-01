d198310e56a40b923c17cf0ddc5022cdb872ee67
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-02 00:54:45 +0900

    add abc040_c

--- abc040/src/bin/c.rs
+++ abc040/src/bin/c.rs
@@ -1,5 +1,6 @@
 #[allow(unused)]
 use itertools::*;
+#[allow(unused)]
 use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
@@ -8,8 +9,19 @@ fn main() {
     input! {
         n:usize,
         a:[usize;n],
-        s:Chars,
     }
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut dp = vec![0; n];
+    dp[1] = abs(a[0], a[1]);
+    for i in 2..n {
+        dp[i] = (dp[i - 1] + abs(a[i - 1], a[i])).min(dp[i - 2] + abs(a[i - 2], a[i]));
+    }
+    println!("{}", dp[n - 1]);
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
