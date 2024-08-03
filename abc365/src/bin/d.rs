d24362441c039ea1a7dc07ee19fda5379f747cdc
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-03 22:46:08 +0900

    add abc365_d

--- abc365/src/bin/d.rs
+++ abc365/src/bin/d.rs
@@ -8,10 +8,24 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut dp = vec![vec![-1 << 30; 3]; n + 1];
+    dp[0][0] = 0i64;
+    dp[0][1] = 0;
+    dp[0][2] = 0;
+    for i in 0..n {
+        if s[i] == 'R' {
+            dp[i + 1][2] = dp[i][0].max(dp[i][1]) + 1;
+            dp[i + 1][0] = dp[i][1].max(dp[i][2]);
+        } else if s[i] == 'S' {
+            dp[i + 1][0] = dp[i][1].max(dp[i][2]) + 1;
+            dp[i + 1][1] = dp[i][0].max(dp[i][2]);
+        } else {
+            dp[i + 1][1] = dp[i][0].max(dp[i][2]) + 1;
+            dp[i + 1][2] = dp[i][0].max(dp[i][1]);
+        }
+    }
+    let ans = dp[n][0].max(dp[n][1]).max(dp[n][2]);
     println!("{}", ans);
 }
