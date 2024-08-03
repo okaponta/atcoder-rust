d3c306b3a2316ffb12c9c8deeb23a9994739dd08
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-03 22:46:07 +0900

    add abc365_c

--- abc365/src/bin/c.rs
+++ abc365/src/bin/c.rs
@@ -8,10 +8,30 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut lower = 0;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(med, m, n, &a) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    if upper == 1 << 60 {
+        println!("infinite");
+        return;
+    }
+    println!("{}", lower);
+}
+
+fn is_ok(med: usize, m: usize, n: usize, a: &Vec<usize>) -> bool {
+    let mut tmp = 0;
+    for i in 0..n {
+        tmp += a[i].min(med);
+    }
+    tmp <= m
 }
