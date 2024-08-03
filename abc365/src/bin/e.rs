656d6e9199ce0f371db7a0575663cf51d03b337a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-03 22:46:08 +0900

    add abc365_e

--- abc365/src/bin/e.rs
+++ abc365/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,24 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut ans = 0i64;
+    for i in 0..30 {
+        let mut cnt = 0;
+        let mut zero = 0;
+        let mut one = 0;
+        for j in 0..n {
+            if a[j] >> i & 1 == 0 {
+                cnt += one;
+                zero += 1;
+            } else {
+                cnt += zero;
+                swap(&mut zero, &mut one);
+                one += 1;
+            }
+        }
+        ans += (1 << i) * cnt;
+    }
     println!("{}", ans);
 }
