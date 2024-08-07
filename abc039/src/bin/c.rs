26206599ee173eda14b2d698bb89de71d2b1ed51
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-07 22:51:48 +0900

    add abc039_c

--- abc039/src/bin/c.rs
+++ abc039/src/bin/c.rs
@@ -7,11 +7,17 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let t = "WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW"
+        .chars()
+        .collect::<Vec<_>>();
+    let w = vec![0, 2, 4, 5, 7, 9, 11];
+    let ans = vec!["Do", "Re", "Mi", "Fa", "So", "La", "Si"];
+    for i in 0..7 {
+        if (0..20).all(|j| s[j] == t[w[i] + j]) {
+            println!("{}", ans[i]);
+            return;
+        }
+    }
 }
