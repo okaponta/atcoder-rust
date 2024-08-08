b825d55c6c043f8c1cc890b2a45053fd83b80dc5
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-08 22:53:23 +0900

    add abc039_d

--- abc039/src/bin/d.rs
+++ abc039/src/bin/d.rs
@@ -5,13 +5,61 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = vec![vec!['.'; w]; h];
+    let d = vec![
+        (!0, !0),
+        (!0, 0),
+        (!0, 1),
+        (0, !0),
+        (0, 0),
+        (0, 1),
+        (1, !0),
+        (1, 0),
+        (1, 1),
+    ];
+    for i in 0..h {
+        for j in 0..w {
+            let mut c = 0;
+            for &(di, dj) in &d {
+                let ni = i.wrapping_add(di);
+                let nj = j.wrapping_add(dj);
+                if h <= ni || w <= nj || s[ni][nj] == '#' {
+                    c += 1;
+                }
+            }
+            if c == 9 {
+                ans[i][j] = '#';
+            }
+        }
+    }
+    let mut t = vec![vec!['.'; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if ans[i][j] == '#' {
+                for &(di, dj) in &d {
+                    let ni = i.wrapping_add(di);
+                    let nj = j.wrapping_add(dj);
+                    if h <= ni || w <= nj {
+                        continue;
+                    }
+                    t[ni][nj] = '#';
+                }
+            }
+        }
+    }
+    if s != t {
+        println!("impossible");
+        return;
+    }
+    println!("possible");
+    for i in 0..h {
+        println!("{}", ans[i].iter().join(""));
+    }
 }
