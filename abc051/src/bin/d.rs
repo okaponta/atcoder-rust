c869f073d0700baf0160973b94a0289aeba536e3
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-09 02:33:44 +0900

    add abc051_d

--- abc051/src/bin/d.rs
+++ abc051/src/bin/d.rs
@@ -1,8 +1,81 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n:usize,
+        m:usize,
+        abc:[(Usize1,Usize1,usize);m],
     }
-    println!("{}", n);
+    let mut g = vec![vec![]; n];
+    for (a, b, c) in abc {
+        g[a].push((b, c));
+        g[b].push((a, c));
+    }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        let d = Dijkstra::new(n, &g, i);
+        for j in 0..n {
+            if d.parent[j] != 1 << 60 {
+                set.insert((j.min(d.parent[j]), j.max(d.parent[j])));
+            }
+        }
+    }
+    println!("{}", m - set.len());
+}
+
+// 計算量は(E+V)logV
+pub struct Dijkstra {
+    distance: Vec<usize>,
+    parent: Vec<usize>,
+}
+
+impl Dijkstra {
+    // n:usize 頂点の数
+    // edges: Vec<Vec<(usize,usize)>> edge[i] = [(2,3), (3,1), (頂点への道,重み)]
+    // init:usize どの頂点を起点に考えるか
+    pub fn new(n: usize, edges: &Vec<Vec<(usize, usize)>>, init: usize) -> Self {
+        const INF: usize = 1 << 60;
+        let mut distance = vec![INF; n];
+        let mut parent = vec![INF; n];
+        let mut heap = std::collections::BinaryHeap::new();
+        for i in 0..n {
+            if i == init {
+                heap.push((std::cmp::Reverse(0), i));
+            }
+            heap.push((std::cmp::Reverse(INF), i));
+        }
+        while let Some((std::cmp::Reverse(d), target)) = heap.pop() {
+            if distance[target] < d {
+                continue;
+            }
+            distance[target] = d;
+            for &(next, cost) in &edges[target] {
+                if distance[next] > d + cost {
+                    distance[next] = d + cost;
+                    heap.push((std::cmp::Reverse(distance[next]), next));
+                    parent[next] = target;
+                }
+            }
+        }
+        Self { distance, parent }
+    }
+
+    pub fn distance(&self, target: usize) -> usize {
+        self.distance[target]
+    }
+
+    pub fn get_path(&self, target: usize) -> Vec<usize> {
+        const INF: usize = 1 << 60;
+        let mut current = target;
+        let mut res = vec![current];
+        while self.parent[current] != INF as usize {
+            let next = self.parent[current];
+            res.push(next);
+            current = next;
+        }
+        res.reverse();
+        res
+    }
 }
