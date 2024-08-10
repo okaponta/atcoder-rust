c72ebc1a7d3516a0eee0546e62a0ed086f19b71a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-10 23:12:36 +0900

    add abc366_e

--- abc366/src/bin/e.rs
+++ abc366/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,43 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        d:i64,
+        mut xy:[(i64,i64);n],
     }
-    #[allow(unused_mut)]
+    let mut x = vec![];
+    let mut y = vec![];
+    for &(xi, yi) in &xy {
+        x.push(xi);
+        y.push(yi);
+    }
+    let xv = f(x, d, n as i64);
+    let mut yv = f(y, d, n as i64);
+    yv.sort();
     let mut ans = 0;
+    for xx in xv {
+        ans += yv.upper_bound(&(d - xx));
+    }
     println!("{}", ans);
+}
+
+fn f(x: Vec<i64>, d: i64, n: i64) -> Vec<i64> {
+    let mut tmp = 0;
+    let mut map = HashMap::new();
+    for &xi in &x {
+        tmp += (-2_000_010 - xi).abs();
+        *map.entry(xi).or_insert(0) += 1;
+    }
+    let mut plus = 0;
+    let mut minus = n;
+    let mut v = vec![];
+    for i in -2_000_010..2_000_010 {
+        if tmp <= d {
+            v.push(tmp);
+        }
+        let diff = map.get(&i).unwrap_or(&0);
+        plus += diff;
+        minus -= diff;
+        tmp += plus - minus;
+    }
+    v
 }
