16e83202d808e35e949c016b2c3ff47f37be0072
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-14 12:16:17 +0900

    add abc364_b

--- abc364/src/bin/b.rs
+++ abc364/src/bin/b.rs
@@ -7,11 +7,32 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        si:Usize1,
+        sj:Usize1,
+        c:[Chars;h],
+        x:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut i = si;
+    let mut j = sj;
+    for x in x {
+        let mut ni = i;
+        let mut nj = j;
+        if x == 'L' {
+            nj = nj.wrapping_add(!0);
+        } else if x == 'R' {
+            nj += 1;
+        } else if x == 'U' {
+            ni = ni.wrapping_add(!0);
+        } else {
+            ni += 1;
+        }
+        if h <= ni || w <= nj || c[ni][nj] == '#' {
+            continue;
+        }
+        i = ni;
+        j = nj;
+    }
+    println!("{} {}", i + 1, j + 1);
 }
