932836fbc52ca348ff59280ff8ad66686669a71d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-16 14:33:12 +0900

    add abc364_d

--- abc364/src/bin/d.rs
+++ abc364/src/bin/d.rs
@@ -5,13 +5,28 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        mut a:[i64;n],
+        bk:[(i64,usize);q],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    a.sort();
+    for (b, k) in bk {
+        let mut lower = -1;
+        let mut upper = 1 << 60;
+        while 1 < upper - lower {
+            let mid = (lower + upper) / 2;
+            let l = a.lower_bound(&(b - mid));
+            let r = a.upper_bound(&(b + mid));
+            if k <= r - l {
+                upper = mid;
+            } else {
+                lower = mid;
+            }
+        }
+        println!("{}", upper);
+    }
 }
