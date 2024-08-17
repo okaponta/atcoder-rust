25d7c789555f25cd1afe2ca0428cb0d807469279
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-17 22:50:39 +0900

    add abc367_d

--- abc367/src/bin/d.rs
+++ abc367/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,26 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut dp = vec![0; m];
+    let mut s = 0;
+    let mut q = VecDeque::new();
+    let mut ans = 0usize;
+    for i in 0..n - 1 {
+        s += a[i];
+        dp[s % m] += 1;
+        q.push_back(s % m);
+    }
+    let mut s2 = 0;
+    for i in 0..n {
+        ans += dp[s2 % m];
+        s += a[(n + i - 1) % n];
+        q.push_back(s % m);
+        dp[s % m] += 1;
+        s2 += a[i];
+        dp[q.pop_front().unwrap()] -= 1;
+    }
     println!("{}", ans);
 }
