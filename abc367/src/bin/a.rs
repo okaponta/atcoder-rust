0e26b33cc9c5696f9095c951e215d4386dbe15b9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-17 22:50:35 +0900

    add abc367_a

--- abc367/src/bin/a.rs
+++ abc367/src/bin/a.rs
@@ -7,11 +7,20 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:usize,
+        mut b:usize,
+        c:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    if b < c {
+        b += 24;
+    }
+    if c < a && a < b {
+        println!("Yes");
+        return;
+    }
+    if c < a + 24 && a + 24 < b {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
