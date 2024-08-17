c337c78da44a021ada2f0f1d244803510136f14e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-18 00:09:41 +0900

    add abc364_e

--- abc364/src/bin/e.rs
+++ abc364/src/bin/e.rs
@@ -1,17 +1,31 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        x:usize,
+        y:usize,
+        ab:[(usize,usize);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut dp = vec![vec![1 << 60; x + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..n {
+        let (a, b) = ab[i];
+        for j in (0..=i).rev() {
+            for k in 0..x {
+                if x < k + a {
+                    break;
+                }
+                dp[j + 1][k + a] = dp[j + 1][k + a].min(dp[j][k] + b);
+            }
+        }
+    }
+    for i in (0..=n).rev() {
+        for j in 0..=x {
+            if dp[i][j] <= y {
+                println!("{}", (i + 1).min(n));
+                return;
+            }
+        }
+    }
 }
