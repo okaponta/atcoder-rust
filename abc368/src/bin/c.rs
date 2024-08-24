9a56c76188da475af6cb7b4c9c95004b7fabd0c7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-24 22:44:16 +0900

    add abc368_c

--- abc368/src/bin/c.rs
+++ abc368/src/bin/c.rs
@@ -8,10 +8,37 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut t = 0;
+    for i in 0..n {
+        t += 1;
+        t = f(t, h[i]);
+    }
+    println!("{t}");
+}
+
+fn f(t: usize, h: usize) -> usize {
+    let mut lower = t - 1;
+    let mut upper = 1 << 60;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if h <= g(t, med) {
+            upper = med;
+        } else {
+            lower = med;
+        }
+    }
+    upper
+}
+
+fn g(a: usize, b: usize) -> usize {
+    h(b) - h(a - 1)
+}
+
+fn h(a: usize) -> usize {
+    let t = a / 3;
+    let mut res = a - t;
+    res += 3 * t;
+    res
 }
