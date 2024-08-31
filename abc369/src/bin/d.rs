69bf2f310ab83faffadc00212891630f628bf194
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-08-31 22:47:05 +0900

    add abc369_d

--- abc369/src/bin/d.rs
+++ abc369/src/bin/d.rs
@@ -8,10 +8,15 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[i64;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut dp = vec![-1 << 60; 2];
+    dp[1] = 0;
+    for a in a {
+        let mut next = dp.clone();
+        next[0] = next[0].max(dp[1] + a);
+        next[1] = next[1].max(dp[0] + a * 2);
+        dp = next;
+    }
+    println!("{}", dp[0].max(dp[1]));
 }
