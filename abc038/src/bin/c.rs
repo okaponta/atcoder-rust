ec32d85f4e87349501a7177945e682987d4e7a15
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-02 01:50:48 +0900

    add abc038_c

--- abc038/src/bin/c.rs
+++ abc038/src/bin/c.rs
@@ -8,10 +8,18 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut ans = n;
+    let mut count = 0;
+    for i in 1..n {
+        if a[i - 1] < a[i] {
+            count += 1;
+        } else {
+            ans += count * (count + 1) / 2;
+            count = 0;
+        }
+    }
+    ans += count * (count + 1) / 2;
     println!("{}", ans);
 }
