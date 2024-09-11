a453fc68e06ea734490b578bf7dcb1b147e47d0d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-12 00:38:22 +0900

    add abc370_e

--- abc370/src/bin/e.rs
+++ abc370/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -5,13 +7,34 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+const MOD: usize = 998244353;
+
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:i64,
+        a:[i64;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let s = ruiseki(&a);
+    let mut map = HashMap::new();
+    map.insert(0, 1);
+    let mut sum = vec![1];
+    for i in 0..n {
+        let mut tmp = sum[i];
+        if let Some(&v) = map.get(&(s[i + 1] - k)) {
+            tmp = (MOD + tmp - v) % MOD;
+        }
+        *map.entry(s[i + 1]).or_insert(0) += tmp;
+        *map.entry(s[i + 1]).or_insert(0) %= MOD;
+        sum.push((sum[i] + tmp) % MOD);
+    }
+    println!("{}", (MOD + sum[n] - sum[n - 1]) % MOD);
+}
+
+fn ruiseki(a: &Vec<i64>) -> Vec<i64> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
