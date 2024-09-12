16bf5c9ab0e99346cbfa8dee4bfa366995535f33
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-13 07:56:30 +0900

    add abc035_b

--- abc035/src/bin/b.rs
+++ abc035/src/bin/b.rs
@@ -1,9 +1,34 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-       a:i32,
+       s:Chars,
+       t:u8,
     }
-    println!("{}", a);
-    todo!();
+    let mut x = 0i64;
+    let mut y = 0i64;
+    let mut q = 0;
+    for c in s {
+        if c == 'U' {
+            x += 1;
+        } else if c == 'D' {
+            x -= 1;
+        } else if c == 'L' {
+            y -= 1;
+        } else if c == 'R' {
+            y += 1;
+        } else {
+            q += 1;
+        }
+    }
+    let d = x.abs() + y.abs();
+    if t == 1 {
+        println!("{}", d + q);
+    } else {
+        if q < d {
+            println!("{}", d - q);
+        } else {
+            println!("{}", (q - d) % 2);
+        }
+    }
 }
