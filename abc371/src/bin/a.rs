19e0456509fc5eca2b8dc9c430a662e5f930333f
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-15 22:29:17 +0900

    add abc371_a

--- abc371/src/bin/a.rs
+++ abc371/src/bin/a.rs
@@ -7,11 +7,21 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        sab:char,
+        sac:char,
+        sbc:char,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    for v in (0..3).into_iter().permutations(3) {
+        if f(v[0], v[1], sab) && f(v[0], v[2], sac) && f(v[1], v[2], sbc) {
+            println!(
+                "{}",
+                (b'A' + v.iter().position(|&i| i == 1).unwrap() as u8) as char
+            );
+            return;
+        }
+    }
+}
+
+fn f(a: u8, b: u8, c: char) -> bool {
+    (a < b && c == '<') || (a > b && c == '>')
 }
