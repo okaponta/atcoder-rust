2fca842bf05a47f010b0728f703a71d7a95795e5
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-21 00:04:12 +0900

    add abc035_c

--- abc035/src/bin/c.rs
+++ abc035/src/bin/c.rs
@@ -1,9 +1,20 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-       a:i32,
+       n:usize,
+       q:usize,
+       lr:[(Usize1,usize);q],
     }
-    println!("{}", a);
-    todo!();
+    let mut flip = vec![false; n + 1];
+    for (l, r) in lr {
+        flip[l] = !flip[l];
+        flip[r] = !flip[r];
+    }
+    let mut ans = vec![if flip[0] { 1u8 } else { 0 }];
+    for i in 1..n {
+        ans.push(if flip[i] { 1 - ans[i - 1] } else { ans[i - 1] });
+    }
+    println!("{}", ans.iter().join(""));
 }
