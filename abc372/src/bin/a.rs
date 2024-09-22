531d9230f5d9b6def99372b616092bc55cb7c126
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-22 23:57:00 +0900

    add abc372_a

--- abc372/src/bin/a.rs
+++ abc372/src/bin/a.rs
@@ -7,11 +7,8 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let ans = s.into_iter().filter(|&c| c != '.').collect::<String>();
     println!("{}", ans);
 }
