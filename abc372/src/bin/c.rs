e77a659c082cdc95b4ed1be2a7006a532f3abbff
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-23 01:52:06 +0900

    add abc372_c

--- abc372/src/bin/c.rs
+++ abc372/src/bin/c.rs
@@ -5,13 +5,31 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        mut s:Chars,
+        xc:[(Usize1,char);q],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = (0..n - 2).into_iter().filter(|&i| f(&s, i)).count();
+    s.insert(0, 'Z');
+    s.insert(0, 'Z');
+    s.push('Z');
+    s.push('Z');
+    for (x, c) in xc {
+        if f(&s, x + 2) || f(&s, x + 1) || f(&s, x) {
+            ans -= 1;
+        }
+        s[x + 2] = c;
+        if f(&s, x + 2) || f(&s, x + 1) || f(&s, x) {
+            ans += 1;
+        }
+        println!("{}", ans);
+    }
+}
+
+fn f(s: &Vec<char>, i: usize) -> bool {
+    s[i] == 'A' && s[i + 1] == 'B' && s[i + 2] == 'C'
 }
