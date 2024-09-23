4780fe2a7c68d055f1eb181de886b3e438cac751
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-24 00:17:27 +0900

    add abc372_d

--- abc372/src/bin/d.rs
+++ abc372/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,31 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let ih = h
+        .iter()
+        .enumerate()
+        .sorted_by_key(|k| k.1)
+        .rev()
+        .collect::<Vec<_>>();
+    let mut diff = vec![0; n + 1];
+    let mut set = BTreeSet::new();
+    for (i, _) in ih {
+        diff[*set.range(..i).last().unwrap_or(&0)] += 1;
+        diff[i] -= 1;
+        set.insert(i);
+    }
+    let mut ans = ruiseki(&diff);
+    ans.remove(0);
+    ans.pop();
+    println!("{}", ans.iter().join(" "));
+}
+
+fn ruiseki(a: &Vec<i64>) -> Vec<i64> {
+    let mut res = vec![0];
+    for i in 0..a.len() {
+        res.push(res[i] + a[i]);
+    }
+    res
 }
