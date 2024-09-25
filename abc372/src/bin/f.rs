bd009e59c47a2644b049ce3f81bdf60156687b93
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-26 01:10:56 +0900

    add abc372_f

--- abc372/src/bin/f.rs
+++ abc372/src/bin/f.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -5,13 +7,42 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+const MOD: usize = 998244353;
+
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        k:usize,
+        xy:[(Usize1,Usize1);m],
     }
-    #[allow(unused_mut)]
+    let mut q = (0..n).into_iter().map(|_i| 0usize).collect::<VecDeque<_>>();
+    q[0] += 1;
+    let mut set = HashSet::new();
+    let mut g = vec![vec![]; n];
+    for (x, y) in xy {
+        g[x].push(y);
+        set.insert(x);
+    }
+    for _ in 0..k {
+        let mut add = vec![];
+        for &i in &set {
+            if q[i] != 0 {
+                for &next in &g[i] {
+                    add.push((next, q[i]));
+                }
+            }
+        }
+        q.rotate_right(1);
+        for (i, v) in add {
+            q[i] += v;
+            q[i] %= MOD;
+        }
+    }
     let mut ans = 0;
-    println!("{}", ans);
+    while let Some(v) = q.pop_front() {
+        ans += v;
+        ans %= MOD;
+    }
+    println!("{ans}");
 }
