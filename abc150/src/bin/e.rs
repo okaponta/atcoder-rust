91e5c13491d006403d6f9bc289f2464651ffa39b
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-27 00:55:09 +0900

    add abc150_e

--- abc150/src/bin/e.rs
+++ abc150/src/bin/e.rs
@@ -5,13 +5,37 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut c:[usize;n],
     }
-    #[allow(unused_mut)]
+    if n == 1 {
+        println!("{}", c[0] * 2 % MOD);
+        return;
+    }
+    c.sort();
+    c.reverse();
+    let b = pow(2, n - 1, MOD);
     let mut ans = 0;
-    println!("{}", ans);
+    for i in 0..n {
+        ans += (((i + 2) * b) % MOD) * c[i];
+        ans %= MOD;
+    }
+    println!("{}", (ans * b) % MOD);
+}
+
+fn pow(mut x: usize, mut n: usize, modulo: usize) -> usize {
+    x %= modulo;
+    let mut ret = if x == 0 { 0 } else { 1 };
+    while n > 0 {
+        if n & 1 == 1 {
+            ret = ret * x % modulo;
+        }
+        x = x * x % modulo;
+        n >>= 1;
+    }
+    ret
 }
