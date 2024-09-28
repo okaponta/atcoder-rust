a2c0e6b7b97f00ebda1aea27612a17635f945ed4
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-28 22:43:59 +0900

    add abc373_d

--- abc373/src/bin/d.rs
+++ abc373/src/bin/d.rs
@@ -8,10 +8,39 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        uvw:[(Usize1,Usize1,i64);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut g = vec![vec![]; n];
+    for &(u, v, w) in &uvw {
+        g[u].push((v, w));
+        g[v].push((u, -w));
+    }
+    let mut used = vec![false; n];
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        if used[i] {
+            continue;
+        }
+        dfs(i, i, 0, &mut used, &mut ans, &g);
+    }
+    println!("{}", ans.iter().join(" "));
+}
+
+fn dfs(
+    cur: usize,
+    _prev: usize,
+    cost: i64,
+    used: &mut Vec<bool>,
+    ans: &mut Vec<i64>,
+    g: &Vec<Vec<(usize, i64)>>,
+) {
+    ans[cur] = cost;
+    used[cur] = true;
+    for &next in &g[cur] {
+        if used[next.0] {
+            continue;
+        }
+        dfs(next.0, cur, cost + next.1, used, ans, g);
+    }
 }
