817a908f05c33b4989d286102cb9d3205e2ccccd
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-09-28 22:43:56 +0900

    add abc373_b

--- abc373/src/bin/b.rs
+++ abc373/src/bin/b.rs
@@ -7,11 +7,28 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
+    let mut pos = vec![];
+    for i in 0..26 {
+        let c = (b'A' + i) as char;
+        for j in 0..26 {
+            if s[j] == c {
+                pos.push(j);
+            }
+        }
+    }
     let mut ans = 0;
+    for i in 0..25 {
+        ans += abs(pos[i], pos[i + 1]);
+    }
     println!("{}", ans);
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
