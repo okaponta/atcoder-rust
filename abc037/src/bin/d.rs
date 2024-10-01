2d1f51f3a91ef3e97e171a6af35fbc0eb7a3e139
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-01 09:47:07 +0900

    add abc037_d

--- abc037/src/bin/d.rs
+++ abc037/src/bin/d.rs
@@ -5,13 +5,41 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        a:[[usize;w];h],
     }
-    #[allow(unused_mut)]
+    let mut ord = vec![];
+    for i in 0..h {
+        for j in 0..w {
+            ord.push((a[i][j], i, j));
+        }
+    }
+    ord.sort();
+    let mut dp = vec![vec![1; w]; h];
+    for (i, hi, wi) in ord {
+        for (dx, dy) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+            let nh = hi.wrapping_add(dx);
+            let nw = wi.wrapping_add(dy);
+            if h <= nh || w <= nw {
+                continue;
+            }
+            if a[nh][nw] < i {
+                dp[hi][wi] += dp[nh][nw];
+                dp[hi][wi] %= MOD;
+            }
+        }
+    }
     let mut ans = 0;
+    for i in 0..h {
+        for j in 0..w {
+            ans += dp[i][j];
+            ans %= MOD;
+        }
+    }
     println!("{}", ans);
 }
