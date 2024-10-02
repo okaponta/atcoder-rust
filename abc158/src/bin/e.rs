f88c4b60f44744a16dad6d99ae549525f05c2207
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-02 21:15:33 +0900

    add abc158_e

--- abc158/src/bin/e.rs
+++ abc158/src/bin/e.rs
@@ -1,17 +1,32 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        p:usize,
+        s:Chars,
     }
-    #[allow(unused_mut)]
     let mut ans = 0;
+    let mut dp = vec![0usize; p];
+    dp[0] += 1;
+    if p == 2 || p == 5 {
+        for i in 0..n {
+            let c = (s[i] as u8 - b'0') as usize;
+            if c % p == 0 {
+                ans += i + 1;
+            }
+        }
+        println!("{}", ans);
+        return;
+    }
+    let mut ten = 1;
+    let mut tmp = 0;
+    for i in (0..n).rev() {
+        let c = (s[i] as u8 - b'0') as usize;
+        tmp = (tmp + c * ten) % p;
+        ans += dp[tmp];
+        dp[tmp] += 1;
+        ten = (ten * 10) % p;
+    }
     println!("{}", ans);
 }
