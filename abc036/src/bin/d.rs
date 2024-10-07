c91de8705bc7a584d3dc4b8b72038a7758df2783
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-07 10:23:39 +0900

    add abc036_d

--- abc036/src/bin/d.rs
+++ abc036/src/bin/d.rs
@@ -5,13 +5,32 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        ab:[(Usize1,Usize1);n-1],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let ans = dfs(0, 0, &g);
+    println!("{}", (ans.0 + ans.1) % MOD);
+}
+
+fn dfs(cur: usize, prev: usize, g: &Vec<Vec<usize>>) -> (usize, usize) {
+    let mut tw = 1;
+    let mut tb = 1;
+    for &next in &g[cur] {
+        if next == prev {
+            continue;
+        }
+        let (w, b) = dfs(next, cur, g);
+        tw = tw * (w + b) % MOD;
+        tb = tb * w % MOD;
+    }
+    (tw, tb)
 }
