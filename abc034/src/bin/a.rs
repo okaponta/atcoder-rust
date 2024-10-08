d68224a7080cf81333ee559044a1207984a23ab1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-08 22:04:14 +0900

    add abc034_a

--- abc034/src/bin/a.rs
+++ abc034/src/bin/a.rs
@@ -7,11 +7,8 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        x:usize,
+        y:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    println!("{}", if x < y { "Better" } else { "Worse" });
 }
