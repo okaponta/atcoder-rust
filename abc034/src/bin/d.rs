b34c4037d7e618dcada8c23342afa0bcab866837
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-11 00:37:48 +0900

    add abc034_d

--- abc034/src/bin/d.rs
+++ abc034/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,30 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        wp:[(i128,i128);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut lower = 0;
+    let mut upper = 10000000;
+    while upper - lower > 1 {
+        let med = (lower + upper) / 2;
+        if is_ok(med, &wp, k) {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", lower as f64 / 100000.0)
+}
+
+fn is_ok(t: i128, wp: &Vec<(i128, i128)>, k: usize) -> bool {
+    let mut heap = BinaryHeap::new();
+    for &(w, p) in wp {
+        heap.push(w * (p * 100000 - t));
+    }
+    let mut res = 0;
+    for _i in 0..k {
+        res += heap.pop().unwrap()
+    }
+    0 < res
 }
