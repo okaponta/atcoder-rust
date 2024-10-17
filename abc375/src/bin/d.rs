67cb04eb92300f0bd4c8d246b74455ec24c2f1e1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-18 00:17:28 +0900

    add abc375_d

--- abc375/src/bin/d.rs
+++ abc375/src/bin/d.rs
@@ -1,17 +1,30 @@
+fn main() {
+    input! {
+        s:Chars,
+    }
+    let mut map = HashMap::new();
+    for i in 0..s.len() {
+        map.entry(s[i]).or_insert(vec![]).push(i);
+    }
+    let mut ans = 0;
+    for (_, v) in map {
+        let n = v.len();
+        let mut tmp = 0;
+        let mut sum = v[0];
+        for i in 1..n {
+            tmp += i * (v[i] - 1) - sum;
+            sum += v[i];
+        }
+        ans += tmp;
+    }
+    println!("{}", ans);
+}
+
+use std::collections::HashMap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
 use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
-
-fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
-    }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
-}
