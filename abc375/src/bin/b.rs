e2c1a6f164f94af6a9a2f36613bc1651efa25506
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-17 23:39:55 +0900

    add abc375_b

--- abc375/src/bin/b.rs
+++ abc375/src/bin/b.rs
@@ -8,10 +8,17 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut xy:[(i64,i64);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    xy.push((0, 0));
+    xy.insert(0, (0, 0));
+    let ans = (0..=n)
+        .into_iter()
+        .map(|i| dist(xy[i], xy[i + 1]))
+        .sum::<f64>();
     println!("{}", ans);
+}
+
+fn dist((x1, y1): (i64, i64), (x2, y2): (i64, i64)) -> f64 {
+    (((x1 - x2).pow(2) + (y1 - y2).pow(2)) as f64).sqrt()
 }
