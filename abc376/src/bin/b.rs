a58034d6e08e90a773ee04e5e5aa588873b97743
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-22 12:07:44 +0900

    add abc376_b

--- abc376/src/bin/b.rs
+++ abc376/src/bin/b.rs
@@ -8,10 +8,28 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        ht:[(char,Usize1);q],
     }
-    #[allow(unused_mut)]
+    let mut l = 0;
+    let mut r = 1;
     let mut ans = 0;
+    for (h, t) in ht {
+        if h == 'L' {
+            if l.min(t) < r && r < l.max(t) {
+                ans += n + l.min(t) - l.max(t);
+            } else {
+                ans += l.max(t) - l.min(t);
+            }
+            l = t;
+        } else {
+            if r.min(t) < l && l < r.max(t) {
+                ans += n + r.min(t) - r.max(t);
+            } else {
+                ans += r.max(t) - r.min(t);
+            }
+            r = t;
+        }
+    }
     println!("{}", ans);
 }
