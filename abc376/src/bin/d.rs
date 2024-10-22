37a685028d8ac5fb9e654ee3ba6fb33812312deb
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-22 17:55:17 +0900

    add abc376_d

--- abc376/src/bin/d.rs
+++ abc376/src/bin/d.rs
@@ -8,10 +8,54 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+    }
+    let d = Dijkstra::new(n, &g).size;
+    if d == 1 << 60 {
+        println!("-1");
+    } else {
+        println!("{d}");
+    }
+}
+
+pub struct Dijkstra {
+    size: usize,
+}
+
+impl Dijkstra {
+    // n:usize 頂点の数
+    // edges: Vec<Vec<(usize,usize)>> edge[i] = [(2,3), (3,1), (頂点への道,重み)]
+    pub fn new(n: usize, edges: &Vec<Vec<usize>>) -> Self {
+        const INF: usize = 1 << 60;
+        let mut distance = vec![INF; n];
+        let mut heap = std::collections::BinaryHeap::new();
+        let mut size = INF;
+        for i in 0..n {
+            if i == 0 {
+                heap.push((std::cmp::Reverse(0), i));
+            }
+            heap.push((std::cmp::Reverse(INF), i));
+        }
+        while let Some((std::cmp::Reverse(d), target)) = heap.pop() {
+            if distance[target] < d {
+                continue;
+            }
+            distance[target] = d;
+            for &next in &edges[target] {
+                if next == 0 {
+                    size = size.min(d + 1);
+                }
+                if distance[next] > d + 1 {
+                    distance[next] = d + 1;
+                    heap.push((std::cmp::Reverse(distance[next]), next));
+                }
+            }
+        }
+        Self { size }
+    }
 }
