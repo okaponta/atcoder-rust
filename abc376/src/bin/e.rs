95f57025af41b3c07420065931a4fe486c281dc1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-23 08:01:32 +0900

    add abc376_e

--- abc376/src/bin/e.rs
+++ abc376/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -7,11 +9,36 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    for _i in 0..t {
+        solve();
+    }
+}
+
+fn solve() {
+    input! {n:usize,k:usize,mut a:[usize;n],b:[usize;n]}
+    let ord = (0..n)
+        .into_iter()
+        .sorted_by_key(|&i| a[i])
+        .collect::<Vec<_>>();
+    let mut maxa = 0;
+    let mut sumb = 0;
+    let mut heap = BinaryHeap::new();
+    for i in 0..k {
+        maxa = a[ord[i]];
+        sumb += b[ord[i]];
+        heap.push(b[ord[i]]);
+    }
+    let mut ans = maxa * sumb;
+    for i in k..n {
+        maxa = a[ord[i]];
+        if let Some(j) = heap.pop() {
+            sumb -= j;
+        }
+        heap.push(b[ord[i]]);
+        sumb += b[ord[i]];
+        ans = ans.min(maxa * sumb)
+    }
+    println!("{ans}");
 }
