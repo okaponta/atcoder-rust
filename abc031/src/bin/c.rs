63a757fb3e25fceaaad4f5ba2ca77b2a34462e2e
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-10-28 23:46:23 +0900

    add abc031_c

--- abc031/src/bin/c.rs
+++ abc031/src/bin/c.rs
@@ -8,10 +8,44 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[i64;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut s1 = vec![0];
+    let mut s2 = vec![0];
+    for i in 0..n {
+        if i % 2 == 0 {
+            s1.push(s1[i] + a[i]);
+            s2.push(s2[i]);
+        } else {
+            s1.push(s1[i]);
+            s2.push(s2[i] + a[i]);
+        }
+    }
+    let mut ans = -1 << 30;
+    for i in 0..n {
+        let mut tmp1 = -1 << 30;
+        let mut tmp2 = -1 << 30;
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            if i.min(j) % 2 == 0 {
+                let ao = s2[i.max(j) + 1] - s2[i.min(j)];
+                let ta = s1[i.max(j) + 1] - s1[i.min(j)];
+                if tmp2 < ao {
+                    tmp1 = ta;
+                    tmp2 = ao;
+                }
+            } else {
+                let ao = s1[i.max(j) + 1] - s1[i.min(j)];
+                let ta = s2[i.max(j) + 1] - s2[i.min(j)];
+                if tmp2 < ao {
+                    tmp1 = ta;
+                    tmp2 = ao;
+                }
+            }
+        }
+        ans = ans.max(tmp1);
+    }
     println!("{}", ans);
 }
