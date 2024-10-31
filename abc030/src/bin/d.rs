cbce25805660c4dcdf971b48abf8b7fbf40476e9
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-01 00:06:34 +0900

    add abc030_d

--- abc030/src/bin/d.rs
+++ abc030/src/bin/d.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
+
 #[allow(unused)]
 use itertools::*;
+use num::BigInt;
 #[allow(unused)]
 use proconio::{marker::*, *};
 #[allow(unused)]
@@ -8,10 +11,41 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:Usize1,
+        k:String,
+        b:[Usize1;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let bk = BigInt::from_str(&k).unwrap();
+    let ans = index_with_loop(a, bk, b);
+    println!("{}", ans + 1);
+}
+
+fn index_with_loop(init: usize, k: BigInt, b: Vec<usize>) -> usize {
+    let mut v = vec![];
+    let mut set = std::collections::HashSet::new();
+    let mut tmp = init;
+    while !set.contains(&tmp) {
+        v.push(tmp);
+        set.insert(tmp);
+        tmp = b[tmp];
+    }
+    if k < usize_to_bint(v.len()) {
+        return v[bint_to_usize(k)];
+    }
+    let offset = v.iter().position(|&x| x == tmp).unwrap();
+    let l = usize_to_bint(v.len() - offset);
+    let idx = offset + (k - offset) % l;
+    v[bint_to_usize(idx)]
+}
+
+fn usize_to_bint(a: usize) -> BigInt {
+    BigInt::new(num_bigint::Sign::Plus, vec![a as u32])
+}
+
+fn bint_to_usize(a: BigInt) -> usize {
+    if a.to_u32_digits().0 == num_bigint::Sign::NoSign {
+        0
+    } else {
+        a.to_u32_digits().1[0] as usize
+    }
 }
