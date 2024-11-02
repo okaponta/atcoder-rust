5d048ce11f6d3450ba8f6997b7df183637fa73b1
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-02 23:41:18 +0900

    add abc378_d

--- abc378/src/bin/d.rs
+++ abc378/src/bin/d.rs
@@ -7,11 +7,50 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        k:usize,
+        s:[Chars;h],
     }
-    #[allow(unused_mut)]
     let mut ans = 0;
+    let mut visited = vec![vec![false; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '#' {
+                continue;
+            }
+            visited[i][j] = true;
+            ans += dfs((i, j), h, w, k, &mut visited, &s);
+            visited[i][j] = false;
+        }
+    }
     println!("{}", ans);
+}
+
+fn dfs(
+    cur: (usize, usize),
+    h: usize,
+    w: usize,
+    k: usize,
+    used: &mut Vec<Vec<bool>>,
+    s: &Vec<Vec<char>>,
+) -> usize {
+    if k == 0 {
+        return 1;
+    }
+    let mut res = 0;
+    for (dx, dy) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+        let nx = cur.0.wrapping_add(dx);
+        let ny = cur.1.wrapping_add(dy);
+        if h <= nx || w <= ny {
+            continue;
+        }
+        if used[nx][ny] || s[nx][ny] == '#' {
+            continue;
+        }
+        used[nx][ny] = true;
+        res += dfs((nx, ny), h, w, k - 1, used, s);
+        used[nx][ny] = false;
+    }
+    res
 }
