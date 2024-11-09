e6df1e36fdf95156febb0b94f8d2f4b8db4b0d1a
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-09 22:49:17 +0900

    add abc379_d

--- abc379/src/bin/d.rs
+++ abc379/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -5,13 +7,32 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut t = 0;
+    let mut qu = VecDeque::new();
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            qu.push_back(t);
+        } else if qi == 2 {
+            input! {ti: usize}
+            t += ti;
+        } else {
+            input! {hi: usize}
+            let mut ans = 0;
+            while let Some(ti) = qu.pop_front() {
+                if hi <= t - ti {
+                    ans += 1;
+                } else {
+                    qu.push_front(ti);
+                    break;
+                }
+            }
+            println!("{ans}");
+        }
+    }
 }
