ec5bcd534e26a314613a0491cc8c0f121d037b94
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-09 22:49:18 +0900

    add abc379_e

--- abc379/src/bin/e.rs
+++ abc379/src/bin/e.rs
@@ -8,10 +8,26 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = vec![0; n];
+    let mut tmp = 0;
+    for i in 0..n {
+        tmp += (s[i].to_digit(10).unwrap() as usize) * (i + 1);
+        ans[i] = tmp;
+    }
+    let mut rev = vec![];
+    let mut rem = 0;
+    ans.reverse();
+    for i in 0..n {
+        let tmp = ans[i] + rem;
+        rev.push(tmp % 10);
+        rem = tmp / 10;
+    }
+    rev.reverse();
+    if 0 < rem {
+        println!("{}{}", rem, rev.iter().join(""));
+    } else {
+        println!("{}", rev.iter().join(""));
+    }
 }
