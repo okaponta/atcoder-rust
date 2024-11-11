68f32921193880c4043ee821b2a07336f12583f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-11 10:39:31 +0900

    add abc022_a

--- abc022/src/bin/a.rs
+++ abc022/src/bin/a.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        a:i32,
+        n:usize,
+        s:i64,
+        t:i64,
+        mut w:i64,
+        mut a:[i64;n-1],
     }
-    println!("{}", a);
-    todo!();
+    a.insert(0, 0);
+    let mut ans = 0;
+    for a in a {
+        w += a;
+        if s <= w && w <= t {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
