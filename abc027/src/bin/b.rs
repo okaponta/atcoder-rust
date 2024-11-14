912c925d83b141a4409331cf4f554d9d44bcecc2
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-14 19:41:24 +0900

    add abc027_b

--- abc027/src/bin/b.rs
+++ abc027/src/bin/b.rs
@@ -8,10 +8,36 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = 1 << 60;
+    for i in 0..=100 {
+        ans = ans.min(solve(i, n, &a));
+    }
+    if ans == 1 << 60 {
+        println!("-1");
+    } else {
+        println!("{ans}");
+    }
+}
+
+fn solve(k: usize, n: usize, a: &Vec<usize>) -> usize {
+    let mut tmp = 0;
+    let mut count = 0;
+    let mut res = 0;
+    for i in 0..n {
+        count += 1;
+        tmp += a[i];
+        if tmp == k * count {
+            tmp = 0;
+            count = 0;
+        } else {
+            res += 1;
+        }
+    }
+    if count != 0 {
+        1 << 60
+    } else {
+        res
+    }
 }
