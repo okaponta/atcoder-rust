2aa227fb4aa36bc8d99a511e1b6089df065d4c20
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-16 23:14:29 +0900

    add abc380_c

--- abc380/src/bin/c.rs
+++ abc380/src/bin/c.rs
@@ -7,11 +7,33 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        k:usize,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let is_ev = s[0] == '0';
+    let mut rl = run_length_encode(s);
+    if is_ev {
+        rl.swap(k * 2 - 1, k * 2 - 2);
+    } else {
+        rl.swap(k * 2 - 2, k * 2 - 3);
+    }
+    let mut ans = vec![];
+    for (c, cnt) in rl {
+        for _ in 0..cnt {
+            ans.push(c);
+        }
+    }
+    println!("{}", ans.iter().join(""));
+}
+
+fn run_length_encode<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
 }
