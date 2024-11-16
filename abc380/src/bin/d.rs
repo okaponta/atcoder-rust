900a364e928371818a3905dd3a05c0493bd8c77b
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-16 23:14:30 +0900

    add abc380_d

--- abc380/src/bin/d.rs
+++ abc380/src/bin/d.rs
@@ -7,11 +7,27 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
+        q:usize,
+        k:[Usize1;q],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let m = s.len();
+    let mut ans = vec![];
+    for k in k {
+        let kk = k / m;
+        if kk.count_ones() % 2 == 0 {
+            ans.push(s[k % m]);
+        } else {
+            ans.push(rev(s[k % m]));
+        }
+    }
+    println!("{}", ans.iter().join(" "));
+}
+
+fn rev(c: char) -> char {
+    if c.is_ascii_lowercase() {
+        c.to_ascii_uppercase()
+    } else {
+        c.to_ascii_lowercase()
+    }
 }
