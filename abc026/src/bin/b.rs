0ed3ba28a91fc07a4c573fea86f881a037bddd6d
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-20 20:59:35 +0900

    add abc026_b

--- abc026/src/bin/b.rs
+++ abc026/src/bin/b.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Reverse, f64::consts::PI};
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,13 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut r:[i64;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    r.sort_by_key(|&i| Reverse(i));
+    let ans = (0..n)
+        .into_iter()
+        .map(|i| (-1i64).pow(i as u32) * r[i] * r[i])
+        .sum::<i64>() as f64
+        * PI;
     println!("{}", ans);
 }
