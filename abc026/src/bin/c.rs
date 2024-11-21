3eccb92a9b4c3e5072cec87c1d3c1781861930d7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-21 09:03:26 +0900

    add abc026_c

--- abc026/src/bin/c.rs
+++ abc026/src/bin/c.rs
@@ -8,10 +8,26 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        b:[Usize1;n-1],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut g = vec![vec![]; n];
+    for i in 0..n - 1 {
+        g[b[i]].push(i + 1);
+    }
+    let ans = dfs(0, &g);
     println!("{}", ans);
+}
+
+fn dfs(cur: usize, g: &Vec<Vec<usize>>) -> usize {
+    if g[cur].len() == 0 {
+        return 1;
+    }
+    let mut min = 1 << 60;
+    let mut max = 0;
+    for &next in &g[cur] {
+        let n = dfs(next, g);
+        min = min.min(n);
+        max = max.max(n);
+    }
+    min + max + 1
 }
