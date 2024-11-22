b7a9683eeb3fbb715f6ab1f1356bcdd326ed3a9c
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-22 20:04:39 +0900

    add abc026_d

--- abc026/src/bin/d.rs
+++ abc026/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -7,11 +9,21 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:f64, b:f64, c:f64,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut lower = 0.0;
+    let mut upper = 200.0;
+    while upper - lower > 1e-12 {
+        let med = (lower + upper) / 2.0;
+        if f(med, a, b, c) < 100.0 {
+            lower = med;
+        } else {
+            upper = med;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn f(t: f64, a: f64, b: f64, c: f64) -> f64 {
+    a * t + b * (c * t * PI).sin()
 }
