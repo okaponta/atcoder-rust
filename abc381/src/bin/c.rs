3c5830b2ef70034d07d3458b1d0812d95bb79f21
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-25 17:52:45 +0900

    add abc381_c

--- abc381/src/bin/c.rs
+++ abc381/src/bin/c.rs
@@ -7,11 +7,30 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let rl = run_length_encode(s);
+    if rl.len() < 2 {
+        println!("1");
+        return;
+    }
+    let mut ans = 1;
+    for i in 0..rl.len() - 2 {
+        if rl[i].0 == '1' && rl[i + 1].0 == '/' && rl[i + 1].1 == 1 && rl[i + 2].0 == '2' {
+            ans = ans.max(rl[i].1.min(rl[i + 2].1) * 2 + 1);
+        }
+    }
     println!("{}", ans);
+}
+
+fn run_length_encode<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
 }
