8d4e7cb5275357315026cf7f4defb1ee91c7124f
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-28 23:26:51 +0900

    add abc025_b

--- abc025/src/bin/b.rs
+++ abc025/src/bin/b.rs
@@ -1,17 +1,29 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:i64,
+        b:i64,
+        sd:[(Chars,i64);n],
     }
-    #[allow(unused_mut)]
     let mut ans = 0;
-    println!("{}", ans);
+    for (s, d) in sd {
+        if s[0] == 'E' {
+            ans += f(d, a, b);
+        } else {
+            ans -= f(d, a, b);
+        }
+    }
+    if 0 < ans {
+        println!("East {}", ans);
+    } else if ans < 0 {
+        println!("West {}", -ans);
+    } else {
+        println!("0");
+    }
+}
+
+fn f(d: i64, a: i64, b: i64) -> i64 {
+    d.max(a).min(b)
 }
