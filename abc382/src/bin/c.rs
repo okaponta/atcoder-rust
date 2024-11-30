da6cd1583efe1ef625fd874507123a57b9332407
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-30 22:47:36 +0900

    add abc382_c

--- abc382/src/bin/c.rs
+++ abc382/src/bin/c.rs
@@ -5,13 +5,27 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        a:[usize;n],
+        b:[usize;m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = vec![0; 200_020];
+    let mut tmp = 200_010;
+    for i in 0..n {
+        while a[i] <= tmp {
+            ans[tmp] = i + 1;
+            tmp -= 1;
+        }
+    }
+    for b in b {
+        if ans[b] == 0 {
+            println!("-1");
+        } else {
+            println!("{}", ans[b]);
+        }
+    }
 }
