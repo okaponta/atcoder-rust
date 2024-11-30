450ff6304d247fc36c998f3d38f3b522685ba643
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-11-30 22:47:38 +0900

    add abc382_e

--- abc382/src/bin/e.rs
+++ abc382/src/bin/e.rs
@@ -8,10 +8,32 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        x:usize,
+        p:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut pp = vec![0.0; n + 1];
+    pp[0] = 1.0;
+    for &p in &p {
+        let p = p as f64 / 100.0;
+        for i in (0..n).rev() {
+            let rare = pp[i] * p;
+            pp[i + 1] += rare;
+            pp[i] -= rare;
+        }
+    }
+    let mut ans = vec![0.0; x + 1];
+    for i in (0..x).rev() {
+        let mut tmp = 1.0;
+        let mut sum = 0.0;
+        for j in 1..=n {
+            if x <= i + j {
+                break;
+            }
+            sum += pp[j] * ans[i + j];
+        }
+        tmp += sum;
+        tmp /= 1.0 - pp[0];
+        ans[i] = tmp;
+    }
+    println!("{}", ans[0]);
 }
