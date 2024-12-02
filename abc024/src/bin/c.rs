cda4358a75744bb482461fd0f10b0243c4e642f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-02 21:54:45 +0900

    add abc024_c

--- abc024/src/bin/c.rs
+++ abc024/src/bin/c.rs
@@ -1,17 +1,37 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        d:usize,
+        k:usize,
+        lr:[(Usize1,Usize1);d],
+        st:[(Usize1,Usize1);k],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    for (s, t) in st {
+        if s < t {
+            let mut left = s;
+            for i in 0..d {
+                if lr[i].0 <= left && left < lr[i].1 {
+                    left = lr[i].1;
+                    if t <= left {
+                        println!("{}", i + 1);
+                        break;
+                    }
+                }
+            }
+        } else {
+            let mut right = s;
+            for i in 0..d {
+                if lr[i].0 < right && right <= lr[i].1 {
+                    right = lr[i].0;
+                    if right <= t {
+                        println!("{}", i + 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
