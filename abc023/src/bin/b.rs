1b87864f7971cd479697666d1212b97445169db3
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-05 21:53:27 +0900

    add abc023_b

--- abc023/src/bin/b.rs
+++ abc023/src/bin/b.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,32 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    if n % 2 == 0 {
+        println!("-1");
+        return;
+    }
+    let mut q = VecDeque::new();
+    q.push_back('b');
+    let mut count = 0;
+    while q.len() < n {
+        if count % 3 == 0 {
+            q.push_front('a');
+            q.push_back('c');
+        } else if count % 3 == 1 {
+            q.push_front('c');
+            q.push_back('a');
+        } else {
+            q.push_front('b');
+            q.push_back('b');
+        }
+        count += 1;
+    }
+    let t = q.into_iter().collect::<Vec<_>>();
+    if s == t {
+        println!("{}", n / 2);
+    } else {
+        println!("-1");
+    }
 }
