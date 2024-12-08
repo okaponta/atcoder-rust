11a125ce81306654209d2a24ed6d9fdea1285119
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-08 23:31:36 +0900

    add abc383_b

--- abc383/src/bin/b.rs
+++ abc383/src/bin/b.rs
@@ -7,11 +7,48 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        d:usize,
+        s:[Chars;h],
     }
-    #[allow(unused_mut)]
+    let mut hum = vec![];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '.' {
+                hum.push((i, j));
+            }
+        }
+    }
+    let n = hum.len();
     let mut ans = 0;
+    for i in 0..n {
+        for j in 0..i {
+            let mut cnt = 0;
+            for ii in 0..h {
+                for jj in 0..w {
+                    if s[ii][jj] == '#' {
+                        continue;
+                    }
+                    if dist(hum[i], (ii, jj)) <= d || dist(hum[j], (ii, jj)) <= d {
+                        cnt += 1;
+                    }
+                }
+            }
+            ans = ans.max(cnt);
+        }
+    }
     println!("{}", ans);
+}
+
+fn dist(a: (usize, usize), b: (usize, usize)) -> usize {
+    abs(a.0, b.0) + abs(a.1, b.1)
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
