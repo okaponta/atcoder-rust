13cfe7cbdf0efae7d38d95b649d1d1131de94563
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-08 23:31:38 +0900

    add abc383_c

--- abc383/src/bin/c.rs
+++ abc383/src/bin/c.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -7,11 +9,45 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        d:usize,
+        s:[Chars;h],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut q = VecDeque::new();
+    let mut ans = vec![vec![false; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == 'H' {
+                q.push_back((i, j, d));
+                ans[i][j] = true;
+            }
+        }
+    }
+    while let Some((i, j, d)) = q.pop_front() {
+        if d == 0 {
+            continue;
+        }
+        for (di, dj) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+            let ni = i.wrapping_add(di);
+            let nj = j.wrapping_add(dj);
+            if h <= ni || w <= nj {
+                continue;
+            }
+            if ans[ni][nj] || s[ni][nj] == '#' {
+                continue;
+            }
+            q.push_back((ni, nj, d - 1));
+            ans[ni][nj] = true;
+        }
+    }
+    let mut cnt = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if ans[i][j] {
+                cnt += 1;
+            }
+        }
+    }
+    println!("{}", cnt);
 }
