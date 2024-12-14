996c4f34f98cfb80a85b6cbd3acfe2e8b810f160
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-14 22:47:55 +0900

    add abc384_d

--- abc384/src/bin/d.rs
+++ abc384/src/bin/d.rs
@@ -8,10 +8,26 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut s:i64,
+        mut a:[i64;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let sum = a.iter().sum::<i64>();
+    s %= sum;
+    for i in 0..n {
+        a.push(a[i]);
+    }
+    let mut tmp = 0;
+    let mut right = 0;
+    for left in 0..n {
+        while tmp + a[right] <= s {
+            tmp += a[right];
+            right += 1;
+        }
+        if tmp == s {
+            println!("Yes");
+            return;
+        }
+        tmp -= a[left];
+    }
+    println!("No");
 }
