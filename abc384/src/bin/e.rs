d7d054cf61570ddd4e649ca2eff48bb3a1f726e7
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-14 22:47:56 +0900

    add abc384_e

--- abc384/src/bin/e.rs
+++ abc384/src/bin/e.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -7,11 +9,46 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        x:u128,
+        p:Usize1,
+        q:Usize1,
+        s:[[u128;w];h],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut ans = s[p][q];
+    let mut heap = BinaryHeap::new();
+    let mut used = vec![vec![false; w]; h];
+    for (di, dj) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+        let ni = p.wrapping_add(di);
+        let nj = q.wrapping_add(dj);
+        if h <= ni || w <= nj {
+            continue;
+        }
+        heap.push(Reverse((s[ni][nj], ni, nj)));
+        used[ni][nj] = true;
+    }
+    used[p][q] = true;
+    while let Some(Reverse((p, i, j))) = heap.pop() {
+        if p * x < ans {
+            ans += p;
+            used[i][j] = true;
+            for (di, dj) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+                let ni = i.wrapping_add(di);
+                let nj = j.wrapping_add(dj);
+                if h <= ni || w <= nj {
+                    continue;
+                }
+                if used[ni][nj] {
+                    continue;
+                }
+                heap.push(Reverse((s[ni][nj], ni, nj)));
+                used[ni][nj] = true;
+            }
+        } else {
+            break;
+        }
+    }
+
     println!("{}", ans);
 }
