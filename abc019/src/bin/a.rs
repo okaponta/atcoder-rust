d5b05d7ac3e8e72201981edcf8ff84a402fcae85
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-15 18:45:37 +0900

    add abc019_a

--- abc019/src/bin/a.rs
+++ abc019/src/bin/a.rs
@@ -7,11 +7,8 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut a:[usize;3],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    a.sort();
+    println!("{}", a[1]);
 }
