191c2ba2890e8bbfaae127b89af8e813883e4256
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-15 18:44:59 +0900

    join abc019

---
+++ abc019/src/bin/d.rs
@@ -0,0 +1,17 @@
+#[allow(unused)]
+use itertools::*;
+#[allow(unused)]
+use proconio::{marker::*, *};
+#[allow(unused)]
+use superslice::*;
+
+fn main() {
+    input! {
+        n:usize,
+        _a:[usize;n],
+        _s:Chars,
+    }
+    #[allow(unused_mut)]
+    let mut ans = 0;
+    println!("{}", ans);
+}
