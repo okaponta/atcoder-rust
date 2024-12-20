cb59033606545061f103d5c14d2a5acaaf6802c6
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-21 02:56:49 +0900

    add abc023_c

--- abc023/src/bin/c.rs
+++ abc023/src/bin/c.rs
@@ -1,17 +1,38 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use std::collections::HashMap;
 
 fn main() {
     input! {
+        r:usize,
+        c:usize,
+        k:usize,
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        rc:[(Usize1,Usize1);n],
     }
-    #[allow(unused_mut)]
+    let mut rcnt = vec![0; r];
+    let mut ccnt = vec![0; c];
+    for &(i, j) in &rc {
+        rcnt[i] += 1;
+        ccnt[j] += 1;
+    }
+    let mut cmap = HashMap::new();
+    for i in 0..c {
+        *cmap.entry(ccnt[i]).or_insert(0usize) += 1;
+    }
     let mut ans = 0;
+    for i in 0..r {
+        if k < rcnt[i] {
+            continue;
+        }
+        ans += cmap.get(&(k - rcnt[i])).unwrap_or(&0);
+    }
+    for &(i, j) in &rc {
+        if rcnt[i] + ccnt[j] == k {
+            ans -= 1;
+        }
+        if rcnt[i] + ccnt[j] == k + 1 {
+            ans += 1;
+        }
+    }
     println!("{}", ans);
 }
