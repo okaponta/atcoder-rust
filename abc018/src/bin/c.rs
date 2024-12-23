21a5548e77cf2116819427fa22142ef10db160a0
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-23 21:23:52 +0900

    add abc018_c

--- abc018/src/bin/c.rs
+++ abc018/src/bin/c.rs
@@ -7,11 +7,54 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        r:usize,
+        c:usize,
+        k:usize,
+        s:[Chars;r],
     }
-    #[allow(unused_mut)]
+    let mut cnt = vec![vec![0; c]; r];
+    for i in 0..r {
+        for j in 0..c {
+            if s[i][j] == 'x' {
+                cnt[i][j] = k;
+            }
+        }
+    }
+    for i in 0..r {
+        for j in 1..c {
+            if 0 < cnt[i][j - 1] {
+                cnt[i][j] = cnt[i][j].max(cnt[i][j - 1] - 1);
+            }
+        }
+    }
+    for i in 0..r {
+        for j in (0..c - 1).rev() {
+            if 0 < cnt[i][j + 1] {
+                cnt[i][j] = cnt[i][j].max(cnt[i][j + 1] - 1);
+            }
+        }
+    }
+    for j in 0..c {
+        for i in 1..r {
+            if 0 < cnt[i - 1][j] {
+                cnt[i][j] = cnt[i][j].max(cnt[i - 1][j] - 1);
+            }
+        }
+    }
+    for j in 0..c {
+        for i in (0..r - 1).rev() {
+            if 0 < cnt[i + 1][j] {
+                cnt[i][j] = cnt[i][j].max(cnt[i + 1][j] - 1);
+            }
+        }
+    }
     let mut ans = 0;
+    for i in k - 1..=r - k {
+        for j in k - 1..=c - k {
+            if cnt[i][j] == 0 {
+                ans += 1;
+            }
+        }
+    }
     println!("{}", ans);
 }
