955f175683ecb9275fb9702d459dc2b0a1b205b5
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-28 11:13:28 +0900

    add abc017_b

--- abc017/src/bin/b.rs
+++ abc017/src/bin/b.rs
@@ -7,11 +7,23 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut x:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut tmp = false;
+    while let Some(c) = x.pop() {
+        if c == 'h' && !tmp {
+            tmp = true;
+        } else if c == 'c' && tmp {
+            tmp = false;
+        } else if c == 'o' || c == 'k' || c == 'u' {
+        } else {
+            println!("NO");
+            return;
+        }
+    }
+    if tmp {
+        println!("NO");
+    } else {
+        println!("YES");
+    }
 }
