ea68a498344a2cf6c53ae36c052a93e8e008ae19
Kohei Okamoto <okaponta.19910919@gmail.com>
2024-12-30 22:20:52 +0900

    add abc017_c

--- abc017/src/bin/c.rs
+++ abc017/src/bin/c.rs
@@ -8,10 +8,25 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        lrs:[(Usize1,usize,i64);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut imos = vec![0; m + 1];
+    let mut sum = 0;
+    for (l, r, s) in lrs {
+        sum += s;
+        imos[l] += s;
+        imos[r] -= s;
+    }
+    let rui = ruiseki(&imos);
+    println!("{}", sum - rui.iter().min().unwrap());
+}
+
+fn ruiseki(a: &Vec<i64>) -> Vec<i64> {
+    let mut res = vec![a[0]];
+    for i in 1..a.len() {
+        res.push(res[i - 1] + a[i]);
+    }
+    res.pop();
+    res
 }
