e5f7f598b8eb44bf6749110b5fff1fb06a6db399
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-01 17:58:36 +0900

    add abc385_c

--- abc385/src/bin/c.rs
+++ abc385/src/bin/c.rs
@@ -8,10 +8,19 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut ans = 1;
+    for i in 1..n {
+        let mut tmp = 1;
+        let mut dp = vec![1; n];
+        for j in 0..n {
+            if i <= j && h[j] == h[j - i] {
+                dp[j] = dp[j - i] + 1;
+            }
+            tmp = tmp.max(dp[j]);
+        }
+        ans = ans.max(tmp);
+    }
     println!("{}", ans);
 }
