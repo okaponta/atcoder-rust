17dde5af3746d8f93f0b4fca6c0f01e47a1c5ba8
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-02 04:40:12 +0900

    add abc385_e

--- abc385/src/bin/e.rs
+++ abc385/src/bin/e.rs
@@ -1,17 +1,31 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        uv:[(Usize1,Usize1);n-1],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut d1 = vec![0; n];
+    let mut g = vec![vec![]; n];
+    for (u, v) in uv {
+        g[u].push(v);
+        g[v].push(u);
+        d1[u] += 1;
+        d1[v] += 1;
+    }
+    let mut ans = n;
+    for i in 0..n {
+        let mut tmp = vec![];
+        for &j in &g[i] {
+            tmp.push(d1[j] - 1);
+        }
+        tmp.sort();
+        let mut ansi = 0;
+        let o = g[i].len();
+        for j in 0..o {
+            ansi = ansi.max(1 + (o - j) + tmp[j] * (o - j));
+        }
+        ans = ans.min(n - ansi);
+    }
     println!("{}", ans);
 }
