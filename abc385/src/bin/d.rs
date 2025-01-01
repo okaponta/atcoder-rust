37e68cae5c75afe241fc3b00d72cf8779cfaba7a
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-01 18:55:05 +0900

    add abc385_d

--- abc385/src/bin/d.rs
+++ abc385/src/bin/d.rs
@@ -1,17 +1,52 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use std::collections::*;
+
+use amplify::confinement::Collection;
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        mut x:i64,
+        mut y:i64,
+        xy:[(i64,i64);n],
+        dc:[(char,i64);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ud_map = HashMap::new();
+    let mut lr_map = HashMap::new();
+    for (x, y) in xy {
+        ud_map.entry(x).or_insert(BTreeSet::new()).push(y);
+        lr_map.entry(y).or_insert(BTreeSet::new()).push(x);
+    }
+    let mut cnt = 0;
+    for (d, c) in dc {
+        let (dx, dy) = match d {
+            'U' => (0, c),
+            'D' => (0, -c),
+            'L' => (-c, 0),
+            _ => (c, 0),
+        };
+        let mut tmp = vec![];
+        if dy == 0 {
+            if let Some(set) = lr_map.get_mut(&y) {
+                for nx in set.range(x.min(x + dx)..=x.max(x + dx)) {
+                    tmp.push((*nx, y));
+                }
+            }
+        } else {
+            if let Some(set) = ud_map.get_mut(&x) {
+                for ny in set.range(y.min(y + dy)..=y.max(y + dy)) {
+                    tmp.push((x, *ny));
+                }
+            }
+        }
+        for (x, y) in tmp {
+            cnt += 1;
+            ud_map.get_mut(&x).unwrap().remove(&y);
+            lr_map.get_mut(&y).unwrap().remove(&x);
+        }
+        x += dx;
+        y += dy;
+    }
+    println!("{} {} {}", x, y, cnt);
 }
