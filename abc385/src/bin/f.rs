a27a5c258a475b27c4aaddd1094ba086ea60de0d
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-03 00:58:02 +0900

    add abc385_f

--- abc385/src/bin/f.rs
+++ abc385/src/bin/f.rs
@@ -1,17 +1,19 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        xh:[(i64,i64);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans: f64 = -1.0;
+    for i in 1..n {
+        let (x1, h1) = xh[i - 1];
+        let (x2, h2) = xh[i];
+        ans = ans.max((h1 * (x2 - x1) - (h2 - h1) * x1) as f64 / (x2 - x1) as f64);
+    }
+    if ans < 0.0 {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
