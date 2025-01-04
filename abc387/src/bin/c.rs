799c33c5628eb634234dab224bdbbd8a2513cd7c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-05 00:02:40 +0900

    add abc387_c

--- abc387/src/bin/c.rs
+++ abc387/src/bin/c.rs
@@ -1,17 +1,45 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        l:Usize1,
+        r:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    println!("{}", f(r) - f(l));
+}
+
+fn f(mut n: usize) -> usize {
+    let mut s = vec![];
+    while 0 < n {
+        s.push(n % 10);
+        n /= 10;
+    }
+    s.reverse();
+    let m = s.len() as u32;
+    let mut res = 0;
+    for i in 1usize..10 {
+        if s[0] < i {
+            res += g(0, m - 1, i);
+        } else if s[0] == i {
+            if (1..m as usize).all(|j| s[j] < i) {
+                res += 1;
+            }
+            for j in 1..m {
+                if s[j as usize] < i {
+                    res += s[j as usize] * i.pow(m - j as u32 - 1);
+                } else {
+                    res += i.pow(m - j as u32);
+                    break;
+                }
+            }
+            res += g(0, m - 1, i);
+        } else {
+            res += g(0, m, i);
+        }
+    }
+    res
+}
+
+fn g(l: u32, r: u32, b: usize) -> usize {
+    (l..r).into_iter().map(|i| b.pow(i)).sum::<usize>()
 }
