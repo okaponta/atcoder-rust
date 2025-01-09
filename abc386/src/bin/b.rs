81e01e97e7ce4b0e05aa3c02b094a1851545d705
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-10 02:37:10 +0900

    add abc386_b

--- abc386/src/bin/b.rs
+++ abc386/src/bin/b.rs
@@ -7,11 +7,16 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut s:Chars,
     }
-    #[allow(unused_mut)]
+    s.reverse();
     let mut ans = 0;
+    while 0 < s.len() {
+        if 1 < s.len() && s[0] == '0' && s[1] == '0' {
+            s.remove(0);
+        }
+        ans += 1;
+        s.remove(0);
+    }
     println!("{}", ans);
 }
