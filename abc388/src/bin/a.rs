df85f7dfcb2c54449da5d97a6fa132dfc7838683
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-11 22:54:03 +0900

    add abc388_a

--- abc388/src/bin/a.rs
+++ abc388/src/bin/a.rs
@@ -7,11 +7,7 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    println!("{}UPC", s[0]);
 }
