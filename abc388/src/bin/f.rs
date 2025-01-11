c62eba15f0b8d2ad6f18a82fd3c91a2fe8112e0c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-12 01:06:10 +0900

    add abc388_f

--- abc388/src/bin/f.rs
+++ abc388/src/bin/f.rs
@@ -1,17 +1,72 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        n:Usize1,
+        m:usize,
+        a:usize,
+        b:usize,
+        lr:[(Usize1,usize);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    if a == b {
+        for &(l, r) in &lr {
+            if l % a == 0 || (l / a) * a + a < r {
+                println!("No");
+                return;
+            }
+        }
+        println!("{}", if n % a != 0 { "No" } else { "Yes" });
+        return;
+    }
+    let mut dp = vec![false; b * b + 1];
+    dp[0] = true;
+    for i in 0..b * b {
+        if !dp[i] {
+            continue;
+        }
+        for j in a..=b {
+            if i + j <= b * b {
+                dp[i + j] = true;
+            }
+        }
+    }
+    let mut ybef = HashSet::new();
+    ybef.insert(0);
+    let mut yall = HashSet::new();
+    for &(l, r) in &lr {
+        let mut x = HashSet::new();
+        for i in ybef {
+            for j in (l.saturating_sub(b)).max(i)..l {
+                if b * b < j - i || dp[j - i] {
+                    x.insert(j);
+                }
+            }
+        }
+        let mut y = HashSet::new();
+        for i in x {
+            for j in a..=b {
+                if r <= i + j {
+                    y.insert(i + j);
+                    yall.insert(i + j);
+                }
+            }
+        }
+        for i in 0..b {
+            if yall.contains(&(r + i)) {
+                y.insert(r + i);
+            }
+        }
+        ybef = y;
+    }
+    for i in ybef {
+        if n < i {
+            continue;
+        }
+        if b * b < n - i || dp[n - i] {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
