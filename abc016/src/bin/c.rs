68550d9e05aa3f1b6661c49300244c54e7868530
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-17 00:21:39 +0900

    add abc016_c

--- abc016/src/bin/c.rs
+++ abc016/src/bin/c.rs
@@ -1,17 +1,52 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut g = vec![vec![]; n];
+    for &(a, b) in &ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let w = WarshallFloyd::new(n, &g);
+    for i in 0..n {
+        println!(
+            "{}",
+            (0..n)
+                .into_iter()
+                .filter(|&j| w.distance[i][j] == 2)
+                .count()
+        );
+    }
+}
+
+// 計算量はN^3
+// 負の場合でも使用でき、任意の点の最短距離がすべて求まる
+pub struct WarshallFloyd {
+    distance: Vec<Vec<usize>>,
+}
+
+impl WarshallFloyd {
+    // n:usize 頂点の数
+    // edges: Vec<Vec<(usize,i64)>> edges[i] = [(2,3), (3,-1), (To,重み)]
+    pub fn new(n: usize, edges: &Vec<Vec<usize>>) -> Self {
+        let mut distance = vec![vec![1 << 60; n]; n];
+        for i in 0..n {
+            distance[i][i] = 0;
+            for &j in &edges[i] {
+                distance[i][j] = 1;
+            }
+        }
+        for k in 0..n {
+            for i in 0..n {
+                for j in 0..n {
+                    distance[i][j] = distance[i][j].min(distance[i][k] + distance[k][j]);
+                }
+            }
+        }
+        Self { distance }
+    }
 }
