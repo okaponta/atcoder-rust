e875b622e118a0bbf335e635a43cc81480a5622c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-16 21:21:02 +0900

    add abc016_d

--- abc016/src/bin/d.rs
+++ abc016/src/bin/d.rs
@@ -1,17 +1,30 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
 
 fn main() {
     input! {
+        a:(i64,i64),
+        b:(i64,i64),
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        xy:[(i64,i64);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    println!(
+        "{}",
+        1 + (0..n)
+            .into_iter()
+            .filter(|&i| g(a, b, xy[i], xy[(i + 1) % n]))
+            .count()
+            / 2
+    );
+}
+
+fn g(p1: (i64, i64), p2: (i64, i64), p3: (i64, i64), p4: (i64, i64)) -> bool {
+    f(p1, p2, p3) * f(p1, p2, p4) < 0 && f(p3, p4, p1) * f(p3, p4, p2) < 0
+}
+
+fn f(p1: (i64, i64), p2: (i64, i64), p3: (i64, i64)) -> i64 {
+    let a = p2.0 - p1.0;
+    let b = p2.1 - p1.1;
+    let c = p3.0 - p1.0;
+    let d = p3.1 - p1.1;
+    a * d - b * c
 }
