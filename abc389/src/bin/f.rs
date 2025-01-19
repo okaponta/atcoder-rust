3bd3a0cf033b052bf5ecb165f0269317206935b8
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-19 15:52:31 +0900

    add abc389_f

--- abc389/src/bin/f.rs
+++ abc389/src/bin/f.rs
@@ -1,17 +1,53 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use ac_library::*;
+use proconio::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        lr:[(usize,usize);n],
+        q:usize,
+        x:[usize;q],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let init = (0..500010).into_iter().collect::<Vec<_>>();
+    let mut lazy = LazySegtree::<F>::from(init);
+    for (l, r) in lr {
+        let l = lazy.max_right(0, |x| x < l);
+        let r = lazy.max_right(0, |x| x <= r);
+        lazy.apply_range(l..r, 1);
+    }
+    for x in x {
+        println!("{}", lazy.get(x));
+    }
+}
+
+struct M;
+impl Monoid for M {
+    type S = usize;
+
+    fn identity() -> Self::S {
+        0
+    }
+
+    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+        *a.max(b)
+    }
+}
+
+struct F;
+impl MapMonoid for F {
+    type M = M;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+        x + f
+    }
+
+    fn composition(f: &Self::F, g: &Self::F) -> Self::F {
+        f + g
+    }
 }
