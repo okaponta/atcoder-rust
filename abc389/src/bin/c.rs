20e114172f0d10a7cadb07289ead5b4db9159b28
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-22 12:05:10 +0900

    add abc389_c

--- abc389/src/bin/c.rs
+++ abc389/src/bin/c.rs
@@ -1,17 +1,21 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut s = vec![0];
+    let mut slide = 0;
+    for _i in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {l:usize}
+            s.push(s[s.len() - 1] + l);
+        } else if qi == 2 {
+            slide += 1;
+        } else {
+            input! {k:Usize1}
+            println!("{}", s[k + slide] - s[slide]);
+        }
+    }
 }
