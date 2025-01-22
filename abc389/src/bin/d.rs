a4e05c1133d76042b954d3e08923d9e6b5414772
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-23 01:41:26 +0900

    add abc389_d

--- abc389/src/bin/d.rs
+++ abc389/src/bin/d.rs
@@ -7,11 +7,21 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        r:f64,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    if r < 1.5 {
+        println!("1");
+        return;
+    }
+    let mut ans = 2 * f(r, 0.5).floor() as usize - 1;
+    let mut dist = 1.5;
+    while 1.0 < f(r, dist) {
+        ans += 2 * (2 * f(r, dist).floor() as usize - 1);
+        dist += 1.0;
+    }
     println!("{}", ans);
+}
+
+fn f(r: f64, d: f64) -> f64 {
+    0.5 + (r * r - d * d).sqrt()
 }
