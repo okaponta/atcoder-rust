d8a436365a9d458ef4823941fcdf8f9b3804a724
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-25 22:10:00 +0900

    add abc015_d

--- abc015/src/bin/d.rs
+++ abc015/src/bin/d.rs
@@ -7,11 +7,22 @@ use superslice::*;
 
 fn main() {
     input! {
+        w:usize,
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        ab:[(usize,usize);n],
     }
-    #[allow(unused_mut)]
     let mut ans = 0;
+    let mut dp = vec![vec![0; w + 1]; k + 1];
+    for (a, b) in ab {
+        for j in (0..k).into_iter().rev() {
+            for l in 0..w {
+                if l + a <= w {
+                    dp[j + 1][l + a] = dp[j + 1][l + a].max(dp[j][l] + b);
+                    ans = ans.max(dp[j + 1][l + a]);
+                }
+            }
+        }
+    }
     println!("{}", ans);
 }
