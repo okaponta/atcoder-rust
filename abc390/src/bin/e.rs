2efce0e65ba75717f168c0dbd73b5603add9b1cc
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-01-30 21:36:08 +0900

    add abc390_e

--- abc390/src/bin/e.rs
+++ abc390/src/bin/e.rs
@@ -1,17 +1,30 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
 use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        x:usize,
+        vac:[(Usize1,usize,usize);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut dp = vec![vec![0; x + 1]; 3];
+    for (v, a, c) in vac {
+        for i in (0..=x).rev() {
+            if i + c <= x {
+                dp[v][i + c] = dp[v][i + c].max(dp[v][i] + a);
+            }
+        }
+    }
+    let mut l = 0;
+    let mut u = 1 << 60;
+    while 1 < u - l {
+        let m = (l + u) / 2;
+        let s: usize = (0..3).into_iter().map(|i| dp[i].lower_bound(&m)).sum();
+        if s <= x {
+            l = m;
+        } else {
+            u = m;
+        }
+    }
+    println!("{}", l);
 }
