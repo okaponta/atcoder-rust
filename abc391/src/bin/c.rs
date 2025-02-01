c329ec54343b378eb06efbad842a5299e6ab09c6
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-01 22:53:42 +0900

    add abc391_c

--- abc391/src/bin/c.rs
+++ abc391/src/bin/c.rs
@@ -1,3 +1,5 @@
+use std::u8;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -5,13 +7,39 @@ use proconio::{marker::*, *};
 #[allow(unused)]
 use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    #[allow(unused_mut)]
+    let mut loc = vec![0; n];
+    let mut num = vec![0; n];
     let mut ans = 0;
-    println!("{}", ans);
+    for i in 0..n {
+        loc[i] = i;
+        num[i] = 1;
+    }
+    for _ in 0..q {
+        input! {
+            qi:u8
+        }
+        if qi == 1 {
+            input! {
+                p:Usize1,
+                h:Usize1,
+            }
+            num[loc[p]] -= 1;
+            if num[loc[p]] == 1 {
+                ans -= 1;
+            }
+            loc[p] = h;
+            num[h] += 1;
+            if num[h] == 2 {
+                ans += 1;
+            }
+        } else {
+            println!("{}", ans);
+        }
+    }
 }
