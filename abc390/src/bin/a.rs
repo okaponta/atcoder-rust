463ce384fbb4a11c0d1147a5c1d24232759233b7
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-02 16:36:50 +0900

    add abc390_a

--- abc390/src/bin/a.rs
+++ abc390/src/bin/a.rs
@@ -7,11 +7,22 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut a:[usize;5],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let ans = vec![1, 2, 3, 4, 5];
+    if a == ans {
+        println!("No");
+        return;
+    }
+    for i in 0..4 {
+        if a[i] != i + 1 {
+            a.swap(i, i + 1);
+            break;
+        }
+    }
+    if a == ans {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
