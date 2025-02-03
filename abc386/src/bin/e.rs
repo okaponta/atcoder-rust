179218f0c79f16b6cdfcd69993144c51936b9ed5
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-03 12:35:20 +0900

    add abc386_e

--- abc386/src/bin/e.rs
+++ abc386/src/bin/e.rs
@@ -1,17 +1,37 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut s = vec![0; n + 1];
+    let mut tmp = 0;
+    for i in (0..n).rev() {
+        tmp ^= a[i];
+        s[i] = tmp;
+    }
+    println!("{}", dfs(0, 0, 0, n, k, &a, &s));
+}
+
+fn dfs(
+    i: usize,
+    j: usize,
+    xor: usize,
+    n: usize,
+    k: usize,
+    a: &Vec<usize>,
+    s: &Vec<usize>,
+) -> usize {
+    let mut res = 0;
+    if j == k {
+        return xor;
+    }
+    if n - i == k - j {
+        return xor ^ s[i];
+    }
+    res = res.max(dfs(i + 1, j + 1, xor ^ a[i], n, k, a, s));
+    res = res.max(dfs(i + 1, j, xor, n, k, a, s));
+    res
 }
