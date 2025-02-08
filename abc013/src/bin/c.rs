7314c8dd8378209a98fb35ccde447bd2eefe83d3
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-08 13:32:00 +0900

    add abc013_c

--- abc013/src/bin/c.rs
+++ abc013/src/bin/c.rs
@@ -8,10 +8,34 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        a:usize,
+        b:usize,
+        c:usize,
+        d:usize,
+        e:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut ans = 1 << 60;
+    for i in 0..=n {
+        if h + b * i < e * (n - i) {
+            continue;
+        }
+        if e * (n - i) < h + d * i {
+            ans = ans.min(c * i);
+            continue;
+        }
+        let mut lower = 0;
+        let mut upper = i + 1;
+        while upper - lower > 1 {
+            let mid = (lower + upper) / 2;
+            let x = h + b * mid + d * (i - mid);
+            if e * (n - i) < x {
+                upper = mid;
+            } else {
+                lower = mid;
+            }
+        }
+        ans = ans.min(a * upper + c * (i - upper));
+    }
     println!("{}", ans);
 }
