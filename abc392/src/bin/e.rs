4c24f9ffc9394ca974aca5d2f906532bdd87cd30
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-10 04:24:46 +0900

    add abc392_e

--- abc392/src/bin/e.rs
+++ abc392/src/bin/e.rs
@@ -1,17 +1,39 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
+use petgraph::unionfind::UnionFind;
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use std::collections::HashSet;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut uf = UnionFind::new(n);
+    let mut rem = vec![];
+    for i in 0..m {
+        if !uf.union(ab[i].0, ab[i].1) {
+            rem.push(i);
+        }
+    }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        set.insert(uf.find(i));
+    }
+    println!("{}", set.len() - 1);
+    let mut ans = vec![];
+    for i in rem {
+        if set.len() == 1 {
+            break;
+        }
+        let a = uf.find(ab[i].0);
+        set.remove(&a);
+        let b = *set.iter().next().unwrap();
+        uf.union(a, b);
+        set.remove(&b);
+        ans.push((i + 1, ab[i].0 + 1, b + 1));
+        set.insert(uf.find(a));
+    }
+    for (a, b, c) in ans {
+        println!("{} {} {}", a, b, c);
+    }
 }
