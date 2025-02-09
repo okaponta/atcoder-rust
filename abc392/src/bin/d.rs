0a0dbb9ee5bdc810966e6a86cd5f19acf6125ae1
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-10 00:30:37 +0900

    add abc392_d

--- abc392/src/bin/d.rs
+++ abc392/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -8,10 +10,30 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut a = vec![];
+    let mut c = vec![];
+    for _ in 0..n {
+        input! {k:usize, ai:[usize;k]}
+        c.push(k);
+        let mut map = HashMap::new();
+        for i in 0..k {
+            *map.entry(ai[i]).or_insert(0usize) += 1;
+        }
+        a.push(map);
+    }
+    let mut ans: f64 = 0.0;
+    for i in 0..n {
+        for j in i + 1..n {
+            let mut count = 0;
+            for (k, v) in a[i].iter() {
+                if a[j].contains_key(k) {
+                    count += v * a[j][k];
+                }
+            }
+            let tmp = count as f64 / (c[i] * c[j]) as f64;
+            ans = ans.max(tmp);
+        }
+    }
     println!("{}", ans);
 }
