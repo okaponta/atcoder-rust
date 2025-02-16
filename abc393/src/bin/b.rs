6f6ece4f0c26b63dfa0050cdfd86e1a327388a80
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-16 18:52:56 +0900

    add abc393_b

--- abc393/src/bin/b.rs
+++ abc393/src/bin/b.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[allow(unused)]
 use itertools::*;
 #[allow(unused)]
@@ -7,11 +9,27 @@ use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
+    let mut a = HashSet::new();
+    let mut b = HashSet::new();
+    let mut c = HashSet::new();
+    for i in 0..s.len() {
+        if s[i] == 'A' {
+            a.insert(i);
+        } else if s[i] == 'B' {
+            b.insert(i);
+        } else if s[i] == 'C' {
+            c.insert(i);
+        }
+    }
     let mut ans = 0;
+    for ai in 0..s.len() {
+        for j in 1..s.len() {
+            if a.contains(&ai) && b.contains(&(ai + j)) && c.contains(&(ai + 2 * j)) {
+                ans += 1;
+            }
+        }
+    }
     println!("{}", ans);
 }
