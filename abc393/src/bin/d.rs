477f45f54eca78d33665b0895712f28f96459d61
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-17 22:34:55 +0900

    add abc393_d

--- abc393/src/bin/d.rs
+++ abc393/src/bin/d.rs
@@ -8,10 +8,26 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
+    let mut m = 0;
+    let mut tmp = 0;
+    let mut imos = vec![0; n + 1];
+    for i in 0..n {
+        if s[i] == '1' {
+            m += 1;
+            tmp += i;
+            imos[i + 1 - m] += 2;
+        }
+    }
+    tmp -= m * (m - 1) / 2;
+    let mut tmp = tmp as i64;
+    let mut diff = -(m as i64);
+    let mut ans = tmp;
+    for i in 0..=n - m {
+        diff += imos[i];
+        tmp += diff;
+        ans = ans.min(tmp);
+    }
     println!("{}", ans);
 }
