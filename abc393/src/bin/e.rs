b650d5aaa2c146eb570c127b603b0e20ad307f8c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-18 20:27:03 +0900

    add abc393_e

--- abc393/src/bin/e.rs
+++ abc393/src/bin/e.rs
@@ -1,17 +1,34 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
+use std::collections::HashMap;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        a:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut map = HashMap::new();
+    for i in 0..n {
+        *map.entry(a[i]).or_insert(0) += 1;
+    }
+    let mut ans = vec![1; 1_000_001];
+    for i in 2..=1_000_000 {
+        let mut count = 0;
+        let mut update_keys = vec![];
+        for j in (i..=1_000_000).step_by(i) {
+            if map.contains_key(&j) {
+                count += map[&j];
+                update_keys.push(j);
+            }
+        }
+        if k <= count {
+            for j in update_keys {
+                ans[j] = i;
+            }
+        }
+    }
+    for a in a {
+        println!("{}", ans[a]);
+    }
 }
