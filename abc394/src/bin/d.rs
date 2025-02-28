c3e3104875689b6e6935055a9acb38a05b187ce7
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-02-28 19:04:41 +0900

    add abc394_d

--- abc394/src/bin/d.rs
+++ abc394/src/bin/d.rs
@@ -1,17 +1,22 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut v = vec![];
+    let br = vec![('(', ')'), ('[', ']'), ('<', '>')];
+    for c in s {
+        v.push(c);
+        if v.len() < 2 {
+            continue;
+        }
+        let c1 = v[v.len() - 2];
+        let c2 = v[v.len() - 1];
+        if br.contains(&(c1, c2)) {
+            v.pop();
+            v.pop();
+        }
+    }
+    println!("{}", if v.len() == 0 { "Yes" } else { "No" });
 }
