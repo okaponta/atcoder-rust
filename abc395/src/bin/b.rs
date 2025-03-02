0e040b0831e839add9649600b727f9fb7fa9e39c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-02 09:58:27 +0900

    add abc395_b

--- abc395/src/bin/b.rs
+++ abc395/src/bin/b.rs
@@ -8,10 +8,23 @@ use superslice::*;
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut ans = vec![vec!['.'; n]; n];
+    for i in 0..n {
+        let j = n - i;
+        if i <= j {
+            for k in i..j {
+                for l in i..j {
+                    if i % 2 == 0 {
+                        ans[k][l] = '#';
+                    } else {
+                        ans[k][l] = '.';
+                    }
+                }
+            }
+        }
+    }
+    for i in 0..n {
+        println!("{}", ans[i].iter().collect::<String>());
+    }
 }
