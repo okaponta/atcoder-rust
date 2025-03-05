48180ac7139e75112fcec8ca77195a8e461a7a4b
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-05 10:50:13 +0900

    add abc395_d

--- abc395/src/bin/d.rs
+++ abc395/src/bin/d.rs
@@ -1,17 +1,31 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut sw = vec![];
+    let mut r_sw = vec![];
+    let mut ans = vec![];
+    for i in 0..n {
+        sw.push(i);
+        r_sw.push(i);
+        ans.push(i);
+    }
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {a: Usize1, b:Usize1}
+            ans[a] = r_sw[b];
+        } else if qi == 2 {
+            input! {a: Usize1, b:Usize1}
+            r_sw.swap(a, b);
+            sw.swap(r_sw[a], r_sw[b]);
+        } else {
+            input! {a: Usize1}
+            println!("{}", sw[ans[a]] + 1);
+        }
+    }
 }
