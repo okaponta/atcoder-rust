a1fb0885708f0f970f1ec9ef5042a864fceb497b
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-12 18:13:08 +0900

    add abc396_d

--- abc396/src/bin/d.rs
+++ abc396/src/bin/d.rs
@@ -1,17 +1,39 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
 use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        uvw:[(Usize1,Usize1,usize);m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut g = vec![vec![]; n];
+    for &(u, v, w) in &uvw {
+        g[u].push((v, w));
+        g[v].push((u, w));
+    }
+    let mut used = vec![false; n];
+    used[0] = true;
+    println!("{}", dfs(0, 0, n, &g, &mut used));
+}
+
+fn dfs(
+    cur: usize,
+    tmp: usize,
+    n: usize,
+    g: &Vec<Vec<(usize, usize)>>,
+    used: &mut Vec<bool>,
+) -> usize {
+    if cur == n - 1 {
+        return tmp;
+    }
+    let mut res = 1 << 62;
+    for &(next, w) in &g[cur] {
+        if used[next] {
+            continue;
+        }
+        used[next] = true;
+        res = res.min(dfs(next, tmp ^ w, n, g, used));
+        used[next] = false;
+    }
+    res
 }
