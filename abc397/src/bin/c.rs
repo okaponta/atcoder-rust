fc3da73c154f40e912c74ee5883a0cc6798e0c97
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-15 23:01:14 +0900

    add abc397_c

--- abc397/src/bin/c.rs
+++ abc397/src/bin/c.rs
@@ -4,8 +4,22 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    println!("{}", 0);
+    let mut map1 = HashMap::new();
+    let mut map2 = HashMap::new();
+    *map1.entry(a[0]).or_insert(0) += 1;
+    for i in 1..n {
+        *map2.entry(a[i]).or_insert(0) += 1;
+    }
+    let mut ans = map1.len() + map2.len();
+    for i in 1..n - 1 {
+        *map1.entry(a[i]).or_insert(0) += 1;
+        *map2.entry(a[i]).or_insert(0) -= 1;
+        if map2[&a[i]] == 0 {
+            map2.remove(&a[i]);
+        }
+        ans = ans.max(map1.len() + map2.len());
+    }
+    println!("{}", ans);
 }
