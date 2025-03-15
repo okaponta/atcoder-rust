ea2f60070de4c78d9d88fb04419240b81a6465ee
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-15 23:01:13 +0900

    add abc397_b

--- abc397/src/bin/b.rs
+++ abc397/src/bin/b.rs
@@ -3,9 +3,28 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut s:Chars,
     }
-    println!("{}", 0);
+    let mut ans = 0;
+    let mut i = 0;
+    while i < s.len() {
+        if i % 2 == 0 {
+            if s[i] != 'i' {
+                s.insert(i, 'i');
+                ans += 1;
+                continue;
+            }
+        } else {
+            if s[i] != 'o' {
+                s.insert(i, 'o');
+                ans += 1;
+                continue;
+            }
+        }
+        i += 1;
+    }
+    if s.len() % 2 != 0 {
+        ans += 1;
+    }
+    println!("{}", ans);
 }
