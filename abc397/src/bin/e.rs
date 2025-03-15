a3eca1c90507051923f852ac295ee3a2a4f19929
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-16 00:02:55 +0900

    add abc397_e

--- abc397/src/bin/e.rs
+++ abc397/src/bin/e.rs
@@ -4,8 +4,47 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        uv:[(Usize1,Usize1);n*k-1],
     }
-    println!("{}", 0);
+    let mut g = vec![vec![]; n * k];
+    for (u, v) in uv {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let mut dp = vec![0; n * k];
+    dfs(0, 0, &g, &mut dp, k);
+    println!("Yes");
+}
+
+fn dfs(cur: usize, prev: usize, g: &Vec<Vec<usize>>, dp: &mut Vec<usize>, k: usize) {
+    let mut c = 0;
+    let mut s = 1;
+    for &next in &g[cur] {
+        if next == prev {
+            continue;
+        }
+        dfs(next, cur, g, dp, k);
+        if 0 < dp[next] {
+            c += 1;
+        }
+        s += dp[next];
+    }
+    if s < k {
+        if 1 < c {
+            println!("No");
+            std::process::exit(0);
+        }
+        dp[cur] = s;
+    } else if s == k {
+        if c <= 2 {
+            dp[cur] = 0;
+        } else {
+            println!("No");
+            std::process::exit(0);
+        }
+    } else {
+        println!("No");
+        std::process::exit(0);
+    }
 }
