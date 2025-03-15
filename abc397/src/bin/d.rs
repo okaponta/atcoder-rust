1d98721ea3cc7bd99fd28914b38c2ae801df4ff4
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-15 23:01:15 +0900

    add abc397_d

--- abc397/src/bin/d.rs
+++ abc397/src/bin/d.rs
@@ -1,11 +1,36 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use num_integer::Roots;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
     }
-    println!("{}", 0);
+    for a in 1.. {
+        if n < a * a * a {
+            break;
+        }
+        if n % a != 0 {
+            continue;
+        }
+        let b1 = (n * 4) / a;
+        if b1 < a * a {
+            continue;
+        }
+        let b2 = b1 - a * a;
+        if b2 % 3 != 0 {
+            continue;
+        }
+        let b3 = b2 / 3;
+        let b4 = b3.sqrt();
+        if b4 * b4 != b3 {
+            continue;
+        }
+        if (a + b4) % 2 != 0 || (b4 - a) % 2 != 0 || (a + b4) == 0 || (b4 - a) == 0 {
+            continue;
+        }
+        println!("{} {}", (a + b4) / 2, (b4 - a) / 2);
+        return;
+    }
+    println!("-1");
 }
