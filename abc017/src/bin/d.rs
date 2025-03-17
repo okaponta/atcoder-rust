b9b2f7ea2ec9d54f472230455820d2603cb62c5f
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-18 00:50:38 +0900

    add abc017_d

--- abc017/src/bin/d.rs
+++ abc017/src/bin/d.rs
@@ -1,17 +1,29 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use proconio::*;
+use std::collections::HashMap;
+
+const MOD: usize = 1_000_000_007;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut n:usize,_m:usize,mut f:[usize;n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    n += 1;
+    f.insert(0, MOD);
+    let mut left = 0;
+    let mut tmp = 1;
+    let mut dp = vec![0; n];
+    dp[0] = 1;
+    let mut map: HashMap<usize, Vec<usize>> = HashMap::new();
+    for i in 1..n {
+        if let Some(j) = map.get(&f[i]) {
+            while left < j[j.len() - 1] {
+                tmp = (tmp + MOD - dp[left]) % MOD;
+                left = (left + 1) % MOD;
+            }
+        }
+        dp[i] = tmp;
+        tmp = (tmp + dp[i]) % MOD;
+        map.entry(f[i]).or_insert(vec![]).push(i);
+    }
+    println!("{}", dp[n - 1]);
 }
