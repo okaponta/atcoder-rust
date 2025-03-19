9293c18b4a631e3bab0346a39af1d4a42e37a6ee
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-20 00:09:39 +0900

    add abc013_d

--- abc013/src/bin/d.rs
+++ abc013/src/bin/d.rs
@@ -1,17 +1,28 @@
-#[allow(unused)]
-use itertools::*;
-#[allow(unused)]
-use proconio::{marker::*, *};
-#[allow(unused)]
-use superslice::*;
+use itertools::Itertools;
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        mut d:usize,
+        a:[usize;m],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    let mut cv = (0..n).into_iter().collect::<Vec<_>>();
+    let mut ans = cv.clone();
+    for a in a {
+        cv.swap(a - 1, a);
+    }
+    while 0 < d {
+        if d & 1 == 1 {
+            let mut tmp = vec![0; n];
+            for i in 0..n {
+                tmp[cv[i]] = ans[i];
+            }
+            ans = tmp;
+        }
+        cv = (0..n).into_iter().map(|i| cv[cv[i]]).collect();
+        d /= 2;
+    }
+    println!("{}", ans.iter().map(|i| i + 1).join("\n"));
 }
