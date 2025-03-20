e83ec2bf0ee856a6722f0749feb62779a8f9a2ea
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-21 00:14:40 +0900

    add abc010_c

--- abc010/src/bin/c.rs
+++ abc010/src/bin/c.rs
@@ -7,11 +7,22 @@ use superslice::*;
 
 fn main() {
     input! {
+        s:(i64,i64),
+        g:(i64,i64),
+        t:usize,
+        v:usize,
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        xy:[(i64,i64);n],
     }
-    #[allow(unused_mut)]
-    let mut ans = 0;
-    println!("{}", ans);
+    for xy in xy {
+        if dist(s, xy) + dist(xy, g) <= ((t * v) as f64) {
+            println!("YES");
+            return;
+        }
+    }
+    println!("NO");
+}
+
+fn dist(a: (i64, i64), b: (i64, i64)) -> f64 {
+    (((a.0 - b.0) * (a.0 - b.0) + (a.1 - b.1) * (a.1 - b.1)) as f64).sqrt()
 }
