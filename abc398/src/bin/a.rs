08b8920888d228080ef0b4c6fba54ddb7766c3ac
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-23 23:52:21 +0900

    add abc398_a

--- abc398/src/bin/a.rs
+++ abc398/src/bin/a.rs
@@ -4,8 +4,11 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
     }
-    println!("{}", 0);
+    let mut ans = (0..n).into_iter().map(|_| '-').collect::<Vec<_>>();
+    ans[n / 2] = '=';
+    if n % 2 == 0 {
+        ans[(n / 2) - 1] = '=';
+    }
+    println!("{}", ans.iter().join(""));
 }
