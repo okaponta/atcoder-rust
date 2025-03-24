239cf4a551993496120e80fd936b1d13dc1b6ddd
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-25 01:40:25 +0900

    add abc398_c

--- abc398/src/bin/c.rs
+++ abc398/src/bin/c.rs
@@ -4,8 +4,24 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    println!("{}", 0);
+    let mut map = HashMap::new();
+    for &a in &a {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    if let Some(j) = map
+        .into_iter()
+        .filter(|(_, v)| 1 == *v)
+        .map(|(k, _)| k)
+        .max()
+    {
+        for i in 0..n {
+            if a[i] == j {
+                println!("{}", i + 1);
+            }
+        }
+    } else {
+        println!("-1");
+    }
 }
