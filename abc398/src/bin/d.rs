593678a33053c824d5bb5d52f1fe60d537d41afe
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-26 00:09:49 +0900

    add abc398_d

--- abc398/src/bin/d.rs
+++ abc398/src/bin/d.rs
@@ -3,9 +3,24 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        r:i64,
+        c:i64,
+        s:Chars,
     }
-    println!("{}", 0);
+    let mut tmp = (-r, c);
+    let mut set = HashSet::new();
+    set.insert((0, 0));
+    let mut ans = vec![];
+    for ci in s {
+        match ci {
+            'N' => tmp.0 -= 1,
+            'E' => tmp.1 -= 1,
+            'W' => tmp.1 += 1,
+            _ => tmp.0 += 1,
+        }
+        set.insert((tmp.0 + r, tmp.1 - c));
+        ans.push(if set.contains(&tmp) { '1' } else { '0' });
+    }
+    println!("{}", ans.iter().join(""));
 }
