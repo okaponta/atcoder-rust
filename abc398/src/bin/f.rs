96e2bcf23e98711e6bd5849fce0518e0620378b1
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-27 08:44:52 +0900

    add abc398_f

--- abc398/src/bin/f.rs
+++ abc398/src/bin/f.rs
@@ -3,9 +3,55 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    println!("{}", 0);
+    let n = s.len();
+    let mut t = vec![];
+    for i in (0..n).rev() {
+        t.push(s[i]);
+    }
+    t.push('#');
+    for i in 0..n {
+        t.push(s[i]);
+    }
+    let m = 2 * n + 1;
+    let z = z(&t);
+    let mut from = 0;
+    for i in n + 1..m {
+        if i + z[i] == m {
+            from = z[i];
+            break;
+        }
+    }
+    let mut ans = s.clone();
+    for i in (0..n - from).rev() {
+        ans.push(s[i]);
+    }
+    println!("{}", ans.iter().join(""));
+}
+
+fn z(s: &Vec<char>) -> Vec<usize> {
+    let n = s.len();
+    let mut res = vec![0; n + 1];
+    res[0] = n;
+    let mut i = 1;
+    let mut j = 0;
+    while i < n {
+        while i + j < n && s[j] == s[i + j] {
+            j += 1;
+        }
+        res[i] = j;
+        if j == 0 {
+            i += 1;
+            continue;
+        }
+        let mut k = 1;
+        while k < j && k + res[k] < j {
+            res[i + k] = res[k];
+            k += 1;
+        }
+        i += k;
+        j -= k;
+    }
+    res
 }
