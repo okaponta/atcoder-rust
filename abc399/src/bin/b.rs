fefbfc23ece4109d0992c3637aec05e08c9fe8e6
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-31 00:18:01 +0900

    add abc399_b

--- abc399/src/bin/b.rs
+++ abc399/src/bin/b.rs
@@ -4,8 +4,25 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        p:[usize;n],
     }
-    println!("{}", 0);
+    let mut pi = vec![];
+    for i in 0..n {
+        pi.push((p[i], i));
+    }
+    pi.sort();
+    pi.reverse();
+    let mut map = HashMap::new();
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        if let Some(&j) = map.get(&pi[i].0) {
+            ans[pi[i].1] = j;
+        } else {
+            ans[pi[i].1] = i + 1;
+            map.insert(pi[i].0, i + 1);
+        }
+    }
+    for i in 0..n {
+        println!("{}", ans[i]);
+    }
 }
