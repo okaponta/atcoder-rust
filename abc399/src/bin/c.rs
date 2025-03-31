0baee92813aef1a6e8178e287f39504885914bd9
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-01 00:14:12 +0900

    add abc399_c

--- abc399/src/bin/c.rs
+++ abc399/src/bin/c.rs
@@ -1,11 +1,19 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use petgraph::unionfind::UnionFind;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        uv:[(Usize1,Usize1);m],
     }
-    println!("{}", 0);
+    let mut uf = UnionFind::new(n);
+    let mut ans = 0;
+    for (u, v) in uv {
+        if !uf.union(u, v) {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
