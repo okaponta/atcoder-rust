5d2602d61e37829453ef1527df3ffe08f3b52868
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-05 23:50:28 +0900

    add abc008_b

--- abc008/src/bin/b.rs
+++ abc008/src/bin/b.rs
@@ -4,8 +4,21 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:[String;n],
     }
-    println!("{}", 0);
+    let mut map = HashMap::new();
+    let mut max = 0;
+    for i in 0..n {
+        let s = s[i].clone();
+        *map.entry(s).or_insert(0) += 1;
+    }
+    for (_, v) in map.iter() {
+        max = max.max(*v);
+    }
+    for (k, v) in map.iter() {
+        if *v == max {
+            println!("{}", k);
+            return;
+        }
+    }
 }
