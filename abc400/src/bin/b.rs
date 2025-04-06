f88b3fb17d00f5096952ba30b12a001672c01d93
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-07 00:14:31 +0900

    add abc400_b

--- abc400/src/bin/b.rs
+++ abc400/src/bin/b.rs
@@ -4,8 +4,17 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
     }
-    println!("{}", 0);
+    let mut ans = 1;
+    let mut tmp = 1;
+    for _i in 0..m {
+        tmp *= n;
+        ans += tmp;
+        if 1_000_000_000 < ans {
+            println!("inf");
+            return;
+        }
+    }
+    println!("{}", ans);
 }
