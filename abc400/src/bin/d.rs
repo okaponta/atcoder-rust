a464a72282ecf1538d7a5aa2c3d69900fba68498
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-10 23:46:08 +0900

    add abc400_d

--- abc400/src/bin/d.rs
+++ abc400/src/bin/d.rs
@@ -3,9 +3,47 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        s:[Chars;h],
+        a:Usize1,
+        b:Usize1,
+        c:Usize1,
+        d:Usize1,
     }
-    println!("{}", 0);
+    let mut ans = vec![vec![1 << 30; w]; h];
+    let mut q = VecDeque::new();
+    q.push_back((a, b, 0));
+    while let Some((i, j, c)) = q.pop_front() {
+        if ans[i][j] <= c {
+            continue;
+        }
+        ans[i][j] = c;
+        for (di, dj) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+            let ni = i.wrapping_add(di);
+            let nj = j.wrapping_add(dj);
+            if h <= ni || w <= nj || s[ni][nj] == '#' {
+                continue;
+            }
+            q.push_front((ni, nj, c));
+        }
+        for (di, dj) in vec![
+            (!0, 0),
+            (0, 1),
+            (0, !0),
+            (1, 0),
+            (!1, 0),
+            (0, 2),
+            (0, !1),
+            (2, 0),
+        ] {
+            let ni = i.wrapping_add(di);
+            let nj = j.wrapping_add(dj);
+            if h <= ni || w <= nj {
+                continue;
+            }
+            q.push_back((ni, nj, c + 1));
+        }
+    }
+    println!("{}", ans[c][d]);
 }
