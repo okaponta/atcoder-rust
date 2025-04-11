823f685f31f90442fee8f45c51227c317f261939
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-12 00:35:56 +0900

    join abc007

---
+++ abc007/src/bin/a.rs
@@ -0,0 +1,9 @@
+#[rustfmt::skip]#[allow(unused)]
+use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+
+fn main() {
+    input! {
+        n:Usize1,
+    }
+    println!("{}", n);
+}
