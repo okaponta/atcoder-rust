fd8db629b6aebf28b46f32b5ce9bd2c9e6c31ac0
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-17 23:53:37 +0900

    add abc007_c

--- abc007/src/bin/c.rs
+++ abc007/src/bin/c.rs
@@ -3,9 +3,28 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        s:(Usize1,Usize1),
+        g:(Usize1,Usize1),
+        c:[Chars;h],
     }
-    println!("{}", 0);
+    let mut ans = vec![vec![1 << 30; w]; h];
+    let mut q = VecDeque::new();
+    q.push_back((s.0, s.1, 0));
+    while let Some((i, j, d)) = q.pop_front() {
+        if ans[i][j] <= d {
+            continue;
+        }
+        ans[i][j] = d;
+        for (di, dj) in vec![(!0, 0), (0, 1), (0, !0), (1, 0)] {
+            let ni = i.wrapping_add(di);
+            let nj = j.wrapping_add(dj);
+            if h <= ni || w <= nj || c[ni][nj] == '#' {
+                continue;
+            }
+            q.push_back((ni, nj, d + 1));
+        }
+    }
+    println!("{}", ans[g.0][g.1]);
 }
