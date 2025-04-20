d0facac1c22d126a51fa4e12b7e972a1f482e257
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-20 23:56:23 +0900

    add abc401_b

--- abc401/src/bin/b.rs
+++ abc401/src/bin/b.rs
@@ -4,8 +4,20 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:[String;n],
     }
-    println!("{}", 0);
+    let mut count = 0;
+    let mut login = false;
+    for s in s {
+        if s == "login" {
+            login = true;
+        } else if s == "logout" {
+            login = false;
+        } else if s == "private" {
+            if !login {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
