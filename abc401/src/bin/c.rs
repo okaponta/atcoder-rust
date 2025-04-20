505a4c7843ebeee5ded861777687bac74332674f
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-21 00:00:03 +0900

    add abc401_c

--- abc401/src/bin/c.rs
+++ abc401/src/bin/c.rs
@@ -4,8 +4,16 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
     }
-    println!("{}", 0);
+    let mut a = vec![1; n + 1];
+    if n < k {
+        println!("1");
+        return;
+    }
+    a[k] = k;
+    for i in k + 1..=n {
+        a[i] = (1_000_000_000 + a[i - 1] * 2 - a[i - k - 1]) % 1_000_000_000;
+    }
+    println!("{}", a[n]);
 }
