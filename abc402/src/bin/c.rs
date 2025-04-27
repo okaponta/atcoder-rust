c2f12b053faa8ec3c930c651daf3d623280c33cf
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-04-28 00:11:46 +0900

    add abc402_c

--- abc402/src/bin/c.rs
+++ abc402/src/bin/c.rs
@@ -1,11 +1,33 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
     }
-    println!("{}", 0);
+    let mut a = vec![HashSet::new(); m];
+    let mut idx = vec![vec![]; n];
+    for i in 0..m {
+        input! {
+            k:usize,
+            ai:[Usize1;k],
+        }
+        for &ai in &ai {
+            idx[ai].push(i);
+            a[i].insert(ai);
+        }
+    }
+    input! {b: [Usize1;n]}
+    let mut count = 0;
+    for &b in &b {
+        for &i in &idx[b] {
+            a[i].remove(&b);
+            if a[i].is_empty() {
+                count += 1;
+            }
+        }
+        println!("{}", count);
+    }
 }
