6cc30a416a9cfafc1e72d5ec7c10e5e01917fbd8
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-05-11 01:09:37 +0900

    add abc403_d

--- abc403/src/bin/d.rs
+++ abc403/src/bin/d.rs
@@ -4,8 +4,39 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        d:usize,
+        mut a:[usize;n],
     }
-    println!("{}", 0);
+    if d == 0 {
+        a.sort();
+        a.dedup();
+        println!("{}", n - a.len());
+        return;
+    }
+    let mut count = vec![0; 2_500_000];
+    let mut max = 0;
+    for i in 0..n {
+        count[a[i]] += 1;
+        max = max.max(a[i]);
+    }
+    let mut ans = 0;
+    for i in 0..d {
+        let mut j = 0;
+        let mut dp = vec![];
+        for k in (i..=max).step_by(d) {
+            if j == 0 {
+                dp.push(0);
+            } else if j == 1 {
+                dp.push(count[k].min(count[k - d]));
+            } else {
+                let next = (dp[j - 1] + count[k]).min(dp[j - 2] + count[k - d]);
+                dp.push(next);
+            }
+            j += 1;
+        }
+        if dp.len() != 0 {
+            ans += dp[dp.len() - 1];
+        }
+    }
+    println!("{}", ans);
 }
