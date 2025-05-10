a813ba25605a4918184f6a327079dcd63757671b
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-05-11 01:09:36 +0900

    add abc403_c

--- abc403/src/bin/c.rs
+++ abc403/src/bin/c.rs
@@ -1,11 +1,33 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        _m:usize,
+        q:usize,
     }
-    println!("{}", 0);
+    let mut all = HashSet::new();
+    let mut map = HashMap::new();
+    for _ in 0..q {
+        input! {qi: u8, x: Usize1}
+        if qi == 1 {
+            input! {y:Usize1}
+            map.entry(x).or_insert(HashSet::new()).insert(y);
+        } else if qi == 2 {
+            all.insert(x);
+        } else {
+            input! {y:Usize1}
+            if all.contains(&x) {
+                println!("Yes");
+                continue;
+            }
+            if map.contains_key(&x) && map[&x].contains(&y) {
+                println!("Yes");
+                continue;
+            }
+            println!("No");
+        }
+    }
 }
