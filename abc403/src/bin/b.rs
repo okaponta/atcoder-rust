f1cc09f150980daf51865012dca6239a88ebd54e
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-05-11 01:09:34 +0900

    add abc403_b

--- abc403/src/bin/b.rs
+++ abc403/src/bin/b.rs
@@ -3,9 +3,20 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:Chars,
+        u:Chars,
     }
-    println!("{}", 0);
+    for i in 0..=t.len() - u.len() {
+        let mut ok = true;
+        for j in 0..u.len() {
+            if t[i + j] != '?' && t[i + j] != u[j] {
+                ok = false;
+            }
+        }
+        if ok {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
