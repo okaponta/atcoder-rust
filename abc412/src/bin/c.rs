a978fefb820c591483189654ad22ff1f7192dbf2
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-06-28 23:26:00 +0900

    add abc412_c

--- abc412/src/bin/c.rs
+++ abc412/src/bin/c.rs
@@ -3,9 +3,40 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    println!("{}", 0);
+    for _ in 0..t {
+        test();
+    }
+}
+
+fn test() {
+    input! {
+        n:usize,
+        s:[usize;n],
+    }
+    let mut ans = 2;
+    let mut cur = s[0];
+    let target = s[n - 1];
+    let mut set = BTreeSet::new();
+    for i in 1..n - 1 {
+        set.insert(s[i]);
+    }
+    loop {
+        if target <= cur * 2 {
+            break;
+        }
+        if let Some(&next) = set.range(..=cur * 2).last() {
+            if next == cur {
+                println!("-1");
+                return;
+            }
+            ans += 1;
+            cur = next;
+        } else {
+            println!("-1");
+            return;
+        }
+    }
+    println!("{}", ans);
 }
