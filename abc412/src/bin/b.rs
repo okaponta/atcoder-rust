9cdcfec3f3e2f7304ed3a72f613181dd4d82d729
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-06-28 23:25:59 +0900

    add abc412_b

--- abc412/src/bin/b.rs
+++ abc412/src/bin/b.rs
@@ -3,9 +3,21 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
+        t:Chars,
     }
-    println!("{}", 0);
+    let mut c = vec![];
+    for i in 1..s.len() {
+        if s[i].is_ascii_uppercase() {
+            c.push(s[i - 1]);
+        }
+    }
+    println!(
+        "{}",
+        if c.iter().all(|c| t.contains(c)) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
