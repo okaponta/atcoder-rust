3b5cf08b7e73fc8958db23bd08262b7a5290213f
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-05 07:54:38 +0900

    add abc405_d

--- abc405/src/bin/d.rs
+++ abc405/src/bin/d.rs
@@ -3,9 +3,30 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        mut s:[Chars;h],
     }
-    println!("{}", 0);
+    let mut q = VecDeque::new();
+    for (i, j) in iproduct!(0..h, 0..w) {
+        if s[i][j] == 'E' {
+            q.push_back((i, j));
+        }
+    }
+    while let Some((i, j)) = q.pop_front() {
+        for (di, dj, c) in vec![(!0, 0, 'v'), (0, 1, '<'), (0, !0, '>'), (1, 0, '^')] {
+            let ni = i.wrapping_add(di);
+            let nj = j.wrapping_add(dj);
+            if h <= ni || w <= nj {
+                continue;
+            }
+            if s[ni][nj] == '.' {
+                s[ni][nj] = c;
+                q.push_back((ni, nj));
+            }
+        }
+    }
+    for s in s {
+        println!("{}", s.iter().collect::<String>());
+    }
 }
