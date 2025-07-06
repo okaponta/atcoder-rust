ec76cb5a994951decd88bdb91237b46ae1a5bcdb
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-06 21:32:09 +0900

    add abc405_e

--- abc405/src/bin/e.rs
+++ abc405/src/bin/e.rs
@@ -1,11 +1,50 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+const MOD: usize = 998244353;
+
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:usize,b:usize,c:usize,d:Usize1,
     }
-    println!("{}", 0);
+    let cb = Binom::init(a + b + c + d + 1, MOD);
+    let ans = (0..=c).fold(0, |s, i| {
+        (s + cb.get(a + b + i, b) * cb.get(c + d - i, d)) % MOD
+    });
+    println!("{}", ans);
+}
+
+pub struct Binom {
+    fact: Vec<usize>,
+    fact_inv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Binom {
+    pub fn init(n: usize, modulo: usize) -> Self {
+        let mut fact = vec![0; n + 1];
+        let mut inv = vec![0; n + 1];
+        let mut fact_inv = vec![0; n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        inv[1] = 1;
+        fact_inv[0] = 1;
+        fact_inv[1] = 1;
+        for i in 2..(n + 1) {
+            fact[i] = fact[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            fact_inv[i] = fact_inv[i - 1] * inv[i] % modulo;
+        }
+        Self {
+            fact,
+            fact_inv,
+            modulo,
+        }
+    }
+
+    // nCk
+    pub fn get(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fact[n] * self.fact_inv[k] % self.modulo * self.fact_inv[n - k] % self.modulo
+    }
 }
