09680d340e8d468d648f20019d2cebe53a48026c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-13 21:59:10 +0900

    add abc405_f

--- abc405/src/bin/f.rs
+++ abc405/src/bin/f.rs
@@ -1,11 +1,163 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        ab:[(Usize1,Usize1);m],
+        q:usize,
+        cd:[(Usize1,Usize1);q],
     }
-    println!("{}", 0);
+    let mut g = vec![vec![]; 2 * n];
+    let mut imos = vec![0i32; 2 * n + 1];
+    for (a, b) in ab {
+        g[a].push(b);
+        imos[a] += 1;
+        imos[b + 1] -= 1;
+    }
+    let mut h = vec![vec![]; 2 * n];
+    for i in 0..q {
+        h[cd[i].0].push((cd[i].1, i));
+    }
+    for i in 0..2 * n {
+        imos[i + 1] += imos[i];
+    }
+    let mut ans = vec![];
+    for (c, d) in cd {
+        ans.push(imos[c] + imos[d]);
+    }
+    let mut seg = SegmentTree::new(2 * n, 0, |a, b| a + b);
+    for i in 0..2 * n {
+        if 0 < g[i].len() {
+            for &j in &g[i] {
+                seg.update(j, seg.get(j) + 1);
+            }
+        }
+        if 0 < h[i].len() {
+            for &(j, k) in &h[i] {
+                ans[k] -= seg.query(j..2 * n) * 2;
+            }
+        }
+    }
+    for ans in ans {
+        println!("{ans}");
+    }
+}
+
+pub struct SegmentTree<T, F> {
+    seg: Vec<T>,
+    n: usize,
+    f: F,
+    initial_value: T,
+}
+
+impl<T: Copy, F> SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(size: usize, initial_value: T, f: F) -> SegmentTree<T, F> {
+        let m = size.next_power_of_two();
+        SegmentTree {
+            seg: vec![initial_value; m * 2],
+            n: m,
+            f,
+            initial_value,
+        }
+    }
+
+    pub fn update(&mut self, mut k: usize, value: T) {
+        k += self.n - 1;
+        self.seg[k] = value;
+        while k > 0 {
+            k = (k - 1) >> 1;
+            self.seg[k] = (self.f)(self.seg[k * 2 + 1], self.seg[k * 2 + 2]);
+        }
+    }
+
+    pub fn update_tmp(&mut self, k: usize, value: T) {
+        self.seg[k + self.n - 1] = value;
+    }
+
+    pub fn update_all(&mut self) {
+        for i in (0..self.n - 1).rev() {
+            self.seg[i] = (self.f)(self.seg[2 * i + 1], self.seg[2 * i + 2]);
+        }
+    }
+
+    // 半開区間なので注意
+    pub fn query(&self, range: std::ops::Range<usize>) -> T {
+        self.query_range(range, 0, 0..self.n)
+    }
+
+    // 半開区間なので注意
+    pub fn get(&self, k: usize) -> T {
+        self.query(k..k + 1)
+    }
+
+    fn query_range(
+        &self,
+        range: std::ops::Range<usize>,
+        k: usize,
+        seg_range: std::ops::Range<usize>,
+    ) -> T {
+        if seg_range.end <= range.start || range.end <= seg_range.start {
+            self.initial_value
+        } else if range.start <= seg_range.start && seg_range.end <= range.end {
+            self.seg[k]
+        } else {
+            let mid = (seg_range.start + seg_range.end) >> 1;
+            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
+            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
+            (self.f)(x, y)
+        }
+    }
+
+    // lowerとupperの間でfを満たす最小の値
+    // ng, ng, ng, (ok), ok, ok
+    pub fn max_right<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                upper = med;
+            } else {
+                lower = med;
+            }
+        }
+        upper
+    }
+
+    // lowerとupperの間でfを満たさない最大の値
+    pub fn min_left<P>(&self, mut lower: usize, mut upper: usize, f: P) -> usize
+    where
+        P: Fn(T) -> bool,
+    {
+        while upper - lower > 1 {
+            let med = (lower + upper) / 2;
+            if f(self.query(0..med)) {
+                lower = med;
+            } else {
+                upper = med;
+            }
+        }
+        lower
+    }
+}
+
+impl<T: Copy + std::fmt::Debug, F> std::fmt::Debug for SegmentTree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut v = vec![];
+        for i in 0..self.n {
+            v.push(self.get(i));
+        }
+        writeln!(f, "{:?}", v)?;
+        Ok(())
+    }
 }
