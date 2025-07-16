8c05669dc1f043d16b50d2ccf932ded24391a306
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-16 13:22:05 +0900

    add abc406_d

--- abc406/src/bin/d.rs
+++ abc406/src/bin/d.rs
@@ -1,11 +1,35 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
+        h:usize,
+        w:usize,
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        xy:[(Usize1,Usize1);n],
+        q:usize,
+        qx:[(u8,Usize1);q],
     }
-    println!("{}", 0);
+    let mut row = vec![HashSet::new(); h];
+    let mut col = vec![HashSet::new(); w];
+    for (x, y) in xy {
+        row[x].insert(y);
+        col[y].insert(x);
+    }
+    for (q, x) in qx {
+        if q == 1 {
+            println!("{}", row[x].len());
+            row[x].iter().for_each(|i| {
+                col[*i].remove(&x);
+            });
+            row[x].clear();
+        } else {
+            println!("{}", col[x].len());
+            col[x].iter().for_each(|i| {
+                row[*i].remove(&x);
+            });
+            col[x].clear();
+        }
+    }
 }
