2960b855c7deca89b5c890b20576440fe9d8969d
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-16 12:39:15 +0900

    add abc406_c

--- abc406/src/bin/c.rs
+++ abc406/src/bin/c.rs
@@ -4,8 +4,32 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        p:[usize;n],
     }
-    println!("{}", 0);
+    let mut ud = vec![(0, 'D')];
+    for i in 1..n - 1 {
+        if p[i - 1] < p[i] && p[i] > p[i + 1] {
+            ud.push((i, 'U'));
+        } else if p[i - 1] > p[i] && p[i] < p[i + 1] {
+            ud.push((i, 'D'));
+        }
+    }
+    ud.push((n, 'D'));
+    let mut ans = 0;
+    for i in 1..ud.len() - 2 {
+        if ud[i].1 == 'U' && ud[i + 1].1 == 'D' {
+            let a = if ud[i - 1].1 == 'D' {
+                ud[i].0 - ud[i - 1].0
+            } else {
+                ud[i].0 - ud[i - 1].0 - 1
+            };
+            let b = if ud[i + 2].1 == 'U' {
+                ud[i + 2].0 - ud[i + 1].0
+            } else {
+                ud[i + 2].0 - ud[i + 1].0 - 1
+            };
+            ans += a * b;
+        }
+    }
+    println!("{}", ans);
 }
