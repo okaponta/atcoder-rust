64e374041863b398d30be5110af652a79d2f8fd6
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-10 14:34:15 +0900

    add abc407_d

--- abc407/src/bin/d.rs
+++ abc407/src/bin/d.rs
@@ -3,9 +3,55 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        a:[[usize;w];h],
     }
-    println!("{}", 0);
+    println!("{}", dfs(0, 0, vec![vec![false; w]; h], h, w, &a));
+}
+
+fn dfs(
+    mut i: usize,
+    mut j: usize,
+    cur: Vec<Vec<bool>>,
+    h: usize,
+    w: usize,
+    a: &Vec<Vec<usize>>,
+) -> usize {
+    let mut res = xor(&cur, a, h, w);
+    if w <= j {
+        j -= w;
+        i += 1;
+    }
+    if h <= i {
+        return res;
+    }
+    // たて
+    if i < h - 1 && !cur[i][j] && !cur[i + 1][j] {
+        let mut next = cur.clone();
+        next[i][j] = true;
+        next[i + 1][j] = true;
+        res = res.max(dfs(i, j + 1, next, h, w, a));
+    }
+    // よこ
+    if j < w - 1 && !cur[i][j] && !cur[i][j + 1] {
+        let mut next = cur.clone();
+        next[i][j] = true;
+        next[i][j + 1] = true;
+        res = res.max(dfs(i, j + 2, next, h, w, a));
+    }
+    res = res.max(dfs(i, j + 1, cur, h, w, a));
+    res
+}
+
+fn xor(cur: &Vec<Vec<bool>>, a: &Vec<Vec<usize>>, h: usize, w: usize) -> usize {
+    let mut res = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if !cur[i][j] {
+                res ^= a[i][j]
+            }
+        }
+    }
+    res
 }
