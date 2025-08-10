daebc9b4489b1d5095f52372d31e0ddfd6f6c225
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-10 15:18:39 +0900

    add abc407_e

--- abc407/src/bin/e.rs
+++ abc407/src/bin/e.rs
@@ -1,11 +1,18 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use {proconio::*, std::collections::*};
 
 fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+    input! {t:usize}
+    (0..t).into_iter().for_each(|_| case());
+}
+
+fn case() {
+    input! {n:usize, a:[usize;2*n]}
+    let mut ans = a[0];
+    let mut heap = BinaryHeap::new();
+    for i in 1..n {
+        heap.push(a[2 * i]);
+        heap.push(a[2 * i - 1]);
+        ans += heap.pop().unwrap();
     }
-    println!("{}", 0);
+    println!("{}", ans);
 }
