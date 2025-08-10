1d942862703014cd758b2e01b6531f5ed9aefa65
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-10 13:24:24 +0900

    add abc407_c

--- abc407/src/bin/c.rs
+++ abc407/src/bin/c.rs
@@ -3,9 +3,19 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut s:Chars,
     }
-    println!("{}", 0);
+    s.reverse();
+    let mut ans = 0;
+    let mut count = 0;
+    for c in s {
+        let ci = c.to_digit(10).unwrap() as usize;
+        while count != ci {
+            count += 1;
+            count %= 10;
+            ans += 1;
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
 }
