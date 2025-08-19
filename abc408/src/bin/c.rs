0801c631f05024e3778d87e738d619828a3b1c3d
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-19 15:26:26 +0900

    add abc408_c

--- abc408/src/bin/c.rs
+++ abc408/src/bin/c.rs
@@ -4,8 +4,18 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        lr:[(Usize1, usize);m],
     }
-    println!("{}", 0);
+    let mut imos = vec![0; n + 1];
+    for (l, r) in lr {
+        imos[l] += 1;
+        imos[r] -= 1;
+    }
+    let mut ans = imos[0];
+    for i in 1..n {
+        imos[i] += imos[i - 1];
+        ans = ans.min(imos[i]);
+    }
+    println!("{}", ans);
 }
