a0fcc58d5f04288fe47a6a7e3070a816d769d834
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-19 16:26:37 +0900

    add abc408_e

--- abc408/src/bin/e.rs
+++ abc408/src/bin/e.rs
@@ -1,11 +1,20 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use petgraph::unionfind::UnionFind;
+use proconio::{marker::*, *};
 
 fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+    input! {n:usize,m:usize,uvw:[(Usize1,Usize1,usize);m]}
+    let mut ans = (1 << 31) - 1;
+    for i in (0..31).rev() {
+        ans ^= 1 << i;
+        let mut uf = UnionFind::new(n);
+        for &(u, v, w) in &uvw {
+            if ans | w == ans {
+                uf.union(u, v);
+            }
+        }
+        if !uf.equiv(0, n - 1) {
+            ans ^= 1 << i;
+        }
     }
-    println!("{}", 0);
+    println!("{}", ans);
 }
