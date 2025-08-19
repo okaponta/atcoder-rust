1115953288497a24fd40c0bf8fd72b159de1e1e0
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-19 15:36:33 +0900

    add abc408_d

--- abc408/src/bin/d.rs
+++ abc408/src/bin/d.rs
@@ -2,10 +2,23 @@
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+    input! {t:usize}
+    (0..t).into_iter().for_each(|_| case());
+}
+
+fn case() {
+    input! {n:usize, s:Chars}
+    let mut c = vec![0; n + 1];
+    let mut sum = 0;
+    let mut max = 0;
+    let mut ans = 0;
+    for i in 0..n {
+        if s[i] == '1' {
+            sum += 1;
+        }
+        c[i + 1] = c[i] + if s[i] == '0' { 1 } else { -1 };
+        ans = ans.min(c[i + 1] - max);
+        max = max.max(c[i + 1]);
     }
-    println!("{}", 0);
+    println!("{}", sum + ans);
 }
