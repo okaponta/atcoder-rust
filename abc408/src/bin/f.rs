cbb3794e7bf2564fbe3b31dca0023d6d81e564c8
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-21 15:50:29 +0900

    add abc408_f

--- abc408/src/bin/f.rs
+++ abc408/src/bin/f.rs
@@ -1,11 +1,24 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
-
+#[rustfmt::skip]
+use {itertools::*,proconio::{marker::*, *}, ac_library::*};
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        n:usize,d:usize,r:usize,
+        h:[Usize1;n]
     }
-    println!("{}", 0);
+    let p = (0..n)
+        .into_iter()
+        .sorted_by(|&a, &b| h[a].cmp(&h[b]))
+        .collect_vec();
+    let mut s = Segtree::<Max<usize>>::new(n);
+    let mut dp = vec![0; n];
+    let mut ans = 0;
+    for i in 0..n {
+        if d <= i {
+            s.set(p[i - d], dp[i - d]);
+        }
+        let l = p[i].saturating_sub(r);
+        dp[i] = dp[i].max(s.prod(l..n.min(p[i] + r + 1)) + 1);
+        ans = ans.max(dp[i] - 1);
+    }
+    println!("{ans}");
 }
