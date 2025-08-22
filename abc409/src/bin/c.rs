b2db01d6368c78d24927bd1b8f6ac86706c410a0
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-22 09:56:19 +0900

    add abc409_c

--- abc409/src/bin/c.rs
+++ abc409/src/bin/c.rs
@@ -4,8 +4,23 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        l:usize,
+        d:[usize;n-1],
     }
-    println!("{}", 0);
+    if l % 3 != 0 {
+        println!("0");
+        return;
+    }
+    let mut count = vec![0usize; l];
+    let mut tmp = 0;
+    count[0] = 1;
+    for &d in &d {
+        tmp = (tmp + d) % l;
+        count[tmp] += 1;
+    }
+    let mut ans = 0;
+    for i in 0..l / 3 {
+        ans += count[i] * count[i + l / 3] * count[i + (2 * l / 3)];
+    }
+    println!("{}", ans);
 }
