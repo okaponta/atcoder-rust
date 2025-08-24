5e78b852f671b155aa56b9817db1e2fb6871cfb7
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-24 09:40:47 +0900

    add abc409_d

--- abc409/src/bin/d.rs
+++ abc409/src/bin/d.rs
@@ -3,9 +3,27 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    println!("{}", 0);
+    for _ in 0..t {
+        case();
+    }
+}
+
+fn case() {
+    input! {n:usize,mut s:Chars}
+    if n == 1 {
+        println!("{}", s[0]);
+        return;
+    }
+    let mut i = 0;
+    while i + 1 < n && s[i] <= s[i + 1] {
+        i += 1;
+    }
+    let mut j = i + 1;
+    while j < n && s[j] <= s[i] {
+        j += 1;
+    }
+    s[i..j].rotate_left(1);
+    println!("{}", s.iter().join(""))
 }
