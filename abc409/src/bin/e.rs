2da50119353f3d077fdc39ec29e2642fca51b53c
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-25 13:06:17 +0900

    add abc409_e

--- abc409/src/bin/e.rs
+++ abc409/src/bin/e.rs
@@ -1,11 +1,24 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use proconio::{marker::*, *};
 
 fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+    input! {n:usize, mut x:[i64;n], uvw:[(Usize1,Usize1,usize);n-1]}
+    let mut g = vec![vec![]; n];
+    for (u, v, w) in uvw {
+        g[u].push((v, w));
+        g[v].push((u, w));
     }
-    println!("{}", 0);
+    println!("{}", dfs(0, 0, &mut x, &g));
+}
+
+fn dfs(cur: usize, prev: usize, x: &mut Vec<i64>, g: &Vec<Vec<(usize, usize)>>) -> usize {
+    let mut res = 0;
+    for &(next, cost) in &g[cur] {
+        if next == prev {
+            continue;
+        }
+        res += dfs(next, cur, x, g);
+        x[cur] += x[next];
+        res += (x[next].abs() as usize) * cost;
+    }
+    res
 }
