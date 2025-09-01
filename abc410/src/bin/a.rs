bcae4ac0804eb29e39f8ec5c6a57d69074d66c33
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-01 15:31:48 +0900

    add abc410_a

--- abc410/src/bin/a.rs
+++ abc410/src/bin/a.rs
@@ -4,8 +4,8 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
+        k:usize,
     }
-    println!("{}", 0);
+    println!("{}", a.into_iter().filter(|i| &k <= i).count());
 }
