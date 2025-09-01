21169519d9298f1b222854ada6cb4f3643a4e9a4
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-01 15:37:06 +0900

    add abc410_b

--- abc410/src/bin/b.rs
+++ abc410/src/bin/b.rs
@@ -4,8 +4,26 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        x:[usize;q],
     }
-    println!("{}", 0);
+    let mut count = vec![0; n];
+    let mut min = 0;
+    let mut ans = vec![];
+    for x in x {
+        if x == 0 {
+            for i in 0..n {
+                if count[i] == min {
+                    ans.push(i + 1);
+                    count[i] += 1;
+                    break;
+                }
+            }
+        } else {
+            count[x - 1] += 1;
+            ans.push(x);
+        }
+        min = *count.iter().min().unwrap();
+    }
+    println!("{}", ans.iter().join(" "));
 }
