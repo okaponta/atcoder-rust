d88eee1a14f40d8aee9030d20b8e75e27cb3d2ac
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-02 17:07:42 +0900

    add abc410_c

--- abc410/src/bin/c.rs
+++ abc410/src/bin/c.rs
@@ -1,11 +1,25 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    println!("{}", 0);
+    let mut r = 0;
+    let mut a = (1..=n).into_iter().collect_vec();
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {p:Usize1, x:usize}
+            a[(n + p + r) % n] = x;
+        } else if qi == 2 {
+            input! {p:Usize1}
+            println!("{}", a[(n + p + r) % n]);
+        } else {
+            input! {k :usize}
+            r = (r + k) % n;
+        }
+    }
 }
