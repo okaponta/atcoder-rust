f3388767c001284fd72cccde17681f6627220408
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-04 16:42:42 +0900

    add abc410_d

--- abc410/src/bin/d.rs
+++ abc410/src/bin/d.rs
@@ -4,8 +4,36 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        abw:[(Usize1,Usize1,usize);m],
     }
-    println!("{}", 0);
+    let mut g = vec![vec![vec![]; 1024]; n];
+    for (a, b, w) in abw {
+        for i in 0..1024 {
+            g[a][i].push((b, i ^ w));
+        }
+    }
+    let mut q = VecDeque::new();
+    let mut set = HashSet::new();
+    q.push_back((0, 0));
+    while let Some(cur) = q.pop_front() {
+        for &next in &g[cur.0][cur.1] {
+            if set.contains(&next) {
+                continue;
+            }
+            set.insert(next);
+            q.push_back(next);
+        }
+    }
+    let mut res = 1 << 20;
+    for i in 0..1024 {
+        if set.contains(&(n - 1, i)) {
+            res = res.min(i);
+        }
+    }
+    if res == 1 << 20 {
+        println!("-1");
+    } else {
+        println!("{}", res)
+    }
 }
