29b3c39435cd8f5726a8f1853ff18202638c7c12
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-05 15:53:37 +0900

    add abc410_e

--- abc410/src/bin/e.rs
+++ abc410/src/bin/e.rs
@@ -1,11 +1,29 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use proconio::*;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        m:usize,
+        ab:[(usize,usize);n],
     }
-    println!("{}", 0);
+    let mut dp = vec![1 << 60; m + 1];
+    dp[0] = 0;
+    for i in 0..n {
+        let mut next = vec![1 << 60; m + 1];
+        for j in 0..m {
+            if dp[j] + ab[i].0 <= h {
+                next[j] = next[j].min(dp[j] + ab[i].0);
+            }
+            if j + ab[i].1 <= m {
+                next[j + ab[i].1] = next[j + ab[i].1].min(dp[j]);
+            }
+        }
+        if next.iter().all(|&i| i == 1 << 60) {
+            println!("{}", i);
+            return;
+        }
+        dp = next;
+    }
+    println!("{}", n);
 }
