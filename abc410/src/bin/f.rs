e62b2ec426a5489c7a1884936bf8d7deaf6cdfaa
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-06 00:24:35 +0900

    add abc410_f

--- abc410/src/bin/f.rs
+++ abc410/src/bin/f.rs
@@ -1,11 +1,61 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+#[rustfmt::skip]
+use {proconio::{marker::*, *}, std::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    println!("{}", 0);
+    for _ in 0..t {
+        case();
+    }
+}
+
+fn case() {
+    input! {
+        mut h:usize,
+        mut w:usize,
+        mut s:[Chars;h],
+    }
+    if w < h {
+        s = rotate(h, w, s);
+        mem::swap(&mut h, &mut w);
+    }
+    let mut ans = 0usize;
+    let n = h * w;
+    let mut cnt = vec![0; 2 * n + 1];
+    for i in 0..h {
+        let mut dp = vec![0; w];
+        for j in i..h {
+            for k in 0..w {
+                dp[k] += if s[j][k] == '.' { 1 } else { -1 };
+            }
+            let mut sum = vec![0; w + 1];
+            sum[0] = n;
+            for k in 0..w {
+                if 0 < dp[k] {
+                    sum[k + 1] = sum[k] + dp[k] as usize;
+                } else {
+                    sum[k + 1] = sum[k] - (-dp[k] as usize);
+                }
+            }
+            for k in 0..=w {
+                ans += cnt[sum[k]];
+                cnt[sum[k]] += 1;
+            }
+            for k in 0..=w {
+                cnt[sum[k]] = 0;
+            }
+        }
+    }
+    println!("{}", ans);
+}
+
+fn rotate(h: usize, w: usize, s: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut res = vec![vec!['.'; h]; w];
+    for i in 0..h {
+        for j in 0..w {
+            res[j][i] = s[i][j];
+        }
+    }
+    res
 }
