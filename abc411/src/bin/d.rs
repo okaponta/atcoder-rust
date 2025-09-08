bac6ec22358634a9c515c79a0e0798b50ecd4d37
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-08 11:52:59 +0900

    add abc411_d

--- abc411/src/bin/d.rs
+++ abc411/src/bin/d.rs
@@ -3,9 +3,38 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        _n:usize,
+        q:usize,
     }
-    println!("{}", 0);
+    let mut query = vec![];
+    for _ in 0..q {
+        input! {qi:u8, p:usize}
+        if qi == 2 {
+            input! {s: Chars}
+            query.push((qi, p, s));
+        } else {
+            query.push((qi, p, vec![]));
+        }
+    }
+    query.reverse();
+    let mut ans = vec![];
+    let mut target = 0;
+    for (qi, p, s) in query {
+        if qi == 1 {
+            if target == p {
+                target = 0;
+            }
+        } else if qi == 2 {
+            if target == p {
+                for c in s.iter().rev() {
+                    ans.push(*c);
+                }
+            }
+        } else {
+            if target == 0 {
+                target = p;
+            }
+        }
+    }
+    println!("{}", ans.iter().rev().join(""));
 }
