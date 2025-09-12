fe7afd7e5a530a2a6cc40cb9fc7447a32dd8fede
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-12 13:59:08 +0900

    add abc413_b

--- abc413/src/bin/b.rs
+++ abc413/src/bin/b.rs
@@ -4,8 +4,20 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:[Chars;n],
     }
-    println!("{}", 0);
+    let mut set = HashSet::new();
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            let mut tmp = s[i].clone();
+            for &c in &s[j] {
+                tmp.push(c);
+            }
+            set.insert(tmp);
+        }
+    }
+    println!("{}", set.len());
 }
