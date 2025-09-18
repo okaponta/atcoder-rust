fa35ef37dd3ad34808a375efacf56cb08536634f
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-18 10:37:15 +0900

    add abc413_c

--- abc413/src/bin/c.rs
+++ abc413/src/bin/c.rs
@@ -1,11 +1,31 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    println!("{}", 0);
+    let mut qu = VecDeque::new();
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {c:usize, x:usize}
+            qu.push_back((c, x));
+        } else {
+            input! {k:usize}
+            let mut tmp = 0;
+            let mut ans = 0;
+            while let Some((c, x)) = qu.pop_front() {
+                tmp += c;
+                ans += c * x;
+                if k <= tmp {
+                    qu.push_front((tmp - k, x));
+                    ans -= (tmp - k) * x;
+                    break;
+                }
+            }
+            println!("{ans}");
+        }
+    }
 }
