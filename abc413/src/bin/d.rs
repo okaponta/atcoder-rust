1c5d71a79420035bd429417d22c5f52160f21755
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-21 09:24:08 +0900

    add abc413_d

--- abc413/src/bin/d.rs
+++ abc413/src/bin/d.rs
@@ -1,10 +1,7 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use proconio::*;
 
 fn main() {
-    input! {
-        t:usize,
-    }
+    input! {t:usize}
     for _ in 0..t {
         case();
     }
@@ -17,45 +14,27 @@ fn case() {
         return;
     }
     let zero = a.iter().filter(|&i| &0 == i).count();
-    if n - 1 <= zero {
-        println!("Yes");
-        return;
-    }
     if 0 < zero {
-        println!("No");
+        pb(n - 1 <= zero);
         return;
     }
     let count = a.iter().map(|&i| if 0 < i { 1 } else { -1 }).sum::<i64>();
     if count.abs() == n as i64 {
         a.sort();
-        println!("{}", if judge(a) { "Yes" } else { "No" })
+        pb(judge(a));
     } else if count.abs() == 1 {
-        println!(
-            "{}",
-            if judge(rearrange(count, a)) {
-                "Yes"
-            } else {
-                "No"
-            }
-        );
+        pb(judge(rearrange(count, &a)));
     } else if count.abs() == 0 {
-        println!(
-            "{}",
-            if judge(rearrange(1, a.clone())) || judge(rearrange(-1, a)) {
-                "Yes"
-            } else {
-                "No"
-            }
-        );
+        pb(judge(rearrange(1, &a)) || judge(rearrange(-1, &a)));
     } else {
         println!("No");
     }
 }
 
-fn rearrange(count: i64, a: Vec<i64>) -> Vec<i64> {
+fn rearrange(count: i64, a: &Vec<i64>) -> Vec<i64> {
     let mut pos = vec![];
     let mut neg = vec![];
-    for a in a {
+    for &a in a {
         if 0 < a {
             pos.push(a);
         } else {
@@ -80,4 +59,8 @@ fn rearrange(count: i64, a: Vec<i64>) -> Vec<i64> {
 
 fn judge(a: Vec<i64>) -> bool {
     a.windows(3).all(|v| v[0] * v[2] == v[1] * v[1])
+}
+
+fn pb(b: bool) {
+    println!("{}", if b { "Yes" } else { "No" })
 }
