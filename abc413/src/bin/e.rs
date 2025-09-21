b042b8467b176b7fc9de8696eca95484a0d8b1f2
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-09-21 10:37:24 +0900

    add abc413_e

--- abc413/src/bin/e.rs
+++ abc413/src/bin/e.rs
@@ -1,11 +1,36 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+#[rustfmt::skip]
+use {itertools::*,proconio::*};
 
+#[fastout]
 fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+    input! {t:usize}
+    for _ in 0..t {
+        case();
     }
-    println!("{}", 0);
+}
+
+fn case() {
+    input! {n:usize, p:[usize;2usize.pow(n as u32)]}
+    let mut v = vec![];
+    for p in p {
+        v.push(vec![p]);
+    }
+    for _ in 0..n {
+        let mut new = vec![];
+        for i in (0..v.len()).step_by(2) {
+            if v[i + 1][0] < v[i][0] {
+                v.swap(i, i + 1);
+            }
+            let mut tmp = vec![];
+            for j in 0..v[i].len() {
+                tmp.push(v[i][j]);
+            }
+            for j in 0..v[i].len() {
+                tmp.push(v[i + 1][j]);
+            }
+            new.push(tmp);
+        }
+        v = new;
+    }
+    println!("{}", v[0].iter().join(" "));
 }
