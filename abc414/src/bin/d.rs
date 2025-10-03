718e0ba5bc7c20e9a0eba8e5a3e4cecfb9f71229
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-10-04 08:13:57 +0900

    add abc414_d

--- abc414/src/bin/d.rs
+++ abc414/src/bin/d.rs
@@ -4,8 +4,25 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        mut x:[usize;n],
     }
-    println!("{}", 0);
+    if n == 1 {
+        println!("0");
+        return;
+    }
+    x.sort();
+    let ans = x[n - 1] - x[0];
+    if m == 1 {
+        println!("{}", ans);
+        return;
+    }
+    let d = (1..n)
+        .into_iter()
+        .map(|i| x[i] - x[i - 1])
+        .sorted()
+        .rev()
+        .collect_vec();
+    let ans = x[n - 1] - x[0] - (0..m - 1).into_iter().map(|i| d[i]).sum::<usize>();
+    println!("{}", ans);
 }
