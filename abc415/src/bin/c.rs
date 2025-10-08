bd3097afc943322ac91e3d47c8db615698d215c7
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-10-09 07:39:07 +0900

    add abc415_c

--- abc415/src/bin/c.rs
+++ abc415/src/bin/c.rs
@@ -1,11 +1,30 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
-fn main() {
-    input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+fn case() {
+    input! {n:usize, mut s:Chars}
+    s.insert(0, '0');
+    let mut ok = vec![false; 1 << n];
+    ok[0] = true;
+    for i in 0..1 << n {
+        if !ok[i] {
+            continue;
+        }
+        for j in 0..n {
+            if i >> j & 1 == 1 {
+                continue;
+            }
+            if s[i | 1 << j] == '0' {
+                ok[i | 1 << j] = true;
+            }
+        }
     }
-    println!("{}", 0);
+    println!("{}", if ok[(1 << n) - 1] { "Yes" } else { "No" })
+}
+
+fn main() {
+    input! {t:usize}
+    for _ in 0..t {
+        case();
+    }
 }
