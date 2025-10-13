47d6684cd179c21a7c4cc9947f5ea620787423ee
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-10-13 12:27:45 +0900

    add abc415_d

--- abc415/src/bin/d.rs
+++ abc415/src/bin/d.rs
@@ -1,11 +1,18 @@
-#[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use proconio::*;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        mut n:usize,m:usize,mut ab:[(usize,usize);m]
     }
-    println!("{}", 0);
+    ab.sort_by(|a, b| (a.0 - a.1).cmp(&(b.0 - b.1)));
+    let mut ans = 0;
+    for (a, b) in ab {
+        if n < a {
+            continue;
+        }
+        let t = (n - a) / (a - b) + 1;
+        ans += t;
+        n -= t * (a - b);
+    }
+    println!("{}", ans);
 }
