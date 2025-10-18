ac4a58a231e9a36e606d04301a845f7210ce5301
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-10-18 14:01:02 +0900

    add abc415_e

--- abc415/src/bin/e.rs
+++ abc415/src/bin/e.rs
@@ -3,9 +3,48 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        h:usize,
+        w:usize,
+        a:[[i64;w];h],
+        p:[i64;h+w-1],
     }
-    println!("{}", 0);
+    let mut lower = -1;
+    let mut upper = 1 << 60;
+    while 1 < upper - lower {
+        let mid = (lower + upper) / 2;
+        if ok(h, w, mid, &a, &p) {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+    println!("{}", upper);
+}
+
+fn ok(h: usize, w: usize, init: i64, a: &Vec<Vec<i64>>, p: &Vec<i64>) -> bool {
+    let mut grid = vec![vec![-1 << 30; w]; h];
+    grid[0][0] = init + a[0][0];
+    for i in 0..h + w - 1 {
+        let mut j = if i < h - 1 { i } else { h - 1 };
+        let mut k = i - j;
+        loop {
+            if p[i] <= grid[j][k] {
+                if j < h - 1 {
+                    grid[j + 1][k] = grid[j + 1][k].max(grid[j][k] + a[j + 1][k] - p[i]);
+                }
+                if k < w - 1 {
+                    grid[j][k + 1] = grid[j][k + 1].max(grid[j][k] + a[j][k + 1] - p[i]);
+                }
+            }
+            if j == 0 {
+                break;
+            }
+            j -= 1;
+            k += 1;
+            if k == w {
+                break;
+            }
+        }
+    }
+    p[h + w - 2] <= grid[h - 1][w - 1]
 }
