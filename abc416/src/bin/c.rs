5dd53422f4c4ee6d77078b086d5ce16e6803cded
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-11-07 08:27:44 +0900

    add abc416_c

--- abc416/src/bin/c.rs
+++ abc416/src/bin/c.rs
@@ -1,11 +1,33 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use std::cmp::Reverse;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        k:usize,
+        x:Usize1,
+        s:[Chars;n],
     }
-    println!("{}", 0);
+    let mut heap: BinaryHeap<_> = BinaryHeap::new();
+    heap.push(Reverse(vec![]));
+    for _ in 0..k {
+        let mut next = BinaryHeap::new();
+        while let Some(Reverse(ss)) = heap.pop() {
+            for i in 0..n {
+                let mut sss = ss.clone();
+                for &j in &s[i] {
+                    sss.push(j);
+                }
+                next.push(Reverse(sss));
+            }
+        }
+        heap = next;
+    }
+    for _ in 0..x {
+        heap.pop();
+    }
+    if let Some(Reverse(s)) = heap.pop() {
+        println!("{}", s.iter().join(""));
+    }
 }
