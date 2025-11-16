3a578db89ec80b44a82fd45aeb372e19e49ef045
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-11-16 09:31:22 +0900

    add abc416_d

--- abc416/src/bin/d.rs
+++ abc416/src/bin/d.rs
@@ -1,11 +1,40 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    println!("{}", 0);
+    for _ in 0..t {
+        case();
+    }
+}
+
+fn case() {
+    input! {
+        n:usize,
+        m:usize,
+        mut a:[usize;n],
+        mut b:[usize;n],
+    }
+    let mut set = BTreeSet::new();
+    for i in 0..n {
+        set.insert((a[i] % m, i));
+    }
+    for i in 0..n {
+        b[i] %= m;
+    }
+    b.sort();
+    let mut ans = 0;
+    for b in b {
+        if let Some(&(a, i)) = set.range(((m - b), 0)..).next() {
+            set.remove(&(a, i));
+            ans += (a + b) % m;
+        } else {
+            ans += b;
+        }
+    }
+    set.into_iter().for_each(|i| ans += i.0);
+    println!("{}", ans);
 }
