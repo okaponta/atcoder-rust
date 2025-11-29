e6908e6871bdaa46226203ac6fb2053bc2509dbf
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-11-29 22:47:46 +0900

    add abc434_c

--- abc434/src/bin/c.rs
+++ abc434/src/bin/c.rs
@@ -3,9 +3,33 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        t:usize,
     }
-    println!("{}", 0);
+    for _ in 0..t {
+        case();
+    }
+}
+
+fn case() {
+    input! {
+        n:usize,
+        h:i64,
+        tlu:[(i64,i64,i64);n],
+    }
+    let mut min = h;
+    let mut max = h;
+    let mut prev = 0;
+    for (t, l, u) in tlu {
+        let diff = t - prev;
+        let nmin = min - diff;
+        let nmax = max + diff;
+        if nmax < l || u < nmin {
+            println!("No");
+            return;
+        }
+        min = nmin.max(l);
+        max = nmax.min(u);
+        prev = t;
+    }
+    println!("Yes");
 }
