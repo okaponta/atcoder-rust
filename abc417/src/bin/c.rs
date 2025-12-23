f158e66e79091b78cb171554eee3cadaabbe306a
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-12-23 09:58:24 +0900

    add abc417_c

--- abc417/src/bin/c.rs
+++ abc417/src/bin/c.rs
@@ -4,8 +4,20 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:[usize;n],
     }
-    println!("{}", 0);
+    let offset = (0..n).into_iter().map(|i| n - 1 - i).collect_vec();
+    let mut ans = 0;
+    let mut map = HashMap::new();
+    let mut map2 = HashMap::new();
+    for i in 0..n {
+        if let Some(&v) = map2.get(&i) {
+            *map.entry(1 + offset[i]).or_insert(0) += v;
+        }
+        if let Some(v) = map.get(&(a[i] + offset[i])) {
+            ans += v;
+        }
+        *map2.entry(i + 1 + a[i]).or_insert(0) += 1usize;
+    }
+    println!("{ans}")
 }
