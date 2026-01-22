4aa3e6af116d66d74ae3a0dd6812a6ee6a0fe41f
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-01-23 06:42:48 +0900

    add abc418_a

--- abc418/src/bin/a.rs
+++ abc418/src/bin/a.rs
@@ -4,8 +4,15 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    println!("{}", 0);
+    if n < 3 {
+        println!("No");
+        return;
+    }
+    if s[n - 3] == 't' && s[n - 2] == 'e' && s[n - 1] == 'a' {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
