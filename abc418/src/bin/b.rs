ba429ae30bc361e45f369699874e1bb0be4b5a1b
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-01-23 06:51:31 +0900

    add abc418_b

--- abc418/src/bin/b.rs
+++ abc418/src/bin/b.rs
@@ -3,9 +3,24 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    println!("{}", 0);
+    let mut ans = 0.0;
+    for i in 0..s.len() {
+        for j in i + 2..s.len() {
+            if s[i] != 't' || s[j] != 't' {
+                continue;
+            }
+            let mut count = 0;
+            for k in i + 1..j {
+                if s[k] == 't' {
+                    count += 1;
+                }
+            }
+            if ans < count as f64 / (j - i - 1) as f64 {
+                ans = count as f64 / (j - i - 1) as f64;
+            }
+        }
+    }
+    println!("{}", ans);
 }
