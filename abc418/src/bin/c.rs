c33599eb3a140adbf0e473658b5d279bf8e4796e
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-01-23 07:22:51 +0900

    add abc418_c

--- abc418/src/bin/c.rs
+++ abc418/src/bin/c.rs
@@ -1,11 +1,25 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
+#[fastout]
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        mut a:[usize;n],
+        b:[Usize1;q],
     }
-    println!("{}", 0);
+    a.sort();
+    let mut s = vec![0];
+    for i in 0..n {
+        s.push(s[i] + a[i]);
+    }
+    for b in b {
+        let c = a.upper_bound(&b);
+        if c == n {
+            println!("-1");
+            continue;
+        }
+        println!("{}", 1 + s[c] + (n - c) * b);
+    }
 }
