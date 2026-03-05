172ae4498fcfed114f5a07ff3b452094baa86b20
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-03-05 15:37:32 +0900

    add abc447_b

--- abc447/src/bin/b.rs
+++ abc447/src/bin/b.rs
@@ -3,9 +3,20 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    println!("{}", 0);
+    let mut map = HashMap::new();
+    s.iter().for_each(|&c| *map.entry(c).or_insert(0) += 1);
+    let max = *map.values().max().unwrap();
+    let set = map
+        .into_iter()
+        .filter(|e| e.1 == max)
+        .map(|e| e.0)
+        .collect::<HashSet<_>>();
+    println!(
+        "{}",
+        s.into_iter()
+            .filter(|c| !set.contains(c))
+            .collect::<String>()
+    );
 }
