21e074c085e03568e01030445979fb0d7e1eeed7
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-03-06 07:50:02 +0900

    add abc447_d

--- abc447/src/bin/d.rs
+++ abc447/src/bin/d.rs
@@ -3,9 +3,25 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:Chars,
     }
-    println!("{}", 0);
+    let mut ca = 0;
+    let mut cb = 0;
+    let mut ans = 0;
+    for c in s {
+        if c == 'A' {
+            ca += 1;
+        } else if c == 'B' {
+            if 0 < ca {
+                ca -= 1;
+                cb += 1;
+            }
+        } else {
+            if 0 < cb {
+                cb -= 1;
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
