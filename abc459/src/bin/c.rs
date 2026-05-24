960e70eab46e1815caa52f6de009417004ca7361
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-05-24 16:32:45 +0900

    add abc459_c

--- abc459/src/bin/c.rs
+++ abc459/src/bin/c.rs
@@ -4,8 +4,23 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        qq:[(u8, Usize1);q],
     }
-    println!("{}", 0);
+    let mut count = vec![0; n];
+    let mut s = vec![0; 1_000_000];
+    let mut offset = 0;
+    s[0] = n;
+    for (qi, i) in qq {
+        if qi == 1 {
+            let bef = count[i];
+            s[bef + 1] += 1;
+            count[i] += 1;
+            if s[bef + 1] == n {
+                offset += 1;
+            }
+        } else {
+            println!("{}", s[i + 1 + offset]);
+        }
+    }
 }
