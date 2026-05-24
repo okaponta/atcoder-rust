9b1811727a6f5ae36450ccedb2ced62e463e9212
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-05-24 16:18:41 +0900

    add abc459_b

--- abc459/src/bin/b.rs
+++ abc459/src/bin/b.rs
@@ -4,8 +4,21 @@ use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        s:[Chars;n],
     }
-    println!("{}", 0);
+    s.into_iter()
+        .map(|v| cv(v[0] as u8 - b'a'))
+        .for_each(|i| print!("{i}"));
+    println!();
+}
+
+fn cv(mut i: u8) -> u8 {
+    if i < 18 {
+        return 2 + i / 3;
+    }
+    i -= 1;
+    if 2 + i / 3 < 9 {
+        return 2 + i / 3;
+    };
+    9
 }
