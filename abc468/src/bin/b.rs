ded283a9bc590fd943c31f666ac1834dd27bfce3
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-07-27 21:59:12 +0900

    add abc468_b

--- abc468/src/bin/b.rs
+++ abc468/src/bin/b.rs
@@ -1,11 +1,20 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use std::{println, vec};
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        m:usize,
+        d:usize,
+        s:Chars,
     }
-    println!("{}", 0);
+    let mut ans = vec![true; m];
+    for i in 0..m {
+        if s[i] == 'G' {
+            for j in i.saturating_sub(d)..(i + d + 1).min(m) {
+                ans[j] = false;
+            }
+        }
+    }
+    println!("{}", ans.into_iter().filter(|i| *i).count());
 }
