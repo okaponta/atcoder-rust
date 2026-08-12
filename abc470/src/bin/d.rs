7adf0db51ae65f9995b991a7f250c8c3ef2e1644
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-08-12 16:05:31 +0900

    add abc470_d

--- abc470/src/bin/d.rs
+++ abc470/src/bin/d.rs
@@ -1,11 +1,26 @@
 #[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*,std::vec};
+use std::println;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
+        mut p:[Usize1;n],
     }
-    println!("{}", 0);
+    let mut pp = vec![0; n];
+    for i in 0..n {
+        pp[p[i]] = i;
+    }
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {x:Usize1, y:Usize1}
+            pp.swap(p[x], p[y]);
+            p.swap(x, y);
+        } else {
+            (p, pp) = (pp, p);
+        }
+    }
+    println!("{}", p.into_iter().map(|i| i + 1).join(" "));
 }
