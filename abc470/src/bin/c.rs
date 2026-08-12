617a5ddb71d0001d0cf0a6548549c1fa67f0c3f7
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-08-12 15:55:34 +0900

    add abc470_c

--- abc470/src/bin/c.rs
+++ abc470/src/bin/c.rs
@@ -1,11 +1,34 @@
 #[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*,std::vec};
+use std::println;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        q:usize,
     }
-    println!("{}", 0);
+    let mut a = vec![0usize; n];
+    let mut ans = 0;
+    let mut set = HashSet::new();
+    for _ in 0..q {
+        input! {qi:u8}
+        if qi == 1 {
+            input! {x:Usize1}
+            ans = ans ^ a[x] ^ (a[x] + 1);
+            a[x] += 1;
+            println!("{ans}");
+            set.insert(x);
+        } else {
+            let mut new_set = HashSet::new();
+            for e in set {
+                if 1 < a[e] {
+                    new_set.insert(e);
+                }
+                ans = ans ^ a[e] ^ (a[e] - 1);
+                a[e] -= 1;
+            }
+            set = new_set;
+            println!("{ans}");
+        }
+    }
 }
