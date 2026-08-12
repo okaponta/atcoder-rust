2fbe2010ce716951e03b5c2b7323b69c5bf3cea8
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-08-12 15:21:48 +0900

    add abc470_a

--- abc470/src/bin/a.rs
+++ abc470/src/bin/a.rs
@@ -1,11 +1,16 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use std::println;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
     }
-    println!("{}", 0);
+    for i in 1..=n {
+        if i % 3 == 0 {
+            println!("Fizz");
+        } else {
+            println!("{i}")
+        }
+    }
 }
