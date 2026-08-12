1ef3b7ceb262caa3ec0d2dfa43540366e8e09c19
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-08-12 15:26:40 +0900

    add abc470_b

--- abc470/src/bin/b.rs
+++ abc470/src/bin/b.rs
@@ -1,11 +1,16 @@
 #[rustfmt::skip]#[allow(unused)]
-use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*};
+use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*,std::vec};
+use std::println;
 
 fn main() {
     input! {
         n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        c:[usize;n],
     }
-    println!("{}", 0);
+    let mut count = vec![0; 101];
+    for c in c {
+        count[c] += 1;
+    }
+    let max = count.into_iter().max().unwrap();
+    println!("{}", n - max);
 }
