8a15029af1ec70073b15ef69b9973a52316aa5b6
Kohei Okamoto <okaponta.19910919@gmail.com>
2026-09-14 00:04:10 +0900

    add abc471_a

--- abc471/src/bin/a.rs
+++ abc471/src/bin/a.rs
@@ -1,11 +1,27 @@
 #[rustfmt::skip]#[allow(unused)]
 use {itertools::*,proconio::{marker::*, *},superslice::*,std::collections::*,std::vec};
+use std::println;
 
 fn main() {
     input! {
-        n:usize,
-        _a:[usize;n],
-        _s:Chars,
+        a:usize,
+        b:usize,
     }
-    println!("{}", 0);
+    if a + b == 9 {
+        println!("Nine");
+        return;
+    }
+    if a + 9 == b || b + 9 == a {
+        println!("Nine");
+        return;
+    }
+    if a * b == 9 {
+        println!("Nine");
+        return;
+    }
+    if b * 9 == a || a * 9 == b {
+        println!("Nine");
+        return;
+    }
+    println!("Nein");
 }
